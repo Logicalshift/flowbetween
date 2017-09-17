31c26cafdafc16399dde7bfc3091e527992e346a
Andrew Hunter <andrew@logicalshift.co.uk>
2017-09-17 10:33:12 +0100

    A layer is rendered by a series of graphics commands

--- animation/src/traits/layer.rs
+++ animation/src/traits/layer.rs
@@ -1,6 +1,11 @@
+use super::graphics::*;
+
 ///
 /// A layer represents a renderable plane in a frame
 ///
 pub trait Layer {
-
+    ///
+    /// Retrieves the rendering commands required to render this layer
+    ///
+    fn render_commands(&self) -> Box<Iterator<Item = GraphicsCommand>>;
 }
--- animation/src/traits/mod.rs
+++ animation/src/traits/mod.rs
@@ -1,7 +1,9 @@
 pub mod animation;
 pub mod frame;
 pub mod layer;
+pub mod graphics;
 
 pub use self::animation::*;
 pub use self::frame::*;
 pub use self::layer::*;
+pub use self::graphics::*;
