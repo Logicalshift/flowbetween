a622b7f476402a7338f0040c4ef612c9e576573d
Andrew Hunter <andrew@logicalshift.co.uk>
2017-09-17 21:58:47 +0100

    Add some more useful definition of a UI control

--- ui/src/control.rs
+++ ui/src/control.rs
@@ -2,21 +2,71 @@
 /// Represents an element in the user interface
 ///
 pub enum Control {
+    // Adds an ID to the contained control
+    Id(String, Box<Control>),
+
+    /// Control container
+    Container(Bounds, Vec<Control>),
+
+    /// Button containing a control
+    Button(Bounds, Box<Control>),
+
+    /// Label with some particular text
+    Label(Bounds, String)
 }
 
 ///
-/// Possible positions of a control
+/// Represents a position coordinate
 ///
-pub enum ControlPosition {
-    /// Control located at a particular point
-    At(f32, f32),
+pub enum Position {
+    /// Point located at a specific value
+    At(f32),
 
-    /// Control located at the start of its container
+    /// Point at an offset from its counterpart (eg, width or height)
+    Offset(f32),
+
+    /// Point located at the start of the container (ie, left or top depending on if this is an x or y position)
     Start,
 
-    /// Control located at the end of its container
+    /// Control located at the end of its container (ie, right or bottom depending on if this is an x or y position)
     End,
 
-    /// Control positioned immediately after the last control
+    /// Same as the last point in this axis (which is 0 initially)
     After
+}
+
+///
+/// Represents the bounds of a particular control
+///
+pub struct Bounds {
+    pub x1: Position,
+    pub y1: Position,
+    pub x2: Position,
+    pub y2: Position
+}
+
+impl Bounds {
+    ///
+    /// Creates a bounding box that fills a container
+    ///
+    pub fn fill() -> Bounds {
+        use Position::*;
+        Bounds { x1: Start, y1: Start, x2: End, y2: End }
+    }
+
+    ///
+    /// Bounding box that fills the container vertically and follows the previous control horizontally
+    ///
+    pub fn next_horiz(width: f32) -> Bounds {
+        use Position::*;
+        Bounds { x1: After, y1: Start, x2: Offset(width), y2: End }
+    }
+
+    ///
+    /// Bounding box that fills the container horizontally and follows the previous control horizontally
+    ///
+    pub fn next_vert(height: f32) -> Bounds {
+        use Position::*;
+        Bounds { x1: Start, y1: After, x2: End, y2: Offset(height) }
+    }
 }
