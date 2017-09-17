30df0cc0b479c10721e6a350766ce89583cac7be
Andrew Hunter <andrew@logicalshift.co.uk>
2017-09-17 11:17:37 +0100

    Layers have attributes

--- animation/src/traits/layer.rs
+++ animation/src/traits/layer.rs
@@ -1,10 +1,11 @@
 use super::frame_parameter::*;
 use super::frame::*;
+use super::attributes::*;
 
 ///
 /// A layer represents a renderable plane in an animation
 ///
-pub trait Layer {
+pub trait Layer : HasAttributes {
     ///
     /// Retrieves a frame from this layer with the specified parameters
     ///
