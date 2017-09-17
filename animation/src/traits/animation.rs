41f7136984a8c7d855aaddba6a5670bc51c98024
Andrew Hunter <andrew@logicalshift.co.uk>
2017-09-17 11:08:37 +0100

    Add method for getting a frame from a layer and the list of layers to the animation

--- animation/src/traits/animation.rs
+++ animation/src/traits/animation.rs
@@ -1,5 +1,11 @@
+use super::layer::*;
+
 ///
 /// Represents an animation
 ///
 pub trait Animation {
+    ///
+    /// Retrieves the layers for this animation
+    ///
+    fn layers(&self) -> Box<Iterator<Item = Layer>>;
 }
--- animation/src/traits/layer.rs
+++ animation/src/traits/layer.rs
@@ -1,5 +1,12 @@
+use super::frame_parameter::*;
+use super::frame::*;
+
 ///
 /// A layer represents a renderable plane in an animation
 ///
 pub trait Layer {
+    ///
+    /// Retrieves a frame from this layer with the specified parameters
+    ///
+    fn get_frame(&self, Box<Iterator<Item = FrameParameter>>) -> Box<Frame>;
 }
