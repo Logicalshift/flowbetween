a705472297ec5bea8f9414c8e1a20b41803028e5
Andrew Hunter <andrew@logicalshift.co.uk>
2017-09-21 22:42:31 +0100

    Add a ‘new session’ event

--- http_ui/src/event.rs
+++ http_ui/src/event.rs
@@ -4,6 +4,11 @@
 #[derive(Clone, Serialize, Deserialize)]
 pub enum Event {
     ///
+    /// Request a new session
+    ///
+    NewSession,
+
+    ///
     /// The user clicked on a named SVG element (in a given spot)
     ///
     ClickSvg(String, f32, f32)
