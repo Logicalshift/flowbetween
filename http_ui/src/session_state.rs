7a92cdaa6e51c01b77f5054c9863dcea20040fa1
Andrew Hunter <andrew@logicalshift.co.uk>
2017-10-01 10:46:09 +0100

    Store the session state in a core object instead of having one mutex per field

--- http_ui/src/session_state.rs
+++ http_ui/src/session_state.rs
@@ -4,22 +4,31 @@ use uuid::*;
 use std::sync::*;
 
 ///
+/// The core of the session state
+///
+struct SessionStateCore {
+    /// The tree attached to this state
+    ui_tree: Box<Bound<Control>>,
+
+    /// The previous state of the tree
+    previous_tree: Binding<Option<Control>>,
+
+    /// Lifetime of the watcher that
+    ui_tree_watcher_lifetime: Box<Releasable>,
+
+    /// Binding that specifies whether or not the tree has changed
+    tree_has_changed: Binding<bool>
+}
+
+///
 /// The session state object represents the stored state of a particular session
 ///
 pub struct SessionState {
     /// A string identifying this session
     session_id: String,
 
-    // TODO: move this stuff into a 'core' struct protected by a mutex instead of having a bunch of mutexes
-
-    /// The UI tree for this session
-    ui_tree: Mutex<Box<Bound<Control>>>,
-
-    /// Lifetime that tracks if we're watching the tree
-    ui_tree_watcher_lifetime: Mutex<Box<Releasable>>,
-
-    /// Binding that tracks if the tree has changed since the last time it was diffed
-    tree_has_changed: Binding<bool>
+    /// The core of the state
+    core: Mutex<SessionStateCore>
 }
 
 impl SessionState {
@@ -30,22 +39,26 @@ impl SessionState {
         let session_id                      = Uuid::new_v4().simple().to_string();
         let mut tree: Box<Bound<Control>>   = Box::new(bind(Control::container()));
         let has_changed                     = bind(false);
-        let watcher_lifetime                = Self::watch_tree(&mut tree, &has_changed);
+        let watcher_lifetime                = Self::watch_tree(&mut tree, has_changed.clone());
+
+        let core = SessionStateCore {
+            ui_tree:                    tree,
+            previous_tree:              bind(None),
+            ui_tree_watcher_lifetime:   watcher_lifetime,
+            tree_has_changed:           has_changed
+        };
 
         SessionState { 
-            session_id:                 session_id,
-            ui_tree:                    Mutex::new(tree),
-            ui_tree_watcher_lifetime:   Mutex::new(watcher_lifetime),
-            tree_has_changed:           has_changed
+            session_id: session_id,
+            core:       Mutex::new(core)
         }
     }
 
     ///
     /// Sets has_changed to true when the ui_tree changes
     ///
-    fn watch_tree(ui_tree: &mut Box<Bound<Control>>, has_changed: &Binding<bool>) -> Box<Releasable> {
-        let mut changed = has_changed.clone();
-        ui_tree.when_changed(notify(move || changed.set(true)))
+    fn watch_tree(ui_tree: &mut Box<Bound<Control>>, mut has_changed: Binding<bool>) -> Box<Releasable> {
+        ui_tree.when_changed(notify(move || has_changed.set(true)))
     }
 
     ///
@@ -59,23 +72,34 @@ impl SessionState {
     /// Replaces the UI tree in this session
     ///
     pub fn set_ui_tree<TBinding: 'static+Bound<Control>>(&self, new_tree: TBinding) {
-        // TODO: this would be way easier to follow if ther were a core class protected by a mutex
-        self.ui_tree_watcher_lifetime.lock().unwrap().done();
-        *self.ui_tree.lock().unwrap() = Box::new(new_tree);
+        let mut core = self.core.lock().unwrap();
 
-        *self.ui_tree_watcher_lifetime.lock().unwrap() = Self::watch_tree(&mut *self.ui_tree.lock().unwrap(), &self.tree_has_changed);
+        // Stop watching the old tree
+        core.ui_tree_watcher_lifetime.done();
+
+        // Store the new UI tree in this object
+        core.ui_tree = Box::new(new_tree);
+
+        // Whenever the new tree is changed, set the has_changed binding
+        let mut has_changed = core.tree_has_changed.clone();
+        has_changed.set(true);
+        core.ui_tree_watcher_lifetime = Self::watch_tree(&mut core.ui_tree, has_changed);
     }
 
     ///
     /// Retrieves the current state of the UI for this session
     ///
     pub fn entire_ui_tree(&self) -> Control {
-        self.ui_tree.lock().unwrap().get()
+        let core = self.core.lock().unwrap();
+
+        core.ui_tree.get()
     }
 }
 
 impl Drop for SessionState {
     fn drop(&mut self) {
-        self.ui_tree_watcher_lifetime.lock().unwrap().done();
+        let mut core = self.core.lock().unwrap();
+
+        core.ui_tree_watcher_lifetime.done();
     }
 }
