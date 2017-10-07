24e9b167199fef9acab8fb6cbcac9af3c81da241
Andrew Hunter <andrew@logicalshift.co.uk>
2017-10-07 10:51:15 +0100

    Using Box<Any> for generics might be faster than serializing/deserializing controller identifiers
    Maybe better for events too?

--- ui/src/controller.rs
+++ ui/src/controller.rs
@@ -2,6 +2,7 @@ use super::binding::*;
 use super::control::*;
 
 use std::ops::Deref;
+use std::any::Any;
 
 use serde::*;
 use serde_json::*;
@@ -9,7 +10,7 @@ use serde_json::*;
 ///
 /// The generic controller is used to hide the types of sub-controllers
 ///
-type GenericController = Controller<SubControllerSpecifier=String>;
+type GenericController = Controller<SubControllerSpecifier=Box<Any>>;
 
 ///
 /// Controllers represent a portion of the UI and provide a hub for
@@ -28,9 +29,55 @@ pub trait Controller {
     fn ui(&self) -> Box<Bound<Control>>;
 
     /// Attempts to retrieve a sub-controller of this controller
-    fn get_subcontroller(&self, id: Self::SubControllerSpecifier) -> Option<Box<GenericController>>;
+    fn get_subcontroller(&self, id: &Self::SubControllerSpecifier) -> Option<Box<GenericController>>;
 }
 
+///
+/// Provides the generic controller interface for any controller
+/// along with a deref implementation so the 'native' interface
+/// is also available.
+///
+pub struct AnyController<TController: Controller>(TController);
+
+impl<TController: Controller> AnyController<TController> {
+    ///
+    /// Creates a new controller whose identifiers can be represented generically
+    ///
+    pub fn from(controller: TController) -> AnyController<TController> {
+        AnyController(controller)
+    }
+}
+
+impl<TController: Controller> Controller for AnyController<TController>
+where TController::SubControllerSpecifier: 'static {
+    type SubControllerSpecifier = Box<Any>;
+
+    fn ui(&self) -> Box<Bound<Control>> {
+        // UI is just passed straight through
+        let real_controller = &self.0;
+
+        real_controller.ui()
+    }
+
+    fn get_subcontroller(&self, id: &Box<Any>) -> Option<Box<GenericController>> {
+        if let Some(real_id) = id.downcast_ref::<TController::SubControllerSpecifier>() {
+            // Valid IDs are passed through
+            let AnyController(ref real_controller) = *self;
+            real_controller.get_subcontroller(real_id)
+        } else {
+            // Invalid IDs just produce no controller
+            None
+        }
+    }
+}
+
+impl<TController: Controller> Deref for AnyController<TController> {
+    type Target = TController;
+
+    fn deref(&self) -> &TController {
+        &self.0   
+    }
+}
 ///
 /// Provides a controller interface where all the identifiers
 /// are JSON strings.
@@ -58,14 +105,14 @@ where for<'de> TController::SubControllerSpecifier: Serialize+Deserialize<'de> {
         real_controller.ui()
     }
 
-    fn get_subcontroller(&self, id: String) -> Option<Box<GenericController>> {
+    fn get_subcontroller(&self, id: &String) -> Option<Box<GenericController>> {
         // Need to deserialize the real ID and pass it through
         let real_id = from_str::<TController::SubControllerSpecifier>(&id);
 
         if let Ok(real_id) = real_id {
             // Valid IDs are passed through
             let JsonController(ref real_controller) = *self;
-            real_controller.get_subcontroller(real_id)
+            real_controller.get_subcontroller(&real_id)
         } else {
             // Invalid IDs just produce no controller
             None
