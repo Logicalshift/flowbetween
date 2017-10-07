de68a8a08af55b1bb81e3b7a17f02de4f0a3ae56
Andrew Hunter <andrew@logicalshift.co.uk>
2017-10-07 10:38:49 +0100

    Make a generic version of the controller that represents its components as strings
    We’ll use serde for these strings. This makes it so we can write controllers with strong types but use them in a more generic UI.
    
    (Some kind of transmuting type using std::any might also be a good approach?)

--- ui/src/controller.rs
+++ ui/src/controller.rs
@@ -1,18 +1,83 @@
 use super::binding::*;
 use super::control::*;
 
+use std::ops::Deref;
+
+use serde::*;
+use serde_json::*;
+
+///
+/// The generic controller is used to hide the types of sub-controllers
+///
+type GenericController = Controller<SubControllerSpecifier=String>;
+
 ///
 /// Controllers represent a portion of the UI and provide a hub for
 /// receiving events related to it and connecting the model or
 /// viewmodel.
 ///
-/// The subcontroller identifier can be used to retrieve controllers
-/// that deal with a different part of the UI.
+/// The subcontroller specifier can be used to retrieve controllers
+/// that deal with a different part of the UI. When passing events
+/// and IDs between controllers, we use strings (JSON, serialized
+/// and deserialized by serde). 
 ///
-pub trait Controller<SubControllerIdentifier> {
+pub trait Controller {
+    type SubControllerSpecifier;
+    
     /// Retrieves a Control representing the UI for this controller
-    fn ui() -> Box<Bound<Control>>;
+    fn ui(&self) -> Box<Bound<Control>>;
 
     /// Attempts to retrieve a sub-controller of this controller
-    fn get_subcontroller(id: SubControllerIdentifier) /* -> ??? - needs to be Option<Box<Controller>> but we want an untyped version here */;
+    fn get_subcontroller(&self, id: Self::SubControllerSpecifier) -> Option<Box<GenericController>>;
+}
+
+///
+/// Provides the generic controller interface for any controller
+/// along with a deref implementation so the 'native' interface
+/// is also available.
+///
+pub struct StringController<TController: Controller>(TController);
+
+impl<TController: Controller> StringController<TController>
+where for<'de> TController::SubControllerSpecifier: Serialize+Deserialize<'de> {
+    ///
+    /// Creates a new controller whose identifiers are strings
+    ///
+    pub fn from(controller: TController) -> StringController<TController> {
+        StringController(controller)
+    }
+}
+
+impl<TController: Controller> Controller for StringController<TController>
+where for<'de> TController::SubControllerSpecifier: Serialize+Deserialize<'de> {
+    type SubControllerSpecifier = String;
+
+    fn ui(&self) -> Box<Bound<Control>> {
+        // UI is just passed straight through
+        let real_controller = &self.0;
+
+        real_controller.ui()
+    }
+
+    fn get_subcontroller(&self, id: String) -> Option<Box<GenericController>> {
+        // Need to deserialize the real ID and pass it through
+        let real_id = from_str::<TController::SubControllerSpecifier>(&id);
+
+        if let Ok(real_id) = real_id {
+            // Valid IDs are passed through
+            let StringController(ref real_controller) = *self;
+            real_controller.get_subcontroller(real_id)
+        } else {
+            // Invalid IDs just produce no controller
+            None
+        }
+    }
+}
+
+impl<TController: Controller> Deref for StringController<TController> {
+    type Target = TController;
+
+    fn deref(&self) -> &TController {
+        &self.0   
+    }
 }
