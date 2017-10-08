8e4fc174ccba59132137133460dd93a2345490c5
Andrew Hunter <andrew@logicalshift.co.uk>
2017-10-08 16:20:19 +0100

    Add a way to assemble a UI tree from a controller
    A coolish thing is that we only actually need to do this once for any controller and everything sticks together as it’s a binding. Using Arc<> for subcontrollers to help with references in the binding system

--- ui/src/controller.rs
+++ ui/src/controller.rs
@@ -1,6 +1,8 @@
 use super::binding::*;
 use super::control::*;
 
+use std::sync::*;
+
 // TODO: it would be nice to use specific enum types for
 // sub-controllers and events. However, this causes a few
 // problems. Firstly, it's pretty tricky to add them as
@@ -23,10 +25,89 @@ use super::control::*;
 /// receiving events related to it and connecting the model or
 /// viewmodel.
 ///
-pub trait Controller {
+pub trait Controller : Send+Sync {
     /// Retrieves a Control representing the UI for this controller
     fn ui(&self) -> Box<Bound<Control>>;
 
     /// Attempts to retrieve a sub-controller of this controller
-    fn get_subcontroller(&self, id: &str) -> Option<Box<Controller>>;
+    fn get_subcontroller(&self, id: &str) -> Option<Arc<Controller>>;
+}
+
+///
+/// Returns a bound control that expands the content of any 
+/// sub-controllers that might be present.
+///
+pub fn assemble_ui(base_controller: Arc<Controller>) -> Box<Bound<Control>> {
+    // Fetch the UI for the controller
+    let base_ui = base_controller.ui();
+
+    // Result is computed
+    return Box::new(computed(move || {
+        base_ui.get().map(&|control| {
+            if let Some(controller_id) = control.controller() {
+                // Has a subcontroller
+                let subcontroller = base_controller.get_subcontroller(controller_id);
+
+                if let Some(subcontroller) = subcontroller {
+                    // If we can look up the subcontroller then this control should have its UI as its subcomponents
+                    let subassembly = assemble_ui(subcontroller);
+                    control.with(vec![subassembly.get()])
+                } else {
+                    // No subcontroller
+                    control.clone()
+                }
+            } else {
+                // Control is untouched
+                control.clone()
+            }
+        })
+    }));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    struct TestController;
+    struct LabelController;
+
+    impl Controller for TestController {
+        fn ui(&self) -> Box<Bound<Control>> {
+            Box::new(bind(Control::container().with_controller("Test")))
+        }
+
+        fn get_subcontroller(&self, _id: &str) -> Option<Arc<Controller>> {
+            Some(Arc::new(LabelController))
+        }
+    }
+
+    impl Controller for LabelController {
+        fn ui(&self) -> Box<Bound<Control>> {
+            Box::new(bind(Control::label()))
+        }
+
+        fn get_subcontroller(&self, _id: &str) -> Option<Arc<Controller>> {
+            None
+        }
+    }
+
+    #[test]
+    fn can_assemble_simple_label() {
+        let label_controller    = Arc::new(LabelController);
+        let assembly            = assemble_ui(label_controller);
+
+        assert!(assembly.get() == Control::label());
+    }
+
+    #[test]
+    fn can_assemble_with_subassembly() {
+        let test_controller = Arc::new(TestController);
+        let assembly        = assemble_ui(test_controller);
+
+        assert!(assembly.get() == Control::container()
+            .with_controller("Test")
+            .with(vec![
+                Control::label()
+            ]));
+    }
 }
