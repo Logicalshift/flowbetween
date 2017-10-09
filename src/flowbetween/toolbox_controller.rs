233097c999dffc058c2483e128db016ae7710784
Andrew Hunter <andrew@logicalshift.co.uk>
2017-10-09 23:09:54 +0100

    Add a test button to the timeline

--- src/flowbetween/toolbox_controller.rs
+++ src/flowbetween/toolbox_controller.rs
@@ -10,16 +10,34 @@ pub struct ToolboxController {
 
 impl ToolboxController {
     pub fn new() -> ToolboxController {
-        let ui = bind(Control::empty());
+        let ui = bind(Control::container()
+            .with(Bounds::fill_all())
+            .with(vec![Self::make_tool()]));
 
         ToolboxController {
             ui: ui
         }
+    }
+
+    ///
+    /// Creates a new tool control
+    ///
+    fn make_tool() -> Control {
+        use ui::ControlAttribute::*;
+        use ui::ActionTrigger::*;
+
+        Control::button()
+            .with(Action(Click, String::from("ToolClick")))
+            .with(Bounds::next_vert(48.0))
     }
 }
 
 impl Controller for ToolboxController {
     fn ui(&self) -> Box<Bound<Control>> {
         Box::new(self.ui.clone())
+    }
+
+    fn action(&self, action_id: &str) {
+        println!("Toolbox action is {}", action_id);
     }
 }
