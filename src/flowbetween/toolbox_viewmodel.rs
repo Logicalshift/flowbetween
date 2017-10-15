e71d9e22645d5eda8d1d4fa5491c40b4c891a23f
Andrew Hunter <andrew@logicalshift.co.uk>
2017-10-15 19:32:35 +0100

    Add a place to store computed bindings

--- src/flowbetween/toolbox_viewmodel.rs
+++ src/flowbetween/toolbox_viewmodel.rs
@@ -7,6 +7,9 @@ pub struct ToolboxViewModel {
     /// Maps bindings in this viewmodel to their values
     bindings: Mutex<HashMap<String, Arc<Binding<PropertyValue>>>>,
 
+    /// Maps computed bindings to their values (we ignore these when setting)
+    computed: Mutex<HashMap<String, Arc<Bound<PropertyValue>>>>,
+
     /// Used for properties that don't exist in this model
     nothing: Arc<Binding<PropertyValue>>
 }
@@ -17,18 +20,40 @@ impl ToolboxViewModel {
     /// 
     pub fn new() -> ToolboxViewModel {
         ToolboxViewModel { 
-            bindings: Mutex::new(HashMap::new()), 
-            nothing: Arc::new(bind(PropertyValue::Nothing)) }
+            bindings:   Mutex::new(HashMap::new()), 
+            computed:   Mutex::new(HashMap::new()),
+            nothing:    Arc::new(bind(PropertyValue::Nothing)) }
+    }
+
+    ///
+    /// Attempts to retrieve the set binding with a particular name
+    ///
+    fn get_binding(&self, property_name: &str) -> Option<Arc<Binding<PropertyValue>>> {
+        let bindings = self.bindings.lock().unwrap();
+
+        bindings.get(&String::from(property_name)).map(|arc| arc.clone())
+    }
+
+    ///
+    /// Attempts to retrieve the computed binding with a paritcular name
+    /// 
+    fn get_computed(&self, property_name: &str) -> Option<Arc<Bound<PropertyValue>>> {
+        let computed = self.computed.lock().unwrap();
+
+        computed.get(&String::from(property_name)).map(|arc| arc.clone())
     }
 }
 
 impl ViewModel for ToolboxViewModel {
     fn get_property(&self, property_name: &str) -> Arc<Bound<PropertyValue>> {
-        let bindings = self.bindings.lock().unwrap();
-
-        if let Some(value) = bindings.get(&String::from(property_name)) {
-            value.clone()
+        if let Some(result) = self.get_computed(property_name) {
+            // Computed values are returned first, so these bindings cannot be set
+            result
+        } else if let Some(result) = self.get_binding(property_name) {
+            // 'Set' bindings are returned next
+            result
         } else {
+            // If an invalid name is requested, we return something bound to nothing
             self.nothing.clone()
         }
     }
