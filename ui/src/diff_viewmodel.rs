a07cc0e66d770f6a4a8cd94d503fa33ce6133b62
Andrew Hunter <andrew@logicalshift.co.uk>
2017-10-22 16:08:02 +0100

    Add a new struct that we’ll use to track how the viewmodel attached to a controller changes
    Hm, we probably need one that tracks how a control tree changes too…

---
+++ ui/src/diff_viewmodel.rs
@@ -0,0 +1,6 @@
+///
+/// Tracks differences in the viewmodel attached to a controller and its subtree
+/// 
+pub struct DiffViewModel {
+
+}
--- ui/src/lib.rs
+++ ui/src/lib.rs
@@ -10,6 +10,7 @@ pub mod controller;
 pub mod property;
 pub mod viewmodel;
 pub mod dynamic_viewmodel;
+pub mod diff_viewmodel;
 
 pub use self::control::*;
 pub use self::layout::*;
@@ -19,3 +20,4 @@ pub use self::controller::*;
 pub use self::property::*;
 pub use self::viewmodel::*;
 pub use self::dynamic_viewmodel::*;
+pub use self::diff_viewmodel::*;
