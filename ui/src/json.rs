7a10a9cdb35e73baf8ef3db5dc4a9997c1fdbbd3
Andrew Hunter <andrew@logicalshift.co.uk>
2017-11-11 16:56:54 +0000

    Thinking about it, we don’t yet need a way to convert back and that’s complicated in any case due to how we’re going to fetch resources

--- ui/src/json.rs
+++ ui/src/json.rs
@@ -8,9 +8,4 @@ pub trait ToJsonValue {
     /// Creates a JSON representation of this item
     ///
     fn to_json(&self) -> serde_json::Value;
-
-    ///
-    /// Reconstitutes this value from its JSON representation
-    ///
-    fn from_json(json: &serde_json::Value);
 }
