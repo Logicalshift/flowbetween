48c2a6e8f8adb8345080fd437d9dbf9df19a6690
Andrew Hunter <andrew@logicalshift.co.uk>
2017-11-25 20:39:48 +0000

    Frames should have a time index

--- animation/src/traits/frame.rs
+++ animation/src/traits/frame.rs
@@ -6,6 +6,11 @@ use super::attributes::*;
 ///
 pub trait Frame : HasAttributes {
     ///
+    /// Time index of this frame, in nanoseconds
+    /// 
+    fn time_index(&self) -> u64;
+
+    ///
     /// Renders this frame to a particular graphics context
     ///
     fn render_to(&self, gc: &mut GraphicsContext);
