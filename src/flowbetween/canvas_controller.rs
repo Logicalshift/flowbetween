c85383e04ed1c1c98dcbc2dd16802ac86d15bf6a
Andrew Hunter <andrew@logicalshift.co.uk>
2017-11-25 22:14:59 +0000

    Render the background layer

--- src/flowbetween/canvas_controller.rs
+++ src/flowbetween/canvas_controller.rs
@@ -17,32 +17,82 @@ pub struct CanvasController<Anim: EditableAnimation> {
 
 impl<Anim: EditableAnimation> CanvasController<Anim> {
     pub fn new(animation: &Arc<Anim>) -> CanvasController<Anim> {
+        // Create the resources
         let canvases = ResourceManager::new();
 
-        let test_canvas = canvases.register(Canvas::new());
-        canvases.assign_name(&test_canvas, "test_canvas");
+        // Create the controller
+        let mut controller = CanvasController {
+            view_model: Arc::new(NullViewModel::new()),
+            ui:         bind(Control::empty()),
+            canvases:   Arc::new(canvases),
+            animation:  animation.clone()
+        };
 
-        let ui = bind(Control::canvas()
-            .with(test_canvas.clone())
+        // Set up the UI
+        let background_canvas = controller.create_background_canvas();
+
+        controller.ui.set(Control::canvas()
+            .with(background_canvas)
             .with(Bounds::fill_all()));
 
-        test_canvas.draw(|gc| {
-            gc.line_width_pixels(2.0);
-            gc.fill_color(Color::Rgba(0.5,0.0,0.0, 1.0));
-            gc.stroke_color(Color::Rgba(0.0,0.0,0.0, 1.0));
+        controller
+    }
 
+    ///
+    /// Creates a canvas for this object
+    /// 
+    fn create_canvas(&self) -> Resource<Canvas> {
+        let canvas = self.canvases.register(Canvas::new());
+        self.clear_canvas(&canvas);
+        canvas
+    }
+
+    ///
+    /// Clears a canvas and sets it up for rendering
+    /// 
+    fn clear_canvas(&self, canvas: &Resource<Canvas>) {
+        let (_width, height) = self.animation.size();
+
+        canvas.draw(move |gc| {
+            gc.clear_canvas();
+            gc.canvas_height((height*1.05) as f32);
+        });
+    }
+
+    ///
+    /// Create the background canvas for this controller
+    ///
+    fn create_background_canvas(&self) -> Resource<Canvas> {
+        let canvas          = self.create_canvas();
+        let (width, height) = self.animation.size();
+
+        canvas.draw(move |gc| {
+            let (width, height)             = (width as f32, height as f32);
+            let (minx, miny, maxx, maxy)    = (
+                -width/2.0, -height/2.0,
+                width/2.0, height/2.0
+            );
+
+            gc.stroke_color(Color::Rgba(0.0, 0.0, 0.0, 1.0));
+            gc.line_width_pixels(1.0);
+
+            // Draw the shadow
+            let offset = height * 0.015;
+
+            gc.fill_color(Color::Rgba(0.1, 0.1, 0.1, 0.4));
             gc.new_path();
-            gc.rect(-0.5, -0.5, 0.5, 0.5);
+            gc.rect(minx, miny-offset, maxx+offset, maxy);
+            gc.fill();
+
+            // Draw the canvas background
+            gc.fill_color(Color::Rgba(1.0, 1.0, 1.0, 1.0));
+            gc.new_path();
+            gc.rect(minx, miny, maxx, maxy);
             gc.fill();
             gc.stroke();
         });
 
-        CanvasController {
-            view_model: Arc::new(NullViewModel::new()),
-            ui:         ui,
-            canvases:   Arc::new(canvases),
-            animation:  animation.clone()
-        }
+        canvas
     }
 }
 
