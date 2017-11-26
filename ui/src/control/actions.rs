62f0478336da13ca3429e917ce71da582428d0ac
Andrew Hunter <andrew@logicalshift.co.uk>
2017-11-26 11:21:10 +0000

    Add a ‘paint’ action for things where we want to track all mouse events.

--- ui/src/control/actions.rs
+++ ui/src/control/actions.rs
@@ -4,5 +4,8 @@
 #[derive(Clone, PartialEq, Serialize, Deserialize)]
 pub enum ActionTrigger {
     /// User clicked this item (pressed down and released while over the same item)
-    Click
+    Click,
+
+    /// Tracks all user clicks and drags over this item
+    Paint
 }
