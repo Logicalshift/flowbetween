2351667ea5ea97534afa9ce760b6e2609a87b445
Andrew Hunter <andrew@logicalshift.co.uk>
2017-11-28 21:31:29 +0000

    Make the layers and the size editable traits of animation rather than fixed ones

--- animation/src/traits/animation.rs
+++ animation/src/traits/animation.rs
@@ -1,11 +1,12 @@
 use super::layer::*;
+use super::editable::*;
 use super::attributes::*;
 use super::super::inmemory::*;
 
 ///
 /// Represents an animation
 ///
-pub trait Animation : HasAttributes+Send+Sync {
+pub trait Animation : Editable<AnimationLayers>+Editable<AnimationSize>+HasAttributes+Send+Sync {
     ///
     /// Returns the dimensions of this animation
     /// 
@@ -15,6 +16,41 @@ pub trait Animation : HasAttributes+Send+Sync {
     /// Retrieves the layers for this animation
     ///
     fn layers<'a>(&'a self) -> Box<'a+Iterator<Item = &'a Layer>>;
+}
+
+///
+/// Represents the size properties of an animation
+/// 
+pub trait AnimationSize {
+    ///
+    /// Retrieves the frame size of this animation
+    /// 
+    fn size(&self) -> (f64, f64);
+
+    ///
+    /// Updates the frame size of this animation
+    ///
+    fn set_size(&self, (f64, f64));
+}
+
+///
+/// Represents the layers associated with an animation
+/// 
+pub trait AnimationLayers {
+    ///
+    /// Retrieves the layers for this animation
+    ///
+    fn layers<'a>(&'a self) -> Box<'a+Iterator<Item = &'a Layer>>;
+
+    ///
+    /// Removes the layer with the specified ID
+    /// 
+    fn remove_layer(&mut self, layer_id: u64);
+
+    ///
+    /// Adds a new layer to this object
+    /// 
+    fn add_new_layer<'a>(&'a mut self) -> &'a Layer;
 }
 
 impl Animation for () {
