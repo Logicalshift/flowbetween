e272f91c146b24390ed3e453d39e224f1cf55e3e
Andrew Hunter <andrew@logicalshift.co.uk>
2017-12-01 23:33:23 +0000

    Add the initial layers implementation to the in-memory animation object

--- animation/src/inmemory/animation.rs
+++ animation/src/inmemory/animation.rs
@@ -1,3 +1,4 @@
+use super::vector_layer::*;
 use super::super::traits::*;
 
 use std::sync::*;
@@ -11,7 +12,12 @@ struct AnimationCore {
     size: (f64, f64),
 
     /// The duration of a frame in the animation
-    frame_duration: Duration
+    frame_duration: Duration,
+
+    /// The layers in this animation
+    layers: Vec<Arc<Layer>>,
+
+    next_layer_id: u64
 }
 
 ///
@@ -27,7 +33,9 @@ impl InMemoryAnimation {
         // Create the core (30fps by default)
         let core = AnimationCore { 
             size:           (1980.0, 1080.0),
-            frame_duration: Duration::from_millis(1000/30)
+            frame_duration: Duration::from_millis(1000/30),
+            layers:         vec![],
+            next_layer_id:  0
         };
 
         // Create the final animation
@@ -55,8 +63,17 @@ impl Editable<AnimationSize+'static> for InMemoryAnimation {
 }
 
 impl Editable<AnimationLayers+'static> for InMemoryAnimation {
-    fn open(&self) -> Option<Editor<AnimationLayers+'static>> { None }
-    fn read(&self) -> Option<Reader<AnimationLayers+'static>> { None }
+    fn open(&self) -> Option<Editor<AnimationLayers+'static>> { 
+        let core: &RwLock<AnimationLayers>  = &self.core;
+
+        Some(Editor::new(core.write().unwrap()))
+    }
+
+    fn read(&self) -> Option<Reader<AnimationLayers+'static>> { 
+        let core: &RwLock<AnimationLayers>  = &self.core;
+
+        Some(Reader::new(core.read().unwrap()))
+    }
 }
 
 impl AnimationSize for AnimationCore {
@@ -64,5 +81,43 @@ impl AnimationSize for AnimationCore {
 
     fn set_size(&mut self, new_size: (f64, f64)) {
         self.size = new_size;
+    }
+}
+
+impl AnimationLayers for AnimationCore {
+    fn layers<'a>(&'a self) -> Box<'a+Iterator<Item = &'a Layer>> {
+        Box::new(self.layers.iter().map(|x| &**x))
+    }
+
+    fn remove_layer(&mut self, layer_id: u64) {
+        // Find the index of the layer with this ID
+        let remove_index = {
+            let mut remove_index = None;
+
+            for index in 0..self.layers.len() {
+                if self.layers[index].id() == layer_id {
+                    remove_index = Some(index);
+                }
+            }
+            remove_index
+        };
+
+        // Remove this layer
+        if let Some(remove_index) = remove_index {
+            self.layers.remove(remove_index);
+        }
+    }
+
+    fn add_new_layer<'a>(&'a mut self) -> &'a Layer {
+        // Pick an ID for this layer
+        let layer_id = self.next_layer_id;
+        self.next_layer_id += 1;
+
+        // Generate the layer
+        let new_layer = Arc::new(VectorLayer::new(layer_id));
+        self.layers.push(new_layer);
+
+        // Result is a reference to the layer
+        &**self.layers.last().unwrap()
     }
 }
