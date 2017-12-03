d916d382b490c9679b2a0b3dbb1b3118bdc4118b
Andrew Hunter <andrew@logicalshift.co.uk>
2017-12-03 11:04:03 +0000

    Add a vector key frame (we’ll use this with the vector layer)

--- animation/src/inmemory/mod.rs
+++ animation/src/inmemory/mod.rs
@@ -1,7 +1,9 @@
 mod animation;
 mod empty_iterator;
 mod vector_layer;
+mod vector_keyframe;
 
 pub use self::animation::*;
 pub use self::empty_iterator::*;
 pub use self::vector_layer::*;
+pub use self::vector_keyframe::*;
---
+++ animation/src/inmemory/vector_keyframe.rs
@@ -0,0 +1,20 @@
+use std::time::Duration;
+
+///
+/// Represents a keuyframe in a vector animation
+/// 
+pub struct VectorKeyFrame {
+    /// When this frame starts
+    start_time: Duration
+}
+
+impl VectorKeyFrame {
+    ///
+    /// Creates a new vector key frame
+    /// 
+    pub fn new(start_time: Duration) -> VectorKeyFrame {
+        VectorKeyFrame {
+            start_time: Duration
+        }
+    }
+}
