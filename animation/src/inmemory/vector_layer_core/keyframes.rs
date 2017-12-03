cdf8cc23e4d6eab098a12065588a21533f1f6359
Andrew Hunter <andrew@logicalshift.co.uk>
2017-12-03 16:16:49 +0000

    Add support for removing key frames

--- animation/src/inmemory/vector_layer_core/keyframes.rs
+++ animation/src/inmemory/vector_layer_core/keyframes.rs
@@ -17,6 +17,12 @@ impl KeyFrameLayer for VectorLayerCore {
     }
 
     fn remove_key_frame(&mut self, time_offset: Duration) {
-        unimplemented!()
+        // Binary search for the key frame
+        let search_result = self.keyframes.binary_search_by(|a| a.start_time().cmp(&time_offset));
+
+        // Remove only if we found an exact match
+        if let Ok(frame_number) = search_result {
+            self.keyframes.remove(frame_number);
+        }
     }
 }
