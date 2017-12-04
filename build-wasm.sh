f9dbea2c8bb73ac34e1956366ea53b144814a274
Andrew Hunter <andrew@logicalshift.co.uk>
2017-12-04 22:34:49 +0000

    Add ui to the wasm build
    Think dependency on desync from here is the issue we’ve got at the moment.

--- build-wasm.sh
+++ build-wasm.sh
@@ -4,6 +4,10 @@ cd binding
 cargo +nightly build --target wasm32-unknown-unknown
 cd ..
 
+cd ui
+cargo +nightly build --target wasm32-unknown-unknown
+cd ..
+
 cd animation
 cargo +nightly build --target wasm32-unknown-unknown
 cd ..
