35d40856fce5a1a0a8a9e47ff9805da468ef797d
Andrew Hunter <andrew@logicalshift.co.uk>
2017-12-10 16:51:15 +0000

    Subdivisions can now use points

--- curves/src/bezier/subdivide.rs
+++ curves/src/bezier/subdivide.rs
@@ -1,20 +1,20 @@
 use super::basis::*;
+use super::super::coordinate::*;
 
 ///
 /// Subdivides a cubic bezier curve at a particular point, returning the weights of
 /// the two component curves
 /// 
-pub fn subdivide4(t: f32, w1: f32, w2: f32, w3: f32, w4: f32) -> 
-    ((f32, f32, f32, f32),
-    (f32, f32, f32, f32)) {
+pub fn subdivide4<Point: Coordinate>(t: f32, w1: Point, w2: Point, w3: Point, w4: Point) -> 
+    ((Point, Point, Point, Point), (Point, Point, Point, Point)) {
     // Weights (from de casteljau)
-    let wn1 = (1.0-t)*w1 + t*w2;
-    let wn2 = (1.0-t)*w2 + t*w3;
-    let wn3 = (1.0-t)*w3 + t*w4;
+    let wn1 = w1*(1.0-t) + w2*t;
+    let wn2 = w2*(1.0-t) + w3*t;
+    let wn3 = w3*(1.0-t) + w4*t;
 
     // Further refine the weights
-    let wnn1 = (1.0-t)*wn1 + t*wn2;
-    let wnn2 = (1.0-t)*wn2 + t*wn3;
+    let wnn1 = wn1*(1.0-t) + wn2*t;
+    let wnn2 = wn2*(1.0-t) + wn3*t;
 
     // Get the point at which the two curves join
     let p = de_casteljau2(t, wnn1, wnn2);
