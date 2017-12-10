b1b8440fa9c5e6a6db3e0fa76e55d78b46b6dcc6
Andrew Hunter <andrew@logicalshift.co.uk>
2017-12-10 17:05:51 +0000

    Add a test for point_at_pos

--- curves/tests/bezier/mod.rs
+++ curves/tests/bezier/mod.rs
@@ -10,10 +10,24 @@ pub fn approx_equal(a: f32, b: f32) -> bool {
 }
 
 #[test]
-fn can_read_curve_points() {
+fn can_read_curve_control_points() {
     let curve = bezier::Curve::from_points(Coord2(1.0, 1.0), Coord2(2.0, 2.0), Coord2(3.0, 3.0), Coord2(4.0, 4.0));
 
     assert!(curve.start_point() == Coord2(1.0, 1.0));
     assert!(curve.end_point() == Coord2(2.0, 2.0));
     assert!(curve.control_points() == (Coord2(3.0, 3.0), Coord2(4.0, 4.0)));
+}
+
+#[test]
+fn can_read_curve_points() {
+    let curve = bezier::Curve::from_points(Coord2(1.0, 1.0), Coord2(2.0, 2.0), Coord2(3.0, 3.0), Coord2(4.0, 4.0));
+
+    for x in 0..100 {
+        let t = (x as f32)/100.0;
+
+        let point           = curve.point_at_pos(t);
+        let another_point   = bezier::de_casteljau4(t, Coord2(1.0, 1.0), Coord2(3.0, 3.0), Coord2(4.0, 4.0), Coord2(2.0, 2.0));
+
+        assert!(point == another_point);
+    }
 }
