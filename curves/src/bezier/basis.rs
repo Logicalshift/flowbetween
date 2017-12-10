ce70d5bebb547156a4eb4ac8833d9dd42f2727a8
Andrew Hunter <andrew@logicalshift.co.uk>
2017-12-10 16:00:35 +0000

    Add an implementation of de casteljau’s algorithm
    … which we can compare with the basis function as they should have identical results

--- curves/src/bezier/basis.rs
+++ curves/src/bezier/basis.rs
@@ -15,3 +15,35 @@ pub fn basis(t: f32, w1: f32, w2: f32, w3: f32, w4: f32) -> f32 {
         + 3.0*w3*one_minus_t*t_squared
         + w4*t_cubed;
 }
+
+
+///
+/// de Casteljau's algorithm for cubic bezier curves
+/// 
+#[inline]
+pub fn de_casteljau4(t: f32, w1: f32, w2: f32, w3: f32, w4: f32) -> f32 {
+    let wn1 = (1.0-t)*w1 + t*w2;
+    let wn2 = (1.0-t)*w2 + t*w3;
+    let wn3 = (1.0-t)*w3 + t*w4;
+
+    de_casteljau3(t, wn1, wn2, wn3)
+}
+
+///
+/// de Casteljau's algorithm for quadratic bezier curves
+/// 
+#[inline]
+pub fn de_casteljau3(t: f32, w1: f32, w2: f32, w3: f32) -> f32 {
+    let wn1 = (1.0-t)*w1 + t*w2;
+    let wn2 = (1.0-t)*w2 + t*w3;
+
+    de_casteljau2(t, wn1, wn2)
+}
+
+///
+/// de Casteljau's algorithm for lines
+/// 
+#[inline]
+pub fn de_casteljau2(t: f32, w1: f32, w2: f32) -> f32 {
+    (1.0-t)*w1 + t*w2
+}
--- curves/tests/bezier/basis.rs
+++ curves/tests/bezier/basis.rs
@@ -9,3 +9,15 @@ fn basis_at_t0_is_w1() {
 fn basis_at_t1_is_w4() {
     assert!(bezier::basis(1.0, 2.0, 3.0, 4.0, 5.0) == 5.0);
 }
+
+#[test]
+fn basis_agrees_with_de_casteljau() {
+    for x in 0..100 {
+        let t               = (x as f32)/100.0;
+
+        let basis           = bezier::basis(t, 2.0, 3.0, 4.0, 5.0);
+        let de_casteljau    = bezier::de_casteljau4(t, 2.0, 3.0, 4.0, 5.0);
+
+        assert!(f32::floor(f32::abs(basis - de_casteljau)*10000.0) == 0.0);
+    }
+}
