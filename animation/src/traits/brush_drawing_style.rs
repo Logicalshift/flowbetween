467d9d76ee4c549c114fbc423cbb189e8f9732ac
Andrew Hunter <andrew@logicalshift.co.uk>
2017-12-28 18:22:12 +0000

    BrushDrawingStyle should be serializable

--- animation/src/traits/brush_drawing_style.rs
+++ animation/src/traits/brush_drawing_style.rs
@@ -1,7 +1,7 @@
 ///
 /// Represents the drawing style to use with a brush
 /// 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Serialize, Deserialize)]
 pub enum BrushDrawingStyle {
     /// Draw this brush directly on to the current layer
     Draw,
