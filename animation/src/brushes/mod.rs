0d461b39b83c8706c3984e88cc7178e4278fd94a
Andrew Hunter <andrew@logicalshift.co.uk>
2017-12-28 17:30:30 +0000

    Add a way to turn a brush definition into an actual brush object

--- animation/src/brushes/mod.rs
+++ animation/src/brushes/mod.rs
@@ -3,3 +3,28 @@ mod ink;
 
 pub use self::simple::*;
 pub use self::ink::*;
+
+use super::traits::*;
+use std::sync::*;
+
+///
+/// Creates a brush from a brush definition
+/// 
+pub fn create_brush_from_definition(definition: &BrushDefinition) -> Arc<Brush> {
+    use BrushDefinition::*;
+
+    match definition {
+        &Ink(ref ink_definition) => Arc::new(InkBrush::new(ink_definition, false))
+    }
+}
+
+///
+/// Creates an eraser from a brush definition
+/// 
+pub fn create_eraser_from_definition(definition: &BrushDefinition) -> Arc<Brush> {
+    use BrushDefinition::*;
+
+    match definition {
+        &Ink(ref ink_definition) => Arc::new(InkBrush::new(ink_definition, true))
+    }
+}
