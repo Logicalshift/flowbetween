20198fd6f99a9a63a9cf9793cb1958efeeda8c51
Andrew Hunter <andrew@logicalshift.co.uk>
2017-12-30 11:26:49 +0000

    Add a ‘standard brush properties’ structure

---
+++ animation/src/traits/brush_properties.rs
@@ -0,0 +1,15 @@
+use ui::canvas::*;
+
+///
+/// Standard properties for a brush stroke
+/// 
+/// These are the properties that are independent of the brush type.
+/// Properties that define a brush can be found in brush_definition.
+/// 
+pub struct BrushProperties {
+    /// The width of the brush stroke
+    pub width: f32,
+
+    /// The colour of the brush stroke
+    pub color: Color
+}
