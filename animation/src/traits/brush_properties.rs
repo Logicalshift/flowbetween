6bd6090c819114e22ece228449dcba5b86ea3857
Andrew Hunter <andrew@logicalshift.co.uk>
2017-12-30 12:41:10 +0000

    Need to derive some properties for this thing too

--- animation/src/traits/brush_properties.rs
+++ animation/src/traits/brush_properties.rs
@@ -5,7 +5,8 @@ use canvas::*;
 /// 
 /// These are the properties that are independent of the brush type.
 /// Properties that define a brush can be found in brush_definition.
-/// 
+///
+#[derive(Clone, Copy, PartialEq)]
 pub struct BrushProperties {
     /// The size of the brush stroke
     pub size: f32,
