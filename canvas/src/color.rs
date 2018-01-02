e992e3cab5c4caa71d7f6f6aa73dac129df1aefe
Andrew Hunter <andrew@logicalshift.co.uk>
2018-01-02 20:31:13 +0000

    Add some more colour conversion routines to the color library

--- canvas/src/color.rs
+++ canvas/src/color.rs
@@ -1,6 +1,14 @@
 use hsluv::*;
 
 ///
+/// Possible formats of a colour value
+/// 
+pub enum ColorFormat {
+    Rgba,
+    Hsluv
+}
+
+///
 /// Representation of a colour 
 ///
 #[derive(Clone, Copy, PartialEq, Debug)]
@@ -25,6 +33,22 @@ impl Color {
     }
 
     ///
+    /// Converts this colour to another format
+    /// 
+    #[inline]
+    pub fn to_format(&self, format: ColorFormat) -> Color {
+        let (r, g, b, a) = self.to_rgba_components();
+
+        match format {
+            ColorFormat::Rgba   => Color::Rgba(r, g, b, a),
+            ColorFormat::Hsluv  => {
+                let (h, s, l) = rgb_to_hsluv((r as f64, g as f64, b as f64));
+                Color::Hsluv(h as f32, s as f32, l as f32, a)
+            }
+        }
+    }
+
+    ///
     /// Returns a new colour that's the same as this one except with a different alpha value
     /// 
     pub fn with_alpha(&self, new_alpha: f32) -> Color {
@@ -32,5 +56,51 @@ impl Color {
             &Color::Rgba(r, g, b, _)    => Color::Rgba(r, g, b, new_alpha),
             &Color::Hsluv(h, s, l, _)   => Color::Hsluv(h, s, l, new_alpha)
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn can_convert_rgba_to_hsluv() {
+        let rgb     = Color::Rgba(0.5, 0.7, 0.2, 0.9);
+        let hsluv   = rgb.to_format(ColorFormat::Hsluv);
+
+        if let Color::Hsluv(h, s, l, a) = hsluv {
+            assert!((h-110.3).abs() < 0.1);
+            assert!((s-89.5).abs() < 0.1);
+            assert!((l-67.1).abs() < 0.1);
+            assert!(a == 0.9);
+        } else {
+            assert!(false)
+        }
+    }
+
+    #[test]
+    fn can_convert_hsluv_to_rgba() {
+        let hsluv   = Color::Hsluv(24.0, 66.0, 60.0, 0.8);
+        let rgb     = hsluv.to_format(ColorFormat::Rgba);
+
+        if let Color::Rgba(r, g, b, a) = rgb {
+            assert!((r-0.89) < 0.1);
+            assert!((g-0.43) < 0.1);
+            assert!((b-0.38) < 0.1);
+            assert!(a == 0.8);
+        } else {
+            assert!(false);
+        }
+    }
+
+    #[test]
+    fn can_get_rgba_components_from_hsluv() {
+        let hsluv           = Color::Hsluv(24.0, 66.0, 60.0, 0.8);
+        let (r, g, b, a)    = hsluv.to_rgba_components();
+
+        assert!((r-0.89) < 0.1);
+        assert!((g-0.43) < 0.1);
+        assert!((b-0.38) < 0.1);
+        assert!(a == 0.8);
     }
 }
