758fce611a45a7b196cc34d335368884b680a239
Andrew Hunter <andrew@logicalshift.co.uk>
2018-01-04 11:30:22 +0000

    Add the start of a colour picker controller

---
+++ flo/src/color/color_picker_controller.rs
@@ -0,0 +1,44 @@
+use ui::*;
+use binding::*;
+
+use std::sync::*;
+
+///
+/// Controller that makes it possible to pick a colour
+/// 
+pub struct ColorPickerController {
+    ui: BindRef<Control>
+}
+
+impl ColorPickerController {
+    ///
+    /// Creates a new color picker controller
+    /// 
+    pub fn new() -> ColorPickerController {
+        let ui = Self::create_ui();
+
+        ColorPickerController {
+            ui: ui
+        }
+    }
+
+    fn create_ui() -> BindRef<Control> {
+        BindRef::from(computed(move || {
+            Control::label()
+                .with("Hello, colour controller")
+                .with(Bounds::fill_all())
+        }))
+    }
+}
+
+impl Controller for ColorPickerController {
+    /// Retrieves a Control representing the UI for this controller
+    fn ui(&self) -> BindRef<Control> {
+        self.ui.clone()
+    }
+
+    /// Retrieves the viewmodel for this controller
+    fn get_viewmodel(&self) -> Arc<ViewModel> {
+        Arc::new(NullViewModel::new())
+    }
+}
--- flo/src/color/mod.rs
+++ flo/src/color/mod.rs
@@ -1,2 +1,5 @@
 pub mod generate_images;
 pub mod images;
+pub mod color_picker_controller;
+
+pub use self::color_picker_controller::*;
