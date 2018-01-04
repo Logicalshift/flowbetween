a31864e1f3e64f84e9bd181298b532ba1a9e2dc5
Andrew Hunter <andrew@logicalshift.co.uk>
2018-01-04 11:59:33 +0000

    Popups need a high z-index so they actually open over other controls

--- ui/src/controllers/popup_controller.rs
+++ ui/src/controllers/popup_controller.rs
@@ -118,6 +118,7 @@ impl<ContentController: Controller> PopupController<ContentController> {
                     .with(Popup::Direction(direction))
                     .with(Popup::Size(width, height))
                     .with(Popup::Offset(offset))
+                    .with(ControlAttribute::ZIndex(1000))
             } else {
                 // Bind the UI only when the controller is open
                 Control::popup()
@@ -125,6 +126,7 @@ impl<ContentController: Controller> PopupController<ContentController> {
                     .with(Popup::Direction(direction))
                     .with(Popup::Size(width, height))
                     .with(Popup::Offset(offset))
+                    .with(ControlAttribute::ZIndex(1000))
                     .with((ActionTrigger::Dismiss, "DismissPopup"))
                     .with(vec![
                         content.get()
