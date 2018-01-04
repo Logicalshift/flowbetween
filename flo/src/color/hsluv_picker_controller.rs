0def2284c168b1177035756dea1df259d2559f60
Andrew Hunter <andrew@logicalshift.co.uk>
2018-01-04 15:54:45 +0000

    Add a colour preview canvas

--- flo/src/color/hsluv_picker_controller.rs
+++ flo/src/color/hsluv_picker_controller.rs
@@ -12,6 +12,7 @@ use std::sync::*;
 pub struct HsluvPickerController {
     ui:         BindRef<Control>,
     images:     Arc<ResourceManager<Image>>,
+    canvases:   Arc<ResourceManager<BindingCanvas>>,
 
     viewmodel:  Arc<DynamicViewModel>
 }
@@ -22,6 +23,7 @@ impl HsluvPickerController {
     /// 
     pub fn new(color: &Binding<Color>) -> HsluvPickerController {
         let images      = ResourceManager::new();
+        let canvases    = ResourceManager::new();
         let color       = color.clone();
         let viewmodel   = DynamicViewModel::new();
 
@@ -30,27 +32,50 @@ impl HsluvPickerController {
         let hsluv_wheel = images.register(hsluv_wheel);
         images.assign_name(&hsluv_wheel, "Wheel");
 
+        let color_preview = Self::create_color_preview_canvas(&color);
+        let color_preview = canvases.register(color_preview);
+
         // Set up the UI
-        let ui          = Self::create_ui(&color, &hsluv_wheel);
+        let ui          = Self::create_ui(&color, &hsluv_wheel, &color_preview);
         
         // Controller is ready to go
         HsluvPickerController {
             ui:         ui,
             images:     Arc::new(images),
+            canvases:   Arc::new(canvases),
             viewmodel:  Arc::new(viewmodel)
         }
     }
 
     ///
+    /// Creates the preview canvas
+    /// 
+    fn create_color_preview_canvas(color: &Binding<Color>) -> BindingCanvas {
+        let color = color.clone();
+
+        // Just a circle with the colour in the center
+        BindingCanvas::with_drawing(move |gc| {
+            gc.stroke_color(Color::Rgba(0.8, 0.9, 1.0, 0.8));
+            gc.line_width(0.04);
+            gc.fill_color(color.get());
+            gc.circle(0.0, 0.0, 1.0-0.02);
+            gc.fill();
+            gc.stroke();
+        })
+    }
+
+    ///
     /// Creates the UI for this controller
     /// 
-    fn create_ui(color: &Binding<Color>, hsluv_wheel: &Resource<Image>) -> BindRef<Control> {
+    fn create_ui(color: &Binding<Color>, hsluv_wheel: &Resource<Image>, preview: &Resource<BindingCanvas>) -> BindRef<Control> {
         // Constants
-        let wheel_size  = 200.0;
+        let wheel_size      = 200.0;
+        let preview_size    = f32::floor((140.0/256.0) * wheel_size)+2.0;
 
         // Bindings and images
         let color       = color.clone();
         let hsluv_wheel = hsluv_wheel.clone();
+        let preview     = preview.clone();
 
         BindRef::from(computed(move || {
             // The hue selector is designed to be cropped at the top of the screen
@@ -62,6 +87,16 @@ impl HsluvPickerController {
                     y2: Position::At(wheel_size/2.0)
                 })
                 .with(hsluv_wheel.clone());
+            
+            // The preview control is in the same container as the hue selector
+            let preview_control = Control::canvas()
+                .with(preview.clone())
+                .with(Bounds {
+                    x1: Position::At((wheel_size-preview_size)/2.0),
+                    y1: Position::At(-preview_size/2.0),
+                    x2: Position::At((wheel_size+preview_size)/2.0),
+                    y2: Position::At(preview_size/2.0)
+                });
             
             // LHS is the luminance control
             let lhs = vec![
@@ -89,7 +124,8 @@ impl HsluvPickerController {
                     Control::cropping_container()
                         .with(Bounds::next_horiz(wheel_size))
                         .with(vec![
-                            hue_selector
+                            hue_selector,
+                            preview_control
                         ]),
 
                     // RHS controls
@@ -129,6 +165,6 @@ impl Controller for HsluvPickerController {
     }
 
     fn get_canvas_resources(&self) -> Option<Arc<ResourceManager<BindingCanvas>>> { 
-        None
+        Some(Arc::clone(&self.canvases))
     }
 }
