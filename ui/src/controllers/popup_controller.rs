5cc840f89b001079039f4fb83458f6096034bcf3
Andrew Hunter <andrew@logicalshift.co.uk>
2018-01-04 11:14:40 +0000

    Actually generate the UI for the popup controller

--- ui/src/controllers/popup_controller.rs
+++ ui/src/controllers/popup_controller.rs
@@ -21,7 +21,10 @@ pub struct PopupController<ContentController: Controller> {
 
     /// Binding that specifies whether or not the popup is open
     /// (This will set to false if the popup is dismissed)
-    open: Binding<bool>
+    open: Binding<bool>,
+
+    /// User interface for this controller
+    ui: BindRef<Control>
 }
 
 impl<ContentController: Controller> PopupController<ContentController> {
@@ -31,12 +34,25 @@ impl<ContentController: Controller> PopupController<ContentController> {
     /// Default settings are a size of 100,100, popup center, offset 8
     /// 
     pub fn new(controller: ContentController, is_open: &Binding<bool>) -> PopupController<ContentController> {
+        // Create the initial set of bindings
+        let content_controller  = controller;
+        let open                = is_open.clone();
+        let popup_size          = BindRef::from(&(100, 100));
+        let popup_direction     = BindRef::from(&PopupDirection::WindowCentered);
+        let offset              = BindRef::from(&8);
+        let content             = content_controller.ui();
+
+        // Derive the basic UI binding
+        let ui                  = Self::create_ui(&content, &open, &popup_size, &popup_direction, &offset);
+
+        // Generate the popup controller
         PopupController {
-            content_controller: controller,
-            open:               is_open.clone(),
-            popup_size:         BindRef::from(bind((100, 100))),
-            popup_direction:    BindRef::from(bind(PopupDirection::WindowCentered)),
-            offset:             BindRef::from(bind(8))
+            content_controller: content_controller,
+            open:               open,
+            popup_size:         popup_size,
+            popup_direction:    popup_direction,
+            offset:             offset,
+            ui:                 ui
         }
     }
 
@@ -45,7 +61,7 @@ impl<ContentController: Controller> PopupController<ContentController> {
     /// 
     pub fn with_size<T: Into<BindRef<(u32, u32)>>>(mut self, size: T) -> PopupController<ContentController> {
         self.popup_size = size.into();
-        self
+        self.regenerate_ui()
     }
 
     ///
@@ -53,7 +69,7 @@ impl<ContentController: Controller> PopupController<ContentController> {
     /// 
     pub fn with_direction<T: Into<BindRef<PopupDirection>>>(mut self, direction: T) -> PopupController<ContentController> {
         self.popup_direction = direction.into();
-        self
+        self.regenerate_ui()
     }
 
     ///
@@ -61,6 +77,55 @@ impl<ContentController: Controller> PopupController<ContentController> {
     /// 
     pub fn with_offset<T: Into<BindRef<u32>>>(mut self, offset: T) -> PopupController<ContentController> {
         self.offset = offset.into();
+        self.regenerate_ui()
+    }
+
+    ///
+    /// Regenerates the UI field from the current bindings
+    /// 
+    fn regenerate_ui(mut self) -> PopupController<ContentController> {
+        self.ui = Self::create_ui(&self.content_controller.ui(), &self.open, &self.popup_size, &self.popup_direction, &self.offset);
         self
+    }
+
+    ///
+    /// Creates the UI binding for this controller
+    /// 
+    fn create_ui(content: &BindRef<Control>, open: &Binding<bool>, size: &BindRef<(u32, u32)>, direction: &BindRef<PopupDirection>, offset: &BindRef<u32>) -> BindRef<Control> {
+        // Clone the model bits
+        let content     = content.clone();
+        let open        = open.clone();
+        let size        = size.clone();
+        let direction   = direction.clone();
+        let offset      = offset.clone();
+
+        // Compute the UI
+        BindRef::from(computed(move || {
+            // Get the standard popup properties
+            let open            = open.get();
+            let direction       = direction.get();
+            let (width, height) = size.get();
+            let offset          = offset.get();
+
+            if !open {
+                // If not open, then generate an empty popup
+                // Not binding the UI here so if the controller updates, nothing happens
+                Control::popup()
+                    .with(Popup::IsOpen(Property::Bool(false)))
+                    .with(Popup::Direction(direction))
+                    .with(Popup::Size(width, height))
+                    .with(Popup::Offset(offset))
+            } else {
+                // Bind the UI only when the controller is open
+                Control::popup()
+                    .with(Popup::IsOpen(Property::Bool(true)))
+                    .with(Popup::Direction(direction))
+                    .with(Popup::Size(width, height))
+                    .with(Popup::Offset(offset))
+                    .with(vec![
+                        content.get()
+                    ])
+            }
+        }))
     }
 }
