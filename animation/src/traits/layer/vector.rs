2eec63d463c5041fedccc954f8e78a97ad0f222c
Andrew Hunter <andrew@logicalshift.co.uk>
2018-01-07 12:37:35 +0000

    Vector layer needs a way to add and read elements

--- animation/src/traits/layer/vector.rs
+++ animation/src/traits/layer/vector.rs
@@ -1,6 +1,16 @@
+use super::super::vector::*;
+
 ///
 /// Represents a layer that contains vector elements
 /// 
 pub trait VectorLayer : Send+Sync {
-    
+    ///
+    /// Retrieves the elements from this layer
+    /// 
+    fn elements<'a>(&'a self) -> Box<'a+Iterator<Item=&VectorElement>>;
+
+    ///
+    /// Adds a new vector element to this layer
+    /// 
+    fn add_element(&mut self, new_element: VectorElement);   
 }
