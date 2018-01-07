a5ee73c887bbcbd736c689262060f96eb737c21f
Andrew Hunter <andrew@logicalshift.co.uk>
2018-01-07 15:16:32 +0000

    Add some vector properties for things that are persistent between vector objects

--- animation/src/traits/vector/mod.rs
+++ animation/src/traits/vector/mod.rs
@@ -1,10 +1,12 @@
 use canvas::*;
 use std::time::Duration;
 
+mod properties;
 mod path;
 mod element;
 mod brush_element;
 
+pub use self::properties::*;
 pub use self::path::*;
 pub use self::element::*;
 pub use self::brush_element::*;
---
+++ animation/src/traits/vector/properties.rs
@@ -0,0 +1,34 @@
+use super::super::brush::*;
+use super::super::brush_properties::*;
+use super::super::brush_definition::*;
+use super::super::brush_drawing_style::*;
+use super::super::super::brushes::*;
+
+use std::sync::*;
+
+///
+/// Represents the active properties for a vector layer
+/// 
+/// Elements can update the properties, which persist to the next element.
+/// This saves some space in that properties don't need to be encoded with
+/// each element.
+/// 
+pub struct VectorProperties {
+    /// The active brush
+    pub brush: Arc<Brush>,
+
+    /// The properties set for the active brush
+    pub brush_properties: BrushProperties
+}
+
+impl VectorProperties {
+    ///
+    /// Creates the default brush properties
+    /// 
+    pub fn default() -> VectorProperties {
+        VectorProperties {
+            brush:              Arc::new(InkBrush::new(&InkDefinition::default(), BrushDrawingStyle::Draw)),
+            brush_properties:   BrushProperties::new()
+        }
+    }
+}
