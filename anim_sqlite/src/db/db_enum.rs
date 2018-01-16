11ae5d8aa138fff24df22f4d0694dc5b72faa6df
Andrew Hunter <andrew@logicalshift.co.uk>
2018-01-16 21:57:45 +0000

    Add stuff for getting the names (field/name) from the enum values

--- anim_sqlite/src/db/db_enum.rs
+++ anim_sqlite/src/db/db_enum.rs
@@ -1,3 +1,6 @@
+/// Provides the enum type and name for a database enum value
+pub struct DbEnumName(pub &'static str, pub &'static str);
+
 ///
 /// Type of edit log item
 /// 
@@ -71,4 +74,93 @@ pub enum DbEnum {
     Color(ColorType),
     Layer(LayerType),
     VectorElement(VectorElementType)
+}
+
+impl From<EditLogType> for DbEnumName {
+    fn from(t: EditLogType) -> DbEnumName {
+        use self::EditLogType::*;
+
+        match t {
+            SetSize                     => DbEnumName("Edit", "SetSize"),
+            AddNewLayer                 => DbEnumName("Edit", "AddNewLayer"),
+            RemoveLayer                 => DbEnumName("Edit", "RemoveLayer"),
+
+            LayerAddKeyFrame            => DbEnumName("Edit", "Layer::AddKeyFrame"),
+            LayerRemoveKeyFrame         => DbEnumName("Edit", "Layer::RemoveKeyFrame"),
+
+            LayerPaintSelectBrush       => DbEnumName("Edit", "Layer::Paint::SelectBrush"),
+            LayerPaintBrushProperties   => DbEnumName("Edit", "Layer::Paint::BrushProperties"),
+            LayerPaintBrushStroke       => DbEnumName("Edit", "Layer::Paint::BrushStroke")
+        }
+    }
+}
+
+impl From<DrawingStyleType> for DbEnumName {
+    fn from(t: DrawingStyleType) -> DbEnumName {
+        use self::DrawingStyleType::*;
+
+        match t {
+            Draw    => DbEnumName("DrawingStyle", "Draw"),
+            Erase   => DbEnumName("DrawingStyle", "Erase")
+        }
+    }
+}
+
+impl From<BrushDefinitionType> for DbEnumName {
+    fn from(t: BrushDefinitionType) -> DbEnumName {
+        use self::BrushDefinitionType::*;
+
+        match t {
+            Simple  => DbEnumName("BrushType", "Simple"),
+            Ink     => DbEnumName("BrushType", "Ink")
+        }
+    }
+}
+
+impl From<ColorType> for DbEnumName {
+    fn from(t: ColorType) -> DbEnumName {
+        use self::ColorType::*;
+
+        match t {
+            Rgb     => DbEnumName("ColorType", "RGB"),
+            Hsluv   => DbEnumName("ColorType", "HSLUV")
+        }
+    }
+}
+
+impl From<LayerType> for DbEnumName {
+    fn from(t: LayerType) -> DbEnumName {
+        use self::LayerType::*;
+
+        match t {
+            Vector  => DbEnumName("LayerType", "Vector")
+        }
+    }
+}
+
+impl From<VectorElementType> for DbEnumName {
+    fn from(t: VectorElementType) -> DbEnumName {
+        use self::VectorElementType::*;
+
+        match t {
+            BrushDefinition => DbEnumName("VectorElementType", "BrushDefinition"),
+            BrushProperties => DbEnumName("VectorElementType", "BrushProperties"),
+            BrushStroke     => DbEnumName("VectorElementType", "BrushStroke")
+        }
+    }
+}
+
+impl From<DbEnum> for DbEnumName {
+    fn from(t: DbEnum) -> DbEnumName {
+        use self::DbEnum::*;
+
+        match t {
+            EditLog(elt)            => DbEnumName::from(elt),
+            DrawingStyle(dst)       => DbEnumName::from(dst),
+            BrushDefinition(bdt)    => DbEnumName::from(bdt),
+            Color(ct)               => DbEnumName::from(ct),
+            Layer(lt)               => DbEnumName::from(lt),
+            VectorElement(vet)      => DbEnumName::from(vet)
+        }
+    }
 }
