405c0956b8afc95344312aeeff3be78bd253d6d9
Andrew Hunter <andrew@logicalshift.co.uk>
2018-01-21 16:46:36 +0000

    Add ‘Any’ to the frame; our tests will use this later on

--- animation/src/traits/frame.rs
+++ animation/src/traits/frame.rs
@@ -1,11 +1,12 @@
 use canvas::*;
 
 use std::time::Duration;
+use std::any::Any;
 
 ///
 /// Represents a single frame in a layer of an animation
 ///
-pub trait Frame : Send+Sync {
+pub trait Frame : Send+Sync+Any {
     ///
     /// Time index of this frame relative to its keyframe
     /// 
