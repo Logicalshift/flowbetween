778168b9d1ad2433ce521e2588960de8f50b6000
Andrew Hunter <andrew@logicalshift.co.uk>
2018-01-27 20:47:38 +0000

    Add a simple drawing routine

--- flo/src/editor/timeline_controller.rs
+++ flo/src/editor/timeline_controller.rs
@@ -6,12 +6,24 @@ use binding::*;
 
 use std::sync::*;
 
+/// Width of an item in a virtualised canvas
+const VIRTUAL_WIDTH: f32    = 400.0;
+
+/// Height of the time scale control
+const SCALE_HEIGHT: f32     = 24.0;
+
+/// Length of a tick on the timeline (in pixels)
+const TICK_LENGTH: f32      = 8.0;
+
 ///
 /// The timeline allows the user to pick a point in time and create layers in the animation
 ///
 pub struct TimelineController {
     /// The canvases for the timeline
     canvases:           Arc<ResourceManager<BindingCanvas>>,
+
+    /// A virtual control that draws the timeline scale
+    virtual_scale:      VirtualCanvas,
 
     /// The UI for the timeline
     ui:                 Binding<Control>
@@ -25,26 +37,54 @@ impl TimelineController {
         // Create the canvases
         let canvases = Arc::new(ResourceManager::new());
 
+        // This draws the scale along the top; we use a virtual control as this allows us unlimited width
+        let virtual_scale = VirtualCanvas::new(Arc::clone(&canvases), Self::draw_scale);
+
         // UI
         let ui = bind(Control::scrolling_container()
             .with(Bounds::fill_all())
-            .with(Scroll::MinimumContentSize(6000.0, 16.0))
+            .with(Scroll::MinimumContentSize(6000.0, 256.0))
             .with(Scroll::HorizontalScrollBar(ScrollBarVisibility::Always))
             .with(Scroll::VerticalScrollBar(ScrollBarVisibility::OnlyIfNeeded))
             .with(Appearance::Background(TIMELINE_BACKGROUND))
             .with(ControlAttribute::Padding((16, 16), (16, 16)))
             .with(vec![
-                Control::empty()
-                    .with(Appearance::Background(Color::Rgba(0.4, 0.0, 0.0, 1.0)))
-                    .with(Bounds::fill_all())
+                Control::container()
+                    .with(Bounds {
+                        x1: Position::At(0.0),
+                        x2: Position::End,
+                        y1: Position::At(0.0),
+                        y2: Position::At(SCALE_HEIGHT)
+                    })
+                    .with(vec![
+                        virtual_scale.control().get()
+                    ])
             ])
-            .with((ActionTrigger::VirtualScroll(400.0, 256.0), "Scroll")));
+            .with((ActionTrigger::VirtualScroll(VIRTUAL_WIDTH, 256.0), "Scroll")));
 
         // Piece it together
         TimelineController {
-            ui:         ui,
-            canvases:   canvases
+            ui:             ui,
+            virtual_scale:  virtual_scale,
+            canvases:       canvases
         }
+    }
+
+    ///
+    /// Draws the timeline scale
+    /// 
+    fn draw_scale(gc: &mut GraphicsPrimitives, (x, _y): (f32, f32)) {
+        // Set up the canvas
+        gc.canvas_height(SCALE_HEIGHT);
+        gc.center_region(x, 0.0, x+VIRTUAL_WIDTH, SCALE_HEIGHT);
+        gc.line_width(1.0);
+
+        // Draw the border line
+        gc.stroke_color(TIMESCALE_BORDER);
+        gc.new_path();
+        gc.move_to(x, SCALE_HEIGHT-0.5);
+        gc.line_to(x+VIRTUAL_WIDTH, SCALE_HEIGHT-0.5);
+        gc.stroke();
     }
 }
 
@@ -62,6 +102,9 @@ impl Controller for TimelineController {
 
         match (action_id, action_parameter) {
             ("Scroll", &VirtualScroll((x, y), (width, height))) => {
+                // The virtual scale is always drawn at the top, so we hard-code the top and height values
+                self.virtual_scale.virtual_scroll((VIRTUAL_WIDTH, SCALE_HEIGHT), (x, 0), (width, 1));
+
                 println!("{:?} {:?}", (x, y), (width, height));
             },
 
--- flo/src/style.rs
+++ flo/src/style.rs
@@ -4,6 +4,9 @@ pub const DEFAULT_TEXT:         Color = Color::Rgba(0.85, 0.95, 0.9, 1.0);
 
 pub const MAIN_BACKGROUND:      Color = Color::Rgba(0.25, 0.25, 0.25, 1.0);
 pub const TIMELINE_BACKGROUND:  Color = Color::Rgba(0.20, 0.20, 0.20, 1.0);
+pub const TIMESCALE_BORDER:     Color = Color::Rgba(0.8, 0.8, 0.8, 1.0);
+pub const TIMESCALE_TICK:       Color = Color::Rgba(0.7, 0.7, 0.7, 1.0);
+pub const TIMESCALE_MAINTICK:   Color = Color::Rgba(1.0, 1.0, 1.0, 1.0);
 pub const MENU_BACKGROUND:      Color = Color::Rgba(0.20, 0.22, 0.25, 1.0);
 pub const TOOLS_BACKGROUND:     Color = Color::Rgba(0.20, 0.22, 0.25, 1.0);
 pub const MENU_BACKGROUND_ALT:  Color = Color::Rgba(0.24, 0.26, 0.30, 1.0);
