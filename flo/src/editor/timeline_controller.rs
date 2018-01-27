e03ebdfc432b2f7098fcf8ce17172df107054704
Andrew Hunter <andrew@logicalshift.co.uk>
2018-01-27 21:11:25 +0000

    Draw the timescale ticks as well

--- flo/src/editor/timeline_controller.rs
+++ flo/src/editor/timeline_controller.rs
@@ -13,7 +13,13 @@ const VIRTUAL_WIDTH: f32    = 400.0;
 const SCALE_HEIGHT: f32     = 24.0;
 
 /// Length of a tick on the timeline (in pixels)
-const TICK_LENGTH: f32      = 8.0;
+const TICK_LENGTH: f32      = 7.0;
+
+/// Height of a 'main' tick
+const TICK_MAIN_HEIGHT: f32 = 10.0;
+
+/// Height of an 'inbetween' tick
+const TICK_HEIGHT: f32      = 5.0;
 
 ///
 /// The timeline allows the user to pick a point in time and create layers in the animation
@@ -79,7 +85,36 @@ impl TimelineController {
         gc.center_region(x, 0.0, x+VIRTUAL_WIDTH, SCALE_HEIGHT);
         gc.line_width(1.0);
 
-        // TODO: draw the ticks
+        // Draw the ticks
+        let start_tick  = (x / TICK_LENGTH).floor() as i32;
+        let end_tick    = ((x+VIRTUAL_WIDTH)/TICK_LENGTH).ceil() as i32;
+
+        gc.stroke_color(TIMESCALE_TICK);
+
+        for tick in start_tick..(end_tick+1) {
+            let tick_x = (tick as f32) * TICK_LENGTH;
+            let tick_x = tick_x + (TICK_LENGTH/2.0);
+
+            gc.new_path();
+
+            if (tick%5) == 0 {
+                gc.stroke_color(TIMESCALE_MAINTICK);
+
+                gc.move_to(tick_x, 0.0);
+                if (tick%10) == 0 {
+                    gc.line_to(tick_x, TICK_MAIN_HEIGHT);
+                } else {
+                    gc.line_to(tick_x, TICK_HEIGHT);
+                }
+                gc.stroke();
+
+                gc.stroke_color(TIMESCALE_TICK);
+            } else {
+                gc.move_to(tick_x, 0.0);
+                gc.line_to(tick_x, TICK_HEIGHT);
+                gc.stroke();
+            }
+        }
 
         // Draw the border line
         gc.stroke_color(TIMESCALE_BORDER);
@@ -103,7 +138,7 @@ impl Controller for TimelineController {
         use ui::ActionParameter::*;
 
         match (action_id, action_parameter) {
-            ("Scroll", &VirtualScroll((x, y), (width, height))) => {
+            ("Scroll", &VirtualScroll((x, _y), (width, _height))) => {
                 // The virtual scale is always drawn at the top, so we hard-code the top and height values
                 self.virtual_scale.virtual_scroll((VIRTUAL_WIDTH, SCALE_HEIGHT), (x, 0), (width, 1));
             },
