d65623fc62e21fe962e605bf47b5c801cc212b59
Andrew Hunter <andrew@logicalshift.co.uk>
2018-01-27 18:36:32 +0000

    Call the tiles ‘tiles’ instead of canvases

--- ui/src/controllers/virtual_canvas.rs
+++ ui/src/controllers/virtual_canvas.rs
@@ -16,7 +16,7 @@ pub struct VirtualCanvas<DrawRegion: Fn(&mut GraphicsPrimitives, (f32, f32)) -> 
     canvas_resources: Arc<ResourceManager<BindingCanvas>>,
 
     /// The canvases that are currently being displayed in this virtual canvas
-    canvases: Binding<Vec<Vec<Resource<BindingCanvas>>>>,
+    tiles: Binding<Vec<Vec<Resource<BindingCanvas>>>>,
 
     /// The top-left grid coordinate
     top_left: Binding<(u32, u32)>,
@@ -39,15 +39,15 @@ impl<DrawRegion: 'static+Send+Sync+Fn(&mut GraphicsPrimitives, (f32, f32)) -> ()
     /// Creates a new virtual canvas
     /// 
     pub fn new(canvas_resources: Arc<ResourceManager<BindingCanvas>>, draw_region: DrawRegion) -> VirtualCanvas<DrawRegion> {
-        let canvases    = bind(vec![]);
+        let tiles       = bind(vec![]);
         let top_left    = bind((0, 0));
         let grid_size   = bind((0, 0));
         let tile_size   = bind((256.0, 256.0));
-        let control     = Self::make_control(&canvases, &top_left, &tile_size);
+        let control     = Self::make_control(&tiles, &top_left, &tile_size);
 
         VirtualCanvas {
             canvas_resources:   canvas_resources,
-            canvases:           canvases,
+            tiles:              tiles,
             top_left:           top_left,
             grid_size:          grid_size,
             tile_size:          tile_size,
@@ -80,14 +80,14 @@ impl<DrawRegion: 'static+Send+Sync+Fn(&mut GraphicsPrimitives, (f32, f32)) -> ()
         if self.top_left.get() != top_left {
             // Top-left coordinate affects what is drawn in the various canvases
             // We need to re-order the canvases to avoid having to redraw all of the tiles
-            self.reorder_canvases(self.top_left.get(), top_left);
+            self.reorder_tiles(self.top_left.get(), top_left);
 
             self.top_left.clone().set(top_left);
         }
 
         if self.grid_size.get() != grid_size {
             // Grid size affects the number of canvases we're drawing overall
-            self.resize_canvases(grid_size);
+            self.resize_tiles(grid_size);
 
             self.grid_size.clone().set(grid_size);
         }
@@ -96,7 +96,7 @@ impl<DrawRegion: 'static+Send+Sync+Fn(&mut GraphicsPrimitives, (f32, f32)) -> ()
     ///
     /// Makes a canvas at a particular grid position
     /// 
-    fn make_canvas(&self, pos: (u32, u32)) -> Resource<BindingCanvas> {
+    fn make_tile(&self, pos: (u32, u32)) -> Resource<BindingCanvas> {
         let draw_region = Arc::clone(&self.draw_region);
         let tile_size   = Binding::clone(&self.tile_size);
 
@@ -118,24 +118,24 @@ impl<DrawRegion: 'static+Send+Sync+Fn(&mut GraphicsPrimitives, (f32, f32)) -> ()
     ///
     /// Updates the canvases grid to match a new grid size
     /// 
-    fn resize_canvases(&self, new_grid_size: (u32, u32)) {
+    fn resize_tiles(&self, new_grid_size: (u32, u32)) {
         let (left, top)     = self.top_left.get();
         let (width, height) = new_grid_size;
-        let mut canvases    = self.canvases.get();
+        let mut tiles       = self.tiles.get();
 
         // Remove any extra rows if we're getting smaller
-        canvases.truncate(height as usize);
+        tiles.truncate(height as usize);
 
         // Resize the existing rows
         let mut ypos = top;
-        for ref mut row in canvases.iter_mut() {
+        for ref mut row in tiles.iter_mut() {
             // Remove canvases if there are too many
             row.truncate(width as usize);
 
             // Add canvses if more are needed
             while row.len() < width as usize {
                 let xpos = left + row.len() as u32;
-                row.push(self.make_canvas((xpos, ypos)));
+                row.push(self.make_tile((xpos, ypos)));
             }
 
             // Update ypos to the next row
@@ -144,30 +144,30 @@ impl<DrawRegion: 'static+Send+Sync+Fn(&mut GraphicsPrimitives, (f32, f32)) -> ()
 
         // Add new rows
         let mut ypos = top;
-        while canvases.len() < height as usize {
+        while tiles.len() < height as usize {
             let new_row = (0..width).into_iter()
                 .map(|x| left + x)
-                .map(|xpos| self.make_canvas((xpos, ypos)))
+                .map(|xpos| self.make_tile((xpos, ypos)))
                 .collect();
-            canvases.push(new_row);
+            tiles.push(new_row);
 
             ypos += 1;
         }
 
         // Update the canvases
-        self.canvases.clone().set(canvases);
+        self.tiles.clone().set(tiles);
     }
 
     ///
     /// Takes the canvas array and regenerates it with a new top-left coordinate
     /// 
-    fn reorder_canvases(&self, old_top_left: (u32, u32), new_top_left: (u32, u32)) {
+    fn reorder_tiles(&self, old_top_left: (u32, u32), new_top_left: (u32, u32)) {
         // Fetch the old array and the size of the grid
         let (old_left, old_top) = old_top_left;
         let (new_left, new_top) = new_top_left;
         let (size_x, size_y)    = self.grid_size.get();
-        let old_canvases        = self.canvases.get();
-        let mut new_canvases    = vec![];
+        let old_tiles           = self.tiles.get();
+        let mut new_tiles       = vec![];
 
         let (old_left, old_top) = (old_left as i32, old_top as i32);
         let (new_left, new_top) = (new_left as i32, new_top as i32);
@@ -186,37 +186,37 @@ impl<DrawRegion: 'static+Send+Sync+Fn(&mut GraphicsPrimitives, (f32, f32)) -> ()
                 let x       = x as i32;
                 let old_x   = x - (old_left - new_left);
 
-                if old_y >= 0 && old_y < old_canvases.len() as i32 {
+                if old_y >= 0 && old_y < old_tiles.len() as i32 {
                     // Row is within the original grid
-                    let old_row = &old_canvases[1];
+                    let old_row = &old_tiles[1];
 
                     if old_x >= 0 && old_x < old_row.len() as i32 {
                         // Use the existing canvas for this grid cell
                         this_row.push(old_row[old_x as usize].clone())
                     } else {
                         // Create a new grid cell
-                        this_row.push(self.make_canvas(((x+new_left) as u32, (y+new_top) as u32)));
+                        this_row.push(self.make_tile(((x+new_left) as u32, (y+new_top) as u32)));
                     }
                 } else {
                     // Create a new grid cell
-                    this_row.push(self.make_canvas(((x+new_left) as u32, (y+new_top) as u32)));
+                    this_row.push(self.make_tile(((x+new_left) as u32, (y+new_top) as u32)));
                 }
             }
 
             // Add this row to the set
-            new_canvases.push(this_row);
+            new_tiles.push(this_row);
         }
 
         // Update the canvases
-        self.canvases.clone().set(new_canvases);
+        self.tiles.clone().set(new_tiles);
     }
 
     ///
     /// Creates the control binding for this virtual canvas
     /// 
-    fn make_control(canvases: &Binding<Vec<Vec<Resource<BindingCanvas>>>>, top_left: &Binding<(u32, u32)>, tile_size: &Binding<(f32, f32)>) -> Arc<Bound<Control>> {
+    fn make_control(tiles: &Binding<Vec<Vec<Resource<BindingCanvas>>>>, top_left: &Binding<(u32, u32)>, tile_size: &Binding<(f32, f32)>) -> Arc<Bound<Control>> {
         // Clone the bindings
-        let canvases    = Binding::clone(canvases);
+        let tiles       = Binding::clone(tiles);
         let top_left    = Binding::clone(top_left);
         let tile_size   = Binding::clone(tile_size);
 
@@ -224,14 +224,14 @@ impl<DrawRegion: 'static+Send+Sync+Fn(&mut GraphicsPrimitives, (f32, f32)) -> ()
         Arc::new(computed(move || {
             let (tile_x, tile_y)    = tile_size.get();
             let (left, top)         = top_left.get();
-            let canvases            = canvases.get();
+            let tiles               = tiles.get();
 
             let (left, top)         = (left as f32, top as f32);
             let (left, top)         = (left*tile_x, top*tile_y);
 
-            // Generate the canvas grid from the canvas array
-            let canvas_controls: Vec<Control> = canvases.iter()
-                .zip((0..canvases.len()).into_iter())
+            // Generate the tile controls from the tiles array
+            let tile_controls: Vec<Control> = tiles.iter()
+                .zip((0..tiles.len()).into_iter())
                 .map(|(row, ypos)| (row, (ypos as f32) * tile_y + top))
                 .map(|(row, ypos)| row.iter()
                     .zip((0..row.len()).into_iter())
@@ -252,7 +252,7 @@ impl<DrawRegion: 'static+Send+Sync+Fn(&mut GraphicsPrimitives, (f32, f32)) -> ()
             // Turn into a container control with all of the canvases in it
             Control::container()
                 .with(Bounds::fill_all())
-                .with(canvas_controls)
+                .with(tile_controls)
         }))
     }
 }
