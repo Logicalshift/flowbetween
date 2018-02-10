728228d118efe98fe34bf4f42f707a73d638aab3
Andrew Hunter <andrew@logicalshift.co.uk>
2018-02-10 16:26:32 +0000

    Our basic UI updates can’t be serialized as controls can’t be serialized
    (Resources are the issue here)

--- ui/src/session/update.rs
+++ ui/src/session/update.rs
@@ -5,13 +5,13 @@ use canvas::*;
 ///
 /// Represents a differnce to the UI
 /// 
-#[derive(Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, PartialEq)]
 pub struct UiDiff {
     /// The address of where the UI tree was changed
     pub address: Vec<u32>,
 
     // The UI that replaces the tree at this address
-    // pub new_ui: Control
+    pub new_ui: Control
 }
 
 ///
@@ -32,7 +32,7 @@ pub struct CanvasDiff {
 ///
 /// Updates that can arrive from the UI
 /// 
-#[derive(Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, PartialEq)]
 pub enum UiUpdate {
     /// Represents a series of updates to the UI tree
     UpdateUi(Vec<UiDiff>),
