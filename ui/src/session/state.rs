668e8df9aa8047c078513d7cc63f9bf8695d9eef
Andrew Hunter <andrew@logicalshift.co.uk>
2018-02-11 15:17:18 +0000

    Add methods for watching the viewmodel and the canvas updates

--- ui/src/session/state.rs
+++ ui/src/session/state.rs
@@ -5,6 +5,11 @@ use super::super::control::*;
 use super::super::controller::*;
 use super::super::diff_viewmodel::*;
 
+use binding::*;
+
+use std::mem;
+use std::sync::*;
+
 ///
 /// Represents the most recent state of a UI session
 /// 
@@ -72,6 +77,71 @@ impl UiSessionState {
 
         // The update is the result
         update
+    }
+
+    ///
+    /// Starts watching the viewmodel for a controller
+    /// 
+    pub fn watch_viewmodel(&mut self, controller: Arc<Controller>) {
+        let new_diff    = DiffViewModel::new(controller);
+        let watcher     = new_diff.watch();
+
+        self.view_model_diff    = Some(new_diff);
+        self.view_model_watcher = Some(watcher);
+    }
+
+    ///
+    /// Retrieves the viewmodel update event, if there is one
+    /// 
+    pub fn get_viewmodel_update(&mut self) -> Option<UiUpdate> {
+        // Pull the watcher out of this object
+        let mut watcher = None;
+        mem::swap(&mut watcher, &mut self.view_model_watcher);
+
+        if let Some((diff, watcher)) = self.view_model_diff.as_ref().and_then(|diff| watcher.map(move |watch| (diff, watch))) {
+            // We're watching a viewmodel; rotate the watch to build the diff
+            let (differences, new_watcher) = diff.rotate_watch(watcher);
+
+            // This becomes our new watcher
+            self.view_model_watcher = Some(new_watcher);
+
+            // No event if there are no differences, otherwise a viewmodel change update
+            if differences.len() == 0 {
+                None
+            } else {
+                Some(UiUpdate::UpdateViewModel(differences))
+            }
+        } else {
+            None
+        }
+    }
+
+    ///
+    /// Watches for updates to canvases in the specified UI
+    /// 
+    pub fn watch_canvases(&mut self, ui_binding: &BindRef<Control>) {
+        self.canvas_state = Some(CanvasState::new(ui_binding));
+    }
+
+    ///
+    /// Retrieves the updates to any canvases attached to this object
+    /// 
+    pub fn get_canvas_update(&mut self) -> Option<UiUpdate> {
+        if let Some(ref canvas_state) = self.canvas_state {
+            // Fetch the updates from the canvas state
+            let updates = canvas_state.latest_updates();
+
+            if updates.len() == 0 {
+                // No updates
+                None
+            } else {
+                // Got some updates
+                Some(UiUpdate::UpdateCanvas(updates))
+            }
+        } else {
+            // Not watching any canvases
+            None
+        }
     }
 }
 
