7a348d95019ca7d6519109fc3ee7398ffb4c8654
Andrew Hunter <andrew@logicalshift.co.uk>
2018-02-25 22:05:26 +0000

    Get rid of the activation state tracking stuff
    There isn’t really any tool activation any more

--- flo/src/viewmodel/tools.rs
+++ flo/src/viewmodel/tools.rs
@@ -5,16 +5,12 @@ use ui::*;
 use binding::*;
 use animation::*;
 
-use typemap::*;
 use std::sync::*;
 
 ///
 /// View model representing the currently selected and available tools
 /// 
 pub struct ToolViewModel<Anim: Animation> {
-    // Binding that tracks the current tool activation state (changes to whatever the current tool uses)
-    effective_tool_activated: Arc<Mutex<BindRef<ToolActivationState>>>,
-
     /// The currently selected tool
     pub selected_tool: Binding<Option<Arc<FloTool<Anim>>>>,
 
@@ -40,7 +36,6 @@ impl<Anim: Animation+'static> ToolViewModel<Anim> {
         ];
 
         // Create the bindings
-        let effective_tool_activated    = bind(ToolActivationState::NeedsReactivation);
         let selected_tool               = bind(None);
         let tool_sets                   = bind(default_tool_sets);
         let current_pointer             = bind((PaintDevice::Mouse(MouseButton::Left), 0));
@@ -50,8 +45,7 @@ impl<Anim: Animation+'static> ToolViewModel<Anim> {
             effective_tool:             Self::effective_tool(selected_tool.clone(), current_pointer.clone(), tool_sets.clone()),
             selected_tool:              selected_tool,
             tool_sets:                  tool_sets,
-            current_pointer:            current_pointer,
-            effective_tool_activated:   Arc::new(Mutex::new(BindRef::from(effective_tool_activated)))
+            current_pointer:            current_pointer
         }
     }
 
@@ -94,41 +88,6 @@ impl<Anim: Animation+'static> ToolViewModel<Anim> {
     }
 
     ///
-    /// Calls activate on the effective tool if it is marked as inactive
-    /// 
-    pub fn activate_tool<'a>(&self, model: &ToolModel<'a, Anim>) {
-        // Check if the tool has been activated
-        let activation_state = self.effective_tool_activated.lock().unwrap().get();
-
-        // If the tool has not been activated, mark it as active
-        if activation_state != ToolActivationState::Activated {
-            let effective_tool  = self.effective_tool.get();
-
-            // Clear the tool state
-            *model.tool_state.lock().unwrap() = SendMap::custom();
-
-            // Mark the effective tool as active
-            let tool_activation = effective_tool.map(|tool| BindRef::from(bind(ToolActivationState::Activated)) /* tool.activate(model) */);
-            
-            if let Some(tool_activation) = tool_activation {
-                // Tool needs reactivation if a different tool is selected
-                let effective_tool_name = self.effective_tool.get().map(|tool| tool.tool_name()).unwrap_or("".to_string());
-                let effective_tool      = BindRef::clone(&self.effective_tool);
-                let tool_activation     = computed(move || {
-                    if effective_tool.get().map(|tool| tool.tool_name()).as_ref() != Some(&effective_tool_name) {
-                        ToolActivationState::NeedsReactivation
-                    } else {
-                        tool_activation.get()
-                    }
-                });
-
-                // Re-activate the effective tool if this state changes
-                *self.effective_tool_activated.lock().unwrap() = BindRef::from(tool_activation);
-            }
-        }
-    }
-
-    ///
     /// Finds the tool with the specified name and marks it as active
     /// 
     pub fn choose_tool_with_name(&self, name: &str) {
@@ -151,7 +110,6 @@ impl<Anim: Animation+'static> ToolViewModel<Anim> {
 impl<Anim: Animation> Clone for ToolViewModel<Anim> {
     fn clone(&self) -> ToolViewModel<Anim> {
         ToolViewModel {
-            effective_tool_activated:   Arc::clone(&self.effective_tool_activated),
             selected_tool:              Binding::clone(&self.selected_tool),
             tool_sets:                  Binding::clone(&self.tool_sets),
             current_pointer:            Binding::clone(&self.current_pointer),
