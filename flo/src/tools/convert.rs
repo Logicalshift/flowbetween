7f86f8fa26dae8b597e9e5b15c0909919cd161de
Andrew Hunter <andrew@logicalshift.co.uk>
2018-03-04 12:36:09 +0000

    Move the conversion functions into their own file

---
+++ flo/src/tools/convert.rs
@@ -0,0 +1,13 @@
+use ui::*;
+use animation::*;
+
+///
+/// Converts a UI Painting struct to a BrushPoint
+/// 
+pub fn raw_point_from_painting(painting: &Painting) -> RawPoint {
+    RawPoint {
+        position:   painting.location,
+        tilt:       (painting.tilt_x, painting.tilt_y),
+        pressure:   painting.pressure
+    }
+}
--- flo/src/tools/mod.rs
+++ flo/src/tools/mod.rs
@@ -1,6 +1,3 @@
-use ui::*;
-use animation::*;
-
 mod tool_action;
 mod brush_preview_action;
 mod overlay_action;
@@ -9,6 +6,7 @@ mod tool_trait;
 mod tool_set;
 mod generic_tool;
 mod tool_runner;
+mod convert;
 
 pub use self::tool_action::*;
 pub use self::brush_preview_action::*;
@@ -18,14 +16,4 @@ pub use self::tool_trait::*;
 pub use self::tool_set::*;
 pub use self::generic_tool::*;
 pub use self::tool_runner::*;
-
-///
-/// Converts a UI Painting struct to a BrushPoint
-/// 
-pub fn raw_point_from_painting(painting: &Painting) -> RawPoint {
-    RawPoint {
-        position:   painting.location,
-        tilt:       (painting.tilt_x, painting.tilt_y),
-        pressure:   painting.pressure
-    }
-}
+pub use self::convert::*;
