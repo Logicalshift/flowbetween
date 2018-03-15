9f0aba61f861413bfb01e1406f04bbd8f3778bb2
Andrew Hunter <andrew@logicalshift.co.uk>
2018-03-15 22:22:07 +0000

    Set up the model for the select tool

--- flo/src/standard_tools/select.rs
+++ flo/src/standard_tools/select.rs
@@ -16,7 +16,7 @@ use std::collections::HashSet;
 /// 
 pub struct SelectModel {
     /// Contains a pointer to the current frame
-    pub frame: BindRef<Arc<Frame>>,
+    pub frame: BindRef<Option<Arc<Frame>>>,
 
     /// Contains the bounding boxes of the elements in the current frame
     pub bounding_boxes: BindRef<Vec<(ElementId, Rect)>>
@@ -74,33 +74,82 @@ impl Select {
 
 impl<Anim: 'static+Animation> Tool<Anim> for Select {
     type ToolData   = ();
-    type Model      = ();
+    type Model      = SelectModel;
 
     fn tool_name(&self) -> String { "Select".to_string() }
 
     fn image_name(&self) -> String { "select".to_string() }
 
-    fn create_model(&self, _flo_model: Arc<FloModel<Anim>>) -> () { }
-
-    fn create_menu_controller(&self, _flo_model: Arc<FloModel<Anim>>, _tool_model: &()) -> Option<Arc<Controller>> {
-        Some(Arc::new(SelectMenuController::new()))
-    }
-
-    fn actions_for_model(&self, flo_model: Arc<FloModel<Anim>>, _tool_model: &()) -> Box<Stream<Item=ToolAction<()>, Error=()>+Send> {
-        // The set of currently selected elements
-        let selected_elements = flo_model.selection().selected_element.clone();
-        let selected_elements = computed(move || -> HashSet<_> { selected_elements.get().into_iter().collect() });
-
+    fn create_model(&self, flo_model: Arc<FloModel<Anim>>) -> SelectModel {
         // Create a binding that works out the frame for the currently selected layer
-        let current_frame = computed(move || {
+        let selected_layer  = flo_model.timeline().selected_layer.clone();
+        let frame_layers    = flo_model.frame().layers.clone();
+
+        let current_frame   = computed(move || {
             // Get the layer ID and the frame layers
-            let layer_id        = flo_model.timeline().selected_layer.get();
-            let frame_layers    = flo_model.frame().layers.get();
+            let layer_id        = selected_layer.get();
+            let frame_layers    = frame_layers.get();
 
             let frame           = frame_layers.into_iter().filter(|frame| Some(frame.layer_id) == layer_id).nth(0);
 
             frame.map(|frame| frame.frame.get()).unwrap_or(None)
         });
+
+        // Create a binding that works out the bounding boxes of the elements in the current frame
+        let frame           = current_frame.clone();
+
+        let bounding_boxes  = computed(move || {
+            // Fetch the current frame
+            let frame = frame.get();
+
+            if let Some(frame) = frame {
+                // Get the elements in the current frame
+                let elements            = frame.vector_elements().unwrap_or_else(|| Box::new(vec![].into_iter()));
+
+                // We need to track the vector properties through all of the elements in the frame
+                let mut properties      = VectorProperties::default();
+                let mut bounding_boxes  = vec![];
+
+                for element in elements {
+                    // Update the properties
+                    element.update_properties(&mut properties);
+
+                    // Get the paths for this element
+                    let paths = element.to_path(&properties).unwrap_or(vec![]);
+
+                    // Turn into a bounding box
+                    let bounds = paths.into_iter()
+                        .map(|path| path.bounding_box())
+                        .fold(Rect::empty(), |current, next| current.union(next));
+
+                    // Add to the result
+                    bounding_boxes.push((element.id(), bounds));
+                }
+
+                bounding_boxes
+            } else {
+                // No bounding boxes if there's no frame
+                vec![]
+            }
+        });
+
+        SelectModel {
+            frame:          BindRef::new(&current_frame),
+            bounding_boxes: BindRef::new(&bounding_boxes)
+        }
+    }
+
+    fn create_menu_controller(&self, _flo_model: Arc<FloModel<Anim>>, _tool_model: &SelectModel) -> Option<Arc<Controller>> {
+        Some(Arc::new(SelectMenuController::new()))
+    }
+
+    fn actions_for_model(&self, flo_model: Arc<FloModel<Anim>>, tool_model: &SelectModel) -> Box<Stream<Item=ToolAction<()>, Error=()>+Send> {
+        // The set of currently selected elements
+        let selected_elements = flo_model.selection().selected_element.clone();
+        let selected_elements = computed(move || -> HashSet<_> { selected_elements.get().into_iter().collect() });
+
+        // Create a binding that works out the frame for the currently selected layer
+        let current_frame = tool_model.frame.clone();
 
         // Follow it, and draw an overlay showing all the bounding boxes
         Box::new(follow(computed(move || (current_frame.get(), selected_elements.get())))
@@ -119,7 +168,7 @@ impl<Anim: 'static+Animation> Tool<Anim> for Select {
 
                         // If the element is selected, draw a highlight around it
                         let element_id = element.id();
-                        if element_id.is_assigned() && selected_elements.contains(&element_id) {
+                        if element_id.is_assigned() /* && selected_elements.contains(&element_id) */ {
                             // Draw the settings for this element
                             selection.extend(Self::highlight_for_selection(&element, &properties));
                         }
