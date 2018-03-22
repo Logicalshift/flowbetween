d43081f5cc9bb1843e6cbf54f595309b01c1a5b4
Andrew Hunter <andrew@logicalshift.co.uk>
2018-03-22 22:11:42 +0000

    Dispatch the various 'sub-action' messages we've defined so far

--- gtk_ui/src/widgets/basic_widget.rs
+++ gtk_ui/src/widgets/basic_widget.rs
@@ -1,22 +1,45 @@
 use super::super::gtk_action::*;
 use super::super::gtk_thread::*;
 
-use gtk::*;
+use flo_ui::*;
+
+use gtk;
 
 ///
 /// Performs the basic processing associated with a widget action (using a generic Gtk widget as the target)
 /// 
-pub fn process_basic_widget_action(widget: &mut Widget, flo_gtk: &mut FloGtk, action: &GtkWidgetAction) {
+pub fn process_basic_widget_action(widget: &mut gtk::Widget, flo_gtk: &mut FloGtk, action: &GtkWidgetAction) {
     use self::GtkWidgetAction::*;
 
     match action {
+        &Layout(ref layout)         => process_basic_widget_layout(widget, flo_gtk, layout),
+        &Content(ref content)       => process_basic_widget_content(widget, flo_gtk, content),
+        &Appearance(ref appearance) => process_basic_widget_appearance(widget, flo_gtk, appearance),
+        &State(ref state)           => process_basic_widget_state(widget, flo_gtk, state),
+        &Font(ref font)             => process_basic_widget_font(widget, flo_gtk, font),
+
         &New(_widget_type)          => (),
         &SetRoot(window_id)         => { flo_gtk.get_window(window_id).map(|window| window.borrow_mut().set_root(flo_gtk, widget)); },
-        &Layout(ref layout)         => unimplemented!(),
-        &Content(ref content)       => unimplemented!(),
-        &Appearance(ref appearance) => unimplemented!(),
-        &State(ref state)           => unimplemented!(),
-        &Font(ref font)             => unimplemented!(),
         &Delete                     => unimplemented!()
     }
+}
+
+pub fn process_basic_widget_layout(widget: &mut gtk::Widget, flo_gtk: &mut FloGtk, layout: &WidgetLayout) { 
+    unimplemented!()
+}
+
+pub fn process_basic_widget_content(widget: &mut gtk::Widget, flo_gtk: &mut FloGtk, content: &WidgetContent) {
+    unimplemented!()
+}
+
+pub fn process_basic_widget_appearance(widget: &mut gtk::Widget, flo_gtk: &mut FloGtk, appearance: &Appearance) {
+    unimplemented!()
+}
+
+pub fn process_basic_widget_state(widget: &mut gtk::Widget, flo_gtk: &mut FloGtk, state: &State) {
+    unimplemented!()
+}
+
+pub fn process_basic_widget_font(widget: &mut gtk::Widget, flo_gtk: &mut FloGtk, font: &Font) {
+    unimplemented!()
 }
