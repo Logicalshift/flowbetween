84c9ed5f9e3c7dc22ac5a31bbf00db358f02ac7d
Andrew Hunter <andrew@logicalshift.co.uk>
2018-04-03 15:11:20 +0100

    Make the buttons toggle buttons for now (better reflects how we use them but side-effects are annoying)

--- gtk_ui/src/widgets/factory.rs
+++ gtk_ui/src/widgets/factory.rs
@@ -20,7 +20,7 @@ pub fn create_widget(id: WidgetId, widget_type: GtkWidgetType, widget_data: Rc<W
         Generic         => Box::new(FloFixedWidget::new(id, gtk::Fixed::new(), widget_data)),
         Layout          => Box::new(FloFixedWidget::new(id, gtk::Layout::new(None, None), widget_data)),
         Fixed           => Box::new(FloFixedWidget::new(id, gtk::Fixed::new(), widget_data)),
-        Button          => Box::new(BasicWidget::new(id, gtk::Button::new())),
+        Button          => Box::new(BasicWidget::new(id, gtk::ToggleButton::new())),
         Label           => Box::new(FloLabelWidget::new(id, gtk::Label::new(None))),
         DrawingArea     => Box::new(BasicWidget::new(id, gtk::DrawingArea::new())),
         Scale           => Box::new(BasicWidget::new(id, gtk::Scale::new(gtk::Orientation::Horizontal, None))),
