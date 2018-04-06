93ed4f58b6ce5738b650b46355b9e0b8edcc5c74
Andrew Hunter <andrew@logicalshift.co.uk>
2018-04-06 14:03:16 +0100

    Missing match case

--- gtk_ui/src/widgets/basic_widget.rs
+++ gtk_ui/src/widgets/basic_widget.rs
@@ -267,6 +267,8 @@ pub fn process_basic_event_request<W: GtkUiWidget>(widget: &W, flo_gtk: &mut Flo
                 }); 
             },
         
+        Paint(device) => { },
+        
         EditValue | SetValue => { }
     }
 }
