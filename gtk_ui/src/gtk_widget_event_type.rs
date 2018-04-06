44db9212035fe9045d21cc696726000cbf52cf2f
Andrew Hunter <andrew@logicalshift.co.uk>
2018-04-06 14:01:59 +0100

    Add a paint event and a way to convert a UI device into a GTK one

--- gtk_ui/src/gtk_widget_event_type.rs
+++ gtk_ui/src/gtk_widget_event_type.rs
@@ -1,3 +1,17 @@
+use flo_ui::*;
+
+///
+/// Device used for painting
+/// 
+#[derive(Copy, Clone, PartialEq, Debug)]
+pub enum GtkPaintDevice {
+    None,
+    Mouse(i32),
+    Touch,
+    Stylus,
+    Eraser
+}
+
 ///
 /// Types of widget event that can be registered
 /// 
@@ -6,9 +20,27 @@ pub enum GtkWidgetEventType {
     /// User pressed and released the mouse over a particular widget (or any of its children, if they do not generate their own event for this situation)
     Click,
 
+    /// User performed painting actions over a widget
+    Paint(GtkPaintDevice),
+
     /// User is in the process of editing a value
     EditValue,
 
     /// User has picked a final value
     SetValue
+}
+
+impl From<PaintDevice> for GtkPaintDevice {
+    fn from(device: PaintDevice) -> GtkPaintDevice {
+        match device {
+            PaintDevice::Other                          => GtkPaintDevice::None,
+            PaintDevice::Mouse(MouseButton::Left)       => GtkPaintDevice::Mouse(0),
+            PaintDevice::Mouse(MouseButton::Middle)     => GtkPaintDevice::Mouse(2),
+            PaintDevice::Mouse(MouseButton::Right)      => GtkPaintDevice::Mouse(1),
+            PaintDevice::Mouse(MouseButton::Other(_))   => GtkPaintDevice::None,
+            PaintDevice::Pen                            => GtkPaintDevice::Stylus,
+            PaintDevice::Eraser                         => GtkPaintDevice::Eraser,
+            PaintDevice::Touch                          => GtkPaintDevice::Touch
+        }
+    }
 }
