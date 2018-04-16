4f6d1b9f5a47b6cd12eaf298c94cdac6e561757b
Andrew Hunter <andrew@logicalshift.co.uk>
2018-04-16 22:57:59 +0100

    Make the edit events occur while a button is being held down, and the set events occur otherwise

--- gtk_ui/src/widgets/flo_scale_widget.rs
+++ gtk_ui/src/widgets/flo_scale_widget.rs
@@ -30,7 +30,10 @@ pub struct FloScaleWidget {
     scale: gtk::Scale,
 
     /// The label as a widget
-    widget: gtk::Widget
+    widget: gtk::Widget,
+
+    /// Flag that indicates if the user is pressing a mouse button (ie, dragging the scale)
+    button_pressed: Rc<RefCell<bool>>
 }
 
 impl FloScaleWidget {
@@ -38,12 +41,40 @@ impl FloScaleWidget {
     /// Creates a new scale widget
     /// 
     pub fn new(id: WidgetId, scale: gtk::Scale) -> FloScaleWidget {
+        let button_pressed = Rc::new(RefCell::new(false));
+
+        Self::connect_button_events(&scale, Rc::clone(&button_pressed));
+
         FloScaleWidget {
-            min:    0.0,
-            max:    0.0,
-            id:     id,
-            widget: scale.clone().upcast::<gtk::Widget>(),
-            scale:  scale,
+            min:            0.0,
+            max:            0.0,
+            id:             id,
+            widget:         scale.clone().upcast::<gtk::Widget>(),
+            scale:          scale,
+            button_pressed: button_pressed
+        }
+    }
+
+    ///
+    /// Hooks up the button pressed event
+    /// 
+    fn connect_button_events(scale: &gtk::Scale, button_pressed: Rc<RefCell<bool>>) {
+        {
+            // Set the button pressed flag when the user clicks the mouse over the scale
+            let button_pressed = Rc::clone(&button_pressed);
+            scale.connect_button_press_event(move |_, _| {
+                *button_pressed.borrow_mut() = true;
+                Inhibit(false)
+            });
+        }
+
+        {
+            // Clear the button pressed flag when the user releases the mouse over the scale
+            let button_pressed = Rc::clone(&button_pressed);
+            scale.connect_button_release_event(move |_, _| {
+                *button_pressed.borrow_mut() = false;
+                Inhibit(false)
+            });
         }
     }
 }
@@ -69,21 +100,48 @@ impl GtkUiWidget for FloScaleWidget {
                 self.scale.set_range(self.min.min(self.max), self.max.max(self.min));
             },
 
-            &RequestEvent(SetValue, ref event_name)     |
-            &RequestEvent(EditValue, ref event_name)    => {
-                // Right now, there doesn't seem to be an easy way to distinguish between values set during a drag and at the end
-                // so we consider 'set' and 'edit' to be the same actions for now.
-                let last_value  = RefCell::new(self.scale.get_value());
-                let id          = self.id;
-                let sink        = RefCell::new(flo_gtk.get_event_sink());
-                let event_name  = event_name.clone();
-                self.scale.connect_value_changed(move |widget| { 
-                    let new_value       = widget.get_value();
-                    let mut last_value  = last_value.borrow_mut();
+            &RequestEvent(SetValue, ref event_name_ref)     => {
+                // Set events are value changes that occur while the mouse button has been released
+                let id              = self.id;
+                let sink            = RefCell::new(flo_gtk.get_event_sink());
+                let event_name      = event_name_ref.clone();
+                let button_pressed  = Rc::clone(&self.button_pressed);
+                
+                self.scale.connect_value_changed(move |widget| {
+                    // Generate when the value is changed and the button is not held down
+                    if *button_pressed.borrow() == false {
+                        let new_value       = widget.get_value();
+                        sink.borrow_mut().start_send(GtkEvent::Event(id, event_name.clone(), GtkEventParameter::ScaleValue(new_value))).unwrap();
+                    }
+                });
 
-                    if new_value != *last_value {
-                        *last_value = new_value;
+                let sink            = RefCell::new(flo_gtk.get_event_sink());
+                let event_name      = event_name_ref.clone();
+                self.scale.connect_button_release_event(move |widget, _| {
+                    // Also generate if the button is released
+                    let new_value       = widget.get_value();
                     sink.borrow_mut().start_send(GtkEvent::Event(id, event_name.clone(), GtkEventParameter::ScaleValue(new_value))).unwrap();
+                    Inhibit(false)
+                });
+            },
+
+            &RequestEvent(EditValue, ref event_name)    => {
+                // Edit events are value changes that occur while the mouse button is held down
+                let last_value      = RefCell::new(self.scale.get_value());
+                let id              = self.id;
+                let sink            = RefCell::new(flo_gtk.get_event_sink());
+                let event_name      = event_name.clone();
+                let button_pressed  = Rc::clone(&self.button_pressed);
+                
+                self.scale.connect_value_changed(move |widget| {
+                    if *button_pressed.borrow() == true {
+                        let new_value       = widget.get_value();
+                        let mut last_value  = last_value.borrow_mut();
+
+                        if new_value != *last_value {
+                            *last_value = new_value;
+                            sink.borrow_mut().start_send(GtkEvent::Event(id, event_name.clone(), GtkEventParameter::ScaleValue(new_value))).unwrap();
+                        }
                     }
                 });
             },
