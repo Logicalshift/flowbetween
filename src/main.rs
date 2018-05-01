3e6b27a8cf611b6a8695c4bbfd4eb32751e13688
Andrew Hunter <andrew@logicalshift.co.uk>
2018-05-01 23:23:00 +0100

    Make gtk and http interfaces optional

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@
 //! # FlowBetween HTTP server
 //!
 
+#[cfg(feature="gtk")]   extern crate flo_gtk_ui;
+#[cfg(feature="http")]  extern crate flo_http_ui;
+#[cfg(feature="http")]  extern crate flo_static_files;
+
 extern crate flo_ui;
 extern crate flo;
-extern crate flo_gtk_ui;
-extern crate flo_http_ui;
 extern crate flo_binding;
 extern crate flo_animation;
 extern crate flo_anim_sqlite;
-extern crate flo_static_files;
 
 extern crate serde;
 extern crate serde_json;
@@ -19,16 +20,17 @@ extern crate iron;
 extern crate mount;
 extern crate tokio_core;
 
-mod http_session;
+#[cfg(feature="http")] mod http_session;
 
-use iron::*;
-use mount::*;
+#[cfg(feature="http")] use iron::*;
+#[cfg(feature="http")] use mount::*;
 use tokio_core::reactor::Core;
 use std::sync::*;
 use std::thread;
+use std::thread::JoinHandle;
 
-use flo_static_files::*;
-use flo_http_ui::*;
+#[cfg(feature="http")] use flo_static_files::*;
+#[cfg(feature="http")] use flo_http_ui::*;
 use flo_gtk_ui::*;
 
 use self::http_session::*;
@@ -39,18 +41,9 @@ const SERVER_PORT: u32      = 3000;
 const WS_SERVER_PORT: u32   = 3001;
 const BIND_ADDRESS: &str    = "0.0.0.0";
 
-fn main() {
-    // TODO: be a bit more sensible about this (right now this is just the GTK version shoved onto the start of the HTTP version)
-
-    let gtk_thread = thread::spawn(|| {
-        // Create a GTK session
-        let gtk_ui      = GtkUserInterface::new();
-        let gtk_session = GtkSession::from(FlowBetweenSession::new(), gtk_ui);
-
-        gtk_session.run();
-    });
-
-    let _http_thread = thread::spawn(|| {
+#[cfg(feature="http")]
+fn main_http() -> Option<JoinHandle<()>> {
+    Some(thread::spawn(|| {
         // Create the web session structure
         let sessions = Arc::new(WebSessions::new());
         
@@ -77,7 +70,44 @@ fn main() {
         println!("{} v{} preparing to serve requests at {}", PACKAGE_NAME, PACKAGE_VERSION, &format!("{}:{}", BIND_ADDRESS, SERVER_PORT));
 
         Iron::new(mount).http(&format!("{}:{}", BIND_ADDRESS, SERVER_PORT)).unwrap();
-    });
+    }))
+}
 
-    gtk_thread.join().unwrap();
+#[cfg(not(feature="http"))]
+fn main_http() -> Option<JoinHandle<()>> {
+    None
+}
+
+#[cfg(feature="gtk")]
+fn main_gtk() -> Option<JoinHandle<()>> {
+    Some(thread::spawn(|| {
+        // Create a GTK session
+        let gtk_ui      = GtkUserInterface::new();
+        let gtk_session = GtkSession::from(FlowBetweenSession::new(), gtk_ui);
+
+        gtk_session.run();
+    }))
+}
+
+#[cfg(not(feature="gtk"))]
+fn main_gtk() -> Option<JoinHandle<()>> {
+    None
+}
+
+#[cfg(not(any(feature="gtk", feature="http")))]
+compile_error!("You must pick a UI implementation as a feature to compile FlowBetween (cargo build scripts cannot autodetect, sadly). Build with cargo --features gtk,http");
+
+fn main() {
+    // TODO: be a bit more sensible about this (right now this is just the GTK version shoved onto the start of the HTTP version)
+
+    let gtk_thread  = main_gtk();
+    let http_thread = main_http();
+
+    if let Some(gtk_thread) = gtk_thread {
+        // If there's a GTK thread, then we stop when it stops
+        gtk_thread.join().unwrap();
+    } else if let Some(http_thread) = http_thread {
+        // Otherwise we monitor the HTTP thread, if it exists
+        http_thread.join().unwrap();
+    }
 }
