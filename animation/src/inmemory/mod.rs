893e8da3eb116d91d9366d53e0a82e7b2e12c2f4
Andrew Hunter <andrew@logicalshift.co.uk>
2018-05-20 17:12:09 +0100

    No longer need the edit log

--- animation/src/inmemory/mod.rs
+++ animation/src/inmemory/mod.rs
@@ -4,13 +4,9 @@ mod animation_sink;
 mod empty_frame;
 mod vector_layer;
 mod vector_frame;
-mod empty_iterator;
 mod vector_keyframe;
 mod vector_layer_core;
-pub mod edit_log;
-pub mod pending_log;
 
 pub use self::animation::*;
-pub use self::empty_iterator::*;
 pub use self::vector_layer::*;
 pub use self::vector_keyframe::*;
