6fa3d408d053a669ecfc0d3052d296a40d296e14
Andrew Hunter <andrew@logicalshift.co.uk>
2018-05-20 16:54:01 +0100

    Add the vector layer editing functions

---
+++ animation/src/inmemory/vector_layer_core/edit.rs
@@ -0,0 +1,88 @@
+use super::*;
+use super::super::super::traits::*;
+
+use std::time::Duration;
+
+impl VectorLayerCore {
+    ///
+    /// Adds a new key frame to this core 
+    /// 
+    pub fn add_key_frame(&mut self, time_offset: Duration) {
+        // TODO: do nothing if the keyframe is already created
+
+        // Generate a new keyframe
+        let new_keyframe = VectorKeyFrame::new(time_offset);
+
+        // Add in order to the existing keyframes
+        self.keyframes.push(Arc::new(new_keyframe));
+        self.sort_key_frames();
+    }
+
+    ///
+    /// Removes a keyframe from this core
+    /// 
+    pub fn remove_key_frame(&mut self, time_offset: Duration) {
+        // Binary search for the key frame
+        let search_result = self.keyframes.binary_search_by(|a| a.start_time().cmp(&time_offset));
+
+        // Remove only if we found an exact match
+        if let Ok(frame_number) = search_result {
+            self.keyframes.remove(frame_number);
+        }
+    }
+
+    ///
+    /// Adds a new vector element to this layer
+    /// 
+    pub fn add_element(&mut self, when: Duration, new_element: Vector) {
+        if let Some(keyframe) = self.find_nearest_keyframe(when) {
+            let when = when - keyframe.start_time();
+
+            keyframe.add_element(when, new_element);
+        }
+    }
+
+    ///
+    /// Performs a paint edit on this layer
+    /// 
+    pub fn paint(&mut self, when: Duration, paint: PaintEdit) {
+        use self::PaintEdit::*;
+
+        match paint {
+            SelectBrush(id, definition, draw_style) => {
+                let select_brush = Vector::new(BrushDefinitionElement::new(id, definition, draw_style));
+
+                self.add_element(when, select_brush);
+            },
+
+            BrushProperties(id, new_properties)     => {
+                let brush_properties = Vector::new(BrushPropertiesElement::new(id, new_properties));
+
+                self.add_element(when, brush_properties);
+            },
+            
+            BrushStroke(id, points)                 => {
+                let brush           = self.active_brush(when);
+                let brush_points    = brush.brush_points_for_raw_points(&points);
+
+                let brush_stroke    = Vector::new(BrushElement::new(id, Arc::new(brush_points)));
+
+                self.add_element(when, brush_stroke);
+            }
+        }
+    }
+
+    ///
+    /// Performs a layer edit on this layer
+    /// 
+    pub fn edit(&mut self, edit: LayerEdit) {
+        use self::LayerEdit::*;
+
+        match edit {
+            Paint(when, edit)           => self.paint(when, edit),
+
+            AddKeyFrame(when)           => self.add_key_frame(when),
+            RemoveKeyFrame(when)        => self.remove_key_frame(when)
+        }
+    }
+}
--- animation/src/inmemory/vector_layer_core/vectors.rs
+++ animation/src/inmemory/vector_layer_core/vectors.rs
@@ -3,17 +3,6 @@ use super::super::super::brushes::*;
 
 impl VectorLayer for VectorLayerCore {
     ///
-    /// Adds a new vector element to this layer
-    /// 
-    fn add_element(&mut self, when: Duration, new_element: Vector) {
-        if let Some(keyframe) = self.find_nearest_keyframe(when) {
-            let when = when - keyframe.start_time();
-
-            keyframe.add_element(when, new_element);
-        }
-    }
-
-    ///
     /// Finds the brush that will be used with the next element added to this layer
     /// 
     fn active_brush(&self, when: Duration) -> Arc<Brush> {
