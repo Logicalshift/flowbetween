3d4c411ef9a70d93ffefda02bae6005f53e1bf46
Andrew Hunter <andrew@logicalshift.co.uk>
2018-05-20 18:12:15 +0100

    Assign IDs to edits before performing them

--- animation/src/inmemory/animation_sink.rs
+++ animation/src/inmemory/animation_sink.rs
@@ -42,15 +42,58 @@ impl AnimationSink {
     }
 
     ///
-    /// Performs a series of edits on this sink
+    /// Assigns element IDs to a set of edits
     /// 
-    pub fn edit(&self, edit: Vec<AnimationEdit>) {
-        // Send the edits to the core
+    fn assign_ids(&self, edits: Vec<AnimationEdit>) -> Vec<AnimationEdit> {
+        use self::AnimationEdit::*;
+        use self::LayerEdit::*;
+        use self::PaintEdit::*;
+
         let mut core = self.core.lock().unwrap();
 
-        edit.iter()
+        // Assign IDs to any element edits
+        let mut next_element_id = core.edit_log.len() as i64;
+        let mut result          = vec![];
+
+        // Convenience function to assign the next ID
+        let mut assign_id       = move || { 
+            let element_id = next_element_id;
+            next_element_id += 1;
+            element_id
+        };
+
+        for elem in edits.into_iter() {
+            let with_id = match elem {
+                Layer(layer_id, Paint(when, BrushProperties(ElementId::Unassigned, props))) =>
+                    Layer(layer_id, Paint(when, BrushProperties(ElementId::Assigned(assign_id()), props))),
+
+                Layer(layer_id, Paint(when, SelectBrush(ElementId::Unassigned, defn, drawing_style))) =>
+                    Layer(layer_id, Paint(when, SelectBrush(ElementId::Assigned(assign_id()), defn, drawing_style))),
+
+                Layer(layer_id, Paint(when, BrushStroke(ElementId::Unassigned, points))) =>
+                    Layer(layer_id, Paint(when, BrushStroke(ElementId::Assigned(assign_id()), points))),
+
+                other => other
+            };
+
+            result.push(with_id);
+        }
+
+        result
+    }
+
+    ///
+    /// Performs a series of edits on this sink
+    /// 
+    pub fn edit(&self, edits: Vec<AnimationEdit>) {
+        let edits       = self.assign_ids(edits);
+        let mut core    = self.core.lock().unwrap();
+
+        // Send the edits to the core
+        edits.iter()
             .for_each(|edit| core.edit(edit));
 
-        core.edit_log.extend(edit.into_iter());
+        // Store them in the edit log
+        core.edit_log.extend(edits.into_iter());
     }
 }
