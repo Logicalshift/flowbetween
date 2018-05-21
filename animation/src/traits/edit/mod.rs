0dd5ff75f2e2ce47bcd5604e86eeed050ca137f2
Andrew Hunter <andrew@logicalshift.co.uk>
2018-05-21 09:00:07 +0100

    Remove the editlog trait as it's not used any more

--- animation/src/traits/edit/mod.rs
+++ animation/src/traits/edit/mod.rs
@@ -3,11 +3,9 @@ mod animation_edit;
 mod layer_edit;
 mod frame_edit;
 mod element_edit;
-mod log;
 
 pub use self::element_id::*;
 pub use self::animation_edit::*;
 pub use self::layer_edit::*;
 pub use self::frame_edit::*;
 pub use self::element_edit::*;
-pub use self::log::*;
