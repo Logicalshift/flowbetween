ced383423ee8516335796575c47eef683378a022
Andrew Hunter <andrew@logicalshift.co.uk>
2018-05-25 23:21:21 +0100

    Move the 'layer write' code to the core so we don't have to create SqliteVectorLayers there

--- anim_sqlite/src/db/animation_core.rs
+++ anim_sqlite/src/db/animation_core.rs
@@ -5,6 +5,8 @@ use super::vector_layer::*;
 use animation::*;
 
 use rusqlite::*;
+use std::sync::*;
+use std::time::Duration;
 use std::collections::HashMap;
 
 ///
@@ -31,6 +33,138 @@ impl<TFile: FloFile+Send> AnimationDbCore<TFile> {
         if self.failure.is_none() {
             self.failure = edit(&mut self.db).err();
         }
+    }
+
+    ///
+    /// Creates a new vector element in an animation DB core, leaving the element ID, key frame ID and time pushed on the DB stack
+    ///
+    /// The element is created without its associated data.
+    ///
+    fn create_new_element(db: &mut TFile, layer_id: i64, when: Duration, element: &PaintEdit) -> Result<()> {
+        if let ElementId::Assigned(assigned_id) = element.id() {
+            db.update(vec![
+                DatabaseUpdate::PushLayerId(layer_id),
+                DatabaseUpdate::PushNearestKeyFrame(when),
+                DatabaseUpdate::PushVectorElementType(VectorElementType::from(element), when),
+                DatabaseUpdate::PushElementAssignId(assigned_id)
+            ])?;
+        } else {
+            db.update(vec![
+                DatabaseUpdate::PushLayerId(layer_id),
+                DatabaseUpdate::PushNearestKeyFrame(when),
+                DatabaseUpdate::PushVectorElementType(VectorElementType::from(element), when)
+            ])?;
+        }
+
+        Ok(())
+    }
+
+    ///
+    /// Writes a brush properties element to the database (popping the element ID)
+    ///
+    fn create_brush_properties(db: &mut TFile, properties: BrushProperties) -> Result<()> {
+        AnimationDbCore::insert_brush_properties(db, &properties)?;
+
+        // Create the element
+        db.update(vec![
+            DatabaseUpdate::PopVectorBrushPropertiesElement
+        ])?;
+
+        Ok(())
+    }
+
+    ///
+    /// Writes a brush definition element to the database (popping the element ID)
+    ///
+    fn create_brush_definition(db: &mut TFile, definition: BrushDefinition, drawing_style: BrushDrawingStyle) -> Result<()> {
+        // Create the brush definition
+        AnimationDbCore::insert_brush(db, &definition)?;
+
+        // Insert the properties for this element
+        db.update(vec![
+            DatabaseUpdate::PopVectorBrushElement(DrawingStyleType::from(&drawing_style))
+        ])?;
+
+        Ok(())
+    }
+
+    ///
+    /// Writes a brush stroke to the database (popping the element ID)
+    ///
+    fn create_brush_stroke(db: &mut TFile, brush_stroke: Arc<Vec<RawPoint>>) -> Result<()> {
+        // TODO: we need to convert the raw points to brush points here
+
+        db.update(vec![
+            DatabaseUpdate::PopBrushPoints(brush_stroke)
+        ])?;
+
+        Ok(())
+    }
+
+    ///
+    /// Adds a new vector element to a vector layer
+    /// 
+    fn paint_vector_layer(&mut self, layer_id: i64, when: Duration, new_element: PaintEdit) -> Result<()> {
+        use animation::PaintEdit::*;
+
+        // Update the state of this object based on the element
+        match new_element {
+            SelectBrush(_id, ref brush_definition, drawing_style)   => {
+                // TODO!
+                // self.active_brush = Some((when, create_brush_from_definition(brush_definition, drawing_style)));
+            },
+
+            _ => ()
+        }
+
+        // Create a new element
+        Self::create_new_element(&mut self.db, layer_id, when, &new_element)?;
+
+        // Record the details of the element itself
+        match new_element {
+            SelectBrush(_id, brush_definition, drawing_style)   => Self::create_brush_definition(&mut self.db, brush_definition, drawing_style)?,
+            BrushProperties(_id, brush_properties)              => Self::create_brush_properties(&mut self.db, brush_properties)?,
+            BrushStroke(_id, brush_stroke)                      => Self::create_brush_stroke(&mut self.db, brush_stroke)?,
+        }
+
+        // create_new_element pushes an element ID, a key frame ID and a time. The various element actions pop the element ID so we need to pop the frame ID and time
+        self.db.update(vec![
+            DatabaseUpdate::Pop,
+            DatabaseUpdate::Pop
+        ])?;
+
+        Ok(())
+    }
+
+    ///
+    /// Performs a layer edit to a vector layer
+    /// 
+    pub fn edit_vector_layer(&mut self, layer_id: i64, edit: LayerEdit) -> Result<()> {
+        use self::LayerEdit::*;
+
+        // Note that we can't access the core at this point (the database implies that the core is already in use)
+
+        match edit {
+            AddKeyFrame(when) => {
+                self.db.update(vec![
+                    DatabaseUpdate::PushLayerId(layer_id),
+                    DatabaseUpdate::PopAddKeyFrame(when)
+                ])?;
+            },
+
+            RemoveKeyFrame(when) => {
+                self.db.update(vec![
+                    DatabaseUpdate::PushLayerId(layer_id),
+                    DatabaseUpdate::PopRemoveKeyFrame(when)
+                ])?;
+            },
+
+            Paint(when, edit) => {
+                self.paint_vector_layer(layer_id, when, edit)?;
+            }
+        }
+
+        Ok(())
     }
 
     ///
@@ -66,22 +200,7 @@ impl<TFile: FloFile+Send> AnimationDbCore<TFile> {
             },
 
             Layer(layer_id, layer_edit) => {
-                // Create the layer if one is not already cached
-                let layer = if !self.layers.contains_key(&layer_id) {
-                    let new_layer = SqliteVectorLayer::from_assigned_id(self, layer_id);
-
-                    if let Some(new_layer) = new_layer {
-                        self.layers.insert(layer_id, new_layer);
-                        self.layers.get_mut(&layer_id)
-                    } else {
-                        None
-                    }
-                } else {
-                    self.layers.get_mut(&layer_id)
-                };
-
-                // Pass the edit on to the layer
-                layer.map(|layer| layer.edit(&mut self.db, layer_edit));
+                self.edit_vector_layer(layer_id as i64, layer_edit);
             },
 
             Element(id, when, edit) => {
