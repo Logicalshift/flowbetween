b571a006cc9086259b754b8a5b53dbe2d9bb15fe
Andrew Hunter <andrew@logicalshift.co.uk>
2018-05-27 16:53:18 +0100

    Add a perform_edits implementation (mainly used by the tests)

--- anim_sqlite/src/animation_sqlite/edit.rs
+++ anim_sqlite/src/animation_sqlite/edit.rs
@@ -1,7 +1,20 @@
 use super::*;
 
 use futures::*;
+use futures::executor;
 use animation::*;
+
+impl SqliteAnimation {
+    ///
+    /// Performs a particular set of edits immediately to this animation
+    ///
+    pub fn perform_edits(&mut self, edits: Vec<AnimationEdit>) {
+        let mut sink = executor::spawn(self.db.create_edit_sink());
+
+        sink.wait_send(edits).unwrap();
+        sink.wait_flush().unwrap();
+    }
+}
 
 impl EditableAnimation for SqliteAnimation {
     fn edit<'a>(&'a self) -> Box<'a+Sink<SinkItem=Vec<AnimationEdit>, SinkError=()>> {
