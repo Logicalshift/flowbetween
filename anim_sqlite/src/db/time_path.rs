d1268e152765719a3ad63e18c6717ca1eefc03b7
Andrew Hunter <andrew@logicalshift.co.uk>
2018-06-03 14:52:05 +0100

    Add a test for the time curve from time points translator

--- anim_sqlite/src/db/time_path.rs
+++ anim_sqlite/src/db/time_path.rs
@@ -25,3 +25,27 @@ pub fn time_curve_from_time_points<PointIterator: IntoIterator<Item=TimePointEnt
     // Result is a time curve
     TimeCurve { points }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn convert_curve_from_points() {
+        let curve = time_curve_from_time_points(vec![
+            TimePointEntry { x: 10.0, y: 20.0, milliseconds: 30.0 },
+            TimePointEntry { x: 20.0, y: 40.0, milliseconds: 60.0 },
+            TimePointEntry { x: 30.0, y: 60.0, milliseconds: 90.0 },
+
+            TimePointEntry { x: 40.0, y: 80.0, milliseconds: 120.0 },
+            TimePointEntry { x: 50.0, y: 100.0, milliseconds: 150.0 },
+            TimePointEntry { x: 60.0, y: 120.0, milliseconds: 180.0 },
+        ]);
+
+        assert!(curve.points.len() == 2);
+        assert!(curve.points[0].point == TimePoint(10.0, 20.0, 30.0));
+        assert!(curve.points[1].point == TimePoint(40.0, 80.0, 120.0));
+        assert!(curve.points[0].past == TimePoint(20.0, 40.0, 60.0));
+        assert!(curve.points[1].future == TimePoint(60.0, 120.0, 180.0));
+    }
+}
