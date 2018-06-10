5f45865aa38bd84edfada1f8ec8f385411cda073
Andrew Hunter <andrew@logicalshift.co.uk>
2018-06-10 23:10:28 +0100

    State for our handler will be a WebSessions object

--- http_ui_actix/src/handler.rs
+++ http_ui_actix/src/handler.rs
@@ -1,12 +1,15 @@
+use flo_http_ui::*;
+
 use actix_web::*;
 use actix_web::dev::Handler;
+
+use std::sync::*;
 
 ///
 /// Creates the handler for an actix UI session
 /// 
-pub fn create_handler() -> impl Handler<()> {
+pub fn create_handler<CoreController: HttpController>() -> impl Handler<Arc<WebSessions<CoreController>>> {
     |req| {
         "Hello, world"
     }
 }
-
