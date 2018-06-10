ac8fea6440458c54c28371089c26222620d04a3b
Andrew Hunter <andrew@logicalshift.co.uk>
2018-06-10 20:43:53 +0100

    Don't need nightly any more to build with wasm

--- build-wasm.sh
+++ build-wasm.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 cd binding
-cargo +nightly build --target wasm32-unknown-unknown
+cargo build --target wasm32-unknown-unknown
 cd ..
 
 cd curves
-cargo +nightly build --target wasm32-unknown-unknown
+cargo build --target wasm32-unknown-unknown
 cd ..
 
 cd ui
-cargo +nightly build --target wasm32-unknown-unknown
+cargo build --target wasm32-unknown-unknown
 cd ..
 
 cd animation
-cargo +nightly build --target wasm32-unknown-unknown
+cargo build --target wasm32-unknown-unknown
 cd ..
