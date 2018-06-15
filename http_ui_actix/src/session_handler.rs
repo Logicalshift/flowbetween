f90e39345057b1e792f835d301b8e2f148e38283
Andrew Hunter <andrew@logicalshift.co.uk>
2018-06-15 09:24:38 +0100

    Add code for parsing the body for JSON requests

--- http_ui_actix/src/session_handler.rs
+++ http_ui_actix/src/session_handler.rs
@@ -3,6 +3,7 @@ use flo_http_ui::*;
 use actix_web::*;
 use actix_web::http::*;
 use actix_web::dev::{Handler, AsyncResult};
+use futures::*;
 use futures::future;
 
 use std::sync::*;
@@ -10,12 +11,27 @@ use std::sync::*;
 ///
 /// Creates the handler for an actix UI session
 /// 
-pub fn session_handler<CoreController: HttpController>() -> impl Handler<Arc<WebSessions<CoreController>>> {
+pub fn session_handler<CoreController: 'static+HttpController>() -> impl Handler<Arc<WebSessions<CoreController>>> {
     |req: HttpRequest<Arc<WebSessions<CoreController>>>| {
         match req.method() {
             &Method::POST => {
                 // POST requests are used to send instructions to sessions
-                unimplemented!()
+                let successful_req  = Arc::new(req);
+                let failed_req      = Arc::clone(&successful_req);
+
+                // Request must contain a JSON body
+                let result = Json::<UiHandlerRequest>::extract(&*successful_req)
+                    .and_then(move |request| {
+                        // Request not implemented
+                        future::ok(successful_req.build_response(StatusCode::NOT_FOUND).body("Not implemented"))
+                    })
+                    .or_else(move |_err| {
+                        // Failed to parse the JSON request for some reason
+                        future::ok(failed_req.build_response(StatusCode::BAD_REQUEST).body("FlowBetween session request is not in the expected format"))
+                    });
+                
+                // Request will be ready in the future
+                AsyncResult::async(Box::new(result))
             },
 
             &Method::GET => {
