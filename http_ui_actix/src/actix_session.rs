04f3f164f18ba865710dcea185929b9d05e29985
Andrew Hunter <andrew@logicalshift.co.uk>
2018-06-16 11:40:11 +0100

    Add an ActixSession trait

---
+++ http_ui_actix/src/actix_session.rs
@@ -0,0 +1,47 @@
+use flo_ui::*;
+use flo_ui::session::*;
+use flo_http_ui::*;
+
+use std::sync::*;
+
+///
+/// Trait implemented by objects that can work as an actix session
+/// 
+pub trait ActixSession {
+    /// The controller type for this session
+    type Controller: HttpController+Controller+'static;
+
+    /// The core user interface for this session
+    type CoreUi: CoreUserInterface+Send+Sync+'static;
+
+    ///
+    /// Creates a new session and returns its ID
+    /// 
+    fn new_session(&self, controller: Self::Controller, base_path: &str) -> String;
+
+    ///
+    /// Retrieves the session with the specified ID, if present
+    /// 
+    fn get_session(&self, session_id: &str) -> Option<Arc<Mutex<HttpSession<Self::CoreUi>>>>;
+}
+
+impl<CoreController: HttpController+Controller+'static> ActixSession for WebSessions<CoreController> {
+    type Controller = CoreController;
+    type CoreUi     = UiSession<CoreController>;
+
+    ///
+    /// Creates a new session and returns its ID
+    /// 
+    #[inline]
+    fn new_session(&self, controller: Self::Controller, base_path: &str) -> String {
+        WebSessions::<CoreController>::new_session(self, controller, base_path)
+    }
+
+    ///
+    /// Retrieves the session with the specified ID, if present
+    /// 
+    #[inline]
+    fn get_session(&self, session_id: &str) -> Option<Arc<Mutex<HttpSession<Self::CoreUi>>>> {
+        WebSessions::<CoreController>::get_session(self, session_id)
+    }
+}
--- http_ui_actix/src/lib.rs
+++ http_ui_actix/src/lib.rs
@@ -1,11 +1,14 @@
+extern crate flo_ui;
 extern crate flo_http_ui;
 extern crate flo_static_files;
 
 extern crate actix_web;
 extern crate futures;
 
+mod actix_session;
 mod session_handler;
 mod static_file_handler;
 
+pub use self::actix_session::*;
 pub use self::session_handler::*;
 pub use self::static_file_handler::*;
