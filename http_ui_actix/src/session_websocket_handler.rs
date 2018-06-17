f8e36180775314b8b02858ce05542d53263afda9
Andrew Hunter <andrew@logicalshift.co.uk>
2018-06-17 11:48:27 +0100

    Add a function for creating a new  websocket session

--- http_ui_actix/src/session_websocket_handler.rs
+++ http_ui_actix/src/session_websocket_handler.rs
@@ -1,10 +1,12 @@
 use flo_http_ui::*;
+use flo_ui::*;
 use flo_ui::session::*;
 
 use actix::*;
 use actix::fut;
 use actix_web::*;
 use futures::*;
+use futures::future;
 use futures::sync::oneshot;
 use serde_json;
 
@@ -19,10 +21,21 @@ struct FloWsSession<CoreUi: CoreUserInterface+Send+Sync+'static> {
     session: Arc<Mutex<HttpSession<CoreUi>>>,
 
     /// The event sink for this session
-    event_sink: Box<Future<Item=CoreUi::EventSink, Error=()>>
+    event_sink: Box<Future<Item=HttpEventSink, Error=()>>
 }
 
 impl<CoreUi: CoreUserInterface+Send+Sync+'static> FloWsSession<CoreUi> {
+    ///
+    /// Creates a new websocket session
+    /// 
+    pub fn new(session: Arc<Mutex<HttpSession<CoreUi>>>) -> FloWsSession<CoreUi> {
+        let event_sink = future::ok(session.lock().unwrap().http_ui().get_input_sink());
+
+        FloWsSession {
+            session:    session,
+            event_sink: Box::new(event_sink)
+        }
+    }
 }
 
 impl<CoreUi: CoreUserInterface+Send+Sync+'static> Actor for FloWsSession<CoreUi> {
@@ -34,12 +47,12 @@ impl<CoreUi: CoreUserInterface+Send+Sync+'static> StreamHandler<ws::Message, ws:
         match msg {
             ws::Message::Text(message) => {
                 // Parse the JSON message
-                let json = serde_json::from_str::<Vec<UiEvent>>(&message);
+                let json = serde_json::from_str::<Vec<Event>>(&message);
 
                 if let Ok(request) = json {
                     // Create a one-shot future for when the event sink is available again
                     let (send_sink, next_sink)  = oneshot::channel();
-                    let mut next_sink: Box<Future<Item=CoreUi::EventSink, Error=()>>    = Box::new(next_sink.map_err(|_| ()));
+                    let mut next_sink: Box<Future<Item=HttpEventSink, Error=()>> = Box::new(next_sink.map_err(|_| ()));
                     mem::swap(&mut self.event_sink, &mut next_sink);
                     
                     // Send to the sink
