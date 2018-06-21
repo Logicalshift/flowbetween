79ac086d0ac185d6da533cecd8f0d96787cf9756
Andrew Hunter <andrew@logicalshift.co.uk>
2018-06-21 22:15:12 +0100

    http UI needs the http feature turned on

--- build-wasm.sh
+++ build-wasm.sh
@@ -4,4 +4,4 @@ cargo build -p flo_binding --release --target wasm32-unknown-unknown
 cargo build -p flo_curves --release --target wasm32-unknown-unknown
 cargo build -p flo_ui --release --target wasm32-unknown-unknown
 cargo build -p flo_animation --release --target wasm32-unknown-unknown
-cargo build -p flo_http_ui --release --target wasm32-unknown-unknown
+cargo build -p flo_http_ui --release --target wasm32-unknown-unknown --features http
