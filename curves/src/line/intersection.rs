79d94b0f571932efca30a295930857af8f2111bf
Andrew Hunter <andrew@logicalshift.co.uk>
2018-06-21 08:44:14 +0100

    First pass at a line/box intersection/clipping algorithm

--- curves/src/line/intersection.rs
+++ curves/src/line/intersection.rs
@@ -1,6 +1,8 @@
 use super::line::*;
 use super::super::coordinate::*;
 
+use std::f64;
+
 ///
 /// Returns the point at which two lines intersect (if they intersect)
 /// 
@@ -25,5 +27,58 @@ where L::Point: Coordinate2D {
         ]))
     } else {
         None
+    }
+}
+
+///
+/// Determines if a 2D line has intersected a bounding box (and returns the intersection if it exists)
+/// 
+pub fn line_clip_to_bounds<L: Line>(line: &L, bounds: &(L::Point, L::Point)) -> Option<L>
+where L::Point: Coordinate2D {
+    // Fetch the points for the line
+    let line_points             = line.points();
+    let ((x1, y1), (x2, y2))    = (line_points.0.coords(), line_points.1.coords());
+    let (dx, dy)                = (x2-x1, y2-y1);
+
+    // ... and the points for the bounding rectangle
+    let (xmin, ymin)            = (bounds.0.x().min(bounds.1.x()), bounds.0.y().min(bounds.1.y()));
+    let (xmax, ymax)            = (bounds.0.x().max(bounds.1.x()), bounds.0.y().max(bounds.1.y()));
+
+    // Our line can be described as '(x1+t*dx, y1+t*dy)' where 0 <= t <= 1
+    // We want to solve for the edges, eg (xmin=x1+tmin*dx => txmin=(xmin-x1)/dx)
+    let (txmin, txmax) = if dx == 0.0 {
+        // Parallel to the x axis
+        (f64::INFINITY, f64::INFINITY)
+    } else {
+        ((xmin-x1)/dx, (xmax-x1)/dx)
+    };
+
+    let (tymin, tymax) = if dy == 0.0 {
+        // Parallel to the y axis
+        (f64::INFINITY, f64::INFINITY)
+    } else {
+        ((ymin-y1)/dy, (ymax-y1)/dy)
+    };
+
+    let tmin = txmin.min(tymin);
+    let tmax = txmax.min(tymax);
+
+    if tmin < 0.0 && tmax < 0.0 {
+        // Outside the bounds
+        None
+    } else if tmin > 1.0 && tmax > 1.0 {
+        // Outside the bounds
+        None
+    } else {
+        // t values on the line range from 0-1
+        let tmin = tmin.max(0.0).min(1.0);
+        let tmax = tmax.max(0.0).min(1.0);
+
+        // Create points from these components
+        let p1 = L::Point::from_components(&[x1+dx*tmin, y1+dy*tmin]);
+        let p2 = L::Point::from_components(&[x1+dx*tmax, y1+dy*tmax]);
+
+        // This is the line clipped to these bounds
+        Some(L::from_points(p1, p2))
     }
 }
--- curves/tests/line/intersection.rs
+++ curves/tests/line/intersection.rs
@@ -15,3 +15,81 @@ fn intersection_at_other_point() {
 fn no_intersection() {
     assert!(line_intersects_line(&(Coord2(12.0, 13.0), Coord2(24.0, 30.0)), &(Coord2(1.0, 1.0), Coord2(0.0, -1.0))) == None);
 }
+
+#[test]
+fn line_in_bounds() {
+    let line    = (Coord2(5.0, 3.0), Coord2(7.0, 9.0));
+    let bounds  = (Coord2(1.0, 1.0), Coord2(10.0, 10.0));
+    let clipped = line_clip_to_bounds(&line, &bounds);
+
+    assert!(clipped.is_some());
+
+    let clipped = clipped.unwrap();
+    assert!(clipped.0.distance_to(&Coord2(5.0, 3.0)) < 0.01);
+    assert!(clipped.1.distance_to(&Coord2(7.0, 9.0)) < 0.01);
+}
+
+#[test]
+fn horizontal_clipped_line() {
+    let line    = (Coord2(-10.0, 4.0), Coord2(20.0, 4.0));
+    let bounds  = (Coord2(1.0, 1.0), Coord2(10.0, 10.0));
+    let clipped = line_clip_to_bounds(&line, &bounds);
+
+    println!("{:?}", clipped);
+    assert!(clipped.is_some());
+
+    let clipped = clipped.unwrap();
+    assert!(clipped.0.distance_to(&Coord2(1.0, 4.0)) < 0.01);
+    assert!(clipped.1.distance_to(&Coord2(10.0, 4.0)) < 0.01);
+}
+
+#[test]
+fn horizontal_clipped_line_inside_to_outside() {
+    let line    = (Coord2(5.0, 4.0), Coord2(20.0, 4.0));
+    let bounds  = (Coord2(1.0, 1.0), Coord2(10.0, 10.0));
+    let clipped = line_clip_to_bounds(&line, &bounds);
+
+    assert!(clipped.is_some());
+
+    let clipped = clipped.unwrap();
+    assert!(clipped.0.distance_to(&Coord2(5.0, 4.0)) < 0.01);
+    assert!(clipped.1.distance_to(&Coord2(10.0, 4.0)) < 0.01);
+}
+
+#[test]
+fn horizontal_clipped_line_inside_to_outside_reverse() {
+    let line    = (Coord2(20.0, 4.0), Coord2(5.0, 4.0));
+    let bounds  = (Coord2(1.0, 1.0), Coord2(10.0, 10.0));
+    let clipped = line_clip_to_bounds(&line, &bounds);
+
+    assert!(clipped.is_some());
+
+    let clipped = clipped.unwrap();
+    assert!(clipped.0.distance_to(&Coord2(5.0, 4.0)) < 0.01);
+    assert!(clipped.1.distance_to(&Coord2(10.0, 4.0)) < 0.01);
+}
+
+#[test]
+fn vertical_clipped_line_inside_to_outside() {
+    let line    = (Coord2(5.0, 4.0), Coord2(4.0, 20.0));
+    let bounds  = (Coord2(1.0, 1.0), Coord2(10.0, 10.0));
+    let clipped = line_clip_to_bounds(&line, &bounds);
+
+    println!("{:?}", clipped);
+    assert!(clipped.is_some());
+
+    let clipped = clipped.unwrap();
+    assert!(clipped.0.distance_to(&Coord2(5.0, 4.0)) < 0.01);
+    assert!(clipped.1.distance_to(&Coord2(4.0, 10.0)) < 0.01);
+}
+
+#[test]
+fn line_out_of_bounds() {
+    let line    = (Coord2(0.0, 9.5), Coord2(20.0, 9.0));
+    let bounds  = (Coord2(1.0, 1.0), Coord2(10.0, 10.0));
+    let clipped = line_clip_to_bounds(&line, &bounds);
+
+    println!("{:?}", clipped);
+
+    assert!(clipped.is_none());
+}
