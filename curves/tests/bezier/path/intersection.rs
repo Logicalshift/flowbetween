a42620f6b5a86b67bcedf3c0f976cfe0930f1fc2
Andrew Hunter <andrew@logicalshift.co.uk>
2018-06-24 11:19:06 +0100

    Oops, calculated angle and expected intersection incorrectly
    New test failure though, and this looks a bit more genuine

--- curves/tests/bezier/path/intersection.rs
+++ curves/tests/bezier/path/intersection.rs
@@ -20,23 +20,26 @@ fn line_intersects_circle() {
 
     for angle in 0..=20 {
         let angle       = angle as f64;
-        let radians     = (2.0*f64::consts::PI)*(20.0/angle);
+        let radians     = (2.0*f64::consts::PI)*(angle/20.0);
 
         let target      = Coord2(radians.sin()*length, radians.cos()*length);
         let target      = target + center;
 
         let expected    = Coord2(radians.sin()*radius, radians.cos()*radius);
-        let expected    = target + center;
+        let expected    = expected + center;
 
         // Should be one intersection with the circle here
         let line            = (center, target);
         let intersection    = path_intersects_line(&circle, &line).collect::<Vec<_>>();
-        println!("{:?}", intersection);
+        println!("{:?} {:?}", intersection, target);
         assert!(intersection.len() == 1);
 
         let intersection    = intersection[0];
         let intersect_point = circle_sections[intersection.0].point_at_pos(intersection.1);
 
+        println!("{:?} {:?}", expected, intersect_point);
         assert!(expected.distance_to(&intersect_point).abs() < 0.01);
     }
+
+    assert!(false);
 }
