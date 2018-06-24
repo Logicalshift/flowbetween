9ad15fa252580849e72668dd8dd34b92e299fa8b
Andrew Hunter <andrew@logicalshift.co.uk>
2018-06-24 16:33:08 +0100

    Corners should be inside the path

--- curves/tests/bezier/path/point.rs
+++ curves/tests/bezier/path/point.rs
@@ -63,9 +63,24 @@ fn point_on_edge_is_in_path() {
         (Coord2(1.0, 6.0), Coord2(1.0, 4.0), Coord2(1.0, 2.0))
     ]);
 
-    // Points on the boundary should be inside
+    // Points just on the boundary should be inside
     assert!(path_contains_point(&path, &Coord2(5.0, 2.0)));
     assert!(path_contains_point(&path, &Coord2(1.0, 4.0)));
+}
+
+#[test]
+fn corner_is_in_path() {
+    // Path is a square
+    let path = (Coord2(1.0, 2.0), vec![
+        (Coord2(3.0, 2.0), Coord2(6.0, 2.0), Coord2(9.0, 2.0)), 
+        (Coord2(9.0, 4.0), Coord2(9.0, 6.0), Coord2(9.0, 8.0)), 
+        (Coord2(6.0, 8.0), Coord2(3.0, 8.0), Coord2(1.0, 8.0)),
+        (Coord2(1.0, 6.0), Coord2(1.0, 4.0), Coord2(1.0, 2.0))
+    ]);
+
+    // Points right on the edge but on the corners are in the path
+    assert!(path_contains_point(&path, &Coord2(1.0, 2.0)));
+    assert!(path_contains_point(&path, &Coord2(9.0, 2.0)));
 }
 
 #[test]
