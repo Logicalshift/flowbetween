c855fcc95ae9a8ee321f6b2c4b5dd92e6eb1c5f7
Andrew Hunter <andrew@logicalshift.co.uk>
2018-06-24 11:03:15 +0100

    Add a 'path intersects line' method
    Want this for a bezier intersection test at the moment

---
+++ curves/src/bezier/path/intersection.rs
@@ -0,0 +1,18 @@
+use super::path::*;
+use super::to_curves::*;
+use super::super::curve::*;
+use super::super::intersection::*;
+use super::super::super::line::*;
+use super::super::super::coordinate::*;
+
+///
+/// Determines the intersections of a path and a line
+/// 
+/// Intersections are returned as the path section index and the 't' parameter along that curve
+/// 
+pub fn path_intersects_line<'a, Path: BezierPath, L: Line<Point=Path::Point>>(path: &Path, line: &'a L) -> impl 'a+Iterator<Item=(usize, f64)> 
+where Path: 'a, Path::Point: 'a+Coordinate2D {
+    path_to_curves::<_, Curve<_>>(path)
+        .enumerate()
+        .flat_map(move |(section_id, curve)| curve_intersects_line(&curve, line).into_iter().map(move |t| (section_id, t)))
+}
--- curves/src/bezier/path/mod.rs
+++ curves/src/bezier/path/mod.rs
@@ -2,8 +2,10 @@ mod path;
 mod to_curves;
 mod point;
 mod bounds;
+mod intersection;
 
 pub use self::path::*;
 pub use self::to_curves::*;
 pub use self::point::*;
 pub use self::bounds::*;
+pub use self::intersection::*;
