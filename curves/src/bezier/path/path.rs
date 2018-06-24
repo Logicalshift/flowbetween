6758f1b5f1f8d922130b7a613b5c24138d0e6073
Andrew Hunter <andrew@logicalshift.co.uk>
2018-06-24 10:47:29 +0100

    Add some of the methods for handling paths to the main path trait
    This allows for things like paths that cache their bounds to increase performance and so on.

--- curves/src/bezier/path/path.rs
+++ curves/src/bezier/path/path.rs
@@ -1,3 +1,6 @@
+use super::bounds::*;
+use super::to_curves::*;
+use super::super::curve::*;
 use super::super::super::geo::*;
 use super::super::super::coordinate::*;
 
@@ -6,7 +9,7 @@ use std::vec;
 ///
 /// Trait representing a path made out of bezier sections
 /// 
-pub trait BezierPath : Geo {
+pub trait BezierPath : Geo+Clone+Sized {
     /// Type of an iterator over the points in this curve. This tuple contains the points ordered as a hull: ie, two control points followed by a point on the curve
     type PointIter: Iterator<Item=(Self::Point, Self::Point, Self::Point)>;
 
@@ -24,6 +27,22 @@ pub trait BezierPath : Geo {
     /// Creates a new instance of this path from a set of points
     /// 
     fn from_points<FromIter: IntoIterator<Item=(Self::Point, Self::Point, Self::Point)>>(start_point: Self::Point, points: FromIter) -> Self;
+
+    ///
+    /// Finds the bounds of this path
+    /// 
+    #[inline]
+    fn bounds(&self) -> (Self::Point, Self::Point) {
+        path_bounds(self)
+    }
+
+    ///
+    /// Changes this path into a set of bezier curves
+    /// 
+    #[inline]
+    fn to_curves<Curve: BezierCurve<Point=Self::Point>>(&self) -> Vec<Curve> {
+        path_to_curves(self).collect()
+    }
 }
 
 impl<Point: Clone+Coordinate> Geo for (Point, Vec<(Point, Point, Point)>) {
