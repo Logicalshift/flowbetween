a801deb63d52275a215f82077a15a6c6aaec06be
Andrew Hunter <andrew@logicalshift.co.uk>
2018-07-01 17:21:41 +0100

    Add a test for the 'get control points' thing in the brush element

--- animation/src/traits/vector/brush_element.rs
+++ animation/src/traits/vector/brush_element.rs
@@ -172,3 +172,47 @@ impl Into<Vector> for BrushElement {
         Vector::BrushStroke(self)
     }
 }
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn fetch_control_points_for_element() {
+        let points = vec![
+            BrushPoint {
+                position: (1.0, 2.0),
+                cp1: (3.0, 4.0),
+                cp2: (5.0, 6.0),
+                width: 0.5
+            },
+
+            BrushPoint {
+                position: (7.0, 8.0),
+                cp1: (9.0, 10.0),
+                cp2: (11.0, 12.0),
+                width: 0.6
+            },
+            
+            BrushPoint {
+                position: (13.0, 14.0),
+                cp1: (15.0, 16.0),
+                cp2: (17.0, 18.0),
+                width: 0.7
+            },
+        ];
+        let element = BrushElement::new(ElementId::Assigned(5), Arc::new(points));
+
+        let control_points = element.control_points();
+
+        assert!(control_points[0] == ControlPoint::BezierPoint(1.0, 2.0));
+        assert!(control_points[1] == ControlPoint::BezierControlPoint(9.0, 10.0));
+        assert!(control_points[2] == ControlPoint::BezierControlPoint(11.0, 12.0));
+        assert!(control_points[3] == ControlPoint::BezierPoint(7.0, 8.0));
+        assert!(control_points[4] == ControlPoint::BezierControlPoint(15.0, 16.0));
+        assert!(control_points[5] == ControlPoint::BezierControlPoint(17.0, 18.0));
+        assert!(control_points[6] == ControlPoint::BezierPoint(13.0, 14.0));
+        assert!(control_points.len() == 7);
+    }
+}
