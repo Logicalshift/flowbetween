060b709a2016c0740c00f93d0b284d127e6f412e
Andrew Hunter <andrew@logicalshift.co.uk>
2018-07-13 15:32:58 +0100

    Add a file model trait for things loaded from files

---
+++ ui_files/src/file_model.rs
@@ -0,0 +1,24 @@
+use std::sync::*;
+use std::path::Path;
+
+///
+/// Trait implemented by model objects that represent open files
+/// 
+pub trait FileModel {
+    /// The part of model that is shared between all open instances of the file
+    type SharedModel;
+
+    /// The model used for each session editing a single file 
+    type InstanceModel;
+
+    ///
+    /// Opens the file found at a particular path, returning the model shared across all instances
+    /// of this file. This is shared across all controllers using the same file.
+    /// 
+    fn open(path: Path) -> Self::SharedModel;
+
+    ///
+    /// Creates a new instance model from the shared model. This is used for a single session.
+    /// 
+    fn new_instance(model: Arc<Self::SharedModel>) -> Self::InstanceModel;
+}
---
+++ ui_files/src/lib.rs
@@ -0,0 +1,3 @@
+extern crate flo_ui;
+
+mod file_model;
