f594c13bc4ee8d98bcff23c571799de6001253db
Andrew Hunter <andrew@logicalshift.co.uk>
2018-07-13 22:27:27 +0100

    Add some tests, get the 'file list path' when querying (as the main path is a complete file path)

--- ui_files/src/sqlite/file_manager.rs
+++ ui_files/src/sqlite/file_manager.rs
@@ -66,6 +66,42 @@ impl SqliteFileManager {
             })
         }
     }
+
+    ///
+    /// Finds the path to request from the file list for a particular file path
+    /// 
+    fn file_list_path(&self, path: &Path) -> Option<PathBuf> {
+        // Construct a path representing where we store our data
+        let mut data_path = self.root_path.clone();
+        data_path.push(DATA_DIR);
+
+        if path.components().count() == 1 && path.is_relative() {
+
+            // A single relative component is left intact
+            path.components().last()
+                .map(|component| component.as_os_str().to_string_lossy())
+                .map(|component| {
+                    let mut buf = PathBuf::new();
+                    buf.push(component.to_string());
+                    buf
+                })
+
+        } else if path.starts_with(data_path) {
+
+            // If the path is in the data path, then use the last component
+            // TODO: this isn't quite right if the path is in a subdirectory
+            path.components().last()
+                .map(|component| component.as_os_str().to_string_lossy())
+                .map(|component| {
+                    let mut buf = PathBuf::new();
+                    buf.push(component.to_string());
+                    buf
+                })
+
+        } else {
+            None
+        }
+    }
 }
 
 impl FileManager for SqliteFileManager {
@@ -89,8 +125,13 @@ impl FileManager for SqliteFileManager {
     /// Returns the display name for a particular path
     /// 
     fn display_name_for_path(&self, path: &Path) -> Option<String> {
-        // TODO: use the path from get_all_files
-        self.core.sync(|core| core.file_list.display_name_for_path(path))
+        let path = self.file_list_path(path);
+
+        if let Some(path) = path {
+            self.core.sync(|core| core.file_list.display_name_for_path(path.as_path()))
+        } else {
+            None
+        }
     }
 
     ///
@@ -120,8 +161,40 @@ impl FileManager for SqliteFileManager {
     /// result)
     ///
     fn set_display_name_for_path(&self, path: &Path, display_name: String) {
-        let path = PathBuf::from(path);
+        let path = self.file_list_path(path);
 
-        self.core.async(move |core| core.file_list.set_display_name_for_path(path.as_path(), &display_name))
+        if let Some(path) = path {
+            self.core.async(move |core| core.file_list.set_display_name_for_path(path.as_path(), &display_name))
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn create_new_path() {
+        let test_files  = SqliteFileManager::new("app.flowbetween.test", "default");
+        let new_path    = test_files.create_new_path();
+
+        assert!(new_path.components().count() > 3);
+    }
+
+    #[test]
+    fn display_name_is_initially_none() {
+        let test_files  = SqliteFileManager::new("app.flowbetween.test", "default");
+        let new_path    = test_files.create_new_path();
+
+        assert!(test_files.display_name_for_path(new_path.as_path()) == None);
+    }
+
+    #[test]
+    fn set_alternative_display_name() {
+        let test_files  = SqliteFileManager::new("app.flowbetween.test", "default");
+        let new_path    = test_files.create_new_path();
+
+        test_files.set_display_name_for_path(new_path.as_path(), "Test display name".to_string());
+        assert!(test_files.display_name_for_path(new_path.as_path()) == Some("Test display name".to_string()));
     }
 }
