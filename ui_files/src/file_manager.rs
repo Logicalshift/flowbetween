f33b9a619eb2bfa746e7d0739abfe050693447ed
Andrew Hunter <andrew@logicalshift.co.uk>
2018-07-13 16:23:52 +0100

    Fix the file manager trait

--- ui_files/src/file_manager.rs
+++ ui_files/src/file_manager.rs
@@ -1,3 +1,6 @@
+use std::rc::*;
+use std::path::Path;
+
 ///
 /// The file manager is used to retrieve what files are available and organize them
 /// 
@@ -5,7 +8,7 @@ pub trait FileManager : Send+Sync {
     ///
     /// Returns a list of all the files that can be opened by this manager
     /// 
-    fn get_all_files(&self) -> Vec<Path>;
+    fn get_all_files(&self) -> Vec<Rc<Path>>;
 
     ///
     /// Returns the display name for a particular path
@@ -13,13 +16,15 @@ pub trait FileManager : Send+Sync {
     fn display_name_for_path(&self, path: Path) -> Option<String>;
 
     ///
-    /// Reserves a path for a new file
+    /// Reserves a path for a new file (this path is valid and won't be re-used by future calls but
+    /// no files will exist here yet)
     /// 
     fn create_new_path(&self) -> Path;
 
     ///
     /// Updates or creates the display name associated with a particular path (which must be
-    /// returned via get_all_files: setting a non-existent)
+    /// returned via get_all_files: setting the name for a non-existent path will just
+    /// result)
     ///
     fn set_display_name_for_path(&self, path: Path, display_name: String);
 }
