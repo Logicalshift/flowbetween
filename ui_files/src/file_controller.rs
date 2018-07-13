cab650079e97d339dde159dde195e27ca98b3116
Andrew Hunter <andrew@logicalshift.co.uk>
2018-07-13 15:56:54 +0100

    Declare a FileController trait for the thing that's created when a file is opened

---
+++ ui_files/src/file_controller.rs
@@ -0,0 +1,16 @@
+use super::file_model::*;
+
+use flo_ui::*;
+
+///
+/// A file controller is a controller that can manage a type of file specified by a file model
+/// 
+pub trait FileController : Controller {
+    /// The model that this controller needs to be constructed
+    type Model: FileModel;
+
+    ///
+    /// Creates this controller with the specified instance model
+    /// 
+    fn open(model: <Self::Model as FileModel>::InstanceModel) -> Self;
+}
--- ui_files/src/lib.rs
+++ ui_files/src/lib.rs
@@ -1,3 +1,7 @@
 extern crate flo_ui;
 
 mod file_model;
+mod file_controller;
+
+pub use self::file_model::*;
+pub use self::file_controller::*;
