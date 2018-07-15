076b9127928bdb75f1c1858f1e4ae9cd30681011
Andrew Hunter <andrew@logicalshift.co.uk>
2018-07-15 22:04:32 +0100

    Add the implementation of the FileChooser trait to FloChooser

--- flo/src/chooser/flo_chooser.rs
+++ flo/src/chooser/flo_chooser.rs
@@ -1,6 +1,10 @@
 use super::consts::*;
+use super::super::model::*;
+use super::super::editor::*;
 
+use flo_animation::*;
 use flo_ui_files::*;
+use flo_ui_files::ui::*;
 use flo_ui_files::sqlite::*;
 
 use std::sync::*;
@@ -8,30 +12,36 @@ use std::sync::*;
 ///
 /// The default file chooser for FlowBetween
 /// 
-pub struct FloChooser {
+pub struct FloChooser<Anim: 'static+FileAnimation> {
     /// The file manager managed by this chooser
     file_manager: Arc<SqliteFileManager>,
+
+    /// The shared open file store for this animation
+    file_store: Arc<OpenFileStore<FloSharedModel<Anim>>>
 }
 
-impl FloChooser {
+impl<Anim: 'static+FileAnimation> FloChooser<Anim> {
     ///
     /// Creates a new chooser
     /// 
-    pub fn new() -> FloChooser {
+    pub fn new() -> FloChooser<Anim> {
         // Create the file manager (we use a single default user by default)
         let file_manager = Arc::new(SqliteFileManager::new(APP_NAME, DEFAULT_USER_FOLDER));
 
+        // Create the file store
+        let file_store = Arc::new(OpenFileStore::new());
+
         // Put everything together
         FloChooser {
-            file_manager: file_manager
+            file_manager:   file_manager,
+            file_store:     file_store
         }
     }
 }
 
-/*
-impl FileChooser for FloChooser {
+impl<Anim: 'static+EditableAnimation+FileAnimation> FileChooser for FloChooser<Anim> {
     /// The controller that edits/displays open files
-    type Controller: FileController;
+    type Controller = EditorController<Anim>;
 
     /// The file manager that finds paths where files can be located
     type FileManager = SqliteFileManager;
@@ -46,8 +56,7 @@ impl FileChooser for FloChooser {
     ///
     /// Retrieves the shared file store for this chooser
     /// 
-    fn get_file_store(&self) -> Arc<OpenFileStore<<Self::Controller as FileController>::Model>> {
-
+    fn get_file_store(&self) -> Arc<OpenFileStore<FloSharedModel<Anim>>> {
+        Arc::clone(&self.file_store)
     }
 }
-*/
