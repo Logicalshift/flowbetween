258526db6ead4007f0c811364ab53404ccbeb277
Andrew Hunter <andrew@logicalshift.co.uk>
2018-07-15 11:29:52 +0100

    Add a controller sub-trait that can be used for things that need to be able to create a controller
    (Eg, things that take a controller as a type parameter)

---
+++ ui/src/controllers/createable_controller.rs
@@ -0,0 +1,11 @@
+use super::super::controller::*;
+
+///
+/// Controller with a factory method
+/// 
+pub trait CreateableController : Controller {
+    ///
+    /// Creates a new instance of this controller
+    /// 
+    fn new() -> Self;
+}
--- ui/src/controllers/mod.rs
+++ ui/src/controllers/mod.rs
@@ -1,5 +1,7 @@
 mod popup_controller;
 mod virtual_canvas;
+mod createable_controller;
 
 pub use self::popup_controller::*;
 pub use self::virtual_canvas::*;
+pub use self::createable_controller::*;
