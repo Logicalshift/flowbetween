d36a4f6b73c4dc3f37e114c7e293dbda5479c1a4
Andrew Hunter <andrew@logicalshift.co.uk>
2018-07-29 17:56:40 +0100

    Test that we generate the attributes right for the container we're generating
    ... we are...

--- http_ui/src/htmlcontrol.rs
+++ http_ui/src/htmlcontrol.rs
@@ -303,6 +303,16 @@ mod test {
     }
 
     #[test]
+    fn can_combine_background_scroll_fix_and_z_index() {
+        let ctrl = Control::container()
+            .with(Appearance::Background(Color::Rgba(0.0, 1.0, 0.0, 1.0)))
+            .with(ControlAttribute::ZIndex(5))
+            .with(Scroll::Fix(FixedAxis::Horizontal));
+
+        assert!(ctrl.to_html("").to_string() == "<flo-container style=\"background-color: rgba(0, 255, 0, 1); z-index: 5;\" flo-scroll-fix=\"horiz\"></flo-container>");
+    }
+
+    #[test]
     fn can_convert_container_to_html() {
         assert!(Control::container().with(vec![Control::button()]).to_html("").to_string() == "<flo-container><flo-button></flo-button></flo-container>")
     }
