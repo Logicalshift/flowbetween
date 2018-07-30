240a2778dd8c1b3f0af6741d1d846f7935f47bc1
Andrew Hunter <andrew@logicalshift.co.uk>
2018-07-30 21:42:16 +0100

    Add a basic '+' and '-' control to the layer controls editor

--- flo/src/editor/timeline_layer_controls_controller.rs
+++ flo/src/editor/timeline_layer_controls_controller.rs
@@ -9,30 +9,63 @@ use flo_animation::*;
 /// Controller that provides controls for adding/deleting/editing layers (generally displayed above the main layer list)
 /// 
 pub struct TimelineLayerControlsController {
-
+    /// the UI for this controller
+    ui: BindRef<Control>
 }
 
 impl TimelineLayerControlsController {
     ///
     /// Creates a new timeline layer controls controller
     /// 
-    pub fn new<Anim: 'static+Animation>(model: &FloModel<Anim>) -> TimelineLayerControlsController {
-        TimelineLayerControlsController {
+    pub fn new<Anim: 'static+Animation>(_model: &FloModel<Anim>) -> TimelineLayerControlsController {
+        let ui = Self::ui();
 
+        TimelineLayerControlsController {
+            ui: ui
         }
+    }
+
+    ///
+    /// Creates the UI for the layer controls controller
+    /// 
+    fn ui() -> BindRef<Control> {
+        // Create the UI
+        let ui = computed(move || {
+            Control::container()
+                .with(Bounds::fill_all())
+                .with(vec![
+                    Control::container()
+                        .with(Font::Size(13.0))
+                        .with(Font::Weight(FontWeight::ExtraBold))
+                        .with(ControlAttribute::Padding((4, 1), (4, 1)))
+                        .with(vec![
+                            Control::empty()
+                                .with(Bounds::stretch_horiz(1.0)),
+                            Control::label()
+                                .with(Bounds::next_horiz(14.0))
+                                .with(TextAlign::Center)
+                                .with((ActionTrigger::Click, "AddNewLayer"))
+                                .with("+"),
+                            Control::label()
+                                .with(Bounds::next_horiz(14.0))
+                                .with(TextAlign::Center)
+                                .with((ActionTrigger::Click, "RemoveLayer"))
+                                .with("-"),
+                        ])
+                        .with(Bounds::stretch_vert(1.0)),
+                    Control::empty()
+                        .with(Appearance::Background(TIMESCALE_BORDER))
+                        .with(Bounds::next_vert(1.0))
+                ])
+        });
+
+        // Turn into a bindref
+        BindRef::from(ui)
     }
 }
 
 impl Controller for TimelineLayerControlsController {
     fn ui(&self) -> BindRef<Control> {
-        BindRef::from(bind(Control::container()
-            .with(Bounds::fill_all())
-            .with(vec![
-                Control::empty()
-                    .with(Bounds::stretch_vert(1.0)),
-                Control::empty()
-                    .with(Appearance::Background(TIMESCALE_BORDER))
-                    .with(Bounds::next_vert(1.0))
-            ])))
+        BindRef::clone(&self.ui)
     }
 }
