76170f8391d1885af828d172d450db7fb108028c
Andrew Hunter <andrew@logicalshift.co.uk>
2018-08-05 17:56:03 +0100

    Finish up the blocking publisher implementation

--- stream/src/blocking_publisher.rs
+++ stream/src/blocking_publisher.rs
@@ -1,15 +1,122 @@
 use super::publisher::*;
+use super::subscriber::*;
+use super::publisher_sink::*;
+
+use futures::*;
+use futures::task;
+use futures::task::Task;
+use futures::future;
+use futures::future::Either;
+use futures::sync::oneshot;
 
 ///
 /// A blocking publisher is a publisher that blocks messages until it has enough subscribers
 /// 
 /// This is useful for cases where a publisher is being used asynchronously and wants to ensure that
-/// its messages are sent to at least one subscriber
+/// its messages are sent to at least one subscriber. Once the required number of subscribers is
+/// reached, this will not block again even if some subscribers are dropped.
 /// 
 pub struct BlockingPublisher<Message> {
+    /// True if there are not currently enouogh subscribers in this publisher
+    insufficient_subscribers: bool,
+
     /// The number of required subscribers
     required_subscribers: usize,
 
     /// The publisher where messages will be relayed
-    publisher: Publisher<Message>
+    publisher: Publisher<Message>,
+
+    /// Notification to be sent when there are enough subscribers in this publisher
+    notify_full: Option<Task>,
+
+    /// Futures to be notified when there are enough subscribers for this publisher
+    notify_futures: Vec<oneshot::Sender<()>>
+}
+
+impl<Message: Clone> BlockingPublisher<Message> {
+    ///
+    /// Creates a new blocking publisher
+    /// 
+    /// This publisher will refuse to receive any items until at least required_subscribers are connected.
+    /// The buffer size indicates the number of queued items permitted per buffer.
+    /// 
+    pub fn new(required_subscribers: usize, buffer_size: usize) -> BlockingPublisher<Message> {
+        BlockingPublisher {
+            insufficient_subscribers:   required_subscribers != 0,
+            required_subscribers:       required_subscribers,
+            publisher:                  Publisher::new(buffer_size),
+            notify_full:                None,
+            notify_futures:             vec![]
+        }
+    }
+
+    ///
+    /// Returns a future that will complete when this publisher has enough subscribers
+    /// 
+    /// This is useful as a way to avoid blocking with `wait_send` when setting up the publisher
+    /// 
+    pub fn when_ready(&mut self) -> impl Future<Item=(), Error=Canceled> {
+        if self.insufficient_subscribers {
+            // Return a future that will be notified when we have enough subscribers
+            let (sender, receiver) = oneshot::channel();
+
+            // Notify when there are enough subscribers
+            self.notify_futures.push(sender);
+
+            Either::A(receiver)
+        } else {
+            // Already ready
+            Either::B(future::ok(()))
+        }
+    }
+}
+
+impl<Message: Clone> PublisherSink<Message> for BlockingPublisher<Message> {
+    fn subscribe(&mut self) -> Subscriber<Message> {
+        // Create the subscription
+        let subscription = self.publisher.subscribe();
+
+        // Wake the sink if we get enough subscribers
+        if self.insufficient_subscribers && self.publisher.count_subscribers() >= self.required_subscribers {
+            // We now have enough subscribers
+            self.insufficient_subscribers = false;
+            
+            // Notify anything that is blocking on this publisher
+            self.notify_full.take().map(|notify| notify.notify());
+
+            // Mark any futures that are waiting on this publisher
+            self.notify_futures.drain(..)
+                .for_each(|sender| { sender.send(()).ok(); });
+        }
+
+        // Result is our new subscription
+        subscription
+    }
+}
+
+impl<Message: Clone> Sink for BlockingPublisher<Message> {
+    type SinkItem   = Message;
+    type SinkError  = ();
+
+    fn start_send(&mut self, item: Message) -> StartSend<Message, ()> {
+        if self.insufficient_subscribers {
+            // Not enough subscribers, so refuse to send
+            self.notify_full = Some(task::current());
+            Ok(AsyncSink::NotReady(item))
+        } else {
+            // Have reached the required number of subscribers, so pass through
+            self.publisher.start_send(item)
+        }
+    }
+
+    fn poll_complete(&mut self) -> Poll<(), ()> {
+        if self.insufficient_subscribers {
+            // Not enough subscribers, so refuse to send
+            self.notify_full = Some(task::current());
+            Ok(Async::NotReady)
+        } else {
+            // Have reached the required number of subscribers, so pass through to the main publisher
+            self.publisher.poll_complete()
+        }
+    }
 }
---
+++ stream/tests/blocking.rs
@@ -0,0 +1,107 @@
+extern crate futures;
+extern crate flo_stream;
+
+use flo_stream::*;
+
+use futures::*;
+use futures::executor;
+use futures::executor::{Notify, NotifyHandle};
+
+use std::thread;
+use std::sync::*;
+use std::time::Duration;
+
+#[derive(Clone)]
+struct NotifyNothing;
+
+impl Notify for NotifyNothing {
+    fn notify(&self, _: usize) { }
+}
+
+#[test]
+pub fn blocks_if_there_are_no_subscribers() {
+    let publisher       = BlockingPublisher::new(2, 10);
+    let mut publisher   = executor::spawn(publisher);
+
+    assert!(publisher.start_send_notify(1, &NotifyHandle::from(&NotifyNothing), 0) == Ok(AsyncSink::NotReady(1)));
+}
+
+#[test]
+pub fn blocks_if_there_are_insufficient_subscribers() {
+    let publisher       = BlockingPublisher::new(2, 10);
+    let mut publisher   = executor::spawn(publisher);
+    let _subscriber     = publisher.get_mut().subscribe();
+
+    assert!(publisher.start_send_notify(1, &NotifyHandle::from(&NotifyNothing), 0) == Ok(AsyncSink::NotReady(1)));
+}
+
+#[test]
+pub fn unblocks_if_there_are_sufficient_subscribers() {
+    let publisher       = BlockingPublisher::new(2, 10);
+    let mut publisher   = executor::spawn(publisher);
+    let _subscriber1    = publisher.get_mut().subscribe();
+    let _subscriber2    = publisher.get_mut().subscribe();
+
+    assert!(publisher.start_send_notify(1, &NotifyHandle::from(&NotifyNothing), 0) == Ok(AsyncSink::Ready));
+}
+
+#[test]
+pub fn read_from_subscribers() {
+    let publisher       = BlockingPublisher::new(2, 10);
+    let mut publisher   = executor::spawn(publisher);
+    let subscriber1     = publisher.get_mut().subscribe();
+    let subscriber2     = publisher.get_mut().subscribe();
+
+    let mut subscriber1 = executor::spawn(subscriber1);
+    let mut subscriber2 = executor::spawn(subscriber2);
+
+    publisher.wait_send(1).unwrap();
+    publisher.wait_send(2).unwrap();
+    publisher.wait_send(3).unwrap();
+
+    assert!(subscriber1.wait_stream() == Some(Ok(1)));
+    assert!(subscriber1.wait_stream() == Some(Ok(2)));
+    assert!(subscriber1.wait_stream() == Some(Ok(3)));
+
+    assert!(subscriber2.wait_stream() == Some(Ok(1)));
+    assert!(subscriber2.wait_stream() == Some(Ok(2)));
+    assert!(subscriber2.wait_stream() == Some(Ok(3)));
+}
+
+#[test]
+pub fn read_from_thread() {
+    let subscriber = {
+        // Create a shared publisher
+        let publisher = BlockingPublisher::new(1, 1);
+        let publisher = executor::spawn(publisher);
+        let publisher = Arc::new(Mutex::new(publisher));
+
+        // Create a thread to publish some values
+        let thread_publisher = publisher.clone();
+        thread::spawn(move || {
+            let wait_for_subscribers = thread_publisher.lock().unwrap().get_mut().when_ready();
+            executor::spawn(wait_for_subscribers).wait_future().unwrap();
+
+            thread_publisher.lock().unwrap().wait_send(1).unwrap();
+            thread_publisher.lock().unwrap().wait_send(2).unwrap();
+            thread_publisher.lock().unwrap().wait_send(3).unwrap();
+        });
+
+        // Pause for a bit to let the thread get ahead of us
+        thread::sleep(Duration::from_millis(20));
+
+        // Subscribe to the thread (which should now wake up)
+        let subscriber = publisher.lock().unwrap().get_mut().subscribe();
+        subscriber
+    };
+
+    let mut subscriber = executor::spawn(subscriber);
+
+    // Should receive the values from the thread
+    assert!(subscriber.wait_stream() == Some(Ok(1)));
+    assert!(subscriber.wait_stream() == Some(Ok(2)));
+    assert!(subscriber.wait_stream() == Some(Ok(3)));
+
+    // As we don't retain the publisher, the thread is its only owner. When it finishes, the stream should close.
+    assert!(subscriber.wait_stream() == None);
+}
