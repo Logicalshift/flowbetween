4e4a568202f27a5f128551447664950b179d3138
Andrew Hunter <andrew@logicalshift.co.uk>
2018-08-05 10:42:04 +0100

    Start work on a library to contain the various implementation of stream and sink

---
+++ stream/src/lib.rs
@@ -0,0 +1,8 @@
+//!
+//! flo_stream provides types of stream used by FlowBetween, most importantly an implementation of the pubsub pattern
+//! 
+
+extern crate futures;
+
+mod publisher;
+mod subscriber;
