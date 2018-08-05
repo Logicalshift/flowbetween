4c959b009921916f842627c0aae1c5862df0e78a
Andrew Hunter <andrew@logicalshift.co.uk>
2018-08-05 15:21:44 +0100

    Basic implementation of the poll() function for the subscriber

--- stream/src/subscriber.rs
+++ stream/src/subscriber.rs
@@ -1,5 +1,8 @@
 use super::pubsub_core::*;
 
+use futures::*;
+use futures::task;
+
 use std::sync::*;
 
 ///
@@ -25,6 +28,31 @@ impl<Message> Subscriber<Message> {
         Subscriber {
             pub_core,
             sub_core
+        }
+    }
+}
+
+impl<Message> Stream for Subscriber<Message> {
+    type Item   = Message;
+    type Error  = ();
+
+    fn poll(&mut self) -> Poll<Option<Message>, ()> {
+        // TODO: notify the publisher if necessary
+
+        // Try to read a message from the waiting list
+        let mut sub_core    = self.sub_core.lock().unwrap();
+        let next_message    = sub_core.waiting.pop_front();
+
+        if let Some(next_message) = next_message {
+            // Return the next message if it's available
+            Ok(Async::Ready(Some(next_message)))
+        } else if self.pub_core.upgrade().is_none() {
+            // Stream has finished if the publisher core is no longer available
+            Ok(Async::Ready(None))
+        } else {
+            // If the publisher is still alive and there are no messages available, store notification and carry on
+            sub_core.notify_waiting = Some(task::current());
+            Ok(Async::NotReady)
         }
     }
 }
