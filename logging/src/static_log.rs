480efb9f6e40413863db9c121e6b27a32f1b43b2
Andrew Hunter <andrew@logicalshift.co.uk>
2018-08-16 20:14:01 +0100

    First pass at a proper logger: call println

--- logging/src/static_log.rs
+++ logging/src/static_log.rs
@@ -1,9 +1,17 @@
+use super::message::*;
 use super::publisher::*;
 
+use desync::*;
+
 use std::cell::*;
+use std::sync::*;
 
 thread_local! {
     static THREAD_LOGGER: RefCell<Option<LogPublisher>> = RefCell::new(None);
+}
+
+lazy_static! {
+    static ref PRINTLN_LOGGER: Arc<Desync<()>> = Arc::new(Desync::new(()));
 }
 
 ///
@@ -18,7 +26,12 @@ pub fn log() -> LogPublisher {
             logger.as_ref().unwrap().clone()
         } else {
             // New logger
-            *logger = Some(LogPublisher::new());
+            let new_logger = LogPublisher::new();
+            
+            // Default to printing the message (via the println logger desync object)
+            pipe_in(Arc::clone(&PRINTLN_LOGGER), new_logger.subscribe_default(), |_, log| { log.map(|log| println!("{}", log.message())).ok(); });
+
+            *logger = Some(new_logger);
             logger.as_ref().unwrap().clone()
         }
     })
