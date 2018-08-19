570ac82542d395db0b7208dbef6cc8fcd661dc7f
Andrew Hunter <andrew@logicalshift.co.uk>
2018-08-19 15:13:14 +0100

    Add a conversion to/from the standard 'log' format for rust for our log levels

--- logging/src/level.rs
+++ logging/src/level.rs
@@ -1,3 +1,5 @@
+use log;
+
 ///
 /// Indicates the verbosity level of a log message
 /// 
@@ -9,4 +11,17 @@ pub enum LogLevel {
     Warning,
     Error,
     Critical
+}
+
+impl Into<log::Level> for LogLevel {
+    fn into(self) -> log::Level {
+        match self {
+            LogLevel::Debug     => log::Level::Trace,
+            LogLevel::Verbose   => log::Level::Debug,
+            LogLevel::Info      => log::Level::Info,
+            LogLevel::Warning   => log::Level::Warn,
+            LogLevel::Error     => log::Level::Error,
+            LogLevel::Critical  => log::Level::Error
+        }
+    }
 }
