fa94042e7ebfef403c4c0954491f1fd4bd72e64d
Andrew Hunter <andrew@logicalshift.co.uk>
2018-08-19 14:25:41 +0100

    Add a 'core' logger that logs everything for the application

--- logging/src/static_log.rs
+++ logging/src/static_log.rs
@@ -1,11 +1,29 @@
 use super::publisher::*;
 use super::log_subscriber::*;
 
-use std::thread;
 use std::cell::*;
 
 thread_local! {
     static THREAD_LOGGER: RefCell<Option<LogPublisher>> = RefCell::new(None);
+}
+
+lazy_static! {
+    static ref CORE_LOGGER: LogPublisher = LogPublisher::new_core_publisher();
+}
+
+impl LogPublisher {
+    ///
+    /// Creates a new 'core' log publisher
+    /// 
+    fn new_core_publisher() -> LogPublisher {
+        // Create a new logger
+        let new_logger = LogPublisher::new_empty(vec![("target", "flo_logger::core")]);
+
+        // Default messages here get the default behaviour
+        send_to_logs(new_logger.subscribe_default());
+
+        new_logger
+    }
 }
 
 ///
@@ -19,13 +37,10 @@ pub fn current_log() -> LogPublisher {
             // Existing logger
             logger.as_ref().unwrap().clone()
         } else {
-            // New logger
-            let new_logger = LogPublisher::new_empty(vec![("target", &format!("{:?}", thread::current().id()) as &str )]);
-            
-            // Default to printing the message (via the println logger desync object)
-            send_to_logs(new_logger.subscribe_default());
+            // Start with the core logger
+            let core_logger = CORE_LOGGER.clone();
+            *logger = Some(core_logger);
 
-            *logger = Some(new_logger);
             logger.as_ref().unwrap().clone()
         }
     })
