ee353abd0a78ec7af9e2ddb702796bd83f7185f8
Andrew Hunter <andrew@logicalshift.co.uk>
2018-08-19 22:25:32 +0100

    Add a test if bounding boxes overlap

--- curves/src/geo/bounding_box.rs
+++ curves/src/geo/bounding_box.rs
@@ -47,6 +47,21 @@ pub trait BoundingBox : Geo+Sized {
             Self::from_min_max(Self::Point::from_smallest_components(self.min(), target.min()), Self::Point::from_biggest_components(self.max(), target.max()))
         }
     }
+
+    ///
+    /// Returns true if this bounding box overlaps another
+    /// 
+    fn overlaps(&self, target: &Self) -> bool {
+        let (min1, max1) = (self.min(), self.max());
+        let (min2, max2) = (target.min(), target.max());
+
+        for p_index in 0..Self::Point::len() {
+            if min1.get(p_index) >= max2.get(p_index) { return false; }
+            if min2.get(p_index) >= max1.get(p_index) { return false; }
+        }
+
+        true
+    }
 }
 
 ///
---
+++ curves/tests/bounds.rs
@@ -0,0 +1,43 @@
+extern crate flo_curves;
+
+use flo_curves::*;
+
+#[test]
+fn overlapping_rects() {
+    let r1 = (Coord2(30.0, 30.0), Coord2(60.0, 40.0));
+    let r2 = (Coord2(20.0, 25.0), Coord2(35.0, 35.0));
+
+    assert!(r1.overlaps(&r2));
+}
+
+#[test]
+fn non_overlapping_rects() {
+    let r1 = (Coord2(30.0, 30.0), Coord2(60.0, 40.0));
+    let r2 = (Coord2(20.0, 25.0), Coord2(9.0, 10.0));
+
+    assert!(!r1.overlaps(&r2));
+}
+
+#[test]
+fn touching_rects() {
+    let r1 = (Coord2(30.0, 30.0), Coord2(60.0, 40.0));
+    let r2 = (Coord2(20.0, 25.0), Coord2(30.0, 30.0));
+
+    assert!(!r1.overlaps(&r2));
+}
+
+#[test]
+fn overlap_interior_rect() {
+    let r1 = (Coord2(30.0, 30.0), Coord2(60.0, 50.0));
+    let r2 = (Coord2(35.0, 35.0), Coord2(55.0, 45.0));
+
+    assert!(r1.overlaps(&r2));
+}
+
+#[test]
+fn overlap_exterior_rect() {
+    let r1 = (Coord2(30.0, 30.0), Coord2(60.0, 40.0));
+    let r2 = (Coord2(20.0, 20.0), Coord2(70.0, 50.0));
+
+    assert!(r1.overlaps(&r2));
+}
