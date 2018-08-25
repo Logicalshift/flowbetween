50315008080512d27de5f31750c09e80115d01fc
Andrew Hunter <andrew@logicalshift.co.uk>
2018-08-25 20:59:00 +0100

    Add an empty file for path arithmetic

--- curves/src/bezier/path/mod.rs
+++ curves/src/bezier/path/mod.rs
@@ -3,9 +3,11 @@ mod to_curves;
 mod point;
 mod bounds;
 mod intersection;
+mod arithmetic;
 
 pub use self::path::*;
 pub use self::to_curves::*;
 pub use self::point::*;
 pub use self::bounds::*;
 pub use self::intersection::*;
+pub use self::arithmetic::*;
