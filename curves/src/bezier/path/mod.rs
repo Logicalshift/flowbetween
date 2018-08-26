a3899804ba208325d87e0166787018a6dcb16f64
Andrew Hunter <andrew@logicalshift.co.uk>
2018-08-26 16:43:39 +0100

    Rename arithmetic to graph_path
    Looks like just building this graph is kind of its own thing

--- curves/src/bezier/path/mod.rs
+++ curves/src/bezier/path/mod.rs
@@ -3,11 +3,11 @@ mod to_curves;
 mod point;
 mod bounds;
 mod intersection;
-mod arithmetic;
+mod graph_path;
 
 pub use self::path::*;
 pub use self::to_curves::*;
 pub use self::point::*;
 pub use self::bounds::*;
 pub use self::intersection::*;
-pub use self::arithmetic::*;
+pub use self::graph_path::*;
