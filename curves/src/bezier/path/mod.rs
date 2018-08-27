b7baaa89c4c9af2b1da6c26a8213bbd9364924fa
Andrew Hunter <andrew@logicalshift.co.uk>
2018-08-27 14:58:40 +0100

    Add a path builder class to help with creating more complex bezier paths

--- curves/src/bezier/path/mod.rs
+++ curves/src/bezier/path/mod.rs
@@ -3,6 +3,7 @@ mod to_curves;
 mod point;
 mod bounds;
 mod intersection;
+mod path_builder;
 mod graph_path;
 
 pub use self::path::*;
@@ -10,4 +11,5 @@ pub use self::to_curves::*;
 pub use self::point::*;
 pub use self::bounds::*;
 pub use self::intersection::*;
+pub use self::path_builder::*;
 pub use self::graph_path::*;
---
+++ curves/src/bezier/path/path_builder.rs
@@ -0,0 +1,56 @@
+use super::path::*;
+
+///
+/// Used to build a bezier path
+/// 
+pub struct BezierPathBuilder<P: BezierPath> {
+    /// Where the path starts
+    start_point: P::Point,
+
+    /// The points in the path
+    points: Vec<(P::Point, P::Point, P::Point)>
+}
+
+impl<P: BezierPath> BezierPathBuilder<P> {
+    ///
+    /// Creates a new bezier path builder with the specified start point
+    /// 
+    pub fn start(start: P::Point) -> BezierPathBuilder<P> {
+        BezierPathBuilder {
+            start_point:    start,
+            points:         vec![]
+        }
+    }
+
+    ///
+    /// Builds the path for this builder
+    /// 
+    pub fn build(self) -> P {
+        P::from_points(self.start_point, self.points)
+    }
+
+    ///
+    /// Adds a line to the specified point
+    /// 
+    pub fn line_to(&mut self, point: P::Point) {
+        // Get the vector from the last point to the new point
+        let distance = if self.points.len() == 0 {
+            point - self.start_point
+        } else {
+            point - self.points[self.points.len()-1].2
+        };
+
+        // A line puts control points at 33% and 66% of the distance
+        let cp1 = point - (distance*0.6666);
+        let cp2 = point - (distance*0.3333);
+
+        self.points.push((cp1, cp2, point));
+    }
+
+    ///
+    /// Adds a curve to a particular point
+    /// 
+    pub fn curve_to(&mut self, (cp1, cp2): (P::Point, P::Point), end_point: P::Point) {
+        self.points.push((cp1, cp2, end_point));
+    }
+}
