58f53d7e60e6e90d230baf657c8248cc112dcab3
Andrew Hunter <andrew@logicalshift.co.uk>
2018-09-09 15:49:53 +0100

    Add tests for cases where the collision involves a point sitting on an existing edge

--- curves/tests/bezier/path/graph_path.rs
+++ curves/tests/bezier/path/graph_path.rs
@@ -336,3 +336,175 @@ fn collision_at_same_point() {
 
     assert!(num_intersects == 4);
 }
+
+#[test]
+fn collision_exactly_on_edge_src() {
+    // Two rectangles, with the collision point already subdivided
+    let rectangle1 = BezierPathBuilder::<SimpleBezierPath>::start(Coord2(1.0, 1.0))
+        .line_to(Coord2(1.0, 5.0))
+        .line_to(Coord2(5.0, 5.0))
+        .line_to(Coord2(5.0, 1.0))
+        .line_to(Coord2(2.0, 1.0))
+        .line_to(Coord2(1.0, 1.0))
+        .build();
+    let rectangle2 = BezierPathBuilder::<SimpleBezierPath>::start(Coord2(4.0, 0.0))
+        .line_to(Coord2(4.0, 6.0))
+        .line_to(Coord2(2.0, 6.0))
+        .line_to(Coord2(2.0, 0.0))
+        .line_to(Coord2(4.0, 0.0))
+        .build();
+    
+    let rectangle1 = GraphPath::from_path(&rectangle1);
+    let rectangle2 = GraphPath::from_path(&rectangle2);
+
+    // Collide them
+    // TODO: find out why setting accuracy to 0.01 here produces only 10 points in the collision
+    let collision = rectangle1.collide(rectangle2, 0.05);
+
+    // 12 points in the collision (but we can allow for the shared point to be left as 'orphaned')
+    assert!(collision.num_points() == 12 || collision.num_points() == 13);
+
+    // If there are 13 points, one should have no edges any more (as another should have been chosen as the shared point)
+    if collision.num_points() == 13 {
+        let mut num_orphaned_points = 0;
+        for point_idx in 0..13 {
+            let edges = collision.edges(point_idx).collect::<Vec<_>>();
+            if edges.len() == 0 { num_orphaned_points += 1; }
+        }
+
+        assert!(num_orphaned_points <= 1);
+    }
+
+    // Check the intersection points
+    let mut num_intersects = 0;
+    for point_idx in 0..collision.num_points() {
+        let edges = collision.edges(point_idx).collect::<Vec<_>>();
+
+        if edges.len() == 2 {
+            num_intersects += 1;
+
+            if edges[0].start_point().distance_to(&Coord2(2.0, 1.0)) < 0.1 {
+                assert!(edges.iter().any(|edge| edge.end_point().distance_to(&Coord2(2.0, 0.0)) < 0.1));
+                assert!(edges.iter().any(|edge| edge.end_point().distance_to(&Coord2(1.0, 1.0)) < 0.1));
+            } else if edges[0].start_point().distance_to(&Coord2(4.0, 1.0)) < 0.1 {
+                assert!(edges.iter().any(|edge| edge.end_point().distance_to(&Coord2(2.0, 1.0)) < 0.1));
+                assert!(edges.iter().any(|edge| edge.end_point().distance_to(&Coord2(4.0, 5.0)) < 0.1));
+            } else if edges[0].start_point().distance_to(&Coord2(2.0, 5.0)) < 0.1 {
+                assert!(edges.iter().any(|edge| edge.end_point().distance_to(&Coord2(2.0, 1.0)) < 0.1));
+                assert!(edges.iter().any(|edge| edge.end_point().distance_to(&Coord2(4.0, 5.0)) < 0.1));
+            } else if edges[0].start_point().distance_to(&Coord2(4.0, 5.0)) < 0.1 {
+                assert!(edges.iter().any(|edge| edge.end_point().distance_to(&Coord2(5.0, 5.0)) < 0.1));
+                assert!(edges.iter().any(|edge| edge.end_point().distance_to(&Coord2(4.0, 6.0)) < 0.1));
+            } else {
+                // These are the only four intersection points that should exist
+                println!("{:?}", edges[0].start_point());
+                assert!(false)
+            }
+        } else if edges.len() == 1 {
+            let edge        = edges.iter().nth(0).unwrap();
+            let start_point = edge.start_point();
+
+            assert!((start_point.x()-1.0).abs() < 0.01 ||
+                    (start_point.x()-5.0).abs() < 0.01 ||
+                    (start_point.x()-2.0).abs() < 0.01 ||
+                    (start_point.x()-4.0).abs() < 0.01);
+            assert!((start_point.y()-1.0).abs() < 0.01 ||
+                    (start_point.y()-5.0).abs() < 0.01 ||
+                    (start_point.y()-0.0).abs() < 0.01 ||
+                    (start_point.y()-6.0).abs() < 0.01);
+        } else {
+            // Should only be 1 edge (corners) or 2 edges (collision points)
+            // TODO: currently we end up here because we're generating point -> same point 'edges' for some reason
+            println!("{:?}", edges);
+            assert!(edges.len() <= 2);
+        }
+    }
+
+    assert!(num_intersects == 4);
+}
+
+#[test]
+fn collision_exactly_on_edge_tgt() {
+    // Two rectangles, with the collision point already subdivided
+    let rectangle1 = BezierPathBuilder::<SimpleBezierPath>::start(Coord2(1.0, 1.0))
+        .line_to(Coord2(1.0, 5.0))
+        .line_to(Coord2(5.0, 5.0))
+        .line_to(Coord2(5.0, 1.0))
+        .line_to(Coord2(1.0, 1.0))
+        .build();
+    let rectangle2 = BezierPathBuilder::<SimpleBezierPath>::start(Coord2(4.0, 0.0))
+        .line_to(Coord2(4.0, 6.0))
+        .line_to(Coord2(2.0, 6.0))
+        .line_to(Coord2(2.0, 1.0))
+        .line_to(Coord2(2.0, 0.0))
+        .line_to(Coord2(4.0, 0.0))
+        .build();
+    
+    let rectangle1 = GraphPath::from_path(&rectangle1);
+    let rectangle2 = GraphPath::from_path(&rectangle2);
+
+    // Collide them
+    // TODO: find out why setting accuracy to 0.01 here produces only 10 points in the collision
+    let collision = rectangle1.collide(rectangle2, 0.05);
+
+    // 12 points in the collision (but we can allow for the shared point to be left as 'orphaned')
+    assert!(collision.num_points() == 12 || collision.num_points() == 13);
+
+    // If there are 13 points, one should have no edges any more (as another should have been chosen as the shared point)
+    if collision.num_points() == 13 {
+        let mut num_orphaned_points = 0;
+        for point_idx in 0..13 {
+            let edges = collision.edges(point_idx).collect::<Vec<_>>();
+            if edges.len() == 0 { num_orphaned_points += 1; }
+        }
+
+        assert!(num_orphaned_points <= 1);
+    }
+
+    // Check the intersection points
+    let mut num_intersects = 0;
+    for point_idx in 0..collision.num_points() {
+        let edges = collision.edges(point_idx).collect::<Vec<_>>();
+
+        if edges.len() == 2 {
+            num_intersects += 1;
+
+            if edges[0].start_point().distance_to(&Coord2(2.0, 1.0)) < 0.1 {
+                assert!(edges.iter().any(|edge| edge.end_point().distance_to(&Coord2(2.0, 0.0)) < 0.1));
+                assert!(edges.iter().any(|edge| edge.end_point().distance_to(&Coord2(1.0, 1.0)) < 0.1));
+            } else if edges[0].start_point().distance_to(&Coord2(4.0, 1.0)) < 0.1 {
+                assert!(edges.iter().any(|edge| edge.end_point().distance_to(&Coord2(2.0, 1.0)) < 0.1));
+                assert!(edges.iter().any(|edge| edge.end_point().distance_to(&Coord2(4.0, 5.0)) < 0.1));
+            } else if edges[0].start_point().distance_to(&Coord2(2.0, 5.0)) < 0.1 {
+                assert!(edges.iter().any(|edge| edge.end_point().distance_to(&Coord2(2.0, 1.0)) < 0.1));
+                assert!(edges.iter().any(|edge| edge.end_point().distance_to(&Coord2(4.0, 5.0)) < 0.1));
+            } else if edges[0].start_point().distance_to(&Coord2(4.0, 5.0)) < 0.1 {
+                assert!(edges.iter().any(|edge| edge.end_point().distance_to(&Coord2(5.0, 5.0)) < 0.1));
+                assert!(edges.iter().any(|edge| edge.end_point().distance_to(&Coord2(4.0, 6.0)) < 0.1));
+            } else {
+                // These are the only four intersection points that should exist
+                println!("{:?}", edges[0].start_point());
+                assert!(false)
+            }
+        } else if edges.len() == 1 {
+            let edge        = edges.iter().nth(0).unwrap();
+            let start_point = edge.start_point();
+
+            assert!((start_point.x()-1.0).abs() < 0.01 ||
+                    (start_point.x()-5.0).abs() < 0.01 ||
+                    (start_point.x()-2.0).abs() < 0.01 ||
+                    (start_point.x()-4.0).abs() < 0.01);
+            assert!((start_point.y()-1.0).abs() < 0.01 ||
+                    (start_point.y()-5.0).abs() < 0.01 ||
+                    (start_point.y()-0.0).abs() < 0.01 ||
+                    (start_point.y()-6.0).abs() < 0.01);
+        } else {
+            // Should only be 1 edge (corners) or 2 edges (collision points)
+            // TODO: currently we end up here because we're generating point -> same point 'edges' for some reason
+            println!("{:?}", edges);
+            assert!(edges.len() <= 2);
+        }
+    }
+
+    assert!(num_intersects == 4);
+}
