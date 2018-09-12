31bf9c84d9229708c74e40e67135528124ab5bde
Andrew Hunter <andrew@logicalshift.co.uk>
2018-09-12 23:48:55 +0100

    Add basic implementation of the code to create a fat line from a curve

--- curves/src/bezier/intersection/fat_line.rs
+++ curves/src/bezier/intersection/fat_line.rs
@@ -1,3 +1,4 @@
+use super::super::curve::*;
 use super::super::super::line::*;
 use super::super::super::coordinate::*;
 
@@ -24,10 +25,49 @@ where L::Point: Coordinate2D {
     ///
     /// Creates a new fat line
     /// 
-    #[inline]
     pub fn new(line: L, d_min: f64, d_max: f64) -> FatLine<L> {
         let (from, to)  = line.points();
         let (a, b, c)   = line_coefficients_2d(&line);
+
+        FatLine {
+            line:   line,
+            d_min:  d_min,
+            d_max:  d_max,
+            coeff:  (a, b, c)
+        }
+    }
+}
+
+impl<P: Coordinate+Coordinate2D> FatLine<(P, P)> {
+    ///
+    /// Creates a new fatline from a curve
+    /// 
+    pub fn from_curve<C: BezierCurve<Point=P>>(curve: &C) -> FatLine<(P, P)> {
+        // Line between the start and end points of the curve
+        let line        = (curve.start_point(), curve.end_point());
+        
+        // Coefficients for the line
+        let (a, b, c)   = line_coefficients_2d(&line);
+
+        // Compute the distances to the control points
+        let (cp1, cp2)  = curve.control_points();
+        let d1          = a*cp1.x() + b*cp1.y() + c;
+        let d2          = a*cp2.x() + b*cp2.y() + c;
+
+        // This is the 'estimated fit' shortcut suggested by Sederberg/Nishta in their paper rather than the tighest ffitting line
+        let (d_min, d_max) = if d1*d2 > 0.0 {
+            // Both control points on the same side of the line
+            (
+                (3.0/4.0) * (d1.min(d2).min(0.0)),
+                (3.0/4.0) * (d1.max(d2).max(0.0))
+            )
+        } else {
+            // Control points on opposite sides of the line
+            (
+                (4.0/9.0) * (d1.min(d2).min(0.0)),
+                (4.0/9.0) * (d1.max(d2).max(0.0))
+            )
+        };
 
         FatLine {
             line:   line,
