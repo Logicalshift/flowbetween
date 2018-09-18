38e20fa8ec703d99963829a17e15f29e8bbad04e
Andrew Hunter <andrew@logicalshift.co.uk>
2018-09-18 23:30:56 +0100

    Going to use the new curve section in the main clipping algorithm here

--- curves/src/bezier/intersection/curve_curve_clip.rs
+++ curves/src/bezier/intersection/curve_curve_clip.rs
@@ -19,7 +19,7 @@ fn curve_hull_length_sq<C: BezierCurve>(curve: &C) -> f64 {
 ///
 /// Determines the points at which two curves intersect using the Bezier clipping algorithm
 /// 
-fn curve_intersects_curve_clip_inner<'a, C: BezierCurveFactory>(curve1: C, curve2: C, accuracy_squared: f64) -> Vec<(f64, f64)>
+fn curve_intersects_curve_clip_inner<'a, C: BezierCurve>(curve1: CurveSection<'a, C>, curve2: CurveSection<'a, C>, accuracy_squared: f64) -> Vec<(f64, f64)>
 where C::Point: 'a+Coordinate2D {
     // Create the fat line from the first curve
     let fat_line = FatLine::from_curve(&curve1);
@@ -36,9 +36,9 @@ where C::Point: 'a+Coordinate2D {
 /// 
 pub fn curve_intersects_curve_clip<'a, C: BezierCurve>(curve1: &'a C, curve2: &'a C, accuracy: f64) -> Vec<(f64, f64)>
 where C::Point: 'a+Coordinate2D {
-    // Convert to the standard curve type
-    let mut curve1 = Curve::from_curve(curve1);
-    let mut curve2 = Curve::from_curve(curve2);
+    // Start with the entire span of both curves
+    let curve1 = curve1.section(0.0, 1.0);
+    let curve2 = curve2.section(0.0, 1.0);
 
     // Perform the clipping algorithm on these curves
     curve_intersects_curve_clip_inner(curve1, curve2, accuracy*accuracy)
