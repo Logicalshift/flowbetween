70f3e360de0317a261fe7a4526542b6ad2f9c1fd
Andrew Hunter <andrew@logicalshift.co.uk>
2018-09-23 18:23:31 +0100

    These circles should intersect at two points
    Looks like we might be generating a bogus intersection somehow

--- curves/tests/bezier/path/intersection.rs
+++ curves/tests/bezier/path/intersection.rs
@@ -102,7 +102,7 @@ fn circle_intersects_circle() {
     let center = Coord2(5.0, 5.0);
     let radius = 4.0;
 
-    // Create a couple of circles (which should )
+    // Create a couple of circles (which should intersect at two points)
     let circle1: SimpleBezierPath = Circle::new(center, radius).to_path();
     let circle2: SimpleBezierPath = Circle::new(center + Coord2(1.0, 0.5), radius).to_path();
 
@@ -111,19 +111,27 @@ fn circle_intersects_circle() {
 
     // The circles should intersect at least once
     assert!(intersections.len() > 0);
+    println!("{:?}", intersections);
 
     // Convert to curves
     let curves1 = circle1.to_curves::<Curve<_>>();
     let curves2 = circle2.to_curves::<Curve<_>>();
 
     // Check that the intersections are in roughly the same place in each circle
-    for ((index1, t1), (index2, t2)) in intersections {
-        let point1 = curves1[index1].point_at_pos(t1);
-        let point2 = curves2[index2].point_at_pos(t2);
+    for ((index1, t1), (index2, t2)) in intersections.iter() {
+        let point1 = curves1[*index1].point_at_pos(*t1);
+        let point2 = curves2[*index2].point_at_pos(*t2);
 
         println!("{:?} {:?} {:?}", point1, point2, point1.distance_to(&point2));
+    }
+
+    for ((index1, t1), (index2, t2)) in intersections.iter() {
+        let point1 = curves1[*index1].point_at_pos(*t1);
+        let point2 = curves2[*index2].point_at_pos(*t2);
 
         // Should be within the tolerance specified by the accuracy value
         assert!(point1.distance_to(&point2) < 0.5);
     }
+
+    assert!(intersections.len() == 2);
 }
