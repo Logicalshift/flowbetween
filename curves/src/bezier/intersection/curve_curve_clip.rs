b93203854c4494d2b44c081d3068339756fdd349
Andrew Hunter <andrew@logicalshift.co.uk>
2018-09-23 16:33:17 +0100

    Add a 'join subsections' that finds sections where we've matched the same point twice

--- curves/src/bezier/intersection/curve_curve_clip.rs
+++ curves/src/bezier/intersection/curve_curve_clip.rs
@@ -30,6 +30,54 @@ where C::Point: 'a+Coordinate2D {
     clip_t.map(|(t1, t2)| if t1 == t2 { (t1-0.01, t2) } else { (t1, t2) })
 }
 
+///
+/// Given a set of intersections found on a left and right curve, joins them in a way that eliminates duplicates
+/// 
+fn join_subsections<'a, C: BezierCurve>(curve1: &CurveSection<'a, C>, left: Vec<(f64, f64)>, right: Vec<(f64, f64)>, accuracy_squared: f64) -> Vec<(f64, f64)> 
+where C::Point: Coordinate2D {
+    if left.len() == 0 {
+        // No further work to do
+        right
+    } else if right.len() == 0 {
+        // No further work to do
+        left
+    } else {
+        // The last intersection in left might be the same as the first in right
+        let (left_t1, _left_t2)     = left[left.len()-1];
+        let (right_t1, _right_t2)   = right[0];
+
+        // We use t1 and curve1 to determine this
+        let left_t1                 = curve1.section_t_for_original_t(left_t1);
+        let right_t1                = curve1.section_t_for_original_t(right_t1);
+
+        if (right_t1-left_t1).abs() < 0.1 {
+            // Could be the same point
+            let p1 = curve1.point_at_pos(left_t1);
+            let p2 = curve1.point_at_pos(right_t1);
+
+            let offset              = p2-p1;
+            let distance_squared    = offset.dot(&offset);
+
+            if distance_squared <= (accuracy_squared*2.0) {
+                // First and last points are the same: only use the version of the LHS
+                let mut combined = left;
+                combined.extend(right.into_iter().skip(1));
+                combined
+            } else {
+                // Not the same points: just combine the two curves
+                let mut combined = left;
+                combined.extend(right);
+                combined
+            }
+        } else {
+            // Not the same points: just combine the two curves
+            let mut combined = left;
+            combined.extend(right);
+            combined
+        }
+    }
+}
+
 fn format_curve<C: BezierCurve>(curve: &C) -> String
 where C::Point: Coordinate2D {
     let start_point = curve.start_point();
@@ -77,9 +125,9 @@ where C::Point: 'a+Coordinate2D {
             if curve2_last_len*0.8 < curve2_len {
                 let (left, right)   = (curve2.subsection(0.0, 0.5), curve2.subsection(0.5, 1.0));
                 let left            = curve_intersects_curve_clip_inner(curve1.clone(), left, accuracy_squared);
-                let right           = curve_intersects_curve_clip_inner(curve1, right, accuracy_squared);
+                let right           = curve_intersects_curve_clip_inner(curve1.clone(), right, accuracy_squared);
 
-                return left.into_iter().chain(right.into_iter()).collect();
+                return join_subsections(&curve1, left, right, accuracy_squared);
             }
 
             // Update the length of the curve
@@ -105,7 +153,7 @@ where C::Point: 'a+Coordinate2D {
                 let left            = curve_intersects_curve_clip_inner(left, curve2.clone(), accuracy_squared);
                 let right           = curve_intersects_curve_clip_inner(right, curve2, accuracy_squared);
 
-                return left.into_iter().chain(right.into_iter()).collect();
+                return join_subsections(&curve1, left, right, accuracy_squared);
             }
 
             // Update the length of the curve
@@ -135,4 +183,3 @@ where C::Point: 'a+Coordinate2D {
     // Perform the clipping algorithm on these curves
     curve_intersects_curve_clip_inner(curve1, curve2, accuracy*accuracy)
 }
-
