6f6d28512eb45f511536eb521a6bed7c1dca5383
Andrew Hunter <andrew@logicalshift.co.uk>
2018-09-29 21:20:20 +0100

    Add a new factory method for creating paths from other paths

--- curves/src/bezier/path/path.rs
+++ curves/src/bezier/path/path.rs
@@ -48,6 +48,13 @@ pub trait BezierPathFactory : BezierPath {
     /// Creates a new instance of this path from a set of points
     /// 
     fn from_points<FromIter: IntoIterator<Item=(Self::Point, Self::Point, Self::Point)>>(start_point: Self::Point, points: FromIter) -> Self;
+
+    ///
+    /// Creates a new instance of this path from the points in another path
+    ///
+    fn from_path<FromPath: BezierPath<Point=Self::Point>>(path: &FromPath) -> Self {
+        Self::from_points(path.start_point(), path.points())
+    }
 }
 
 impl<Point: Clone+Coordinate> Geo for (Point, Vec<(Point, Point, Point)>) {
