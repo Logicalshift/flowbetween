ab2bd692ac538508c0a2b5949e29fc480b87337e
Andrew Hunter <andrew@logicalshift.co.uk>
2018-09-30 18:13:14 +0100

    We'll need to support adding paths where the direction reverses
    I think the issue here is that TODO in the exterior edges routine

--- curves/tests/bezier/path/arithmetic_add.rs
+++ curves/tests/bezier/path/arithmetic_add.rs
@@ -41,6 +41,47 @@ fn add_two_overlapping_circles() {
     assert!(num_points_on_both == 2);
 }
 
+#[test]
+fn add_two_overlapping_circles_with_one_reversed() {
+    // Two overlapping circles (one clockwise, one anti-clockwise)
+    let circle1 = Circle::new(Coord2(5.0, 5.0), 4.0).to_path::<SimpleBezierPath>();
+    let circle2 = Circle::new(Coord2(7.0, 5.0), 4.0).to_path::<SimpleBezierPath>();
+    let circle2 = circle2.reversed::<SimpleBezierPath>();
+
+    // Combine them
+    let combined_circles = path_add::<_, _, _, SimpleBezierPath>(&vec![circle1], &vec![circle2], 0.01);
+
+    println!("{:?}", combined_circles);
+    assert!(combined_circles.len() == 1);
+
+    // All points should be on either circle, and two should be on both
+    let points = combined_circles[0].points().map(|(_, _, end_point)| end_point);
+
+    let mut num_points_on_circle1   = 0;
+    let mut num_points_on_circle2   = 0;
+    let mut num_points_on_both      = 0;
+
+    for point in points {
+        let distance_to_circle1 = Coord2(5.0, 5.0).distance_to(&point);
+        let distance_to_circle2 = Coord2(7.0, 5.0).distance_to(&point);
+
+        // Must be on either circle
+        assert!((distance_to_circle1-4.0).abs() < 0.01 || (distance_to_circle2-4.0).abs() < 0.01);
+
+        println!("{:?} {:?} {:?}", point, distance_to_circle1, distance_to_circle2);
+
+        if (distance_to_circle1-4.0).abs() < 0.01 && (distance_to_circle2-4.0).abs() < 0.01 { num_points_on_both += 1 }
+        else if (distance_to_circle1-4.0).abs() < 0.01 { num_points_on_circle1 += 1 }
+        else if (distance_to_circle2-4.0).abs() < 0.01 { num_points_on_circle2 += 1 }
+    }
+
+    println!("{:?} {:?} {:?}", num_points_on_circle1, num_points_on_circle2, num_points_on_both);
+
+    assert!(num_points_on_circle1 == 2);
+    assert!(num_points_on_circle2 == 2);
+    assert!(num_points_on_both == 2);
+}
+
 /* -- should pass once we add support for mutliple paths
 #[test]
 fn add_two_non_overlapping_circles() {
