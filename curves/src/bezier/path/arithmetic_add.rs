f1287e863c026996e3ba337895762f92ac29fa8e
Andrew Hunter <andrew@logicalshift.co.uk>
2018-10-04 00:34:38 +0100

    Clean up old code from arithmetic_add

--- curves/src/bezier/path/arithmetic_add.rs
+++ curves/src/bezier/path/arithmetic_add.rs
@@ -19,18 +19,6 @@ enum PathDirection {
     Anticlockwise
 }
 
-impl PathDirection {
-    #[inline]
-    fn reversed(&self) -> PathDirection {
-        use self::PathDirection::*;
-
-        match self {
-            Clockwise       => Anticlockwise,
-            Anticlockwise   => Clockwise
-        }
-    }
-}
-
 impl<'a, P: BezierPath> From<&'a P> for PathDirection
 where P::Point: Coordinate2D {
     #[inline]
@@ -44,61 +32,11 @@ where P::Point: Coordinate2D {
 }
 
 //
-// There are actually a couple of ways to determine which path continues on the outside edge at an intersection.
+// This uses a simple ray casting algorithm to perform the addition
 // 
-// One way is to pick the path that has the shallowest angle with the incoming path (anti-clockwise or clockwise depending on
-// the direction of the path containing the incoming edge)
-// 
-// Another way is to assume no self-intersections. This means every intersection should switch from one of the paths to the other
-// and reduces the number of choices. The correct choice should always be the edge from the other path that continues moving in
-// the direction we're going (ie, if we're moving clockwise, we should pick the clockwise edge)
-// 
-// One problem with this approach is that it's technically valid to have 'holes' with points that share a path, which may increase
-// the number of choices beyond just two.
+// Basic idea is to cast a ray at an edge which is currently uncategorised, and mark the edges it crosses as interior or
+// exterior depending on whether or not we consider it as crossing into or out of the final shape.
 //
-
-///
-/// From an edge marked as being on the outside of the path, traces 
-///
-fn trace_exterior_edges_add<Point>(graph_path: &mut GraphPath<Point, (SourcePath, PathDirection)>, exterior_edge: GraphEdgeRef) 
-where Point: Coordinate+Coordinate2D {
-    // Trace the external edges from the path to find the exterior edges. We always follow an edge moving in the same direction on the other path.
-    graph_path.classify_exterior_edges(exterior_edge, |_graph, start_edge, edge_choices| {
-        // Fetch the direction of the source edge
-        let (source_path, source_direction) = start_edge.label();
-        let source_reversed                 = start_edge.is_reversed();
-
-        // At an intersection, the target must be on the other path
-        let target_path = match source_path {
-            SourcePath::Path1 => SourcePath::Path2,
-            SourcePath::Path2 => SourcePath::Path1
-        };
-
-        for edge in edge_choices.into_iter() {
-            let (edge_path, edge_direction) = edge.label();
-            let edge_reversed               = edge.is_reversed();
-
-            // Ignore edges from the source path
-            if edge_path != target_path {
-                continue;
-            }
-
-            if edge_reversed == source_reversed {
-                // The target should be on the other path in the same direction
-                if edge_direction == source_direction {
-                    return edge.into();
-                }
-            } else {
-                // Match against the opposite direction if the edge is reversed
-                if edge_direction == source_direction.reversed() {
-                    return edge.into();
-                }
-            }
-        }
-
-        panic!("Could not find a following edge")
-    });
-}
 
 ///
 /// Generates the path formed by adding two sets of paths
