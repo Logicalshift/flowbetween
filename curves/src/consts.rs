133c054e9524e0658b5b36e5f1fe1436998c3884
Andrew Hunter <andrew@logicalshift.co.uk>
2018-10-21 22:48:53 +0100

    Update comment

--- curves/src/consts.rs
+++ curves/src/consts.rs
@@ -1,2 +1,2 @@
-/// Length we consider a small distance
+/// Length we consider a small distance (points closer than this far apart are considered to be the same)
 pub const SMALL_DISTANCE: f64 = 0.001;
