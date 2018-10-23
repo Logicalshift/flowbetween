25e10801278980faf8e678e45545543e2f739137
Andrew Hunter <andrew@logicalshift.co.uk>
2018-10-23 23:53:19 +0100

    Remove/round collisions that are too close together to prevent the path degenerating into one where we can't perform ray collisions adequately

--- curves/src/bezier/path/graph_path.rs
+++ curves/src/bezier/path/graph_path.rs
@@ -697,17 +697,19 @@ impl<Point: Coordinate+Coordinate2D, Label: Copy> GraphPath<Point, Label> {
                         if src_idx == tgt_idx && src_edge_idx == tgt_edge_idx { continue; }
 
                         // Create edge objects for each side
-                        let src_curve           = GraphEdge::new(self, GraphEdgeRef { start_idx: src_idx, edge_idx: src_edge_idx, reverse: false });
-                        let tgt_curve           = GraphEdge::new(self, GraphEdgeRef { start_idx: tgt_idx, edge_idx: tgt_edge_idx, reverse: false });
+                        let src_curve               = GraphEdge::new(self, GraphEdgeRef { start_idx: src_idx, edge_idx: src_edge_idx, reverse: false });
+                        let tgt_curve               = GraphEdge::new(self, GraphEdgeRef { start_idx: tgt_idx, edge_idx: tgt_edge_idx, reverse: false });
 
                         // Quickly reject edges with non-overlapping bounding boxes
-                        let src_edge_bounds     = src_curve.fast_bounding_box::<Bounds<_>>();
-                        let tgt_edge_bounds     = tgt_curve.fast_bounding_box::<Bounds<_>>();
+                        let src_edge_bounds         = src_curve.fast_bounding_box::<Bounds<_>>();
+                        let tgt_edge_bounds         = tgt_curve.fast_bounding_box::<Bounds<_>>();
                         if !src_edge_bounds.overlaps(&tgt_edge_bounds) { continue; }
 
                         // Find the collisions between these two edges
-                        let curve_collisions    = curve_intersects_curve_clip(&src_curve, &tgt_curve, accuracy);
-                        // debug_assert!(curve_collisions.len() < 21); // TODO
+                        let mut curve_collisions    = curve_intersects_curve_clip(&src_curve, &tgt_curve, accuracy);
+
+                        // Remove any pairs of collisions that are too close together
+                        remove_and_round_close_collisions(&mut curve_collisions, &src_curve, &tgt_curve);
 
                         // The are the points we need to divide the existing edges at and add branches
                         let tgt_idx = tgt_idx;
@@ -1548,6 +1550,80 @@ impl<Point: Coordinate2D+Coordinate+fmt::Debug, Label: Copy> fmt::Debug for Grap
         }
 
         Ok(())
+    }
+}
+
+///
+/// Removes any pairs of collisions that are closer than `CLOSE_DISTANCE` apart, and also rounds the 
+/// first and last collisions to 0.0 and 1.0
+/// 
+/// When colliding two bezier curves we want to avoid subdividing excessively to produce very small 
+/// sections as they have a tendency to produce extra collisions due to floating point or root finding
+/// errors.
+///
+fn remove_and_round_close_collisions<P: Coordinate+Coordinate2D, C: BezierCurve<Point=P>>(collisions: &mut Vec<(f64, f64)>, src: &C, tgt: &C) {
+    let close_distance_sq = CLOSE_DISTANCE * CLOSE_DISTANCE;
+
+    // Nothing to do if there are no collisions
+    if collisions.len() == 0 {
+        return;
+    }
+
+    // Work out the positions of each point
+    let mut positions = collisions.iter().map(|(t1, _t2)| src.point_at_pos(*t1)).collect::<Vec<_>>();
+
+    // Find any pairs of points that are too close together
+    let mut collision_idx = 0;
+    while collision_idx+1 < collisions.len() {
+        // Work out the distance between this collision and the next
+        let offset      = positions[collision_idx] - positions[collision_idx+1];
+        let distance_sq = offset.dot(&offset);
+
+        // Just remove both of these if they are too close together (as each collision crosses the curve once, removing collisions in pairs means that there'll still be at least one collision left if the curves actually end up crossing over)
+        if distance_sq < close_distance_sq {
+            collisions.remove(collision_idx); positions.remove(collision_idx);
+            collisions.remove(collision_idx); positions.remove(collision_idx);
+        } else {
+            collision_idx += 1;
+        }
+    }
+    
+    // If the first point or the last point is close to the end of the source or target curve, clip to 0 or 1
+    if collisions.len() > 0 {
+        // Get the start/end points of the source and target
+        let src_start   = src.start_point();
+        let src_end     = src.end_point();
+        let tgt_start   = tgt.start_point();
+        let tgt_end     = tgt.end_point();
+
+        // Snap collisions to 0.0 or 1.0 if they're very close to the start or end of either curve
+        for collision_idx in 0..collisions.len() {
+            // Snap the source side
+            if collisions[collision_idx].0 > 0.0 && collisions[collision_idx].0 < 1.0 {
+                let start_offset = src_start - positions[collision_idx];
+                if start_offset.dot(&start_offset) < close_distance_sq {
+                    collisions[collision_idx].0 = 0.0;
+                }
+
+                let end_offset = src_end - positions[collision_idx];
+                if end_offset.dot(&end_offset) < close_distance_sq {
+                    collisions[collision_idx].0 = 1.0;
+                }
+            }
+
+            // Snap the target side
+            if collisions[collision_idx].1 > 0.0 && collisions[collision_idx].1 < 1.0 {
+                let start_offset = tgt_start - positions[collision_idx];
+                if start_offset.dot(&start_offset) < close_distance_sq {
+                    collisions[collision_idx].1 = 0.0;
+                }
+
+                let end_offset = tgt_end - positions[collision_idx];
+                if end_offset.dot(&end_offset) < close_distance_sq {
+                    collisions[collision_idx].1 = 1.0;
+                }
+            }
+        }
     }
 }
 
