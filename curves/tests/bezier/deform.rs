3aa71a55454f7061bf9eed34a367f6def3832f69
Andrew Hunter <andrew@logicalshift.co.uk>
2018-11-09 21:26:22 +0000

    Fix test

--- curves/tests/bezier/deform.rs
+++ curves/tests/bezier/deform.rs
@@ -4,7 +4,7 @@ use flo_curves::bezier;
 #[test]
 fn deform_line_upwards() {
     let curve       = bezier::Curve::from_points(Coord2(0.0, 0.0), Coord2(10.0, 0.0), Coord2(2.5, 0.0), Coord2(7.5, 0.0));
-    let deformed    = bezier::move_point(&curve, 0.5, Coord2(0.0, 4.0));
+    let deformed    = bezier::move_point::<_, _, bezier::Curve<_>>(&curve, 0.5, &Coord2(0.0, 4.0));
 
     let original_point  = curve.point_at_pos(0.5);
     let new_point       = deformed.point_at_pos(0.5);
@@ -21,7 +21,7 @@ fn deform_line_upwards() {
 #[test]
 fn deform_curve_at_halfway_point() {
     let curve       = bezier::Curve::from_points(Coord2(10.0, 20.0), Coord2(20.0, 15.0), Coord2(0.0, 15.0), Coord2(16.0, 30.0));
-    let deformed    = bezier::move_point(&curve, 0.5, Coord2(3.0, 4.0));
+    let deformed    = bezier::move_point::<_, _, bezier::Curve<_>>(&curve, 0.5, &Coord2(3.0, 4.0));
 
     let original_point  = curve.point_at_pos(0.5);
     let new_point       = deformed.point_at_pos(0.5);
@@ -39,7 +39,7 @@ fn deform_curve_at_halfway_point() {
 fn deform_curve_at_other_point() {
     let t           = 0.32;
     let curve       = bezier::Curve::from_points(Coord2(10.0, 20.0), Coord2(20.0, 15.0), Coord2(0.0, 15.0), Coord2(16.0, 30.0));
-    let deformed    = bezier::move_point(&curve, t, Coord2(3.0, 4.0));
+    let deformed    = bezier::move_point::<_, _, bezier::Curve<_>>(&curve, t, &Coord2(3.0, 4.0));
 
     let original_point  = curve.point_at_pos(t);
     let new_point       = deformed.point_at_pos(t);
@@ -61,7 +61,7 @@ fn deform_curve_at_many_other_points() {
         let t           = (0.9*t)+0.05;
 
         let curve       = bezier::Curve::from_points(Coord2(5.0, 23.0), Coord2(22.0, 17.0), Coord2(-10.0, 15.0), Coord2(26.0, 30.0));
-        let deformed    = bezier::move_point(&curve, t, Coord2(6.0, -4.0));
+        let deformed    = bezier::move_point::<_, _, bezier::Curve<_>>(&curve, t, &Coord2(6.0, -4.0));
 
         let original_point  = curve.point_at_pos(t);
         let new_point       = deformed.point_at_pos(t);
