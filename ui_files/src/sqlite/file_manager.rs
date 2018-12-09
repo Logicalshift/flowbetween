3966fdcd7913386ea350bd58be2ce46bb8a910ef
Andrew Hunter <andrew@logicalshift.co.uk>
2018-12-09 18:51:33 +0000

    Can replace the mutexes with Desyncs

--- ui_files/src/sqlite/file_manager.rs
+++ ui_files/src/sqlite/file_manager.rs
@@ -23,10 +23,10 @@ const DATA_DIR: &str = "data";
 
 lazy_static! {
     // Prevents multiple threads from trying to create the database all at the same time
-    static ref CREATING_DATABASE: Mutex<()> = Mutex::new(());
+    static ref CREATING_DATABASE: Desync<()> = Desync::new(());
 
     // Exising file manager cores for particular application paths
-    static ref FILE_CORES: Mutex<HashMap<(String, String), Arc<Desync<SqliteFileManagerCore>>>> = Mutex::new(HashMap::new());
+    static ref FILE_CORES: Desync<HashMap<(String, String), Arc<Desync<SqliteFileManagerCore>>>> = Desync::new(HashMap::new());
 }
 
 struct SqliteFileManagerCore {
@@ -69,46 +69,47 @@ impl SqliteFileManager {
     /// Creates a new file manager core
     ///
     fn new_core(application_path: &str, sub_path: &str) -> Arc<Desync<SqliteFileManagerCore>> {
-        let _creating   = CREATING_DATABASE.lock().unwrap();
-        let log         = LogPublisher::new(module_path!());
+        CREATING_DATABASE.sync(|_| {
+            let log         = LogPublisher::new(module_path!());
 
-        // This will be the 'root' data directory for the user
-        let mut root_path = dirs::data_local_dir()
-            .or_else(|| dirs::data_dir())
-            .unwrap();
+            // This will be the 'root' data directory for the user
+            let mut root_path = dirs::data_local_dir()
+                .or_else(|| dirs::data_dir())
+                .unwrap();
 
-        // Append the path components
-        root_path.push(application_path);
-        root_path.push(sub_path);
+            // Append the path components
+            root_path.push(application_path);
+            root_path.push(sub_path);
 
-        // Create the data directory if it does not exist
-        fs::create_dir_all(root_path.as_path()).unwrap();
+            // Create the data directory if it does not exist
+            fs::create_dir_all(root_path.as_path()).unwrap();
 
-        // Create the subdirectories too
-        let mut data_dir = root_path.clone();
-        data_dir.push(DATA_DIR);
-        fs::create_dir_all(data_dir.as_path()).unwrap();
+            // Create the subdirectories too
+            let mut data_dir = root_path.clone();
+            data_dir.push(DATA_DIR);
+            fs::create_dir_all(data_dir.as_path()).unwrap();
 
-        log.log((Level::Info, format!("Using data directory at `{}`", data_dir.to_str().unwrap_or("<Missing path>"))));
+            log.log((Level::Info, format!("Using data directory at `{}`", data_dir.to_str().unwrap_or("<Missing path>"))));
 
-        // Check for the file list database file
-        let mut database_file = root_path.clone();
-        database_file.push(FILES_DB);
+            // Check for the file list database file
+            let mut database_file = root_path.clone();
+            database_file.push(FILES_DB);
 
-        // Connect to the Sqlite database
-        let database_connection     = Connection::open(database_file.as_path()).unwrap();
-        let file_list               = FileList::new(database_connection).unwrap();
+            // Connect to the Sqlite database
+            let database_connection     = Connection::open(database_file.as_path()).unwrap();
+            let file_list               = FileList::new(database_connection).unwrap();
 
-        // Create the update publisher
-        let update_publisher = Publisher::new(100);
-        let update_publisher = executor::spawn(update_publisher);
+            // Create the update publisher
+            let update_publisher = Publisher::new(100);
+            let update_publisher = executor::spawn(update_publisher);
 
-        Arc::new(Desync::new(SqliteFileManagerCore {
-            file_list:  file_list,
-            root_path:  root_path,
-            updates:    update_publisher,
-            log:        log    
-        }))
+            Arc::new(Desync::new(SqliteFileManagerCore {
+                file_list:  file_list,
+                root_path:  root_path,
+                updates:    update_publisher,
+                log:        log    
+            }))
+        })
     }
 
     ///
@@ -119,10 +120,10 @@ impl SqliteFileManager {
     /// 
     pub fn new(application_path: &str, sub_path: &str) -> SqliteFileManager {
         // Create the core, or use an existing one if there is one
-        let core = FILE_CORES.lock().unwrap()
-            .entry((String::from(application_path), String::from(sub_path)))
+        let core = FILE_CORES.sync(|file_cores|
+            file_cores.entry((String::from(application_path), String::from(sub_path)))
             .or_insert_with(|| Self::new_core(application_path, sub_path))
-            .clone();
+            .clone());
 
         // Fetch information from it
         let root_path   = core.sync(|core| core.root_path.clone());
