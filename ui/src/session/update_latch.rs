b01c304cbf255bbd0718d81fb7319b857cc41271
Andrew Hunter <andrew@logicalshift.co.uk>
2018-12-15 12:27:07 +0000

    Fix typo

--- ui/src/session/update_latch.rs
+++ ui/src/session/update_latch.rs
@@ -3,7 +3,7 @@
 ///
 #[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
 pub enum UpdateLatch {
-    /// Stop processing updatges
+    /// Stop processing updates
     Suspend,
 
     /// Start processing updates
