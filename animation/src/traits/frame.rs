1d30aea223df58a84f55fb0ca4d53dc8617c2fa6
Andrew Hunter <andrew@logicalshift.co.uk>
2018-12-28 20:07:51 +0000

    Add an implementation of Frame for Arc<dyn Frame>

--- animation/src/traits/frame.rs
+++ animation/src/traits/frame.rs
@@ -6,6 +6,7 @@ use super::brush_drawing_style::*;
 
 use canvas::*;
 
+use std::sync::*;
 use std::time::Duration;
 
 ///
@@ -48,4 +49,43 @@ pub trait Frame : Send+Sync {
     /// these are the properties that will be used)
     /// 
     fn active_brush_properties(&self) -> Option<BrushProperties>;
+}
+
+impl Frame for Arc<dyn Frame> {
+    ///
+    /// Time index of this frame relative to its keyframe
+    /// 
+    #[inline] fn time_index(&self) -> Duration { (**self).time_index() }
+
+    ///
+    /// Renders this frame to a particular graphics context
+    ///
+    #[inline] fn render_to(&self, gc: &mut dyn GraphicsPrimitives) { (**self).render_to(gc) }
+
+    ///
+    /// Attempts to retrieve the vector elements associated with this frame, if there are any
+    /// 
+    #[inline] fn vector_elements<'a>(&'a self) -> Option<Box<dyn 'a+Iterator<Item=Vector>>> { (**self).vector_elements() }
+
+    ///
+    /// Retrieves a copy of the element with the specifed ID from this frame, if it exists
+    /// 
+    #[inline] fn element_with_id<'a>(&'a self, id: ElementId) -> Option<Vector> { (**self).element_with_id(id) }
+
+    ///
+    /// The brush that is active after all the elements are drawn in this frame
+    /// 
+    /// (If new elements are added to the layer at the time index of this frame,
+    /// this is the brush that will be used)
+    /// 
+    #[inline] fn active_brush(&self) -> Option<(BrushDefinition, BrushDrawingStyle)> { (**self).active_brush() }
+
+    ///
+    /// The brush properties that are active after all the elements are drawn
+    /// in this frame.
+    /// 
+    /// (If new elements are added to the layer at the time index of this frame,
+    /// these are the properties that will be used)
+    /// 
+    #[inline] fn active_brush_properties(&self) -> Option<BrushProperties> { (**self).active_brush_properties() }
 }
