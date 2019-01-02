b32c6f5b837613bd5fdacfc7fcde14fe1bee10cf
Andrew Hunter <andrew@logicalshift.co.uk>
2019-01-02 11:42:15 +0000

    Add the basics of the session dispatch implementation

--- user_interfaces/cocoa_ui/src/session.rs
+++ user_interfaces/cocoa_ui/src/session.rs
@@ -9,6 +9,7 @@ use objc::rc::*;
 use objc::runtime::*;
 
 use std::sync::*;
+use std::collections::HashMap;
 
 ///
 /// Basis class for a Cocoa session
@@ -16,6 +17,12 @@ use std::sync::*;
 pub struct CocoaSession {
     /// Reference to the FloControl we'll relay the stream via
     target_object: StrongPtr,
+
+    /// Maps IDs to windows
+    windows: HashMap<usize, StrongPtr>,
+
+    /// Maps IDs to views
+    views: HashMap<usize, StrongPtr>,
 
     /// The stream of actions for this session (or None if we aren't monitoring for actions)
     actions: Option<Spawn<Box<dyn Stream<Item=AppAction, Error=()>+Send>>>
@@ -42,6 +49,8 @@ impl CocoaSession {
     pub fn new(obj: &StrongPtr) -> CocoaSession {
         CocoaSession {
             target_object:  obj.clone(),
+            windows:        HashMap::new(),
+            views:          HashMap::new(),
             actions:        None
         }
     }
@@ -53,6 +62,11 @@ impl CocoaSession {
     where Actions: 'static+Send+Stream<Item=AppAction, Error=()> {
         // Spawn the actions stream
         self.actions = Some(executor::spawn(Box::new(actions)));
+
+        unsafe {
+            // Wake up the object on the main thread
+            msg_send![class!(NSObject), performSelectorOnMainThread: sel!(actionStreamReady) withObject: self.target_object.clone() waitUntilDone: NO];
+        }
     }
 
     ///
@@ -63,28 +77,72 @@ impl CocoaSession {
         let notify = Arc::new(CocoaSessionNotify::new(&self.target_object));
 
         // Drain the stream until it's empty or it blocks
-        if let Some(actions) = self.actions.as_mut() {
-            loop {
-                let next = actions.poll_stream_notify(&notify, 0);
+        loop {
+            let next = self.actions
+                .as_mut()
+                .map(|actions| actions.poll_stream_notify(&notify, 0))
+                .unwrap_or_else(|| Ok(Async::NotReady));
 
-                match next {
-                    Ok(Async::NotReady)     => { break; }
-                    Ok(Async::Ready(None))  => {
-                        // Session has finished
-                        break;
-                    }
+            match next {
+                Ok(Async::NotReady)     => { break; }
+                Ok(Async::Ready(None))  => {
+                    // Session has finished
+                    break;
+                }
 
-                    Ok(Async::Ready(action)) => {
-                        // Perform the action
-                    }
+                Ok(Async::Ready(Some(action))) => {
+                    // Perform the action
+                    self.dispatch_app_action(action);
+                }
 
-                    Err(_) => {
-                        // Action stream should never produce any errors
-                        unimplemented!("Action stream should never produce any errors")
-                    }
+                Err(_) => {
+                    // Action stream should never produce any errors
+                    unimplemented!("Action stream should never produce any errors")
                 }
             }
         }
+    }
+
+    ///
+    /// Performs an application action on this object
+    ///
+    pub fn dispatch_app_action(&mut self, action: AppAction) {
+        use self::AppAction::*;
+
+        match action {
+            CreateWindow(window_id)             => { self.create_window(window_id); }
+            Window(window_id, window_action)    => { self.windows.get(&window_id).map(|window| self.dispatch_window_action(window, window_action)); }
+            CreateView(view_id, view_type)      => { self.create_view(view_id, view_type); },
+            View(view_id, view_action)          => { self.views.get(&view_id).map(|view| self.dispatch_view_action(view, view_action)); }
+        }
+    }
+
+    ///
+    /// Creates a new window and assigns the specified ID to it
+    ///
+    fn create_window(&mut self, new_window_id: usize) {
+
+    }
+
+    ///
+    /// Dispatches an action to the specified window
+    ///
+    fn dispatch_window_action(&self, window: &StrongPtr, action: WindowAction) {
+
+    }
+
+    ///
+    /// Creates a new view and assigns the specified ID to it
+    ///
+    fn create_view(&mut self, new_view_id: usize, view_type: ViewType) {
+
+    }
+
+    ///
+    /// Dispatches an action to the specified view
+    ///
+    fn dispatch_view_action(&self, window: &StrongPtr, action: ViewAction) {
+
     }
 }
 
@@ -116,7 +174,7 @@ impl executor::Notify for CocoaSessionNotify {
         // If it still exists, send the message to the object on the main thread
         unsafe {
             if *target_object != nil {
-                msg_send![class!(NSObject), performSelectorOnMainThread: sel!(actionStreamReady) withObject: nil waitUntilDone: NO];
+                msg_send![class!(NSObject), performSelectorOnMainThread: sel!(actionStreamReady) withObject: target_object waitUntilDone: NO];
             }
         }
     }
