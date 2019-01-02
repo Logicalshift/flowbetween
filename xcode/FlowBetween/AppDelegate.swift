7cfbd50c727e0035bed10fefd4977b051278b711
Andrew Hunter <andrew@logicalshift.co.uk>
2019-01-02 12:16:08 +0000

    Add a basic XCode project

---
+++ xcode/FlowBetween/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FlowBetween
+//
+//  Created by Andrew Hunter on 02/01/2019.
+//  Copyright © 2019 Andrew Hunter. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
