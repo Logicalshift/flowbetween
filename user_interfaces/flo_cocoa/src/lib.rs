94fe32b53269df0272c853d4f26baaa1dc2c578a
Andrew Hunter <andrew@logicalshift.co.uk>
2019-01-02 15:36:41 +0000

    Add a basic 'create the session' function to the 'bridging' library

---
+++ user_interfaces/flo_cocoa/src/lib.rs
@@ -0,0 +1,16 @@
+use flo_cocoa_ui::*;
+use objc::*;
+use objc::runtime::*;
+
+#[no_mangle]
+pub unsafe extern fn create_flo_session(window_class: *mut Class, view_class: *mut Class) -> *mut Object {
+    // Create the session
+    let session: *mut Object    = msg_send!(&**FLO_CONTROL, alloc);
+    let session: *mut Object    = msg_send!(session, init);
+
+    // Set the properties
+    msg_send!(session, setWindowClass: window_class);
+    msg_send!(session, setViewClass: view_class);
+
+    session
+}
