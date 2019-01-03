08b7c6176534eba3619febc9248c1a89582750f3
Andrew Hunter <andrew@logicalshift.co.uk>
2019-01-03 14:05:49 +0000

    Add blank implementations for all of the control attributes

--- user_interfaces/cocoa_pipe/src/actions_from_control_attribute.rs
+++ user_interfaces/cocoa_pipe/src/actions_from_control_attribute.rs
@@ -5,6 +5,25 @@ use flo_ui::*;
 
 impl ActionsFrom<ViewAction> for ControlAttribute {
     fn actions_from(&self) -> Vec<ViewAction> { 
-        vec![]
+        use self::ControlAttribute::*;
+
+        match self {
+            BoundingBox(bounds)                     => vec![],
+            ZIndex(z_index)                         => vec![],
+            Padding((left, top), (right, bottom))   => vec![],
+            Text(text_val)                          => vec![],
+            FontAttr(font_attr)                     => vec![],
+            StateAttr(state_attr)                   => vec![],
+            PopupAttr(popup_attr)                   => vec![],
+            AppearanceAttr(appearance_attr)         => vec![],
+            ScrollAttr(scroll_attr)                 => vec![],
+            HintAttr(hint_attr)                     => vec![],
+            Id(id)                                  => vec![],
+            Controller(name)                        => vec![],
+            Action(trigger, name)                   => vec![],
+            Canvas(canvas_resource)                 => vec![],
+
+            SubComponents(_components)              => vec![]               // Handled separately by ViewState
+        }
     }
 }
