8cb3001d53134da0bdb58c10e816fb1c583a2cad
Andrew Hunter <andrew@logicalshift.co.uk>
2019-01-06 11:31:51 +0000

    Add in a delay before sending events
    (This prevents rapid events like canvas updates from being sent too quickly)

--- user_interfaces/cocoa_ui/src/event.rs
+++ user_interfaces/cocoa_ui/src/event.rs
@@ -1,6 +1,9 @@
+use super::session::*;
+
 use flo_stream::*;
 use flo_cocoa_pipe::*;
 
+use cocoa::base::nil;
 use objc::*;
 use objc::rc::*;
 use objc::declare::*;
@@ -9,8 +12,15 @@ use objc::runtime::*;
 use futures::executor;
 use futures::executor::Spawn;
 
+use std::mem;
 use std::sync::*;
 use std::collections::HashMap;
+
+/// The length of time to wait between receiving an event and sending it
+/// (Ideally we want to send all of the events at the start of an animation frame: however, Cocoa does not
+/// provide a particularly convenient way to do this, or even a good way to get the current frame rate so we 
+/// just use a hard-coded value here instead. A disadvantage of this approach will be choppy updates)
+const EVENT_DELAY_SECS: f64 = 1.0 / 60.0;
 
 lazy_static! {
     /// The events class
@@ -27,16 +37,29 @@ lazy_static! {
 /// Event target for sending events from the objective-C side to the rust side
 ///
 pub struct FloEvents {
-    events_publisher: Spawn<Publisher<Vec<AppEvent>>>
+    /// The ID of the view that will be sending these events
+    view_id: usize,
+
+    /// Where events are published
+    events_publisher: Spawn<Publisher<Vec<AppEvent>>>,
+
+    /// Set to true if we are going to receive a callback to send the events
+    queued_update: bool,
+
+    /// The events that are waiting to be sent
+    pending_events: Vec<AppEvent>
 }
 
 impl FloEvents {
     ///
     /// Creates a new FloEvents
     ///
-    pub fn init(publisher: Publisher<Vec<AppEvent>>) -> FloEvents {
+    pub fn init(publisher: Publisher<Vec<AppEvent>>, view_id: usize) -> FloEvents {
         FloEvents {
-            events_publisher: executor::spawn(publisher)
+            view_id:            view_id,
+            events_publisher:   executor::spawn(publisher),
+            queued_update:      false,
+            pending_events:     vec![]
         }
     }
 
@@ -71,8 +94,8 @@ impl FloEvents {
     ///
     /// Creates a new FloEvents object for objective-C
     ///
-    pub fn create_object(publisher: Publisher<Vec<AppEvent>>) -> StrongPtr {
-        let events = FloEvents::init(publisher);
+    pub fn create_object(publisher: Publisher<Vec<AppEvent>>, view_id: usize) -> StrongPtr {
+        let events = FloEvents::init(publisher, view_id);
         Self::object_from_events(Arc::new(Mutex::new(events)))
     }
 }
@@ -89,24 +112,69 @@ pub fn declare_flo_events_class() -> &'static Class {
         flo_events.add_ivar::<usize>("_eventsId");
 
         // Sends an event to the events object
-        unsafe fn send_event(flo_events: *mut Object, event: AppEvent) {
-            let events_id   = (*flo_events).get_ivar::<usize>("_eventsId");
+        unsafe fn send_event(this: &mut Object, event: AppEvent) {
+            // Fetch the rust events structure
+            let events_id   = (*this).get_ivar::<usize>("_eventsId");
             let flo_events  = FLO_EVENTS_STORE.lock().unwrap().get(events_id).cloned();
 
-            flo_events.map(|flo_events| flo_events.lock().unwrap().events_publisher.wait_send(vec![event]).ok());
+            flo_events.map(|flo_events| {
+                let mut flo_events = flo_events.lock().unwrap();
+
+                // Add to the pending events list
+                flo_events.pending_events.push(event);
+
+                // Add a runloop callback to actually send the events
+                if !flo_events.queued_update {
+                    // Build an array of all the modes
+                    let modes: *mut Object  = msg_send!(class!(NSMutableArray), alloc);
+                    let modes               = msg_send!(modes, init);
+                    let modes               = StrongPtr::new(modes);
+
+                    msg_send!(*modes, addObject: NSDefaultRunLoopMode);
+                    msg_send!(*modes, addObject: NSModalPanelRunLoopMode);
+                    msg_send!(*modes, addObject: NSEventTrackingRunLoopMode);
+
+                    // Call this object back after a delay
+                    msg_send!(this, performSelector: sel!(finishSendingEvents) withObject: nil afterDelay: EVENT_DELAY_SECS inModes: modes);
+
+                    flo_events.queued_update = true;
+                }
+            });
+        }
+
+        // Clears the list of pending events
+        extern fn finish_sending_events(this: &mut Object, _sel: Sel) {
+            unsafe {
+                // Fetch the rust events structure
+                let events_id   = (*this).get_ivar::<usize>("_eventsId");
+                let flo_events  = FLO_EVENTS_STORE.lock().unwrap().get(events_id).cloned();
+
+                flo_events.map(|flo_events| {
+                    let mut flo_events  = flo_events.lock().unwrap();
+                    let mut pending     = vec![];
+
+                    // Fetch and clear the list of pending events
+                    mem::swap(&mut pending, &mut flo_events.pending_events);
+                    flo_events.queued_update = false;
+
+                    // Send to the publisher
+                    flo_events.events_publisher.wait_send(pending).ok();
+                });
+            }
         }
 
         // Deallocates the flo_events object
-        extern fn dealloc_flo_events(flo_events: &mut Object, _sel: Sel) {
+        extern fn dealloc_flo_events(this: &mut Object, _sel: Sel) {
             unsafe {
                 // Remove the Rust type that we're managing
-                let events_id = (*flo_events).get_ivar::<usize>("_eventsId");
+                let events_id = (*this).get_ivar::<usize>("_eventsId");
                 FLO_EVENTS_STORE.lock().unwrap().remove(&events_id);
             }
         }
 
         // Register the events methods
         flo_events.add_method(sel!(dealloc), dealloc_flo_events as extern fn(&mut Object, Sel));
+        flo_events.add_method(sel!(finishSendingEvents), finish_sending_events as extern fn(&mut Object, Sel));
     }
 
     // Finalize the class
