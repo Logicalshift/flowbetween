05466f5b9d3f6b0f63636b8b103b14f46d88fec9
Andrew Hunter <andrew@logicalshift.co.uk>
2019-01-29 23:13:02 +0000

    Gah, issue looks like a weird problem with how objective C specifies parameters :-/

--- xcode/FlowBetween/CInterop/flo_cocoa.h
+++ xcode/FlowBetween/CInterop/flo_cocoa.h
@@ -29,10 +29,10 @@ typedef struct AppPainting AppPainting;
 
 - (void) sendClick: (NSString*) name;
 - (void) sendVirtualScroll: (NSString*) name left: (uint32_t) left top: (uint32_t) top width: (uint32_t) width height: (uint32_t) height;
-- (void) sendPaintStartForDevice: (uint32_t) deviceId name: (NSString*) name action: AppPainting;
-- (void) sendPaintContinueForDevice: (uint32_t) deviceId name: (NSString*) name action: AppPainting;
-- (void) sendPaintFinishForDevice: (uint32_t) deviceId name: (NSString*) name action: AppPainting;
-- (void) sendPaintCancelForDevice: (uint32_t) deviceId name: (NSString*) name action: AppPainting;
+- (void) sendPaintStartForDevice: (uint32_t) deviceId name: (NSString*) name action: (AppPainting) action;
+- (void) sendPaintContinueForDevice: (uint32_t) deviceId name: (NSString*) name action: (AppPainting) action;
+- (void) sendPaintFinishForDevice: (uint32_t) deviceId name: (NSString*) name action: (AppPainting) action;
+- (void) sendPaintCancelForDevice: (uint32_t) deviceId name: (NSString*) name action: (AppPainting) action;
 - (void) redrawCanvasWithSize: (NSSize) size viewport: (NSRect) viewport;
 
 @end
