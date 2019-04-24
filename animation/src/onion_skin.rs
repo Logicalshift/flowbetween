8f55b3c6c38cd97ccbaa14348774ff4505fad96f
Andrew Hunter <andrew@logicalshift.co.uk>
2019-04-24 20:34:52 +0100

    Remove interior points before generating an onion skin path

--- animation/src/onion_skin.rs
+++ animation/src/onion_skin.rs
@@ -47,6 +47,8 @@ pub fn onion_skin_for_layer(layer: Arc<dyn Layer>, when: Duration) -> CacheProce
 
                 // Add this element to the onion skin path
                 if let Some(element_path) = element.to_path(&properties) {
+                    let element_path = path_remove_interior_points::<_, _, Path>(&element_path, 0.1);
+
                     match (*properties).brush.drawing_style() {
                         BrushDrawingStyle::Draw     => { onion_skin = path_add(&onion_skin, &element_path, 0.1); }
                         BrushDrawingStyle::Erase    => { onion_skin = path_sub(&onion_skin, &element_path, 0.1); }
