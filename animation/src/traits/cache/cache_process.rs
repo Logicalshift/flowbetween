640a5fe9e02b5e4b93173b0e793f65f63a9130b4
Andrew Hunter <andrew@logicalshift.co.uk>
2019-04-24 13:51:08 +0100

    Now we're returning an Arc<Vec<Draw>> instead of just a Vec<Draw> we can reasonably clone it when returning the result of polling a CacheProcess

--- animation/src/traits/cache/cache_process.rs
+++ animation/src/traits/cache/cache_process.rs
@@ -1,7 +1,5 @@
 use futures::*;
 
-use std::mem;
-
 ///
 /// Represents an item that is in the cache, or is in the process of being generated
 ///
@@ -11,28 +9,16 @@ pub enum CacheProcess<Result, Process: Future<Item=Result>+Send> {
 
     /// The item has not been cached and is being generated
     Process(Process),
-
-    /// The item was cached but has been consumed
-    Consumed
 }
 
-impl<Result, Process: Future<Item=Result>+Send> Future for CacheProcess<Result, Process> {
+impl<Result: Clone, Process: Future<Item=Result>+Send> Future for CacheProcess<Result, Process> {
     type Item   = Result;
     type Error  = Process::Error;
 
     fn poll(&mut self) -> Poll<Result, Process::Error> {
         match self {
-            CacheProcess::Cached(_)         => {
-                let mut result = CacheProcess::Consumed;
-                mem::swap(&mut result, self);
-
-                match result {
-                    CacheProcess::Cached(result)    => Ok(Async::Ready(result)),
-                    _                               => panic!("Result vanished")
-                }
-            },
+            CacheProcess::Cached(result)    => Ok(Async::Ready(result.clone())),
             CacheProcess::Process(process)  => process.poll(),
-            CacheProcess::Consumed          => panic!("Cache process result has already been consumed")
         }
     }
 }
