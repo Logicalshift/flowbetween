83dc6170863df635634be56e4ebe532ffa1483b4
Andrew Hunter <andrew@logicalshift.co.uk>
2019-06-16 20:39:16 +0100

    First pass at fixing the session handler

--- user_interfaces/http_ui_actix/src/session_handler.rs
+++ user_interfaces/http_ui_actix/src/session_handler.rs
@@ -5,7 +5,7 @@ use flo_http_ui::*;
 
 use actix_web::*;
 use actix_web::http::*;
-use actix_web::dev::{Handler, AsyncResult};
+use actix_web::web::Json;
 use actix_web::Error;
 use futures::*;
 use futures::future;
@@ -16,7 +16,7 @@ use std::sync::*;
 ///
 /// Retrieves the base URL for a request
 /// 
-fn base_url<TState>(req: &HttpRequest<TState>) -> String {
+fn base_url(req: &HttpRequest) -> String {
     let full_url    = req.uri().path();
     let tail        = req.match_info().get("tail").unwrap_or("");
     let base_len    = full_url.len() - tail.len();
@@ -96,83 +96,77 @@ fn handle_no_session<Session: ActixSession>(session: Arc<Session>, base_url: Str
 ///
 /// Handles a JSON UI request
 /// 
-fn handle_ui_request<Session: ActixSession+'static>(req: HttpRequest<Arc<Session>>, ui_request: &UiHandlerRequest) -> impl Future<Item=HttpResponse, Error=Error> {
-    // Fetch the session ID from the request
-    let session_id  = ui_request.session_id.clone();
+fn handle_ui_request<Session: ActixSession+'static>(req: HttpRequest, ui_request: &UiHandlerRequest) -> impl Future<Item=HttpResponse, Error=Error> {
+    let session_state = req.app_data::<Arc<Session>>();
 
-    // Generate the response
-    let response: Box<dyn Future<Item=UiHandlerResponse, Error=Error>> = match session_id {
-        None                => Box::new(handle_no_session(Arc::clone(req.state()), base_url(&req), ui_request)),
-        Some(session_id)    => {
-            // Try to fetch the session corresponding to this ID
-            let session = req.state().get_session(&session_id);
+    if let Some(session_state) = session_state {
+        let session_state = Arc::clone(&session_state);
 
-            // Send the events to the appropriate session if we find one
-            match session {
-                Some(session)   => Box::new(handle_with_session::<Session>(&mut *session.lock().unwrap(), ui_request)),
-                None            => Box::new(handle_no_session(Arc::clone(req.state()), base_url(&req), ui_request))
+        // Fetch the session ID from the request
+        let session_id  = ui_request.session_id.clone();
+
+        // Generate the response
+        let response: Box<dyn Future<Item=UiHandlerResponse, Error=Error>> = match session_id {
+            None                => Box::new(handle_no_session(session_state, base_url(&req), ui_request)),
+            Some(session_id)    => {
+                // Try to fetch the session corresponding to this ID
+                let session = session_state.get_session(&session_id);
+
+                // Send the events to the appropriate session if we find one
+                match session {
+                    Some(session)   => Box::new(handle_with_session::<Session>(&mut *session.lock().unwrap(), ui_request)),
+                    None            => Box::new(handle_no_session(session_state, base_url(&req), ui_request))
+                }
             }
-        }
-    };
-    
-    // Turn the UI response into a JSON response
-    response
-        .map(move |response| {
-            req.build_response(StatusCode::OK)
-                .header(http::header::CONTENT_TYPE, "application/json; charset=utf-8")
-                .json(response)
-        })
+        };
+        
+        // Turn the UI response into a JSON response
+        response
+            .map(move |response| {
+                req.build_response(StatusCode::OK)
+                    .header(http::header::CONTENT_TYPE, "application/json; charset=utf-8")
+                    .json(response)
+            })
+    }
 }
 
 ///
 /// Creates the handler for an actix UI session
 /// 
-pub fn session_handler<Session: 'static+ActixSession>() -> impl Handler<Arc<Session>> {
-    // Create the session resource handler
-    let get_handler = Mutex::new(session_resource_handler());
+pub fn session_handler<Session: 'static+ActixSession>(req: &HttpRequest) -> Box<dyn Future<Item=HttpResponse, Error=Error>> {
+    match req.method() {
+        &Method::POST => {
+            // POST requests are used to send instructions to sessions
+            let req = req.clone();
 
-    // Wrap into a function
-    move |req: &HttpRequest<Arc<Session>>| {
-        match req.method() {
-            &Method::POST => {
-                // POST requests are used to send instructions to sessions
-                let req = req.clone();
+            // Request must contain a JSON body
+            let result = Json::<UiHandlerRequest>::extract(&req)
+                .then(move |ui_request| -> Box<dyn Future<Item=HttpResponse, Error=Error>> {
+                    match ui_request {
+                        Ok(ui_request) => {
+                            // Process this UI request
+                            Box::new(handle_ui_request(req, &*ui_request))
+                        },
 
-                // Request must contain a JSON body
-                let result = Json::<UiHandlerRequest>::extract(&req)
-                    .then(move |ui_request| -> Box<dyn Future<Item=HttpResponse, Error=Error>> {
-                        match ui_request {
-                            Ok(ui_request) => {
-                                // Process this UI request
-                                Box::new(handle_ui_request(req, &*ui_request))
-                            },
-
-                            Err(_err) => {
-                                // Failed to parse the JSON request for some reason
-                                Box::new(future::ok(req.build_response(StatusCode::BAD_REQUEST).body("FlowBetween session request is not in the expected format")))
-                            }
+                        Err(_err) => {
+                            // Failed to parse the JSON request for some reason
+                            Box::new(future::ok(req.build_response(StatusCode::BAD_REQUEST).body("FlowBetween session request is not in the expected format")))
                         }
-                    });
-                
-                // Request will be ready in the future
-                AsyncResult::future(Box::new(result))
-            },
+                    }
+                });
+            
+            // Request will be ready in the future
+            Box::new(result)
+        },
 
-            &Method::GET => {
-                // Get requests are handled by the session resource handler
-                let resource_request    = get_handler.lock().unwrap().handle(req);
-                let response            = resource_request.respond_to(&req);
+        &Method::GET => {
+            // Get requests are handled by the session resource handler
+            session_resource_handler(req)
+        },
 
-                match response {
-                    Ok(response)    => response.into(),
-                    Err(err)        => AsyncResult::err(err)
-                }
-            },
-
-            _ => {
-                // Other requests are not supported
-                AsyncResult::future(Box::new(future::ok(req.build_response(StatusCode::METHOD_NOT_ALLOWED).body("Method not allowed"))))
-            }
+        _ => {
+            // Other requests are not supported
+            Box::new(future::ok(req.build_response(StatusCode::METHOD_NOT_ALLOWED).body("Method not allowed")))
         }
     }
 }
