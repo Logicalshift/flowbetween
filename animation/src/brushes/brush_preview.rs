f48819a6b251e8fb72968d046a0bd518d46675a7
Andrew Hunter <andrew@logicalshift.co.uk>
2019-07-06 12:04:16 +0100

    Use the to_path() function from the vector element trait instead of turning brush strokes into paths manually

--- animation/src/brushes/brush_preview.rs
+++ animation/src/brushes/brush_preview.rs
@@ -175,13 +175,19 @@ impl BrushPreview {
         actions.push(LayerEdit::Path(when, BrushProperties(ElementId::Unassigned, self.brush_properties.clone())));
 
         // Path itself
-        let brush_points    = self.current_brush.brush_points_for_raw_points(&self.points);
-        let path            = vec![Path::from_drawing(self.current_brush.render_brush(&self.brush_properties, &brush_points))];
-        let path            = path.into_iter();
-        let path            = path.filter(|path| path.elements().count() > 2);
-        let path            = path.map(|path| path_remove_interior_points::<_, Path>(&vec![path], 0.01)).flatten();
-        let path            = path.map(|path| path.elements().collect::<Vec<_>>()).flatten();
-        let path            = path.collect::<Vec<_>>();
+        let mut vector_properties           = VectorProperties::default();
+        vector_properties.brush             = self.current_brush.clone();
+        vector_properties.brush_properties  = vector_properties.brush_properties;
+
+        let brush_points                    = self.current_brush.brush_points_for_raw_points(&self.points);
+        let brush_element                   = BrushElement::new(ElementId::Unassigned, Arc::new(brush_points));
+
+        let path                            = brush_element.to_path(&vector_properties);
+        let path                            = path.into_iter().flatten();
+        let path                            = path.filter(|path| path.elements().count() > 2);
+        let path                            = path.map(|path| path_remove_interior_points::<_, Path>(&vec![path], 0.01)).flatten();
+        let path                            = path.map(|path| path.elements().collect::<Vec<_>>()).flatten();
+        let path                            = path.collect::<Vec<_>>();
 
         if path.len() <= 0 {
             return;
