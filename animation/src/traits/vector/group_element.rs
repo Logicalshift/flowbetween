f0b2f22a90645ba0f9ab9b20141574678b2a333c
Andrew Hunter <andrew@logicalshift.co.uk>
2019-07-07 10:40:48 +0100

    Implement the various element methods for the grouped element
    Vector properties need to be local to the group (we can't inherit them outside), need a new 'when' property for the 'render' method to get this to fully work

--- animation/src/traits/vector/group_element.rs
+++ animation/src/traits/vector/group_element.rs
@@ -8,6 +8,7 @@ use super::super::path::*;
 use super::super::motion::*;
 
 use flo_canvas::*;
+use flo_curves::bezier::path::*;
 
 use std::sync::*;
 use std::time::Duration;
@@ -24,7 +25,64 @@ pub struct GroupElement {
     group_type: GroupType,
 
     /// The elements that make up this group
-    grouped_elements: Vec<Vector>
+    grouped_elements: Arc<Vec<Vector>>,
+}
+
+impl GroupElement {
+    ///
+    /// Creates a new group from a set of elements
+    ///
+    pub fn new(id: ElementId, group_type: GroupType, grouped_elements: Arc<Vec<Vector>>) -> GroupElement {
+        GroupElement {
+            id:                 id,
+            group_type:         group_type,
+            grouped_elements:   grouped_elements
+        }
+    }
+
+    ///
+    /// Renders the contents of this group in 'normal' mode
+    ///
+    fn render_normal(&self, gc: &mut dyn GraphicsPrimitives, properties: &VectorProperties) {
+        // Properties update internally to the group
+        let mut properties = Arc::new(properties.clone());
+
+        for elem in self.grouped_elements.iter() {
+            properties = elem.update_properties(properties);
+            properties.render(gc, elem.clone(), Duration::from_millis(0) /* TODO: need to pass time into elem, also consider making the properties.render take a vector reference rather than a vector directly */);
+        }
+    }
+
+    ///
+    /// Returns the added path for this element
+    ///
+    fn added_path(&self, properties: &VectorProperties) -> Vec<Path> {
+        // Get the paths for this rendering
+        let paths = self.grouped_elements.iter()
+            .flat_map(|elem| elem.to_path(properties))
+            .map(|path| path_remove_interior_points(&path, 0.01))
+            .collect::<Vec<_>>();
+
+        // Render if there are more than one path
+        if paths.len() > 0 {
+            // Add the paths into a single path
+            let paths = path_add_chain(&paths, 0.01);
+            paths
+        } else {
+            vec![]
+        }
+    }
+
+    ///
+    /// Renders the contents of this group in 'added' mode
+    ///
+    fn render_added(&self, gc: &mut dyn GraphicsPrimitives, properties: &VectorProperties) {
+        let paths = self.added_path(properties);
+
+        gc.draw_list(properties.brush.prepare_to_render(&properties.brush_properties));
+        paths.into_iter()
+            .for_each(|path| gc.draw_list(properties.brush.render_path(&properties.brush_properties, &path)));
+    }
 }
 
 impl VectorElement for GroupElement {
@@ -39,20 +97,27 @@ impl VectorElement for GroupElement {
     /// Retrieves the paths for this element, if there are any
     /// 
     fn to_path(&self, properties: &VectorProperties) -> Option<Vec<Path>> {
-        unimplemented!()
+        match self.group_type {
+            GroupType::Normal   => Some(self.grouped_elements.iter().flat_map(|elem| elem.to_path(properties)).flatten().collect()),
+            GroupType::Added    => Some(self.added_path(properties))
+        }
     }
 
     ///
     /// Renders this vector element
     /// 
     fn render(&self, gc: &mut dyn GraphicsPrimitives, properties: &VectorProperties) {
-        unimplemented!()
+        match self.group_type {
+            GroupType::Normal   => self.render_normal(gc, properties),
+            GroupType::Added    => self.render_added(gc, properties)
+        }
     }
 
     ///
     /// Returns the properties to use for future elements
     /// 
     fn update_properties(&self, properties: Arc<VectorProperties>) -> Arc<VectorProperties> { 
+        // Groups do not update properties
         properties
     }
 
@@ -61,14 +126,18 @@ impl VectorElement for GroupElement {
     /// in time.
     /// 
     fn motion_transform(&self, motion: &Motion, when: Duration) -> Vector {
-        unimplemented!()
+        let new_elements = self.grouped_elements.iter()
+            .map(|old_elem| old_elem.motion_transform(motion, when))
+            .collect();
+
+        Vector::Group(GroupElement::new(self.id, self.group_type, Arc::new(new_elements)))
     }
 
     ///
     /// Fetches the control points for this element
     /// 
     fn control_points(&self) -> Vec<ControlPoint> {
-        unimplemented!()
+        vec![]
     }
 
     ///
@@ -77,6 +146,6 @@ impl VectorElement for GroupElement {
     /// The vector here specifies the updated position for each control point in control_points
     /// 
     fn with_adjusted_control_points(&self, new_positions: Vec<(f32, f32)>) -> Vector {
-        unimplemented!()
+        Vector::Group(self.clone())
     }
 }
