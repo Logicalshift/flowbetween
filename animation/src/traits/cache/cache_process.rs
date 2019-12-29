1f5aae0e377922fa81f0232e39e1b6ae23059d29
Andrew Hunter <andrew@logicalshift.co.uk>
2019-12-29 19:02:41 +0000

    Work around pinning a bit more

--- animation/src/traits/cache/cache_process.rs
+++ animation/src/traits/cache/cache_process.rs
@@ -14,15 +14,16 @@ pub enum CacheProcess<Result, Process: Future<Output=Result>+Send> {
     Process(Process),
 }
 
-impl<Result: Clone, Process: Future<Output=Result>+Send> Future for CacheProcess<Result, Process> {
+impl<Result: Clone, Process: Unpin+Future<Output=Result>+Send> Future for CacheProcess<Result, Process>
+where Self: Unpin {
     type Output = Result;
 
     fn poll(mut self: Pin<&mut Self>, context: &mut Context) -> Poll<Result> {
-        match self {
+        match &mut *self {
             CacheProcess::Cached(result)    => Poll::Ready(result.clone()),
             CacheProcess::Process(process)  => {
                 // Cache value will become available in the future: poll for it
-                let poll_result = process.poll(context);
+                let poll_result = process.poll_unpin(context);
 
                 if let Poll::Ready(poll_result) = poll_result {
                     // Cache value is now available. Update the state to be just 'Cached' so we don't need to poll again
