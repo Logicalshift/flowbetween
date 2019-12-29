fbbab1ce015598abe0a493380347fe18be3fd539
Andrew Hunter <andrew@logicalshift.co.uk>
2019-12-29 15:53:27 +0000

    Context should no longer use spawned executors (not a futures 0.3 thing)

--- logging/src/context.rs
+++ logging/src/context.rs
@@ -2,18 +2,15 @@ use super::log_msg::*;
 
 use flo_stream::*;
 
-use futures::executor;
-use futures::executor::Spawn;
-
 ///
 /// Represents the context of a publisher
 /// 
 pub struct LogContext {
     /// Where messages for this context should be published
-    pub (crate) publisher: Spawn<Publisher<LogMsg>>,
+    pub (crate) publisher: Publisher<LogMsg>,
 
     /// If there are no subscribers to a particular log, messages are sent here instead
-    pub (crate) default: Option<Spawn<Publisher<LogMsg>>>,
+    pub (crate) default: Option<Publisher<LogMsg>>,
 
     /// The fields to add to log messages sent to this context
     pub (crate) fields: Vec<(String, String)>
@@ -25,7 +22,7 @@ impl LogContext {
     /// 
     pub fn new() -> LogContext {
         LogContext {
-            publisher:  executor::spawn(Publisher::new(100)),
+            publisher:  Publisher::new(100),
             default:    None,
             fields:     vec![]
         }
