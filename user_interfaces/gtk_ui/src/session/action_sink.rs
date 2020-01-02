7318b90a7b65cc4f2e85179a2103c606d277d878
Andrew Hunter <andrew@logicalshift.co.uk>
2020-01-02 23:30:50 +0000

    Add a 'run actions' style future as a replacement for the action sink

--- user_interfaces/gtk_ui/src/session/action_sink.rs
+++ user_interfaces/gtk_ui/src/session/action_sink.rs
@@ -1,10 +1,61 @@
 use super::super::gtk_thread::*;
 use super::super::gtk_action::*;
 
+use flo_stream::*;
+
 use futures::*;
+use futures::task::{Poll};
 
 use std::sync::*;
 
+///
+/// Returns a future that runs actions published to a publisher on a thread
+///
+pub async fn run_gtk_actions_for_thread(thread: Arc<GtkThread>, actions: WeakPublisher<Vec<GtkAction>>) {
+    // Subscribe to the actions that are being published
+    let mut actions = actions.subscribe();
+
+    // Dispatch them to the thread as they arrive
+    loop {
+        let actions = read_actions(&mut actions).await;
+
+        match actions {
+            None            => { return; }
+            Some(actions)   => { thread.perform_actions(actions) }
+        }
+    }
+}
+
+///
+/// Waits for an event and then reads as many as possible from the queue
+///
+async fn read_actions(subscriber: &mut Subscriber<Vec<GtkAction>>) -> Option<Vec<GtkAction>> {
+    // Await the first event
+    let events = subscriber.next().await;
+    if events.is_none() {
+        return None;
+    }
+
+    // Read any other waiting events from the subscriber
+    let mut poll_events     = events;
+    let mut poll_subscriber = Some(subscriber);
+
+    let (events, returned_subscriber) = future::poll_fn(move |context| {
+        // Add as many extra events as we can retrieve
+        while let Poll::Ready(Some(more_events)) = poll_subscriber.as_mut().unwrap().poll_next_unpin(context) {
+            poll_events.as_mut().unwrap().extend(more_events)
+        }
+
+        // Return the events (and the subscriber) to the sender
+        Poll::Ready((poll_events.take(), poll_subscriber.take()))
+    }).await;
+
+    // Return the events that we retrieved
+    subscriber = returned_subscriber.unwrap();
+    events
+}
+
+/*
 ///
 /// Sink used to send GTK actions to a thread
 ///
@@ -42,3 +93,4 @@ impl Sink for ActionSink {
         Ok(Async::Ready(()))
     }
 }
+*/
