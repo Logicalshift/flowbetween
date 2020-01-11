0b3b6a62268011b5ce63fb7ea2066c76690f8fb0
Andrew Hunter <andrew@logicalshift.co.uk>
2020-01-11 16:32:52 +0000

    Extract the 'run_command' routine and provide a way to generate the failure condition

--- cmdline/commands/src/command_runner.rs
+++ cmdline/commands/src/command_runner.rs
@@ -1,4 +1,5 @@
 use super::state::*;
+use super::error::*;
 use super::output::*;
 use super::command::*;
 use super::subcommands::*;
@@ -34,6 +35,39 @@ where InputStream: 'static+Stream<Item=FloCommand>+Unpin+Send {
 }
 
 ///
+/// Runs a single command
+///
+fn run_command<'a>(command: FloCommand, output: &'a mut Publisher<FloCommandOutput>, state: &'a mut CommandState) -> impl Future<Output=Result<(), CommandError>>+'a {
+    async move {
+        // Commands begin and end with a 'begin/finish' output
+        output.publish(FloCommandOutput::BeginCommand(command.clone())).await;
+
+        // Dispatch the command action
+        match command {
+            FloCommand::Version                     =>  { 
+                let msg = format!("{} ({}) v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_DESCRIPTION"), env!("CARGO_PKG_VERSION"));
+
+                output.publish(FloCommandOutput::Message(msg)).await;
+            }
+
+            FloCommand::ReadState                   => { output.publish(FloCommandOutput::State(state.clone())).await; }
+            FloCommand::SetState(ref new_state)     => { *state = new_state.clone(); }
+
+            FloCommand::ListAnimations              => { list_files(output, state).await }
+            FloCommand::ReadFrom(ref read_location) => { unimplemented!() }
+            FloCommand::WriteTo(ref write_location) => { unimplemented!() }
+            FloCommand::ReadAllEdits                => { unimplemented!() }
+            FloCommand::SummarizeEdits              => { unimplemented!() }
+        }
+
+        // Finish the command
+        output.publish(FloCommandOutput::FinishCommand(command.clone())).await;
+
+        Ok(())
+    }
+}
+
+///
 /// Runs the specified series of commands and writes the output to the given publisher
 ///
 fn run_commands<InputStream>(mut commands: InputStream, mut output: Publisher<FloCommandOutput>) -> impl Future<Output=()>+Send
@@ -43,28 +77,15 @@ where InputStream: 'static+Stream<Item=FloCommand>+Send+Unpin {
 
     async move {
         while let Some(command) = commands.next().await {
-            // Commands begin and end with a 'begin/finish' output
-            output.publish(FloCommandOutput::BeginCommand(command.clone())).await;
-
-            match command {
-                FloCommand::Version                     =>  { 
-                    let msg = format!("{} ({}) v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_DESCRIPTION"), env!("CARGO_PKG_VERSION"));
-
-                    output.publish(FloCommandOutput::Message(msg)).await;
+            // Run the next command
+            match run_command(command, &mut output, &mut state).await {
+                Ok(())      => { }
+                Err(err)    => {
+                    // Stop running commands if we get an error
+                    output.publish(FloCommandOutput::Failure(err)).await;
+                    break;
                 }
-
-                FloCommand::ReadState                   => { output.publish(FloCommandOutput::State(state.clone())).await; }
-                FloCommand::SetState(ref new_state)     => { state = new_state.clone(); }
-
-                FloCommand::ListAnimations              => { list_files(&mut output, &mut state).await }
-                FloCommand::ReadFrom(ref read_location) => { unimplemented!() }
-                FloCommand::WriteTo(ref write_location) => { unimplemented!() }
-                FloCommand::ReadAllEdits                => { unimplemented!() }
-                FloCommand::SummarizeEdits              => { unimplemented!() }
             }
-
-            // Finish the command
-            output.publish(FloCommandOutput::FinishCommand(command.clone())).await;
         }
     }
 }
