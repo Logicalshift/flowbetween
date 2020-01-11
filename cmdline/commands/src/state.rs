34a88a21ac8c5ef116dda7ad4b5459def1204abd
Andrew Hunter <andrew@logicalshift.co.uk>
2020-01-11 15:40:02 +0000

    Initialise the state with animations in memory

--- cmdline/commands/src/state.rs
+++ cmdline/commands/src/state.rs
@@ -1,3 +1,8 @@
+use super::storage_descriptor::*;
+
+use flo_animation::*;
+use flo_anim_sqlite::*;
+
 use std::sync::*;
 
 ///
@@ -7,9 +12,20 @@ use std::sync::*;
 pub struct CommandState(Arc<StateValue>);
 
 ///
+/// How an animation is stored within the command state
+///
+#[derive(Clone)]
+struct AnimationState(StorageDescriptor, Arc<dyn Animation>);
+
+///
 /// The internal value of a command state
 ///
 struct StateValue { 
+    /// The animation that this will read from
+    input_animation: AnimationState,
+
+    /// The animation that this will write to
+    output_animation: AnimationState
 }
 
 impl CommandState {
@@ -17,8 +33,16 @@ impl CommandState {
     /// Creates a new command state with the default settings
     ///
     pub fn new() -> CommandState {
-        CommandState(Arc::new(StateValue {
+        // Create the input and output animation in memory
+        let input_animation     = SqliteAnimation::new_in_memory();
+        let output_animation    = SqliteAnimation::new_in_memory();
+        let input_animation     = AnimationState(StorageDescriptor::InMemory, Arc::new(input_animation));
+        let output_animation    = AnimationState(StorageDescriptor::InMemory, Arc::new(output_animation));
 
+        // Generate the initial command state
+        CommandState(Arc::new(StateValue {
+            input_animation:    input_animation,
+            output_animation:   output_animation
         }))
     }
 }
