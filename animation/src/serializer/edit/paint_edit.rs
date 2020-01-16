263e292443230093cf6e66617777fe109ce9db80
Andrew Hunter <andrew@logicalshift.co.uk>
2020-01-16 23:00:46 +0000

    Add support for writing out the raw points of a brush stroke

--- animation/src/serializer/edit/paint_edit.rs
+++ animation/src/serializer/edit/paint_edit.rs
@@ -11,7 +11,29 @@ impl PaintEdit {
         match self {
             SelectBrush(elem, brush_defn, drawing_style)    => { data.write_chr('*'); elem.serialize(data); brush_defn.serialize(data); drawing_style.serialize(data); }
             BrushProperties(elem, props)                    => { data.write_chr('P'); elem.serialize(data); props.serialize(data); }
-            BrushStroke(elem, points)                       => { data.write_chr('S'); elem.serialize(data); unimplemented!("BrushStroke") }
+            
+            BrushStroke(elem, points)                       => {
+                data.write_chr('S'); 
+                elem.serialize(data); 
+
+                // Version 0
+                data.write_small_u64(0);
+
+                data.write_usize(points.len());
+                let mut last_pos = RawPoint::from((0.0, 0.0));
+
+                for point in points.iter() {
+                    data.write_next_f64(last_pos.position.0 as f64, point.position.0 as f64);
+                    data.write_next_f64(last_pos.position.1 as f64, point.position.1 as f64);
+                    data.write_next_f64(last_pos.pressure as f64, point.pressure as f64);
+                    data.write_next_f64(last_pos.tilt.0 as f64, point.tilt.0 as f64);
+                    data.write_next_f64(last_pos.tilt.1 as f64, point.tilt.1 as f64);
+
+                    last_pos = *point;
+                }
+                unimplemented!("BrushStroke");
+            }
         }
     }
 }
+
