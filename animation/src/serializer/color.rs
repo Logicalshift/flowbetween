45284d7143434dd8c6cac1df4497bf6100145dbb
Andrew Hunter <andrew@logicalshift.co.uk>
2020-01-16 22:54:31 +0000

    Add a more compact representation of colours (16-bit fixed point for values >0 and <1)

--- animation/src/serializer/color.rs
+++ animation/src/serializer/color.rs
@@ -8,8 +8,37 @@ use flo_canvas::*;
 pub fn serialize_color<Tgt: AnimationDataTarget>(color: &Color, data: &mut Tgt) {
     use self::Color::*;
 
+    // a, b, c, d are float values >= 0.0 and <= 1.0: this generates a compact byte representation
+    fn small_quad((a, b, c, d): (f32, f32, f32, f32)) -> Vec<u8> {
+        let mut result = vec![];
+
+        result.extend(((a*65535.0) as u16).to_le_bytes().iter());
+        result.extend(((b*65535.0) as u16).to_le_bytes().iter());
+        result.extend(((c*65535.0) as u16).to_le_bytes().iter());
+        result.extend(((d*65535.0) as u16).to_le_bytes().iter());
+
+        result
+    }
+
     match color {
-        Rgba(r, g, b, a)    => { data.write_chr('R'); data.write_f32(*r); data.write_f32(*g); data.write_f32(*b); data.write_f32(*a); }
-        Hsluv(h, s, l, a)   => { data.write_chr('H'); data.write_f32(*h); data.write_f32(*s); data.write_f32(*l); data.write_f32(*a); }
+        Rgba(r, g, b, a)    => { 
+            if r >= &0.0 && r <= &1.0 && g >= &0.0 && g <= &1.0 && b >= &0.0 && b <= &1.0 && a >= &0.0 && a <= &1.0 {
+                data.write_chr('r');
+                data.write_bytes(&small_quad((*r, *g, *b, *a)));
+            } else {
+                data.write_chr('R'); 
+                data.write_f32(*r); data.write_f32(*g); data.write_f32(*b); data.write_f32(*a);
+            }
+        }
+
+        Hsluv(h, s, l, a)   => { 
+            if h >= &0.0 && h <= &1.0 && s >= &0.0 && s <= &1.0 && l >= &0.0 && l <= &1.0 && a >= &0.0 && a <= &1.0 {
+                data.write_chr('h');
+                data.write_bytes(&small_quad((*h, *s, *l, *a)));
+            } else {
+                data.write_chr('H'); 
+                data.write_f32(*h); data.write_f32(*s); data.write_f32(*l); data.write_f32(*a);
+            }
+        }
     }
 }
