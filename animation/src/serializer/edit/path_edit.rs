8022225b3693ee3755a71bb451b16a4d7101ca97
Andrew Hunter <andrew@logicalshift.co.uk>
2020-01-21 09:29:18 +0000

    Add a path edit deserializer

--- animation/src/serializer/edit/path_edit.rs
+++ animation/src/serializer/edit/path_edit.rs
@@ -1,5 +1,8 @@
+use super::super::source::*;
 use super::super::target::*;
 use super::super::super::traits::*;
+
+use std::sync::*;
 
 impl PathEdit {
     ///
@@ -22,6 +25,41 @@ impl PathEdit {
                     last_point = component.serialize_next(&last_point, data);
                 }
             },
+        }
+    }
+
+    ///
+    /// Reads a path edit from the specified data source
+    ///
+    pub fn deserialize<Src: AnimationDataSource>(data: &mut Src) -> Option<PathEdit> {
+        match data.next_chr() {
+            'S' => {
+                ElementId::deserialize(data)
+                    .and_then(|elem| BrushDefinition::deserialize(data)
+                        .and_then(move |defn| BrushDrawingStyle::deserialize(data)
+                            .map(move |style| PathEdit::SelectBrush(elem, defn, style))))
+            }
+
+            'P' => {
+                ElementId::deserialize(data)
+                    .and_then(|elem| BrushProperties::deserialize(data)
+                        .map(move |props| PathEdit::BrushProperties(elem, props)))
+            }
+
+            '+' => {
+                ElementId::deserialize(data)
+                    .and_then(|elem| {
+                        let num_components  = data.next_usize();
+                        let components      = (0..num_components).into_iter()
+                            .map(|_idx| PathComponent::deserialize(data))
+                            .collect::<Option<Vec<_>>>();
+
+                        components.map(move |components|
+                            PathEdit::CreatePath(elem, Arc::new(components)))
+                    })
+            }
+
+            _   => None
         }
     }
 }
