8b5499e13682901648d6167319f949c385596acc
Andrew Hunter <andrew@logicalshift.co.uk>
2020-01-23 22:05:12 +0000

    Deserialize the brush properties element

--- animation/src/serializer/vector/brush_properties.rs
+++ animation/src/serializer/vector/brush_properties.rs
@@ -1,3 +1,4 @@
+use super::super::source::*;
 use super::super::target::*;
 use super::super::super::traits::*;
 
@@ -7,5 +8,30 @@ impl BrushPropertiesElement {
     ///
     pub fn serialize<Tgt: AnimationDataTarget>(&self, data: &mut Tgt) {
         self.brush_properties().serialize(data);
+    }
+
+    ///
+    /// Deserializes a brush properties element from a data source
+    ///
+    pub fn deserialize<Src: AnimationDataSource>(element_id: ElementId, data: &mut Src) -> Option<BrushPropertiesElement> {
+        BrushProperties::deserialize(data)
+            .map(move |props| BrushPropertiesElement::new(element_id, props))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn brush_properties_element() {
+        let mut encoded = String::new();
+        let element     = BrushPropertiesElement::new(ElementId::Assigned(1), BrushProperties::new());
+        element.serialize(&mut encoded);
+
+        let decoded     = BrushPropertiesElement::deserialize(ElementId::Assigned(1), &mut encoded.chars());
+        let decoded     = decoded.unwrap();
+
+        assert!(decoded.brush_properties() == element.brush_properties());
     }
 }
