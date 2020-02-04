50d398fe03298a6b4b07271e12c70a312bbbad7a
Andrew Hunter <andrew@logicalshift.co.uk>
2020-02-04 21:39:02 +0000

    Add a 'set size' test

--- animation/src/storage/editor/tests.rs
+++ animation/src/storage/editor/tests.rs
@@ -2,7 +2,11 @@ use super::*;
 use super::super::*;
 use super::super::super::traits::*;
 
+use flo_stream::*;
 use futures::prelude::*;
+use futures::executor;
+
+use std::sync::*;
 
 #[test]
 fn retrieve_default_size() {
@@ -13,5 +17,20 @@ fn retrieve_default_size() {
     // Default animation size is 1920x1080
     let size            = animation.size();
     assert!(size == (1920.0, 1080.0));
+}
+
+#[test]
+fn set_and_retrieve_size() {
+    // Create an animation
+    let in_memory_store = InMemoryStorage::new();
+    let animation       = create_animation_editor(move |commands| in_memory_store.get_responses(commands).boxed());
+
+    executor::block_on(async {
+        animation.edit().publish(Arc::new(vec![AnimationEdit::SetSize(1080.0, 720.0)])).await;
+    });
+
+    // Default animation size is 1920x1080
+    let size            = animation.size();
+    assert!(size == (1080.0, 720.0));
 }
 
