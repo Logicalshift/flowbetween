2796b9352ecb26f9cf7a1caad8741553f07cb5dd
Andrew Hunter <andrew@logicalshift.co.uk>
2020-03-07 15:37:45 +0000

    Test the storage code we just added

--- sqlite_storage/src/sqlite_core_tests.rs
+++ sqlite_storage/src/sqlite_core_tests.rs
@@ -1,3 +1,5 @@
+use flo_animation::storage::*;
+
 use rusqlite;
 use super::sqlite_core::*;
 
@@ -9,4 +11,25 @@ fn initialize_database() {
     if result.is_err() { println!("{:?}", result.as_ref().err()) }
 
     assert!(result.is_ok());
+}
+
+#[test]
+fn read_no_properties() {
+    let mut core    = SqliteCore::new(rusqlite::Connection::open_in_memory().unwrap());
+    core.initialize().unwrap();
+
+    let no_props    = core.run_commands(vec![StorageCommand::ReadAnimationProperties]);
+    assert!(no_props == vec![StorageResponse::NotFound]);
+}
+
+#[test]
+fn read_and_write_properties() {
+    let mut core    = SqliteCore::new(rusqlite::Connection::open_in_memory().unwrap());
+    core.initialize().unwrap();
+
+    let no_props    = core.run_commands(vec![StorageCommand::WriteAnimationProperties("Test".to_string())]);
+    assert!(no_props == vec![StorageResponse::Updated]);
+
+    let props       = core.run_commands(vec![StorageCommand::ReadAnimationProperties]);
+    assert!(props == vec![StorageResponse::AnimationProperties("Test".to_string())]);
 }
