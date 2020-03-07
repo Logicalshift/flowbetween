eca578299fcb1e13e6af4f7fcf2d5971e667f713
Andrew Hunter <andrew@logicalshift.co.uk>
2020-03-07 14:38:40 +0000

    Add some error state tracking and dispatch for individual storage commands

--- sqlite_storage/src/sqlite_core.rs
+++ sqlite_storage/src/sqlite_core.rs
@@ -9,7 +9,10 @@ const BASE_DATA_DEFN: &[u8]          = include_bytes!["../sql/flo_storage.sql"];
 ///
 pub (super) struct SqliteCore {
     /// The database connection
-    connection: rusqlite::Connection
+    connection: rusqlite::Connection,
+
+    /// If the core has encountered an error it can't recover from, this is what it is
+    error: Option<(StorageError, String)>
 }
 
 impl SqliteCore {
@@ -18,7 +21,22 @@ impl SqliteCore {
     ///
     pub fn new(connection: rusqlite::Connection) -> SqliteCore {
         SqliteCore {
-            connection: connection
+            connection: connection,
+            error:      None
+        }
+    }
+
+    ///
+    /// Checks a SQLite result for an error and sets the error flag if one has occurred
+    ///
+    fn check_error<T>(&mut self, val: Result<T, rusqlite::Error>) -> Result<T, rusqlite::Error> {
+        match val {
+            Err(e) => {
+                self.error = Some((StorageError::General, e.to_string()));
+                Err(e)
+            },
+
+            Ok(r)   => Ok(r)
         }
     }
 
@@ -28,13 +46,34 @@ impl SqliteCore {
     pub fn initialize(&mut self) -> Result<(), rusqlite::Error> {
         let defn = String::from_utf8_lossy(BASE_DATA_DEFN);
 
-        self.connection.execute_batch(&defn)
+        self.check_error(self.connection.execute_batch(&defn))
     }
 
     ///
     /// Runs some commands on this storage database
     ///
     pub fn run_commands(&mut self, commands: Vec<StorageCommand>) -> Vec<StorageResponse> {
-        vec![]
+        // If we're in an error state, then the result is just to indicate that we can't continue
+        if let Some((_err, msg)) = self.error.as_ref() {
+            return vec![StorageResponse::Error(StorageError::CannotContinueAfterError, msg.clone())];
+        }
+
+        // Process each of the commands in turn and flatten to a single response
+        let result = commands.into_iter()
+            .map(|cmd| self.run_command(cmd))
+            .collect::<Result<Vec<Vec<StorageResponse>>, _>>()
+            .map(|vec_of_vec| vec_of_vec.into_iter().flatten().collect());
+
+        match self.check_error(result) {
+            Err(err)    => vec![StorageResponse::Error(StorageError::General, err.to_string())],
+            Ok(result)  => result
+        }
+    }
+
+    ///
+    /// Runs an individual command and returns the values to generate in the response
+    ///
+    pub fn run_command(&mut self, command: StorageCommand) -> Result<Vec<StorageResponse>, rusqlite::Error> {
+        Ok(vec![])
     }
 }
