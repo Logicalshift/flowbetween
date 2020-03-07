68eaa9d25a32090521cb3b15adc095c8d467dd96
Andrew Hunter <andrew@logicalshift.co.uk>
2020-03-07 15:33:42 +0000

    Add a test module for the core, and check we can actually init the database

--- sqlite_storage/src/lib.rs
+++ sqlite_storage/src/lib.rs
@@ -1,4 +1,6 @@
 mod sqlite_core;
 mod sqlite_storage;
 
+#[cfg(test)] mod sqlite_core_tests;
+
 pub use self::sqlite_storage::*;
---
+++ sqlite_storage/src/sqlite_core_tests.rs
@@ -0,0 +1,12 @@
+use rusqlite;
+use super::sqlite_core::*;
+
+#[test]
+fn initialize_database() {
+    let mut core    = SqliteCore::new(rusqlite::Connection::open_in_memory().unwrap());
+    let result      = core.initialize();
+
+    if result.is_err() { println!("{:?}", result.as_ref().err()) }
+
+    assert!(result.is_ok());
+}
