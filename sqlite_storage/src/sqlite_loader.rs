22971ca5c5768d374a14655beac842d680c7eb7d
Andrew Hunter <andrew@logicalshift.co.uk>
2020-03-15 15:05:11 +0000

    Open existing animations or create new ones depending on if the path exists

--- sqlite_storage/src/sqlite_loader.rs
+++ sqlite_storage/src/sqlite_loader.rs
@@ -12,8 +12,15 @@ use rusqlite::{Connection, OpenFlags};
 pub fn sqlite_animation_loader() -> impl FileAnimation {
     AnimationLoader(|path| {
         // Connect to the database
-        let connection  = Connection::open_with_flags(path, OpenFlags::SQLITE_OPEN_READ_WRITE).unwrap();
-        let storage     = SqliteAnimationStorage::new_from_connection(connection);
+        let storage = if path.exists() {
+            // Open/restore an existing animation
+            let connection  = Connection::open_with_flags(path, OpenFlags::SQLITE_OPEN_READ_WRITE).unwrap();
+            SqliteAnimationStorage::from_connection(connection)
+        } else {
+            // Create a new animation
+            let connection  = Connection::open_with_flags(path, OpenFlags::SQLITE_OPEN_READ_WRITE | OpenFlags::SQLITE_OPEN_CREATE).unwrap();
+            SqliteAnimationStorage::new_from_connection(connection)
+        };
 
         // Create the editor for this animation
         create_animation_editor(move |commands| storage.get_responses(commands).boxed())
