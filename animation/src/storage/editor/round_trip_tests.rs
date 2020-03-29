1b6777ce345431b909dfff1f1cfc912d30bbbcb6
Andrew Hunter <andrew@logicalshift.co.uk>
2020-03-29 15:53:20 +0100

    Similar test to the delete test, but looking at the detach function

--- animation/src/storage/editor/round_trip_tests.rs
+++ animation/src/storage/editor/round_trip_tests.rs
@@ -754,6 +754,67 @@ fn delete_element() {
 }
 
 #[test]
+fn detach_element() {
+    let anim = create_animation();
+
+    anim.perform_edits(vec![
+        AnimationEdit::AddNewLayer(2),
+        AnimationEdit::Layer(2, LayerEdit::AddKeyFrame(Duration::from_millis(50))),
+        AnimationEdit::Layer(2, LayerEdit::Paint(Duration::from_millis(442), PaintEdit::SelectBrush(
+                ElementId::Unassigned,
+                BrushDefinition::Ink(InkDefinition::default()),
+                BrushDrawingStyle::Draw
+            )
+        )),
+        AnimationEdit::Layer(2, LayerEdit::Paint(Duration::from_millis(442), PaintEdit::
+            BrushProperties(ElementId::Assigned(125), BrushProperties { color: Color::Rgba(0.5, 0.2, 0.7, 1.0), opacity: 1.0, size: 32.0 }))),
+        AnimationEdit::Layer(2, LayerEdit::Paint(Duration::from_millis(442), PaintEdit::BrushStroke(ElementId::Assigned(126), Arc::new(vec![
+                    RawPoint::from((10.0, 10.0)),
+                    RawPoint::from((20.0, 5.0))
+                ])))),
+        AnimationEdit::Layer(2, LayerEdit::Paint(Duration::from_millis(442), PaintEdit::BrushStroke(ElementId::Assigned(127), Arc::new(vec![
+                    RawPoint::from((10.0, 10.0)),
+                    RawPoint::from((20.0, 5.0))
+                ])))),
+        AnimationEdit::Layer(2, LayerEdit::Paint(Duration::from_millis(442), PaintEdit::BrushStroke(ElementId::Assigned(128), Arc::new(vec![
+                    RawPoint::from((10.0, 10.0)),
+                    RawPoint::from((20.0, 5.0))
+                ])))),
+    ]);
+
+    {
+        let layer               = anim.get_layer_with_id(2).unwrap();
+        let frame               = layer.get_frame_at_time(Duration::from_millis(442));
+
+        let all_elements        = frame.vector_elements().unwrap()
+            .map(|elem| elem.id())
+            .collect::<Vec<_>>();
+        assert!(all_elements == vec![ElementId::Assigned(126), ElementId::Assigned(127), ElementId::Assigned(128)]);
+    }
+
+    // Detach is similar to delete but the element is not actually removed from anywhere other than the frame itself
+    anim.perform_edits(vec![
+        AnimationEdit::Element(vec![ElementId::Assigned(127)], ElementEdit::DetachFromFrame)
+    ]);
+
+    {
+        let layer               = anim.get_layer_with_id(2).unwrap();
+        let frame               = layer.get_frame_at_time(Duration::from_millis(442));
+        let element127          = frame.element_with_id(ElementId::Assigned(127));
+
+        assert!(match element127 {
+            Some(Vector::BrushStroke(ref brush_stroke)) => Some(brush_stroke.id()),
+            _ => None
+        } == None);
+
+        let all_elements        = frame.vector_elements().unwrap()
+            .map(|elem| elem.id())
+            .collect::<Vec<_>>();
+        assert!(all_elements == vec![ElementId::Assigned(126), ElementId::Assigned(128)]);
+    }
+}
+
+#[test]
 fn delete_layer_after_drawing_brush_stroke() {
     let anim = create_animation();
 
