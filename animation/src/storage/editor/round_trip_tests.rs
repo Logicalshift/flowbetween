81ebfb6bd9585128dadc921836dc0fc72965c382
Andrew Hunter <andrew@logicalshift.co.uk>
2020-04-01 21:16:01 +0100

    Deleting the first or the last element can mean we need to update the layer properties
    ... was kind of expecting this to break but it seems OK?

--- animation/src/storage/editor/round_trip_tests.rs
+++ animation/src/storage/editor/round_trip_tests.rs
@@ -754,6 +754,126 @@ fn delete_element() {
 }
 
 #[test]
+fn delete_first_element() {
+    let anim = create_animation();
+
+    anim.perform_edits(vec![
+        AnimationEdit::AddNewLayer(2),
+        AnimationEdit::Layer(2, LayerEdit::AddKeyFrame(Duration::from_millis(50))),
+        AnimationEdit::Layer(2, LayerEdit::Paint(Duration::from_millis(442), PaintEdit::SelectBrush(
+                ElementId::Unassigned,
+                BrushDefinition::Ink(InkDefinition::default()),
+                BrushDrawingStyle::Draw
+            )
+        )),
+        AnimationEdit::Layer(2, LayerEdit::Paint(Duration::from_millis(442), PaintEdit::
+            BrushProperties(ElementId::Assigned(125), BrushProperties { color: Color::Rgba(0.5, 0.2, 0.7, 1.0), opacity: 1.0, size: 32.0 }))),
+        AnimationEdit::Layer(2, LayerEdit::Paint(Duration::from_millis(442), PaintEdit::BrushStroke(ElementId::Assigned(126), Arc::new(vec![
+                    RawPoint::from((10.0, 10.0)),
+                    RawPoint::from((20.0, 5.0))
+                ])))),
+        AnimationEdit::Layer(2, LayerEdit::Paint(Duration::from_millis(442), PaintEdit::BrushStroke(ElementId::Assigned(127), Arc::new(vec![
+                    RawPoint::from((10.0, 10.0)),
+                    RawPoint::from((20.0, 5.0))
+                ])))),
+        AnimationEdit::Layer(2, LayerEdit::Paint(Duration::from_millis(442), PaintEdit::BrushStroke(ElementId::Assigned(128), Arc::new(vec![
+                    RawPoint::from((10.0, 10.0)),
+                    RawPoint::from((20.0, 5.0))
+                ])))),
+    ]);
+
+    {
+        let layer               = anim.get_layer_with_id(2).unwrap();
+        let frame               = layer.get_frame_at_time(Duration::from_millis(442));
+
+        let all_elements        = frame.vector_elements().unwrap()
+            .map(|elem| elem.id())
+            .collect::<Vec<_>>();
+        assert!(all_elements == vec![ElementId::Assigned(126), ElementId::Assigned(127), ElementId::Assigned(128)]);
+    }
+
+    anim.perform_edits(vec![
+        AnimationEdit::Element(vec![ElementId::Assigned(126)], ElementEdit::Delete)
+    ]);
+
+    {
+        let layer               = anim.get_layer_with_id(2).unwrap();
+        let frame               = layer.get_frame_at_time(Duration::from_millis(442));
+        let element126          = frame.element_with_id(ElementId::Assigned(126));
+
+        assert!(match element126 {
+            Some(Vector::BrushStroke(ref brush_stroke)) => Some(brush_stroke.id()),
+            _ => None
+        } == None);
+
+        let all_elements        = frame.vector_elements().unwrap()
+            .map(|elem| elem.id())
+            .collect::<Vec<_>>();
+        assert!(all_elements == vec![ElementId::Assigned(127), ElementId::Assigned(128)]);
+    }
+}
+
+#[test]
+fn delete_last_element() {
+    let anim = create_animation();
+
+    anim.perform_edits(vec![
+        AnimationEdit::AddNewLayer(2),
+        AnimationEdit::Layer(2, LayerEdit::AddKeyFrame(Duration::from_millis(50))),
+        AnimationEdit::Layer(2, LayerEdit::Paint(Duration::from_millis(442), PaintEdit::SelectBrush(
+                ElementId::Unassigned,
+                BrushDefinition::Ink(InkDefinition::default()),
+                BrushDrawingStyle::Draw
+            )
+        )),
+        AnimationEdit::Layer(2, LayerEdit::Paint(Duration::from_millis(442), PaintEdit::
+            BrushProperties(ElementId::Assigned(125), BrushProperties { color: Color::Rgba(0.5, 0.2, 0.7, 1.0), opacity: 1.0, size: 32.0 }))),
+        AnimationEdit::Layer(2, LayerEdit::Paint(Duration::from_millis(442), PaintEdit::BrushStroke(ElementId::Assigned(126), Arc::new(vec![
+                    RawPoint::from((10.0, 10.0)),
+                    RawPoint::from((20.0, 5.0))
+                ])))),
+        AnimationEdit::Layer(2, LayerEdit::Paint(Duration::from_millis(442), PaintEdit::BrushStroke(ElementId::Assigned(127), Arc::new(vec![
+                    RawPoint::from((10.0, 10.0)),
+                    RawPoint::from((20.0, 5.0))
+                ])))),
+        AnimationEdit::Layer(2, LayerEdit::Paint(Duration::from_millis(442), PaintEdit::BrushStroke(ElementId::Assigned(128), Arc::new(vec![
+                    RawPoint::from((10.0, 10.0)),
+                    RawPoint::from((20.0, 5.0))
+                ])))),
+    ]);
+
+    {
+        let layer               = anim.get_layer_with_id(2).unwrap();
+        let frame               = layer.get_frame_at_time(Duration::from_millis(442));
+
+        let all_elements        = frame.vector_elements().unwrap()
+            .map(|elem| elem.id())
+            .collect::<Vec<_>>();
+        assert!(all_elements == vec![ElementId::Assigned(126), ElementId::Assigned(127), ElementId::Assigned(128)]);
+    }
+
+    anim.perform_edits(vec![
+        AnimationEdit::Element(vec![ElementId::Assigned(128)], ElementEdit::Delete)
+    ]);
+
+    {
+        let layer               = anim.get_layer_with_id(2).unwrap();
+        let frame               = layer.get_frame_at_time(Duration::from_millis(442));
+        let element128          = frame.element_with_id(ElementId::Assigned(128));
+
+        assert!(match element128 {
+            Some(Vector::BrushStroke(ref brush_stroke)) => Some(brush_stroke.id()),
+            _ => None
+        } == None);
+
+        let all_elements        = frame.vector_elements().unwrap()
+            .map(|elem| elem.id())
+            .collect::<Vec<_>>();
+        assert!(all_elements == vec![ElementId::Assigned(126), ElementId::Assigned(127)]);
+    }
+}
+
+#[test]
 fn detach_element() {
     let anim = create_animation();
 
