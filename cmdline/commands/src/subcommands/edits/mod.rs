7b7483b3e5a77965cc5628ba9a49e48b5e25dc22
Andrew Hunter <andrew@logicalshift.co.uk>
2020-04-17 10:03:04 +0100

    Organise the subcommands into subdirectories as they're starting to grow a lot more

---
+++ cmdline/commands/src/subcommands/edits/mod.rs
@@ -0,0 +1,12 @@
+mod read_all_edits;
+mod write_all_edits;
+mod serialize_edits;
+mod deserialize_edits;
+mod summarize_edit_log;
+
+pub use self::read_all_edits::*;
+pub use self::write_all_edits::*;
+pub use self::serialize_edits::*;
+pub use self::write_to_catalog::*;
+pub use self::deserialize_edits::*;
+pub use self::summarize_edit_log::*;
---
+++ cmdline/commands/src/subcommands/list/mod.rs
@@ -0,0 +1,7 @@
+mod list_files;
+mod list_layers;
+mod list_elements;
+
+pub use self::list_files::*;
+pub use self::list_layers::*;
+pub use self::list_elements::*;
