8b7222ee49ca98cb9b31223ef852e721efed3d30
Andrew Hunter <andrew@logicalshift.co.uk>
2020-05-08 17:11:07 +0100

    Calculate the origin poing using the overall bounding box instead

--- animation/src/storage/editor/element_transform.rs
+++ animation/src/storage/editor/element_transform.rs
@@ -10,9 +10,9 @@ use std::sync::*;
 
 impl StreamAnimationCore {
     ///
-    /// Returns the origin point for an element
+    /// Returns the bounding box for an element
     ///
-    fn origin_for_element(frame: &KeyFrameCore, element_id: i64) -> Option<Coord2> {
+    fn bounds_for_element(frame: &KeyFrameCore, element_id: i64) -> Option<Rect> {
         if let Some(wrapper) = frame.elements.get(&ElementId::Assigned(element_id)) {
             // Get the properties for this element
             let properties      = frame.apply_properties_for_element(&wrapper.element, Arc::new(VectorProperties::default()), wrapper.start_time);
@@ -33,7 +33,7 @@ impl StreamAnimationCore {
             }
 
             // Origin is at the center of the path bounds
-            bounding_box.map(|bounding_box| bounding_box.center())
+            bounding_box
         } else {
             // Element does not exist
             None
@@ -51,35 +51,32 @@ impl StreamAnimationCore {
             }
 
             // The origin starts as the center point of all of the elments
-            let mut origin_sum          = Coord2(0.0, 0.0);
-            let mut num_elements: usize = 0;
+            let mut bounding_box: Option<Rect>  = None;
 
             for element_id in element_ids.iter() {
                 let element_id = *element_id;
 
                 if let Some(frame) = self.edit_keyframe_for_element(element_id).await {
                     // Calculate the origin for this element
-                    let element_origin = frame.future(move |frame| {
+                    let bounds = frame.future(move |frame| {
                         async move {
-                            Self::origin_for_element(frame, element_id)
+                            Self::bounds_for_element(frame, element_id)
                         }.boxed()
                     }).await.unwrap();
 
                     // Add to the sum of the origins
-                    if let Some(element_origin) = element_origin {
-                        origin_sum      = origin_sum + element_origin;
-                        num_elements    += 1;
+                    if let Some(bounds) = bounds {
+                        bounding_box = if let Some(bounding_box) = bounding_box {
+                            Some(bounding_box.union(bounds))
+                        } else {
+                            Some(bounds)
+                        }
                     }
                 }
             }
 
             // Set up the initial origin for the transformation
-            let mut transform_origin = if num_elements > 0 {
-                // Average of all the origin points of the elements
-                Some(origin_sum * (1.0 / (num_elements as f64)))
-            } else {
-                None
-            };
+            let mut transform_origin = bounding_box.map(|bounding_box| bounding_box.center());
         }
     }
 }
