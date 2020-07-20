bfaaf5829392bb4ad6dca08a3e508005b2d6e6aa
Andrew Hunter <andrew@logicalshift.co.uk>
2020-07-20 21:25:47 +0100

    Initialise and clear to a dull red to demonstrate that we can call OpenGL

--- user_interfaces/gtk_ui/src/widgets/flo_gfx_canvas_widget.rs
+++ user_interfaces/gtk_ui/src/widgets/flo_gfx_canvas_widget.rs
@@ -16,7 +16,10 @@ pub struct FloGfxCanvasWidget {
     id: WidgetId,
 
     /// The widget that the rest of the code will deal with
-    as_widget: gtk::Widget
+    as_widget: gtk::Widget,
+
+    /// The widget as a GL area
+    as_glarea: gtk::GLArea
 }
 
 impl FloGfxCanvasWidget {
@@ -24,14 +27,52 @@ impl FloGfxCanvasWidget {
     /// Creates a new GFX canvas widget that renders to the specified GL area
     ///
     pub fn new_opengl<W: Clone+Cast+IsA<gtk::GLArea>>(widget_id: WidgetId, widget: W) -> FloGfxCanvasWidget {
-        let id          = widget_id;
-        let as_glarea   = widget.clone().upcast::<gtk::GLArea>();
-        let as_widget   = as_glarea.clone().upcast::<gtk::Widget>();
+        // Get the widget as a GL area
+        let id              = widget_id;
+        let mut as_glarea   = widget.clone().upcast::<gtk::GLArea>();
+        let as_widget       = as_glarea.clone().upcast::<gtk::Widget>();
+
+        // Set it up
+        as_glarea.set_has_alpha(true);
+        as_glarea.set_has_stencil_buffer(true);
+
+        // Initialise on realize
+        Self::on_realize(&mut as_glarea);
+        Self::on_render(&mut as_glarea);
 
         FloGfxCanvasWidget {
             id,
-            as_widget
+            as_widget,
+            as_glarea
         }
+    }
+
+    ///
+    /// Installs the callback that deals with realizing the GLArea
+    ///
+    fn on_realize(glarea: &mut gtk::GLArea) {
+        glarea.connect_realize(move |gl_widget| { 
+            gl_widget.make_current();
+        });
+    }
+
+    ///
+    /// Installs the callback that deals with rendering the GLArea
+    ///
+    fn on_render(glarea: &mut gtk::GLArea) {
+        glarea.connect_render(move |gl_widget, _ctxt| {
+            let allocation      = gl_widget.get_allocation();
+            let scale           = gl_widget.get_scale_factor();
+
+            // Prepare to render
+            unsafe {
+                gl::ClearColor(0.5, 0.0, 0.0, 1.0);
+                gl::Clear(gl::COLOR_BUFFER_BIT);
+                gl::Viewport(0, 0, allocation.width*scale, allocation.height*scale);
+            }
+
+            Inhibit(true)
+        });
     }
 }
 
