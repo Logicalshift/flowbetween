d8f26f0c8062ace8b4b633c4bffc788eb16f9da9
Andrew Hunter <andrew@logicalshift.co.uk>
2020-07-22 23:05:41 +0100

    Make it so we can have multiple references to a texture if needed

--- gfx/src/gl_renderer/texture.rs
+++ gfx/src/gl_renderer/texture.rs
@@ -1,13 +1,19 @@
 use gl;
 
 use std::ptr;
+use std::rc::*;
 use std::ops::{Deref};
+
+struct TextureRef {
+    texture_id: gl::types::GLuint
+}
 
 ///
 /// Abstraction that manages an OpenGL texture
 ///
+#[derive(Clone)]
 pub struct Texture {
-    texture: gl::types::GLuint
+    texture: Rc<TextureRef>
 }
 
 impl Texture {
@@ -20,7 +26,7 @@ impl Texture {
             gl::GenTextures(1, &mut new_texture);
 
             Texture {
-                texture: new_texture
+                texture: Rc::new(TextureRef { texture_id: new_texture })
             }
         }
     }
@@ -30,10 +36,12 @@ impl Texture {
     ///
     pub fn create_empty(&mut self, width: u16, height: u16) {
         unsafe {
-            gl::BindTexture(gl::TEXTURE_2D, self.texture);
+            let texture_id = self.texture.texture_id;
 
-            gl::TextureParameteri(self.texture, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
-            gl::TextureParameteri(self.texture, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+            gl::BindTexture(gl::TEXTURE_2D, texture_id);
+
+            gl::TextureParameteri(texture_id, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+            gl::TextureParameteri(texture_id, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
 
             gl::TexImage2D(gl::TEXTURE_2D, 0, gl::RGBA as i32, width as gl::types::GLsizei, height as gl::types::GLsizei, 0, gl::RGBA, gl::UNSIGNED_BYTE, ptr::null());
         }
@@ -44,26 +52,28 @@ impl Texture {
     ///
     pub fn create_empty_msaa(&mut self, width: u16, height: u16, samples: usize) {
         unsafe {
+            let texture_id = self.texture.texture_id;
+
             // Clamp the number of samples to the maximum supported by the driver
             let mut max_samples = 1;
             gl::GetIntegerv(gl::MAX_COLOR_TEXTURE_SAMPLES, &mut max_samples);
             let samples = max_samples.min(samples as i32);
 
             // Set up a MSAA texture
-            gl::BindTexture(gl::TEXTURE_2D_MULTISAMPLE, self.texture);
+            gl::BindTexture(gl::TEXTURE_2D_MULTISAMPLE, texture_id);
 
-            gl::TextureParameteri(self.texture, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
-            gl::TextureParameteri(self.texture, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+            gl::TextureParameteri(texture_id, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+            gl::TextureParameteri(texture_id, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
 
             gl::TexImage2DMultisample(gl::TEXTURE_2D_MULTISAMPLE, samples, gl::RGBA, width as gl::types::GLsizei, height as gl::types::GLsizei, gl::FALSE);
         }
     }
 }
 
-impl Drop for Texture {
+impl Drop for TextureRef {
     fn drop(&mut self) {
         unsafe {
-            gl::DeleteTextures(1, &mut self.texture);
+            gl::DeleteTextures(1, &mut self.texture_id);
         }
     }
 }
@@ -72,6 +82,6 @@ impl Deref for Texture {
     type Target = gl::types::GLuint;
 
     fn deref(&self) -> &gl::types::GLuint {
-        &self.texture
+        &self.texture.texture_id
     }
 }
