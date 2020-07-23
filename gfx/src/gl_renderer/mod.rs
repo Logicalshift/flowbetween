510d4968d0c09bbb472c5e44ce2f3fe9883201b6
Andrew Hunter <andrew@logicalshift.co.uk>
2020-07-23 10:10:36 +0100

    Make the various OpenGL structure public (we only re-export GlRenderer though)

--- gfx/src/gl_renderer/mod.rs
+++ gfx/src/gl_renderer/mod.rs
@@ -1,4 +1,5 @@
 mod renderer;
+
 mod vertex_array;
 mod buffer;
 mod vertex;
@@ -8,3 +9,11 @@ mod render_target;
 mod shader_program;
 
 pub use self::renderer::*;
+
+pub use self::vertex_array::*;
+pub use self::buffer::*;
+pub use self::vertex::*;
+pub use self::shader::*;
+pub use self::texture::*;
+pub use self::render_target::*;
+pub use self::shader_program::*;
