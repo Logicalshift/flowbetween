db3d9ba6b898b8d8c6d1c06f383eee0eeffe7f2d
Andrew Hunter <andrew@logicalshift.co.uk>
2020-07-25 21:07:30 +0100

    Layers are built up from 'render entities': when we re-render the layer, we re-render the render entities

--- render_canvas/src/canvas_renderer.rs
+++ render_canvas/src/canvas_renderer.rs
@@ -1,5 +1,6 @@
 use super::tessellate::*;
 
+use flo_render as render;
 use flo_canvas as canvas;
 use flo_stream::*;
 
@@ -7,15 +8,53 @@ use ::desync::*;
 
 use futures::prelude::*;
 use num_cpus;
+use lyon::tessellation::{VertexBuffers};
 
 use std::sync::*;
+
+///
+/// Operation to use when drawing an item on a layer
+///
+#[derive(Clone, Copy)]
+enum LayerOperation {
+    /// Draw the vertex buffer
+    Draw,
+
+    /// Erase the vertex buffer
+    Erase
+}
+
+///
+/// Single rendering operation for a layer
+///
+enum RenderEntity {
+    /// Render operation is waiting to be tessellated
+    Tessellating(LayerOperation),
+
+    /// Tessellation waiting to be sent to the renderer
+    VertexBuffer(LayerOperation, VertexBuffers<render::Vertex2D, u16>),
+
+    /// Render a vertex buffer
+    DrawIndexed(LayerOperation, render::VertexBufferId, render::VertexBufferId)
+}
+
+///
+/// Definition of a layer in the canvas
+///
+struct Layer {
+    /// The render order for this layer
+    render_order: Vec<RenderEntity>
+}
 
 ///
 /// Changes commands for `flo_canvas` into commands for `flo_render`
 ///
 pub struct CanvasRenderer {
     /// The worker threads
-    workers: Vec<Arc<Desync<Tessellator>>>
+    workers: Vec<Arc<Desync<Tessellator>>>,
+
+    /// Layers defined by the canvas
+    layers: Vec<Layer>
 }
 
 impl CanvasRenderer {
@@ -33,7 +72,8 @@ impl CanvasRenderer {
 
         // Generate the final renderer
         CanvasRenderer {
-            workers: workers
+            workers:    workers,
+            layers:     vec![]
         }
     }
 }
