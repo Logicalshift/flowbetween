d2a79484f0267aabaa049d568bfa7286a4283119
Andrew Hunter <andrew@logicalshift.co.uk>
2020-07-27 11:17:21 +0100

    Test a double-fill of the same circle

--- render_canvas/tests/render_tests.rs
+++ render_canvas/tests/render_tests.rs
@@ -40,3 +40,52 @@ fn fill_simple_circle() {
         assert!(end_of_stream.is_none());
     })
 }
+
+#[test]
+fn fill_two_circles() {
+    // Draw a simple circle
+    let mut draw_circle: Vec<Draw> = vec![];
+    draw_circle.circle(0.0,0.0, 100.0);
+    draw_circle.fill();
+    draw_circle.fill();
+
+    executor::block_on(async {
+        // Create the renderer
+        let mut renderer    = CanvasRenderer::new();
+
+        // Get the upates for a drawing operation
+        let mut draw_stream = renderer.draw(draw_circle.into_iter());
+
+        // Should be a 'clear', an 'upload vertex buffer', an 'upload index buffer' and two 'draw indexed' instructions
+        let clear           = draw_stream.next().await;
+        assert!(clear.is_some());
+        assert!(match clear { Some(RenderAction::Clear(_)) => true, _ => false });
+
+        let upload_vertices = draw_stream.next().await;
+        assert!(upload_vertices.is_some());
+        assert!(match upload_vertices { Some(RenderAction::CreateVertex2DBuffer(_, _)) => true, _ => false });
+
+        let upload_indices  = draw_stream.next().await;
+        assert!(upload_indices.is_some());
+        assert!(match upload_indices { Some(RenderAction::CreateIndexBuffer(_, _)) => true, _ => false });
+
+        let draw_vertices   = draw_stream.next().await;
+        assert!(draw_vertices.is_some());
+        assert!(match draw_vertices { Some(RenderAction::DrawIndexedTriangles(_, _, _)) => true, _ => false });
+
+        let upload_vertices_2 = draw_stream.next().await;
+        assert!(upload_vertices_2.is_some());
+        assert!(match upload_vertices_2 { Some(RenderAction::CreateVertex2DBuffer(_, _)) => true, _ => false });
+
+        let upload_indices_2 = draw_stream.next().await;
+        assert!(upload_indices_2.is_some());
+        assert!(match upload_indices_2 { Some(RenderAction::CreateIndexBuffer(_, _)) => true, _ => false });
+
+        let draw_vertices_2  = draw_stream.next().await;
+        assert!(draw_vertices_2.is_some());
+        assert!(match draw_vertices_2 { Some(RenderAction::DrawIndexedTriangles(_, _, _)) => true, _ => false });
+
+        let end_of_stream   = draw_stream.next().await;
+        assert!(end_of_stream.is_none());
+    })
+}
