1dcaad43bf51b0398bd266b6891e611f1ecd2c7b
Andrew Hunter <andrew@logicalshift.co.uk>
2020-07-27 13:52:05 +0100

    Add a matrix type to the render library

---
+++ render/src/buffer/matrix.rs
@@ -0,0 +1,6 @@
+///
+/// Represents an OpenGL transformation matrix
+///
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Matrix(pub [[f32; 4]; 4]);
+
--- render/src/buffer/mod.rs
+++ render/src/buffer/mod.rs
@@ -1,3 +1,5 @@
 mod vertex;
+mod matrix;
 
 pub use self::vertex::*;
+pub use self::matrix::*;
