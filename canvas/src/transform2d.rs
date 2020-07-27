b375d197544174f90e1c5c1294ffa472ed5d66aa
Andrew Hunter <andrew@logicalshift.co.uk>
2020-07-27 14:32:56 +0100

    Add routines for creating scaled, rotated and multiplied transforms

--- canvas/src/transform2d.rs
+++ canvas/src/transform2d.rs
@@ -1,3 +1,5 @@
+use std::ops::{Mul};
+
 ///
 /// Represents a 2D affine transformation matrix
 ///
@@ -9,14 +11,45 @@ impl Transform2D {
     /// Creates the identity transform
     ///
     pub fn identity() -> Transform2D {
-        Transform2D([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
+        Transform2D([
+            [1.0, 0.0, 0.0], 
+            [0.0, 1.0, 0.0], 
+            [0.0, 0.0, 1.0]])
     }
 
     ///
     /// Creates a translation transformation
     ///
     pub fn translate(x: f32, y: f32) -> Transform2D {
-        Transform2D([[1.0, 0.0, x], [0.0, 1.0, y], [0.0, 0.0, 1.0]])
+        Transform2D([
+            [1.0, 0.0, x    ], 
+            [0.0, 1.0, y    ], 
+            [0.0, 0.0, 1.0  ]
+        ])
+    }
+
+    ///
+    /// Creates a scaling transformation
+    ///
+    pub fn scale(scale_x: f32, scale_y: f32) -> Transform2D {
+        Transform2D([
+            [scale_x,   0.0,        0.0], 
+            [0.0,       scale_y,    0.0], 
+            [0.0,       0.0,        1.0]])
+    }
+
+    ///
+    /// Creates a rotation transformation
+    ///
+    pub fn rotate(radians: f32) -> Transform2D {
+        let cos = f32::cos(radians);
+        let sin = f32::sin(radians);
+
+        Transform2D([
+            [cos,   -sin,   0.0],
+            [sin,   cos,    0.0],
+            [0.0,   0.0,    1.0]
+        ])
     }
 
     ///
@@ -88,5 +121,35 @@ impl Transform2D {
 
         Self::invert_matrix(matrix)
             .map(|inverted| Transform2D(inverted))
+    }
+}
+
+impl Mul<Transform2D> for Transform2D {
+    type Output=Transform2D;
+
+    fn mul(self, other: Transform2D) -> Transform2D {
+        let Transform2D(a) = self;
+        let Transform2D(b) = other;
+
+        Transform2D([
+            [a[0][0]*b[0][0] + a[0][1]*b[1][0] + a[0][2]*b[2][0],   a[0][0]*b[0][1] + a[0][1]*b[1][1] + a[0][2]*b[2][1],    a[0][0]*b[0][2] + a[0][1]*b[1][2] + a[0][2]*b[2][2]],
+            [a[1][0]*b[0][0] + a[1][1]*b[1][0] + a[1][2]*b[2][0],   a[1][0]*b[0][1] + a[1][1]*b[1][1] + a[1][2]*b[2][1],    a[1][0]*b[0][2] + a[1][1]*b[1][2] + a[1][2]*b[2][2]],
+            [a[2][0]*b[0][0] + a[2][1]*b[1][0] + a[2][2]*b[2][0],   a[2][0]*b[0][1] + a[2][1]*b[1][1] + a[2][2]*b[2][1],    a[2][0]*b[0][2] + a[2][1]*b[1][2] + a[2][2]*b[2][2]],
+        ])
+    }
+}
+
+impl Mul<&Transform2D> for &Transform2D {
+    type Output=Transform2D;
+
+    fn mul(self, other: &Transform2D) -> Transform2D {
+        let Transform2D(a) = self;
+        let Transform2D(b) = other;
+
+        Transform2D([
+            [a[0][0]*b[0][0] + a[0][1]*b[1][0] + a[0][2]*b[2][0],   a[0][0]*b[0][1] + a[0][1]*b[1][1] + a[0][2]*b[2][1],    a[0][0]*b[0][2] + a[0][1]*b[1][2] + a[0][2]*b[2][2]],
+            [a[1][0]*b[0][0] + a[1][1]*b[1][0] + a[1][2]*b[2][0],   a[1][0]*b[0][1] + a[1][1]*b[1][1] + a[1][2]*b[2][1],    a[1][0]*b[0][2] + a[1][1]*b[1][2] + a[1][2]*b[2][2]],
+            [a[2][0]*b[0][0] + a[2][1]*b[1][0] + a[2][2]*b[2][0],   a[2][0]*b[0][1] + a[2][1]*b[1][1] + a[2][2]*b[2][1],    a[2][0]*b[0][2] + a[2][1]*b[1][2] + a[2][2]*b[2][2]],
+        ])
     }
 }
