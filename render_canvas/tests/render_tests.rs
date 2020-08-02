6b870f5b43e87f9c35c254c2f31253a8d0e5660e
Andrew Hunter <andrew@logicalshift.co.uk>
2020-08-02 12:00:50 +0100

    Update tests to check for the new render instructions we generation

--- render_canvas/tests/render_tests.rs
+++ render_canvas/tests/render_tests.rs
@@ -5,6 +5,23 @@ use flo_canvas::*;
 use futures::prelude::*;
 use futures::executor;
 
+///
+/// Checks that the instructions beginning a new layer are valid
+///
+async fn check_layer_preamble<S: Unpin+Stream<Item=RenderAction>>(stream: &mut S) {
+    let select_render_target = stream.next().await;
+    assert!(match select_render_target { Some(RenderAction::SelectRenderTarget(_)) => true, _ => false });
+
+    let use_shader = stream.next().await;
+    assert!(match use_shader { Some(RenderAction::UseShader(_)) => true, _ => false });
+
+    let blend_mode = stream.next().await;
+    assert!(match blend_mode { Some(RenderAction::BlendMode(_)) => true, _ => false });
+
+    let set_transform = stream.next().await;
+    assert!(match set_transform { Some(RenderAction::SetTransform(_)) => true, _ => false });
+}
+
 #[test]
 fn fill_simple_circle() {
     // Draw a simple circle
@@ -19,10 +36,15 @@ fn fill_simple_circle() {
         // Get the upates for a drawing operation
         let mut draw_stream = renderer.draw(draw_circle.into_iter());
 
-        // Should be a 'clear', an 'upload vertex buffer', an 'upload index buffer' and a 'draw indexed'
-        let clear           = draw_stream.next().await;
-        assert!(clear.is_some());
-        assert!(match clear { Some(RenderAction::Clear(_)) => true, _ => false });
+        // Rendering starts at a 'clear', after some pre-rendering instructions, an 'upload vertex buffer', an 'upload index buffer' and a 'draw indexed'
+        loop {
+            let next = draw_stream.next().await;
+            assert!(next.is_some());
+
+            if let Some(RenderAction::Clear(_)) = &next {
+                break;
+            }
+        }
 
         let set_transform   = draw_stream.next().await;
         assert!(set_transform.is_some());
@@ -36,12 +58,14 @@ fn fill_simple_circle() {
         assert!(upload_indices.is_some());
         assert!(match upload_indices { Some(RenderAction::CreateIndexBuffer(_, _)) => true, _ => false });
 
+        // Layer preamble occurs after uploading the buffers
+        check_layer_preamble(&mut draw_stream).await;
+
         let draw_vertices   = draw_stream.next().await;
         assert!(draw_vertices.is_some());
         assert!(match draw_vertices { Some(RenderAction::DrawIndexedTriangles(_, _, _)) => true, _ => false });
 
-        let end_of_stream   = draw_stream.next().await;
-        assert!(end_of_stream.is_none());
+        // Stream then has some post-rendering instructions
     })
 }
 
@@ -61,9 +85,14 @@ fn fill_two_circles() {
         let mut draw_stream = renderer.draw(draw_circle.into_iter());
 
         // Should be a 'clear', an 'upload vertex buffer', an 'upload index buffer' and two 'draw indexed' instructions
-        let clear           = draw_stream.next().await;
-        assert!(clear.is_some());
-        assert!(match clear { Some(RenderAction::Clear(_)) => true, _ => false });
+        loop {
+            let next = draw_stream.next().await;
+            assert!(next.is_some());
+
+            if let Some(RenderAction::Clear(_)) = &next {
+                break;
+            }
+        }
 
         let set_transform   = draw_stream.next().await;
         assert!(set_transform.is_some());
@@ -77,10 +106,6 @@ fn fill_two_circles() {
         assert!(upload_indices.is_some());
         assert!(match upload_indices { Some(RenderAction::CreateIndexBuffer(_, _)) => true, _ => false });
 
-        let draw_vertices   = draw_stream.next().await;
-        assert!(draw_vertices.is_some());
-        assert!(match draw_vertices { Some(RenderAction::DrawIndexedTriangles(_, _, _)) => true, _ => false });
-
         let upload_vertices_2 = draw_stream.next().await;
         assert!(upload_vertices_2.is_some());
         assert!(match upload_vertices_2 { Some(RenderAction::CreateVertex2DBuffer(_, _)) => true, _ => false });
@@ -89,12 +114,16 @@ fn fill_two_circles() {
         assert!(upload_indices_2.is_some());
         assert!(match upload_indices_2 { Some(RenderAction::CreateIndexBuffer(_, _)) => true, _ => false });
 
+        // Layer preamble occurs after uploading the buffers
+        check_layer_preamble(&mut draw_stream).await;
+
+        let draw_vertices   = draw_stream.next().await;
+        assert!(draw_vertices.is_some());
+        assert!(match draw_vertices { Some(RenderAction::DrawIndexedTriangles(_, _, _)) => true, _ => false });
+
         let draw_vertices_2  = draw_stream.next().await;
         assert!(draw_vertices_2.is_some());
         assert!(match draw_vertices_2 { Some(RenderAction::DrawIndexedTriangles(_, _, _)) => true, _ => false });
-
-        let end_of_stream   = draw_stream.next().await;
-        assert!(end_of_stream.is_none());
     })
 }
 
@@ -114,32 +143,42 @@ fn draw_twice() {
             let mut draw_stream     = renderer.draw(draw_circle.into_iter());
 
             // Should be a 'clear', an 'upload vertex buffer', an 'upload index buffer' and a 'draw indexed'
-            let _clear              = draw_stream.next().await;
+            loop {
+                let next = draw_stream.next().await;
+                assert!(next.is_some());
+
+                if let Some(RenderAction::Clear(_)) = &next {
+                    break;
+                }
+            }
+
             let _set_transform      = draw_stream.next().await;
             let _upload_vertices    = draw_stream.next().await;
             let _upload_indices     = draw_stream.next().await;
             let _draw_vertices      = draw_stream.next().await;
-            let end_of_stream       = draw_stream.next().await;
-            assert!(end_of_stream.is_none());
         }
 
         // Draw again: re-render without regenerating the buffers
         let mut draw_stream = renderer.draw(vec![].into_iter());
 
-        // Should be a 'clear', an 'upload vertex buffer', an 'upload index buffer' and a 'draw indexed'
-        let clear           = draw_stream.next().await;
-        assert!(clear.is_some());
-        assert!(match clear { Some(RenderAction::Clear(_)) => true, _ => false });
+        // Should be a 'clear', and a 'draw indexed'
+        loop {
+            let next = draw_stream.next().await;
+            assert!(next.is_some());
+
+            if let Some(RenderAction::Clear(_)) = &next {
+                break;
+            }
+        }
 
         let set_transform   = draw_stream.next().await;
         assert!(set_transform.is_some());
         assert!(match set_transform { Some(RenderAction::SetTransform(_)) => true, _ => false });
 
+        check_layer_preamble(&mut draw_stream).await;
+
         let draw_vertices   = draw_stream.next().await;
         assert!(draw_vertices.is_some());
         assert!(match draw_vertices { Some(RenderAction::DrawIndexedTriangles(_, _, _)) => true, _ => false });
-
-        let end_of_stream   = draw_stream.next().await;
-        assert!(end_of_stream.is_none());
     })
 }
