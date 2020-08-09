909853f2df23921297135b7d2f2ee7d7786cad2f
Andrew Hunter <andrew@logicalshift.co.uk>
2020-08-09 21:11:32 +0100

    Add a buffer helper struct

---
+++ render/src/metal_renderer/buffer.rs
@@ -0,0 +1,40 @@
+use super::bindings::*;
+use crate::buffer::*;
+
+use metal;
+
+use std::ffi::{c_void};
+
+///
+/// Manages a metal buffer
+///
+pub struct Buffer {
+    /// The device that owns this buffer
+    device: metal::Device,
+
+    /// The buffer that this is managing
+    buffer: metal::Buffer
+}
+
+impl Buffer {
+    ///
+    /// Creates a buffer from some vertices
+    ///
+    pub fn from_vertices<VertexIterator: IntoIterator<Item=Vertex2D>>(device: &metal::Device, vertices: VertexIterator) -> Buffer {
+        // Convert the vertices to Metal format
+        let vertices = vertices.into_iter()
+            .map(|vertex| MetalVertex2D::from(vertex))
+            .collect::<Vec<_>>();
+
+        // Generate the buffer
+        let buffer = device.new_buffer_with_data(vertices.as_ptr() as *const c_void,
+            (vertices.len() * std::mem::size_of::<MetalVertex2D>()) as u64,
+            metal::MTLResourceOptions::CPUCacheModeDefaultCache | metal::MTLResourceOptions::StorageModeManaged);
+
+        // Return the buffer object
+        Buffer {
+            device: device.clone(),
+            buffer: buffer
+        }
+    }
+}
--- render/src/metal_renderer/mod.rs
+++ render/src/metal_renderer/mod.rs
@@ -1,5 +1,6 @@
 mod metal_renderer;
 mod bindings;
 mod convert;
+mod buffer;
 
 pub use self::metal_renderer::*;
