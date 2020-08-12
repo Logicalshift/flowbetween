152ddf5c703e5f723bca1259b82c48d265fc4e0e
Andrew Hunter <andrew@logicalshift.co.uk>
2020-08-12 20:59:57 +0100

    Add shader compile steps to build.rs

--- render/build.rs
+++ render/build.rs
@@ -4,6 +4,7 @@ use bindgen;
 
 use std::env;
 use std::path::{PathBuf};
+use std::process::{Command};
 
 fn main() {
     compile_metal();
@@ -13,8 +14,54 @@ fn main() {
 fn compile_metal() {
 }
 
+///
+/// Compiles a shader in the Metal shader language
+///
+#[cfg(feature="osx-metal")]
+fn compile_metal_shader(input_path: &str, output_path: &str) {
+    let shader_compile_output = Command::new("xcrun")
+        .args(&["-sdk", "macosx"])
+        .arg("metal")
+        .args(&["-I", "."])
+        .args(&["-c", input_path])
+        .args(&["-o", output_path])
+        .spawn()
+        .unwrap()
+        .wait_with_output()
+        .unwrap();
+
+    if !shader_compile_output.status.success() {
+        panic!("{}\n\n{}", String::from_utf8_lossy(&shader_compile_output.stdout), String::from_utf8_lossy(&shader_compile_output.stderr));
+    }
+}
+
+///
+/// Links some shaders compiled by compile_metal_shader
+///
+#[cfg(feature="osx-metal")]
+fn link_metal_shaders(input_paths: Vec<&str>, output_path: &str) {
+    let shader_link_output = Command::new("xcrun")
+        .args(&["-sdk", "macosx"])
+        .arg("metallib")
+        .args(input_paths)
+        .args(&["-o", output_path])
+        .spawn()
+        .unwrap()
+        .wait_with_output()
+        .unwrap();
+
+    if !shader_link_output.status.success() {
+        panic!("{}\n\n{}", String::from_utf8_lossy(&shader_link_output.stdout), String::from_utf8_lossy(&shader_link_output.stderr));
+    }
+}
+
 #[cfg(feature="osx-metal")]
 fn compile_metal() {
+    // Compile the shaders
+    println!("cargo:rerun-if-changed=shaders");
+    compile_metal_shader("shaders/simple/simple.metal", "simple.air");
+    link_metal_shaders(vec!["simple.air"], "simple.metallib");
+
     // Generate .rs files from the binding headers
     println!("cargo:rerun-if-changed=bindings");
 
