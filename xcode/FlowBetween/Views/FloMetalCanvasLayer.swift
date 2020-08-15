e9b63ba5514fa4b28eb21a657ea57abde5d11176
Andrew Hunter <andrew@logicalshift.co.uk>
2020-08-15 09:54:06 +0100

    Add a blank class to contain our metal layer

---
+++ xcode/FlowBetween/Views/FloMetalCanvasLayer.swift
@@ -0,0 +1,16 @@
+//
+//  FloMetalCanvasLayer.swift
+//  FlowBetween
+//
+//  Created by Andrew Hunter on 14/08/2020.
+//  Copyright © 2020 Andrew Hunter. All rights reserved.
+//
+
+import Cocoa
+import Metal
+
+///
+/// Layer that renders a canvas using GPU accelleration from Metal
+///
+class FloMetalCanvasLayer : CAMetalLayer {
+}
