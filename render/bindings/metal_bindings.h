e60a5ff579571f7a18c6723e1305ae5bfa5274a6
Andrew Hunter <andrew@logicalshift.co.uk>
2020-08-17 20:38:37 +0100

    Add some fragment shaders for rendering with textures or multisampled textures

--- render/bindings/metal_bindings.h
+++ render/bindings/metal_bindings.h
@@ -1,5 +1,5 @@
 ///
-/// The vertex input locations for the Metal shaders
+/// The input locations for the Metal vertex shaders
 ///
 typedef enum VertexInputIndex {
     /// The transformation matrix
@@ -8,3 +8,11 @@ typedef enum VertexInputIndex {
     /// The vertices to render
     VertexInputIndexVertices    = 1
 } VertexInputIndex;
+
+///
+/// The input locations for the Metal fragment shaders
+///
+typedef enum FragmentInputIndex {
+    /// The texture to render
+    FragmentIndexTexture        = 0
+} FragmentInputIndex;
