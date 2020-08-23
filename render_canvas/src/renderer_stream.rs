dafb9dfacd2e94a63e5639ba14bd2048ad39b10e
Andrew Hunter <andrew@logicalshift.co.uk>
2020-08-23 21:57:47 +0100

    Recursively call the renderer to render sprites

--- render_canvas/src/renderer_stream.rs
+++ render_canvas/src/renderer_stream.rs
@@ -100,7 +100,33 @@ impl RenderCore {
                 },
 
                 RenderSprite(sprite_id) => { 
-                    todo!("Sprite rendering")
+                    let sprite_id = *sprite_id;
+
+                    // Set the transform for the preceding rendering instructions
+                    let combined_transform  = &viewport_transform * &active_transform;
+                    let combined_matrix     = transform_to_matrix(&combined_transform);
+
+                    render_layer_stack.push(render::RenderAction::SetTransform(combined_matrix));
+
+                    if active_blend_mode == render::BlendMode::DestinationOut {
+                        // Preceding renders need to update the erase texture
+                        render_layer_stack.push(render::RenderAction::BlendMode(render::BlendMode::AllChannelAlphaDestinationOver));
+                        render_layer_stack.push(render::RenderAction::UseShader(render::ShaderType::Simple { erase_texture: None }));
+                        render_layer_stack.push(render::RenderAction::SelectRenderTarget(render::RenderTargetId(1)));
+                    } else {
+                        render_layer_stack.push(render::RenderAction::BlendMode(active_blend_mode));
+                        render_layer_stack.push(render::RenderAction::UseShader(render::ShaderType::Simple { erase_texture: None }));
+                        render_layer_stack.push(render::RenderAction::SelectRenderTarget(render::RenderTargetId(0)));
+                    }
+
+                    // Render the layer associated with the sprite
+                    if let Some(sprite_layer) = core.sprites.get(&sprite_id) {
+                        let sprite_layer = *sprite_layer;
+                        render_layer_stack.extend(core.render_layer(viewport_transform, sprite_layer));
+                    }
+
+                    // Reborrow the layer
+                    layer                   = core.layer(layer_handle);
                 },
 
                 SetTransform(new_transform) => {
