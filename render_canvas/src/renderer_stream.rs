fdb8ce7d70954dfd4daa2e3cc4e38fe17c9179a2
Andrew Hunter <andrew@logicalshift.co.uk>
2020-09-04 21:20:19 +0100

    Always start on render target 0, with no eraser specified

--- render_canvas/src/renderer_stream.rs
+++ render_canvas/src/renderer_stream.rs
@@ -142,6 +142,8 @@ impl RenderCore {
         let mut layer               = core.layer(layer_handle);
 
         render_state.transform      = Some(&viewport_transform * &active_transform);
+        render_state.render_target  = Some(render::RenderTargetId(0));
+        render_state.shader         = Some(render::ShaderType::Simple { erase_texture: None });
 
         for render_idx in 0..layer.render_order.len() {
             match &layer.render_order[render_idx] {
