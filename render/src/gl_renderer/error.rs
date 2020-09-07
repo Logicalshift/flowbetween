3b53825ac11821b99a0b7c1761807cdb8f4c1798
Andrew Hunter <andrew@logicalshift.co.uk>
2020-09-07 22:26:17 +0100

    println debug assertions if we get an open GL error in a release build instead of panicking

--- render/src/gl_renderer/error.rs
+++ render/src/gl_renderer/error.rs
@@ -12,11 +12,24 @@ pub enum GlError {
 ///
 /// Collects OpenGL errors and panics if there are any
 ///
+#[cfg(debug_assertions)]
 pub fn panic_on_gl_error(context: &str) {
     let errors = check_for_gl_errors();
 
     if errors.len() > 0 {
         panic!("{}: Unexpected OpenGL errors: {:?}", context, errors);
+    }
+}
+
+///
+/// Collects OpenGL errors and panics if there are any
+///
+#[cfg(not(debug_assertions))]
+pub fn panic_on_gl_error(context: &str) {
+    let errors = check_for_gl_errors();
+
+    if errors.len() > 0 {
+        println!("{}: Unexpected OpenGL errors: {:?}", context, errors);
     }
 }
 
