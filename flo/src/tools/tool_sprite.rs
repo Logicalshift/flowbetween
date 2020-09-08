470cbb2f5725b803275225e6cc215d2ab33608d0
Andrew Hunter <andrew@logicalshift.co.uk>
2020-09-08 09:42:17 +0100

    Add a sprite ID for rendering the selection outline to

--- flo/src/tools/tool_sprite.rs
+++ flo/src/tools/tool_sprite.rs
@@ -2,6 +2,7 @@ use flo_canvas::*;
 
 pub const SPRITE_BEZIER_POINT: SpriteId         = SpriteId(1);
 pub const SPRITE_BEZIER_CONTROL_POINT: SpriteId = SpriteId(2);
+pub const SPRITE_SELECTION_OUTLINE: SpriteId    = SpriteId(3);
 
 /// Sprites with IDs higher than this are not allocated to tools
 pub const SPRITE_FIRST_UNALLOCATED: SpriteId    = SpriteId(1024);
