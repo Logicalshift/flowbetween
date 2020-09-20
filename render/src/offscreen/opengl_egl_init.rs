9bc2c4b4fa70f737df18110378e8408f38244087
Andrew Hunter <andrew@logicalshift.co.uk>
2020-09-20 17:11:54 +0100

    Need to specify a shader before the rendering test will actually work (but we can now render an offscreen buffer)

--- render/src/offscreen/opengl_egl_init.rs
+++ render/src/offscreen/opengl_egl_init.rs
@@ -100,15 +100,43 @@ mod test {
         let mut renderer    = OpenGlOffscreenRenderer::new(100, 100);
         let black           = [0, 0, 0, 255];
         renderer.render(vec![
-            Clear(Rgba8([255, 255, 255, 255])),
+            Clear(Rgba8([128, 128, 128, 255])),
+            UseShader(ShaderType::Simple { erase_texture: None }),
             CreateVertex2DBuffer(VertexBufferId(0), vec![
-                Vertex2D { pos: [-1.0, -1.0], tex_coord: [0.0, 0.0], color: black },
-                Vertex2D { pos: [1.0, 1.0], tex_coord: [0.0, 0.0], color: black },
-                Vertex2D { pos: [1.0, -1.0], tex_coord: [0.0, 0.0], color: black },
+                Vertex2D { pos: [-1.0, -1.0],   tex_coord: [0.0, 0.0], color: black },
+                Vertex2D { pos: [1.0, 1.0],     tex_coord: [0.0, 0.0], color: black },
+                Vertex2D { pos: [1.0, -1.0],    tex_coord: [0.0, 0.0], color: black },
             ]),
             DrawTriangles(VertexBufferId(0), 0..3)
         ]);
 
         let image           = renderer.realize();
+
+        assert!(image.len() == 100*100*4);
+
+        // First pixel should be black
+        assert!(image[0] == 0);
+        assert!(image[1] == 0);
+        assert!(image[2] == 0);
+        assert!(image[3] == 255);
+
+        for y in 0..100 {
+            for x in 0..100 {
+                let pos         = (x + y*100) * 4;
+                let pixel       = (image[pos], image[pos+1], image[pos+2], image[pos+3]);
+
+                let expected    = if x >= y {
+                    (0, 0, 0, 255)
+                } else {
+                    (128, 128, 128, 255)
+                };
+
+                if pixel != expected {
+                    println!("{} {} {:?} {:?}", x, y, pixel, expected);
+                }
+
+                assert!(pixel == expected);
+            }
+        }
     }
 }
