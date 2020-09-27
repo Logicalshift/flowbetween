5e85a0e4348b2d63e05175525447e0be1fbc3737
Andrew Hunter <andrew@logicalshift.co.uk>
2020-09-27 15:55:59 +0100

    Move the offscreen rendering test into its own file so we can use it to check both CGL and EGL initialisation

--- render/src/offscreen/mod.rs
+++ render/src/offscreen/mod.rs
@@ -10,3 +10,5 @@ pub use self::offscreen_trait::*;
 
 #[cfg(all(feature="opengl", target_os = "linux"))]                          pub use self::opengl_egl_init::*;
 #[cfg(all(feature="opengl", target_os = "macos", not(feature="metal")))]    pub use self::opengl_cgl_init::*;
+
+#[cfg(test)] mod test;
