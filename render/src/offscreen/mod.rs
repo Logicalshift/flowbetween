036f8ca36f20bce10e2219e032f38fb1be5dd04b
Andrew Hunter <andrew@logicalshift.co.uk>
2020-09-27 15:53:42 +0100

    Add an empty CGL initialisation routine

--- render/src/offscreen/mod.rs
+++ render/src/offscreen/mod.rs
@@ -1,10 +1,12 @@
 mod error;
 mod offscreen_trait;
 
-#[cfg(feature="opengl")] mod opengl;
-#[cfg(all(feature="opengl", target_os = "linux"))] mod opengl_egl_init;
+#[cfg(feature="opengl")]                                                    mod opengl;
+#[cfg(all(feature="opengl", target_os = "linux"))]                          mod opengl_egl_init;
+#[cfg(all(feature="opengl", target_os = "macos", not(feature="metal")))]    mod opengl_cgl_init;
 
 pub use self::error::*;
 pub use self::offscreen_trait::*;
 
-#[cfg(all(feature="opengl", target_os = "linux"))] pub use self::opengl_egl_init::*;
+#[cfg(all(feature="opengl", target_os = "linux"))]                          pub use self::opengl_egl_init::*;
+#[cfg(all(feature="opengl", target_os = "macos", not(feature="metal")))]    pub use self::opengl_cgl_init::*;
