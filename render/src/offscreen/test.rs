918380e254fc2b5e42e70ac49202716f62ef2cd1
Andrew Hunter <andrew@logicalshift.co.uk>
2020-09-30 10:04:24 +0100

    Add an RGBA clear test that checks that the pixels are in the expected order

--- render/src/offscreen/test.rs
+++ render/src/offscreen/test.rs
@@ -22,7 +22,6 @@ mod test {
 
         assert!(image.len() == 100*100*4);
 
-        // First pixel should be black
         assert!(image[0] == 128);
         assert!(image[1] == 128);
         assert!(image[2] == 128);
@@ -34,6 +33,45 @@ mod test {
                 let pixel       = (image[pos], image[pos+1], image[pos+2], image[pos+3]);
 
                 let expected    = (128, 128, 128, 255);
+
+                if pixel != expected {
+                    println!("{} {} {:?} {:?}", x, y, pixel, expected);
+                }
+
+                assert!(pixel == expected);
+            }
+        }
+    }
+
+    #[test]
+    fn clears_in_rgba_order() {
+        // Initialise offscreen rendering
+        let mut context     = initialize_offscreen_rendering().unwrap();
+
+        // Draw a triangle in a 100x100 buffer
+        use self::RenderAction::*;
+
+        let mut renderer    = context.create_render_target(100, 100);
+        let black           = [0, 0, 0, 255];
+        renderer.render(vec![
+            Clear(Rgba8([128, 129, 130, 255])),
+        ]);
+
+        let image           = renderer.realize();
+
+        assert!(image.len() == 100*100*4);
+
+        assert!(image[0] == 128);
+        assert!(image[1] == 129);
+        assert!(image[2] == 130);
+        assert!(image[3] == 255);
+
+        for y in 0..100 {
+            for x in 0..100 {
+                let pos         = (x + y*100) * 4;
+                let pixel       = (image[pos], image[pos+1], image[pos+2], image[pos+3]);
+
+                let expected    = (128, 129, 130, 255);
 
                 if pixel != expected {
                     println!("{} {} {:?} {:?}", x, y, pixel, expected);
@@ -120,7 +158,8 @@ mod test {
 
         assert!(image.len() == 100*100*4);
 
-        // First pixel should be black
+        println!("({:x}, {:x}, {:x}, {:x})", image[0], image[1], image[2], image[3]);
+
         assert!(image[0] == 1);
         assert!(image[1] == 2);
         assert!(image[2] == 3);
