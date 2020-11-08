0b3812e1484c8b51fa487f800443042ad33ffaa0
Andrew Hunter <andrew@logicalshift.co.uk>
2020-11-08 11:55:43 +0000

    Add a simple test of the layer cut operation

--- animation/src/storage/tests/layers.rs
+++ animation/src/storage/tests/layers.rs
@@ -50,27 +50,6 @@ fn remove_layer() {
 }
 
 #[test]
-fn cannot_add_same_layer_again() {
-    /*
-    assert!(false); // TODO
-
-    let anim = create_animation();
-
-    anim.perform_edits(vec![
-        AnimationEdit::AddNewLayer(2)
-    ]);
-
-    assert!(anim.retrieve_and_clear_error().is_none());
-
-    anim.perform_edits(vec![
-        AnimationEdit::AddNewLayer(2)
-    ]);
-
-    assert!(anim.retrieve_and_clear_error().is_some());
-    */
-}
-
-#[test]
 fn single_layer_id() {
     let anim = create_animation();
 
@@ -333,4 +312,58 @@ fn get_active_brush() {
     let active_brush    = layer.as_vector_layer().unwrap().active_brush(Duration::from_millis(0));
 
     assert!(active_brush.is_some());
+}
+
+#[test]
+fn cut_square_into_doughnut() {
+    let anim = create_animation();
+
+    // Create a square
+    let square = Arc::new(vec![
+        PathComponent::Move(PathPoint::new(100.0, 100.0)),
+        PathComponent::Line(PathPoint::new(200.0, 100.0)),
+        PathComponent::Line(PathPoint::new(200.0, 200.0)),
+        PathComponent::Line(PathPoint::new(100.0, 200.0)),
+        PathComponent::Line(PathPoint::new(100.0, 100.0))
+    ]);
+
+    anim.perform_edits(vec![
+        AnimationEdit::AddNewLayer(2),
+        AnimationEdit::Layer(2, LayerEdit::AddKeyFrame(Duration::from_millis(0))),
+        AnimationEdit::Layer(2, LayerEdit::Path(Duration::from_millis(0), PathEdit::SelectBrush(
+                ElementId::Assigned(1),
+                BrushDefinition::Ink(InkDefinition::default()),
+                BrushDrawingStyle::Draw
+            ))),
+        AnimationEdit::Layer(2, LayerEdit::Path(Duration::from_millis(0), PathEdit::BrushProperties(ElementId::Assigned(2), BrushProperties::new()))),
+        AnimationEdit::Layer(2, LayerEdit::Path(Duration::from_millis(0), PathEdit::CreatePath(ElementId::Assigned(3), square)))
+    ]);
+
+    // Layer should contain the square
+    let layer       = anim.get_layer_with_id(2).unwrap();
+    let frame       = layer.get_frame_at_time(Duration::from_millis(0));
+    let elements    = frame.vector_elements().unwrap().collect::<Vec<_>>();
+
+    assert!(elements.len() == 1);
+
+    // Cut out its center
+    let center_square = Arc::new(vec![
+        PathComponent::Move(PathPoint::new(125.0, 125.0)),
+        PathComponent::Line(PathPoint::new(175.0, 125.0)),
+        PathComponent::Line(PathPoint::new(175.0, 175.0)),
+        PathComponent::Line(PathPoint::new(125.0, 175.0)),
+        PathComponent::Line(PathPoint::new(125.0, 125.0))
+    ]);
+
+    anim.perform_edits(vec![
+        AnimationEdit::Layer(2, LayerEdit::Cut { path: center_square, when: Duration::from_millis(0), inside_group: ElementId::Assigned(100), outside_group: ElementId::Assigned(101) })
+    ]);
+
+    // Layer should contain 2 groups with the inside and outside elements in it
+    let layer       = anim.get_layer_with_id(2).unwrap();
+    let frame       = layer.get_frame_at_time(Duration::from_millis(0));
+    let elements    = frame.vector_elements().unwrap().collect::<Vec<_>>();
+
+    assert!(elements.len() != 1);
+    assert!(elements.len() == 2);
 }
