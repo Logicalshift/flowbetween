e262e46e39e4b6f496786d5285d1873ce892584b
Andrew Hunter <andrew@logicalshift.co.uk>
2020-11-25 10:07:43 +0000

    Tell cargo to build in the build directory (not sure if xcode is smart enough to pick up the libraries this way though)

--- xcode/build-rust.sh
+++ xcode/build-rust.sh
@@ -4,17 +4,17 @@ CARGO=${HOME}/.cargo/bin/cargo
 
 case ${ARCHS} in
 	arm64*)
-		${CARGO} +nightly build -p flo_cocoa --target aarch64-apple-darwin --features cocoa --target-dir target-arm64
-        mkdir target
-        mkdir target/debug
-        cp -Ra target-arm64/aarch64-apple-darwin/debug target/debug
+		${CARGO} +nightly build -p flo_cocoa --target aarch64-apple-darwin --features cocoa --target-dir ${BUILD_DIR}/target-arm64
+        mkdir ${BUILD_DIR}/target
+        mkdir ${BUILD_DIR}/target/debug
+        cp -Ra ${BUILD_DIR}/target-arm64/aarch64-apple-darwin/debug target/debug
 		;;
 
     x86_64*)
-        ${CARGO} build -p flo_cocoa --target x86_64-apple-darwin --features cocoa --target-dir target-x86
-        mkdir target
-        mkdir target/debug
-        cp -Ra target-x86/x86_64-apple-darwin/debug target/debug
+        ${CARGO} build -p flo_cocoa --target x86_64-apple-darwin --features cocoa --target-dir ${BUILD_DIR}/target-x86
+        mkdir ${BUILD_DIR}/target
+        mkdir ${BUILD_DIR}/target/debug
+        cp -Ra ${BUILD_DIR}/target-x86/x86_64-apple-darwin/debug ${BUILD_DIR}/target/debug
         ;;
 
     *)
