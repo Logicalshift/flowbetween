b447fbb0cef718d9fbf1f131ed7d2294081b5bcc
Andrew Hunter <andrew@logicalshift.co.uk>
2020-11-25 20:11:42 +0000

    Update built settings to support different search paths for libraries in different architectures

--- xcode/build-rust.sh
+++ xcode/build-rust.sh
@@ -3,22 +3,42 @@
 CARGO=${HOME}/.cargo/bin/cargo
 
 case ${ARCHS} in
-	arm64*)
+    arm64*)
         echo -- BUILDING RUST COMPONENTS FOR ARM ARCHITECTURE
 
-		${CARGO} +nightly build -p flo_cocoa --target aarch64-apple-darwin --features cocoa --target-dir ${BUILD_DIR}/target-arm64
-        mkdir ${BUILD_DIR}/target
-        mkdir ${BUILD_DIR}/target/debug
-        cp -Ra ${BUILD_DIR}/target-arm64/aarch64-apple-darwin/debug target/debug
-		;;
+        ${CARGO} +nightly build -p flo_cocoa --target aarch64-apple-darwin --features cocoa --target-dir ${BUILD_DIR}/target-arm64
+        
+        mkdir ${BUILT_PRODUCTS_DIR}/debug
+        mkdir ${BUILT_PRODUCTS_DIR}/debug/arm64
+        mkdir ${BUILT_PRODUCTS_DIR}/release
+        mkdir ${BUILT_PRODUCTS_DIR}/release/arm64
+
+        if [ -e ${BUILD_DIR}/target-arm64/aarch64-apple-darwin/debug ]; then
+            cp -Ra ${BUILD_DIR}/target-arm64/aarch64-apple-darwin/debug/*.a ${BUILD_DIR}/target-arm64/aarch64-apple-darwin/debug/*.dylib ${BUILT_PRODUCTS_DIR}/debug/arm64
+        fi
+
+        if [ -e ${BUILD_DIR}/target-arm64/aarch64-apple-darwin/release ]; then
+            cp -Ra ${BUILD_DIR}/target-arm64/aarch64-apple-darwin/release/*.a ${BUILD_DIR}/target-arm64/aarch64-apple-darwin/release/*.dylib ${BUILT_PRODUCTS_DIR}/release/arm64
+        fi
+        ;;
 
     x86_64*)
         echo -- BUILDING RUST COMPONENTS FOR X86 ARCHITECTURE
 
-        ${CARGO} build -p flo_cocoa --target x86_64-apple-darwin --features cocoa --target-dir ${BUILD_DIR}/target-x86
-        mkdir ${BUILD_DIR}/target
-        mkdir ${BUILD_DIR}/target/debug
-        cp -Ra ${BUILD_DIR}/target-x86/x86_64-apple-darwin/debug ${BUILD_DIR}/target/debug
+        ${CARGO} build -p flo_cocoa --target x86_64-apple-darwin --features cocoa --target-dir ${BUILD_DIR}/target-x64
+        
+        mkdir ${BUILT_PRODUCTS_DIR}/debug
+        mkdir ${BUILT_PRODUCTS_DIR}/debug/x64
+        mkdir ${BUILT_PRODUCTS_DIR}/release
+        mkdir ${BUILT_PRODUCTS_DIR}/release/x64
+
+        if [ -e ${BUILD_DIR}/target-x64/x86_64-apple-darwin/debug ]; then
+            cp -Ra ${BUILD_DIR}/target-x64/x86_64-apple-darwin/debug/*.a ${BUILD_DIR}/target-x64/x86_64-apple-darwin/debug/*.dylib ${BUILT_PRODUCTS_DIR}/debug/x64
+        fi
+
+        if [ -e ${BUILD_DIR}/target-x64/x86_64-apple-darwin/release ]; then
+            cp -Ra ${BUILD_DIR}/target-x64/x86_64-apple-darwin/release/*.a ${BUILD_DIR}/target-x64/x86_64-apple-darwin/release/*.dylib ${BUILT_PRODUCTS_DIR}/release/x64
+        fi
         ;;
 
     *)
