144a649e70d9b3e79a0e26a782769389e3bbdd77
Andrew Hunter <andrew@logicalshift.co.uk>
2020-11-25 21:27:10 +0000

    bindgen needs some custom parameters to compile on the ARM64 architecture

--- render/build.rs
+++ render/build.rs
@@ -73,11 +73,23 @@ fn compile_metal() {
     // Generate .rs files from the binding headers
     println!("cargo:rerun-if-changed=bindings");
 
-    let bindings = bindgen::Builder::default()
-        .header("bindings/metal_vertex2d.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Unable to generate bindings");
+    let bindings = match &env::var("CARGO_CFG_TARGET_ARCH").unwrap()[..] {
+        "aarch64"   |
+        "arm64"     => {
+            bindgen::Builder::default()
+                .header("bindings/metal_vertex2d.h")
+                .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+                .clang_args(vec![ "-arch", "arm64" ])
+                .generate()
+            }
+
+        _ => {
+            bindgen::Builder::default()
+                .header("bindings/metal_vertex2d.h")
+                .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+                .generate()
+        }
+    }.expect("Unable to generate bindings");
 
     let out = PathBuf::from(env::var("OUT_DIR").unwrap());
     let out = out.join("metal_vertex2d.rs");
