fc6a3dd337dd069a7080010557bd1c2518f5dacf
Andrew Hunter <andrew@logicalshift.co.uk>
2020-12-28 13:31:41 +0000

    Add the ability to drag an edge instead of a control point

--- flo/src/standard_tools/adjust.rs
+++ flo/src/standard_tools/adjust.rs
@@ -812,6 +812,91 @@ impl Adjust {
     }
 
     ///
+    /// Performs a drag on an edge
+    ///
+    async fn drag_edge<Anim: 'static+EditableAnimation>(state: &mut AdjustToolState<Anim>, selected_edge: AdjustCurvePoint, initial_event: Painting) {
+        // Fetch the element being transformed and their properties
+        let when                = state.flo_model.timeline().current_time.get();
+        let frame               = state.flo_model.frame().frame.get();
+        let frame               = if let Some(frame) = frame { frame } else { return; };
+
+        let element             = frame.element_with_id(selected_edge.start_point.owner);
+        let element             = if let Some(element) = element { element } else { return; };
+        let element_properties  = frame.apply_properties_for_element(&element, Arc::new(VectorProperties::default()));
+
+        // Decompose the initial position
+        let (x1, y1) = initial_event.location;
+
+        // Read the inputs for the drag
+        while let Some(event) = state.input.next().await {
+            match event {
+                ToolInput::Paint(paint_event) => {
+                    if paint_event.pointer_id != initial_event.pointer_id {
+                        // Ignore events from other devices
+                        continue;
+                    }
+
+                    match paint_event.action {
+                        PaintAction::Continue |
+                        PaintAction::Prediction => {
+                            // Drag the control points and redraw the preview
+                            let (x2, y2)                = paint_event.location;
+                            let (dx, dy)                = (x2-x1, y2-y1);
+
+                            let adjusted_control_points = state.adjusted_control_points_for_curve_drag(&selected_edge, dx as f64, dy as f64);
+
+                            // Create the adjusted element
+                            let adjusted_element        = element.with_adjusted_control_points(adjusted_control_points, &*element_properties);
+
+                            // Draw the updated elements
+                            let mut preview = vec![Draw::Layer(LAYER_SELECTION), Draw::ClearLayer];
+                            preview.extend(Select::highlight_for_selection(&adjusted_element, &*element_properties, true).0);
+                            preview.extend(vec![Draw::Layer(LAYER_PREVIEW), Draw::ClearLayer]);
+
+                            state.actions.send_actions(vec![ToolAction::Overlay(OverlayAction::Draw(preview))]);
+                        }
+
+                        PaintAction::Finish => {
+                            // Commit the drag to the drawing
+                            let (x2, y2)                = paint_event.location;
+                            let (dx, dy)                = (x2-x1, y2-y1);
+
+                            // Compile the edits
+                            let adjusted_control_points = state.adjusted_control_points_for_curve_drag(&selected_edge, dx as f64, dy as f64);
+                            let edits                   = vec![AnimationEdit::Element(vec![element.id()], ElementEdit::SetControlPoints(adjusted_control_points, when))];
+
+                            // Send to the animation (invalidating the canvas will redraw the selection to its final value)
+                            state.flo_model.edit().publish(Arc::new(edits)).await;
+                            state.flo_model.timeline().invalidate_canvas();
+
+                            // Drag is finished
+                            return;
+                        }
+
+                        PaintAction::Start  |
+                        PaintAction::Cancel => {
+                            // Reset the preview
+                            let mut preview = vec![Draw::Layer(LAYER_SELECTION), Draw::ClearLayer];
+                            preview.extend(Self::drawing_for_selection_preview(&*state.flo_model));
+                            preview.extend(vec![Draw::Layer(LAYER_PREVIEW), Draw::ClearLayer]);
+                            preview.extend(Self::drawing_for_control_points(&*state.control_points.get(), &state.selected_control_points.get()));
+
+                            state.actions.send_actions(vec![ToolAction::Overlay(OverlayAction::Draw(preview))]);
+
+                            // Abort the drag
+                            return;
+                        }
+                    }
+                }
+
+                _ => { }
+            }
+        }
+
+        // Input stream ended prematurely
+    }
+
+    ///
     /// Starts a drag if the user moves far enough away from their current position (returning true if a drag was started)
     ///
     async fn maybe_drag<'a, Anim, ContinueFn, DragFuture>(state: &'a mut AdjustToolState<Anim>, initial_event: Painting, on_drag: ContinueFn) -> bool 
@@ -929,11 +1014,13 @@ impl Adjust {
             // Select the start and end point of the edge
             let mut selected_control_points = state.selected_control_points.get();
 
-            selected_control_points.insert(selected_edge.start_point);
-            selected_control_points.insert(selected_edge.end_point);
+            selected_control_points.insert(selected_edge.start_point.clone());
+            selected_control_points.insert(selected_edge.end_point.clone());
 
             state.selected_control_points.set(selected_control_points);
 
+            // Drag the edge if the user moves the cursor far enough
+            Self::maybe_drag(state, initial_event, move |state, initial_event| async move { Self::drag_edge(state, selected_edge, initial_event).await; }).await;
 
         } else if let Some(selected_element) = state.element_at_position(initial_event.location.0 as f64, initial_event.location.1 as f64) {
             
