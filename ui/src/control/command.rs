38881e8c7eec9899dd044d340dcd03784507857b
Andrew Hunter <andrew@logicalshift.co.uk>
2021-01-10 14:19:52 +0000

    Add equality and hashing routines for commands

--- ui/src/control/command.rs
+++ ui/src/control/command.rs
@@ -1,8 +1,11 @@
+use std::hash::{Hash, Hasher};
+
 ///
 /// A command is used to send a message to a UI component without specifically knowing how to operate it.
 ///
 /// Commands are usually bound to a single controller, but in the event that they're bound to multiple places,
-/// the command is sent to all of the places it's bound.
+/// the command is sent to all of the places it's bound. A command may optional have a set of required parameters
+/// which are specified by giving them names.
 ///
 /// Commands can be used for numerous functions:
 ///     * Keyboard shortcuts can invoke commands
@@ -10,6 +13,9 @@
 ///     * Control actions can invoke commands
 ///     * Command palettes can provide a way to find all available commands in the application and a shortcut to them
 ///     * Tests can use commands to test the UI 'headless' (FlowBetween's 'split' UI design makes true headless operation of the UI practical)
+///
+/// Two commands are considered the same if they have the same ID: the other parts (name, required parameters) are 
+/// only used to describe the command to the user.
 ///
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Command {
@@ -58,5 +64,26 @@ impl Command {
             name:                   name,
             required_parameters:    parameters
         }
+    }
+}
+
+impl PartialEq for Command {
+    ///
+    /// Two commands are equal if they have the same identifier
+    ///
+    fn eq(&self, other: &Command) -> bool {
+        self.identifier.eq(&other.identifier)
+    }
+}
+
+impl Eq for Command {
+}
+
+impl Hash for Command {
+    ///
+    /// Commands are hashed by their identifier only
+    ///
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.identifier.hash(state);
     }
 }
