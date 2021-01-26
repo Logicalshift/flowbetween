5861481ef650a1ceb8e7a119e88b37059de6c9e6
Andrew Hunter <andrew@logicalshift.co.uk>
2021-01-26 20:55:39 +0000

    Return a DrawEvent subscriber as well as the render action publisher when creating the render window

--- draw/src/glutin_thread_event.rs
+++ draw/src/glutin_thread_event.rs
@@ -1,11 +1,12 @@
+use super::draw_event::*;
+
 use flo_stream::*;
 use flo_render::*;
 
 ///
 /// Event that can be sent to a glutin thread
 ///
-#[derive(Clone)]
 pub enum GlutinThreadEvent {
     /// Creates a window that will render the specified actions
-    CreateRenderOnlyWindow(Subscriber<Vec<RenderAction>>)
+    CreateRenderWindow(Subscriber<Vec<RenderAction>>, Publisher<DrawEvent>)
 }
--- draw/src/render_window.rs
+++ draw/src/render_window.rs
@@ -1,3 +1,4 @@
+use super::draw_event::*;
 use super::glutin_thread::*;
 use super::glutin_thread_event::*;
 
@@ -7,14 +8,17 @@ use flo_render::*;
 ///
 /// Creates a window that can be rendered to by sending groups of render actions
 ///
-pub fn create_window_render() -> Publisher<Vec<RenderAction>> {
+pub fn create_window_render() -> (Publisher<Vec<RenderAction>>, Subscriber<DrawEvent>) {
     // Create the publisher to send the render actions to the stream
-    let mut publisher   = Publisher::new(1);
+    let mut render_publisher    = Publisher::new(1);
+    let mut event_publisher     = Publisher::new(1);
+
+    let event_subscriber        = event_publisher.subscribe();
 
     // Create a window that subscribes to the publisher
-    let glutin_thread   = glutin_thread();
-    glutin_thread.send_event(GlutinThreadEvent::CreateRenderOnlyWindow(publisher.subscribe()));
+    let glutin_thread           = glutin_thread();
+    glutin_thread.send_event(GlutinThreadEvent::CreateRenderWindow(render_publisher.subscribe(), event_publisher));
 
     // Publisher can now be used to render to the window
-    publisher
+    (render_publisher, event_subscriber)
 }
