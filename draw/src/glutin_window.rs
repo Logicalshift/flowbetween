e05d7145bde32b395fc564044f7766f53c905e0c
Andrew Hunter <andrew@logicalshift.co.uk>
2021-01-27 00:00:53 +0000

    Call gl::load_with to set up the GL functions for rendering

--- draw/src/glutin_window.rs
+++ draw/src/glutin_window.rs
@@ -4,6 +4,7 @@ use flo_render::*;
 
 use glutin::{WindowedContext, NotCurrent};
 use futures::prelude::*;
+use gl;
 
 ///
 /// Manages the state of a Glutin window
@@ -50,16 +51,26 @@ pub (super) async fn send_actions_to_window(window: GlutinWindow, render_actions
             let current_context = current_context.make_current();
             let current_context = if let Ok(context) = current_context { context } else { break; };
 
+            // Get informtion about the current context
+            let size            = current_context.window().inner_size();
+            let width           = size.width as usize;
+            let height          = size.height as usize;
+
             // Create the renderer (needs the OpenGL functions to be loaded)
             if window.renderer.is_none() {
+                // Load the functions for the current context
+                // TODO: we're assuming they stay loaded to avoid loading them for every render, which might not be safe
+                gl::load_with(|symbol_name| {
+                    unsafe {
+                        current_context.get_proc_address(symbol_name)
+                    }
+                });
+
+                // Create the renderer
                 window.renderer = Some(GlRenderer::new());
             }
 
-            // Draw the render actions to the window context
-            let size        = current_context.window().inner_size();
-            let width       = size.width as usize;
-            let height      = size.height as usize;
-
+            // Perform the rendering actions
             window.renderer.as_mut().map(move |renderer| {
                 renderer.prepare_to_render_to_active_framebuffer(width, height);
                 renderer.render(next_action);
