baf4e17faf168b47a067ddf5d163e703b7726d7f
Andrew Hunter <andrew@logicalshift.co.uk>
2021-01-29 23:05:45 +0000

    Make it so we can queue a lot of events

--- draw/src/render_window.rs
+++ draw/src/render_window.rs
@@ -11,7 +11,7 @@ use flo_render::*;
 pub fn create_render_window() -> (Publisher<Vec<RenderAction>>, Subscriber<DrawEvent>) {
     // Create the publisher to send the render actions to the stream
     let mut render_publisher    = Publisher::new(1);
-    let mut event_publisher     = Publisher::new(1);
+    let mut event_publisher     = Publisher::new(1000);
 
     let event_subscriber        = event_publisher.subscribe();
 
