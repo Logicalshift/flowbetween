20f9318434871d85b3d0f5dc13425a5dc42025e0
Andrew Hunter <andrew@logicalshift.co.uk>
2021-01-31 22:18:17 +0000

    Update the canvas background style so it looks the same as it usually does

--- flo/src/style.rs
+++ flo/src/style.rs
@@ -3,7 +3,7 @@ use flo_canvas::*;
 pub const DEFAULT_TEXT:                     Color = Color::Rgba(0.85, 0.95, 0.9, 1.0);
 
 pub const MAIN_BACKGROUND:                  Color = Color::Rgba(0.25, 0.25, 0.25, 1.0);
-pub const CANVAS_BACKGROUND:                Color = Color::Rgba(0.4, 0.4, 0.4, 1.0);
+pub const CANVAS_BACKGROUND:                Color = Color::Rgba(0.25, 0.25, 0.25, 1.0);
 pub const TIMELINE_BACKGROUND:              Color = Color::Rgba(0.20, 0.20, 0.20, 1.0);
 pub const TIMELINE_SELECTED_LAYER:          Color = Color::Rgba(0.20, 0.35, 0.50, 1.0);
 pub const TIMESCALE_BORDER:                 Color = Color::Rgba(0.65, 0.65, 0.65, 1.0);
