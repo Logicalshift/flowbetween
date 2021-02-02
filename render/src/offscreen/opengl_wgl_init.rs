da89653b5e7b33c81b9e2f0d92853de485be08a1
Andrew Hunter <andrew@logicalshift.co.uk>
2021-02-02 09:21:27 +0000

    Fix up setting up the window class

--- render/src/offscreen/opengl_wgl_init.rs
+++ render/src/offscreen/opengl_wgl_init.rs
@@ -4,10 +4,12 @@ use super::offscreen_trait::*;
 
 use flo_render_gl_offscreen::wgl;
 use flo_render_gl_offscreen::winapi;
-use flo_render_gl_offscreen::winapi::um::winuser::{CreateWindowExW, RegisterClassExW, GetDC, WNDCLASSEXW, CS_OWNDC, WS_OVERLAPPEDWINDOW};
+use flo_render_gl_offscreen::winapi::um::winuser::{CreateWindowExW, RegisterClassExW, GetDC, DefWindowProcW, WNDCLASSEXW, CS_OWNDC, WS_EX_APPWINDOW, WS_OVERLAPPEDWINDOW};
 use flo_render_gl_offscreen::winapi::um::wingdi::{ChoosePixelFormat, SetPixelFormat, PIXELFORMATDESCRIPTOR, PFD_DRAW_TO_WINDOW, PFD_SUPPORT_OPENGL, PFD_DOUBLEBUFFER, PFD_TYPE_RGBA, PFD_MAIN_PLANE};
 use flo_render_gl_offscreen::winapi::um::libloaderapi::{GetModuleHandleW};
+use flo_render_gl_offscreen::winapi::um::errhandlingapi::{GetLastError};
 use flo_render_gl_offscreen::winapi::shared::windef::{HWND};
+use flo_render_gl_offscreen::winapi::shared::winerror::{ERROR_CLASS_ALREADY_EXISTS};
 
 use std::mem;
 use std::ptr;
@@ -31,26 +33,33 @@ pub fn initialize_offscreen_rendering() -> Result<impl OffscreenRenderContext, R
     // See also https://www.khronos.org/opengl/wiki/Creating_an_OpenGL_Context_(WGL)
     unsafe {
         // Set up the window class
-        let class_name              = OsStr::new("flo_draw OpenGL window").encode_wide().chain(Some(0).into_iter()).collect::<Vec<_>>();
+        let class_name              = OsStr::new("FloDraw Class").encode_wide().chain(Some(0).into_iter()).collect::<Vec<_>>();
         let window_name             = OsStr::new("flo_draw OpenGL window").encode_wide().chain(Some(0).into_iter()).collect::<Vec<_>>();
+
         let mut window_class        = mem::zeroed::<WNDCLASSEXW>();
+
+        window_class.cbSize         = mem::size_of::<WNDCLASSEXW>() as u32;
         window_class.lpszClassName  = class_name.as_ptr();
         window_class.style          = CS_OWNDC;
+        window_class.hInstance      = GetModuleHandleW(ptr::null());
+        window_class.lpfnWndProc    = Some(DefWindowProcW);
 
-        if RegisterClassExW(&window_class) != 0 {
-            panic!("Unable to register window class for offscreen rendering");
+        if RegisterClassExW(&window_class) == 0 {
+            if GetLastError() != ERROR_CLASS_ALREADY_EXISTS {
+                panic!("Unable to register window class for offscreen rendering: {}", GetLastError());
+            }
         }
 
         // Create a window (we never show this or render to it, but we need a device context)
-        let window                  = CreateWindowExW(WS_OVERLAPPEDWINDOW, class_name.as_ptr(), window_name.as_ptr(), WS_OVERLAPPEDWINDOW, 0, 0, 1024, 768, ptr::null_mut(), ptr::null_mut(), GetModuleHandleW(ptr::null()), ptr::null_mut());
+        let window                  = CreateWindowExW(WS_EX_APPWINDOW, class_name.as_ptr(), window_name.as_ptr(), WS_OVERLAPPEDWINDOW, 0, 0, 1024, 768, ptr::null_mut(), ptr::null_mut(), GetModuleHandleW(ptr::null()), ptr::null_mut());
         if window.is_null() {
-            panic!("Unable to create window for offscreen rendering");
+            panic!("Unable to create window for offscreen rendering: {}", GetLastError());
         }
 
         // Fetch the device context for the window
         let dc                      = GetDC(window);
         if dc.is_null() {
-            panic!("Unable to fetch device context for offscreen window");
+            panic!("Unable to fetch device context for offscreen window: {}", GetLastError());
         }
 
         // Set up the pixel format descriptor
