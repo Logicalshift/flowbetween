ae49d0784f2814eea3a04a775a3bf9277c2dbfde
Andrew Hunter <andrew@logicalshift.co.uk>
2021-02-05 22:13:51 +0000

    Read render actions using the new stream

--- draw/src/glutin_window.rs
+++ draw/src/glutin_window.rs
@@ -3,6 +3,7 @@ use super::window_properties::*;
 
 use flo_stream::*;
 use flo_render::*;
+use flo_binding::*;
 
 use glutin::{WindowedContext, NotCurrent};
 use futures::prelude::*;
@@ -39,59 +40,71 @@ impl GlutinWindow {
 ///
 pub (super) async fn send_actions_to_window(window: GlutinWindow, render_actions: Subscriber<Vec<RenderAction>>, events: Publisher<DrawEvent>, window_properties: WindowProperties) {
     // Read events from the render actions list
-    let mut render_actions  = render_actions;
     let mut window          = window;
     let mut events          = events;
+    let mut window_actions  = WindowUpdateStream { 
+        render_stream:      render_actions, 
+        title_stream:       follow(window_properties.title),
+        size:               follow(window_properties.size),
+        fullscreen:         follow(window_properties.fullscreen),
+        has_decorations:    follow(window_properties.has_decorations)
+    };
 
-    while let Some(next_action) = render_actions.next().await {
-        // Do nothing if there are no actions
-        if next_action.len() == 0 {
-            continue;
-        }
+    while let Some(next_action) = window_actions.next().await {
+        match next_action {
+            WindowUpdate::Render(next_action)   => {
+                // Do nothing if there are no actions
+                if next_action.len() == 0 {
+                    continue;
+                }
 
-        unsafe {
-            // TODO: report errors if we can't set the context rather than just stopping mysteriously
+                unsafe {
+                    // TODO: report errors if we can't set the context rather than just stopping mysteriously
 
-            // Make the current context current
-            let current_context = window.context.take().expect("Window context");
-            let current_context = current_context.make_current();
-            let current_context = if let Ok(context) = current_context { context } else { break; };
+                    // Make the current context current
+                    let current_context = window.context.take().expect("Window context");
+                    let current_context = current_context.make_current();
+                    let current_context = if let Ok(context) = current_context { context } else { break; };
 
-            // Get informtion about the current context
-            let size            = current_context.window().inner_size();
-            let width           = size.width as usize;
-            let height          = size.height as usize;
+                    // Get informtion about the current context
+                    let size            = current_context.window().inner_size();
+                    let width           = size.width as usize;
+                    let height          = size.height as usize;
 
-            // Create the renderer (needs the OpenGL functions to be loaded)
-            if window.renderer.is_none() {
-                // Load the functions for the current context
-                // TODO: we're assuming they stay loaded to avoid loading them for every render, which might not be safe
-                // TODO: probably better to have the renderer load the functions itself (gl::load doesn't work well
-                // when we load GL twice, which could happen if we want to use the offscreen renderer)
-                gl::load_with(|symbol_name| {
-                    current_context.get_proc_address(symbol_name)
-                });
+                    // Create the renderer (needs the OpenGL functions to be loaded)
+                    if window.renderer.is_none() {
+                        // Load the functions for the current context
+                        // TODO: we're assuming they stay loaded to avoid loading them for every render, which might not be safe
+                        // TODO: probably better to have the renderer load the functions itself (gl::load doesn't work well
+                        // when we load GL twice, which could happen if we want to use the offscreen renderer)
+                        gl::load_with(|symbol_name| {
+                            current_context.get_proc_address(symbol_name)
+                        });
 
-                // Create the renderer
-                window.renderer = Some(GlRenderer::new());
+                        // Create the renderer
+                        window.renderer = Some(GlRenderer::new());
+                    }
+
+                    // Perform the rendering actions
+                    window.renderer.as_mut().map(move |renderer| {
+                        renderer.prepare_to_render_to_active_framebuffer(width, height);
+                        renderer.render(next_action);
+                    });
+
+                    // Swap buffers to finish the drawing (TODO: only if ShowFrameBuffer was in the rendering instructions)
+                    current_context.swap_buffers().ok();
+
+                    // Release the current context
+                    let context     = current_context.make_not_current();
+                    let context     = if let Ok(context) = context { context } else { break; };
+                    window.context  = Some(context);
+
+                    // Notify that a new frame has been drawn
+                    events.publish(DrawEvent::NewFrame).await;
+                }
             }
 
-            // Perform the rendering actions
-            window.renderer.as_mut().map(move |renderer| {
-                renderer.prepare_to_render_to_active_framebuffer(width, height);
-                renderer.render(next_action);
-            });
-
-            // Swap buffers to finish the drawing (TODO: only if ShowFrameBuffer was in the rendering instructions)
-            current_context.swap_buffers().ok();
-
-            // Release the current context
-            let context     = current_context.make_not_current();
-            let context     = if let Ok(context) = context { context } else { break; };
-            window.context  = Some(context);
-
-            // Notify that a new frame has been drawn
-            events.publish(DrawEvent::NewFrame).await;
+            _ => {}
         }
     }
 
