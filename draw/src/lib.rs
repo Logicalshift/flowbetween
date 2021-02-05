685d8349a88bae38bd0876e295f24f684539b78f
Andrew Hunter <andrew@logicalshift.co.uk>
2021-02-05 21:32:05 +0000

    Add a window properties trait that we can use to specify how the window should appear

--- draw/src/lib.rs
+++ draw/src/lib.rs
@@ -12,9 +12,11 @@ mod render_window;
 mod glutin_thread;
 mod glutin_window;
 mod glutin_runtime;
+mod window_properties;
 mod glutin_thread_event;
 
 pub use self::draw_event::*;
 pub use self::canvas_window::*;
 pub use self::render_window::*;
 pub use self::glutin_thread::{with_2d_graphics};
+pub use self::window_properties::*;
---
+++ draw/src/window_properties.rs
@@ -0,0 +1,82 @@
+use flo_binding::*;
+
+///
+/// Trait implemented by objects that can provide properties for creating/updating a flo_draw window
+///
+/// Window properties are supplied as bindings to make it possible to update them after the window has
+/// been created.
+///
+pub trait FloWindowProperties {
+    ///
+    /// The title of the window
+    ///
+    fn title(&self) -> BindRef<String>;
+
+    ///
+    /// The initial size of the window
+    ///
+    fn size(&self) -> BindRef<(u64, u64)>;
+
+    ///
+    /// Set to true if the window should be fullscreen
+    ///
+    fn fullscreen(&self) -> BindRef<bool>;
+
+    ///
+    /// Set to true if the window should have decorations
+    ///
+    fn has_decorations(&self) -> BindRef<bool>;
+}
+
+///
+/// '()' can be used to create a window with the default title
+///
+impl FloWindowProperties for () {
+    fn title(&self) -> BindRef<String>          { BindRef::from(bind("flo_draw".to_string())) }
+    fn size(&self) -> BindRef<(u64, u64)>       { BindRef::from(bind((1024, 768))) }
+    fn fullscreen(&self) -> BindRef<bool>       { BindRef::from(bind(false)) }
+    fn has_decorations(&self) -> BindRef<bool>  { BindRef::from(bind(false)) }
+}
+
+///
+/// A string can be used to set just the window title
+///
+impl<'a> FloWindowProperties for &'a str {
+    fn title(&self) -> BindRef<String>          { BindRef::from(bind(self.to_string())) }
+    fn size(&self) -> BindRef<(u64, u64)>       { BindRef::from(bind((1024, 768))) }
+    fn fullscreen(&self) -> BindRef<bool>       { BindRef::from(bind(false)) }
+    fn has_decorations(&self) -> BindRef<bool>  { BindRef::from(bind(false)) }
+}
+
+///
+/// The window properties struct provides a copy of all of the bindings for a window, and is a good way to provide
+/// custom bindings (for example, if you want to be able to toggle the window betwen fullscreen and a normal display)
+///
+#[derive(Clone)]
+pub struct WindowProperties {
+    pub title:              BindRef<String>,
+    pub size:               BindRef<(u64, u64)>,
+    pub fullscreen:         BindRef<bool>,
+    pub has_decorations:    BindRef<bool>
+}
+
+impl WindowProperties {
+    ///
+    /// Creates a clone of an object implementing the FloWindowProperties trait
+    ///
+    pub fn from_properties<T: FloWindowProperties>(properties: &T) -> WindowProperties {
+        WindowProperties {
+            title:              properties.title(),
+            size:               properties.size(),
+            fullscreen:         properties.fullscreen(),
+            has_decorations:    properties.has_decorations()
+        }
+    }
+}
+
+impl FloWindowProperties for WindowProperties {
+    fn title(&self) -> BindRef<String>          { self.title.clone() }
+    fn size(&self) -> BindRef<(u64, u64)>       { self.size.clone() }
+    fn fullscreen(&self) -> BindRef<bool>       { self.fullscreen.clone() }
+    fn has_decorations(&self) -> BindRef<bool>  { self.has_decorations.clone()}
+}
