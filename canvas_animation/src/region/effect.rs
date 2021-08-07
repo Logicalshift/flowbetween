b630f80e0a6ae4a05b9a4331315051d55df22561
Andrew Hunter <andrew@logicalshift.co.uk>
2021-08-07 20:36:49 +0100

    Remove the idea of the 'cached animation' for now
    API is a bit awkward and it's not clear how useful it will be

--- canvas_animation/src/region/effect.rs
+++ canvas_animation/src/region/effect.rs
@@ -11,16 +11,4 @@ pub trait AnimationEffect : Clone+Send+Sync {
     /// Given the contents of the regions for this effect, calculates the path that should be rendered
     ///
     fn animate(&self, region_contents: Arc<Vec<AnimationRegionContent>>, time: f64) -> Vec<AnimationPath>;
-
-    ///
-    /// Creates a cached animation for a set of region contents that don't change over time
-    ///
-    fn animate_cached<'a>(&'a self, region_contents: Arc<Vec<AnimationRegionContent>>) -> Box<dyn 'a+Fn(f64) -> Vec<AnimationPath>> {
-        let cloned              = self.clone();
-        let animation_function  = move |time| {
-            cloned.animate(Arc::clone(&region_contents), time)
-        };
-
-        Box::new(animation_function)
-    }
 }
