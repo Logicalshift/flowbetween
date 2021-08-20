d471e38bc0e857a9d0b9bcdd1882ebc128cfa71c
Andrew Hunter <andrew@logicalshift.co.uk>
2021-08-20 21:12:06 +0100

    Bring back the caching option (this may help a lot for some of the effects I want to make)

--- canvas_animation/src/region/effect.rs
+++ canvas_animation/src/region/effect.rs
@@ -11,6 +11,18 @@ pub trait AnimationEffect : Clone+Send+Sync {
     /// Given the contents of the regions for this effect, calculates the path that should be rendered
     ///
     fn animate(&self, region_contents: Arc<AnimationRegionContent>, time: f64) -> Vec<AnimationPath>;
+
+    ///
+    /// Given an input region that will remain fixed throughout the time period, returns a function that
+    /// will animate it. This can be used to speed up operations when some pre-processing is required for
+    /// the region contents, but is not always available as the region itself might be changing over time
+    /// (eg, if many effects are combined)
+    ///
+    fn animate_cached<'a>(&'a self, region_contents: Arc<AnimationRegionContent>) -> Box<dyn 'a+Fn(f64) -> Vec<AnimationPath>> {
+        Box::new(move |time| {
+            self.animate(Arc::clone(&region_contents), time)
+        })
+    }
 }
 
 impl<T> AnimationEffect for Box<T>
