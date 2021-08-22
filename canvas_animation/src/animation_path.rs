5232b2afac2254d77665b23a4d4765e7b1ba22f4
Andrew Hunter <andrew@logicalshift.co.uk>
2021-08-22 17:14:42 +0100

    Add an offset_by() routine to AnimationPath

--- canvas_animation/src/animation_path.rs
+++ canvas_animation/src/animation_path.rs
@@ -6,6 +6,7 @@ use std::sync::*;
 ///
 /// Describes a path rendered as part of an animation
 ///
+#[derive(Clone)]
 pub struct AnimationPath {
     /// The time in milliseconds from the start of the keyframe where this content appears
     pub appearance_time: f64,
@@ -18,4 +19,27 @@ pub struct AnimationPath {
 
     /// The path that will be rendered by this animation
     pub path: SimpleBezierPath
+}
+
+impl AnimationPath {
+    ///
+    /// Creates a copy of this path that is offset by the specified distance
+    ///
+    pub fn offset_by(&self, distance: Coord2) -> AnimationPath {
+        let (start_point, points) = &self.path;
+
+        let new_start_point = *start_point + distance;
+        let new_points = points.iter()
+            .map(|(cp1, cp2, end_point)| {
+                (*cp1 + distance, *cp2 + distance, *end_point + distance)
+            })
+            .collect();
+
+        AnimationPath {
+            appearance_time:    self.appearance_time,
+            disappearance_time: self.disappearance_time,
+            attributes:         Arc::clone(&self.attributes),
+            path:               (new_start_point, new_points)
+        }
+    }
 }
