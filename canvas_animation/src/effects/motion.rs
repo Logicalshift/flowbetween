839c7890dee752ed98dc23f183b9c663a91c75a6
Andrew Hunter <andrew@logicalshift.co.uk>
2021-08-24 09:40:33 +0100

    Add some tests to make sure the distance between points is consistent
    (Thought the technique I'd picked for finding distances would be more consistent, but it isn't)

--- canvas_animation/src/effects/motion.rs
+++ canvas_animation/src/effects/motion.rs
@@ -6,7 +6,7 @@ use flo_curves::bezier::*;
 
 use std::sync::*;
 
-const TOLERANCE: f64 = 0.1;
+const TOLERANCE: f64 = 0.01;
 
 ///
 /// A point in a motion path
@@ -153,7 +153,7 @@ impl AnimationEffect for MotionEffect {
     ///
     fn animate(&self, region_contents: Arc<AnimationRegionContent>, time: f64) -> Vec<AnimationPath> {
         // Get the offset for the region contents
-        let offset = self.offset_at_time(time, TOLERANCE);
+        let offset = self.offset_at_time(time, 0.01);
 
         // Move all of the paths in the region by the offset
         region_contents.paths.iter()
@@ -161,4 +161,65 @@ impl AnimationEffect for MotionEffect {
             .collect()
     }
 
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    pub fn basic_spacing() {
+        let effect              = MotionEffect::from_points(10000.0, Coord2(20.0, 30.0), vec![(Coord2(20.0, 100.0), Coord2(200.0, 200.0), Coord2(300.0, 400.0))]);
+
+        let initial_offset      = effect.offset_at_time(0.0, 0.01);
+        assert!(initial_offset.magnitude() <= 0.1);
+
+        let mut last_offset     = initial_offset;
+        let mut time            = 1000.0/60.0;
+        let mut last_distance   = Option::<f64>::None;
+        while time <= 10000.0 {
+            let offset          = effect.offset_at_time(time, 0.01);
+            let distance        = last_offset.distance_to(&offset);
+
+            println!("{:?}", distance);
+
+            if let Some(last_distance) = last_distance {
+                assert!((distance-last_distance).abs() < 0.1);
+            }
+
+            last_offset         = offset;
+            last_distance       = Some(distance);
+
+            // 60 fps
+            time += 1000.0 / 60.0;
+        }
+    }
+
+    #[test]
+    pub fn two_segment_spacing() {
+        let effect              = MotionEffect::from_points(10000.0, Coord2(20.0, 30.0), vec![(Coord2(20.0, 100.0), Coord2(200.0, 200.0), Coord2(300.0, 400.0)), (Coord2(500.0, 400.0), Coord2(200.0, 100.0), Coord2(100.0, 100.0))]);
+
+        let initial_offset      = effect.offset_at_time(0.0, 0.01);
+        assert!(initial_offset.magnitude() <= 0.1);
+
+        let mut last_offset     = initial_offset;
+        let mut time            = 1000.0/60.0;
+        let mut last_distance   = Option::<f64>::None;
+        while time <= 10000.0 {
+            let offset          = effect.offset_at_time(time, 0.01);
+            let distance        = last_offset.distance_to(&offset);
+
+            println!("{:?}", distance);
+
+            if let Some(last_distance) = last_distance {
+                assert!((distance-last_distance).abs() < 0.1);
+            }
+
+            last_offset         = offset;
+            last_distance       = Some(distance);
+
+            // 60 fps
+            time += 1000.0 / 60.0;
+        }
+    }
 }
