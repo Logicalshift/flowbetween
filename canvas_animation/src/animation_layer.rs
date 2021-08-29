cb540de625dc2e4178a5615c562b811be1b8dd87
Andrew Hunter <andrew@logicalshift.co.uk>
2021-08-29 12:29:13 +0100

    Add the first bits of the animation layer implementation

--- canvas_animation/src/animation_layer.rs
+++ canvas_animation/src/animation_layer.rs
@@ -1,8 +1,28 @@
+use crate::animation_path::*;
+
+use flo_canvas::*;
+
 ///
 /// Represents an animated layer of a vector drawing. This accepts commands in the form
 /// of `Draw` instructions, although it will only render to a single layer in the finished
 /// rendering: sprite and layer commands will be ignored.
 ///
 pub struct AnimationLayer {
+    drawing: Vec<AnimationPath>
+}
 
+impl AnimationLayer {
+    ///
+    /// Adds a new path to this layer
+    ///
+    pub fn add_path(&mut self, path: AnimationPath) {
+        self.drawing.push(path);
+    }
+
+    ///
+    /// Adds drawing onto this layer
+    ///
+    pub fn draw<DrawIter: IntoIterator<Item=Draw>>(&mut self, drawing: DrawIter) {
+        // TODO
+    }
 }
