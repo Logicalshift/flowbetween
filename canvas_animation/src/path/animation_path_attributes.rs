03dd2f7d688398ebf7fa8389d093ac2f203b7318
Andrew Hunter <andrew@logicalshift.co.uk>
2021-08-30 21:46:05 +0100

    Add an 'AnimationPathAttribute' type that has the extra details we need for animation paths

---
+++ canvas_animation/src/path/animation_path_attributes.rs
@@ -0,0 +1,22 @@
+use flo_canvas::*;
+
+///
+/// Attributes used to render a bezier path
+///
+#[derive(Copy, Clone, Debug)]
+pub enum AnimationPathAttribute {
+    /// Path is drawn as a stroke with the specified width and colour
+    Stroke(f32, Color, LineJoin, LineCap),
+
+    /// Path is drawn as a stroke with the specified pixel width and colour
+    StrokePixels(f32, Color, LineJoin, LineCap),
+
+    /// Path is filled with the specified colour
+    Fill(Color, WindingRule),
+
+    /// Path is filled with the specified texture
+    FillTexture(TextureId, (f32, f32), (f32, f32), Option<Transform2D>, WindingRule),
+
+    /// Path is filled with the specified gradient
+    FillGradient(GradientId, (f32, f32), (f32, f32), Option<Transform2D>, WindingRule)
+}
--- canvas_animation/src/path/mod.rs
+++ canvas_animation/src/path/mod.rs
@@ -1,6 +1,8 @@
 mod layer_state;
 mod animation_path;
 mod drawing_to_path;
+mod animation_path_attributes;
 
 pub use self::animation_path::*;
 pub use self::drawing_to_path::*;
+pub use self::animation_path_attributes::*;
