59f201f03597f02ca447c2ed6d3731909aba522a
Andrew Hunter <andrew@logicalshift.co.uk>
2021-09-01 20:46:30 +0100

    Add a test that the circle bounding box is calculated properly
    (Calculating bounding boxes will be used when matching regions against the canvas)

--- canvas_animation/tests/path.rs
+++ canvas_animation/tests/path.rs
@@ -1,4 +1,5 @@
 use flo_canvas::*;
+use flo_curves::*;
 use flo_curves::bezier::path::*;
 use flo_canvas_animation::*;
 
@@ -42,4 +43,26 @@ pub fn components_from_simple_circle() {
     
     assert!(circle.len()                == 1);
     assert!(circle[0].points().count()  == 4);
+}
+
+#[test]
+pub fn simple_circle_bounds() {
+    let mut drawing         = vec![];
+    let mut drawing_to_path = LayerDrawingToPaths::new();
+
+    drawing.circle(100.0, 200.0, 50.0);
+    drawing.stroke_color(Color::Rgba(0.1, 0.2, 0.3, 0.4));
+    drawing.fill_color(Color::Rgba(0.3, 0.4, 0.5, 0.6));
+    drawing.fill();
+
+    let paths               = drawing_to_path.draw(drawing).collect::<Vec<_>>();
+
+    assert!(paths.len() == 1);
+
+    let circle          = PathComponent::components_from_path(&paths[0]);
+    let circle_bounds   = circle[0].bounding_box::<(Coord2, Coord2)>();
+    
+    assert!(circle.len() == 1);
+    assert!(circle_bounds.0.distance_to(&Coord2(50.0, 150.0)) < 0.1);
+    assert!(circle_bounds.1.distance_to(&Coord2(150.0, 250.0)) < 0.1);
 }
