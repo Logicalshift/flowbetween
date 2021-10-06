31b70e0532443943aa932224308b95ab5f03a7fb
Andrew Hunter <andrew@logicalshift.co.uk>
2021-10-06 23:55:44 +0100

    Add a layer ID for the element overlay layer

--- flo/src/animation_canvas/overlay_layers.rs
+++ flo/src/animation_canvas/overlay_layers.rs
@@ -1,4 +1,5 @@
 use flo_canvas::*;
 
-pub const OVERLAY_TOOL: LayerId         = LayerId(0);
-pub const OVERLAY_ONIONSKINS: LayerId   = LayerId(1);
+pub const OVERLAY_ELEMENTS: LayerId         = LayerId(0);
+pub const OVERLAY_TOOL: LayerId             = LayerId(1);
+pub const OVERLAY_ONIONSKINS: LayerId       = LayerId(2);
