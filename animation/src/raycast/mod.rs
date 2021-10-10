0b4d785e9b67d6f287c3fd040594f4032630d904
Andrew Hunter <andrew@logicalshift.co.uk>
2021-10-10 21:18:05 +0100

    Add a 'point in path' that uses a non-zero winding rule to work on a path with subpaths

--- animation/src/raycast/mod.rs
+++ animation/src/raycast/mod.rs
@@ -5,6 +5,8 @@
 pub (crate) mod edge;
 mod path_combine;
 mod vector_frame_raycast;
+mod point_in_path;
 
 pub use self::vector_frame_raycast::*;
 pub use self::path_combine::*;
+pub use self::point_in_path::*;
---
+++ animation/src/raycast/point_in_path.rs
@@ -0,0 +1,43 @@
+use flo_curves::*;
+use flo_curves::bezier::*;
+use flo_curves::bezier::path::*;
+
+///
+/// Returns true if a particular point is within a bezier path
+/// 
+pub fn point_is_in_path<P: BezierPath>(path: &Vec<P>, point: &P::Point) -> bool
+where P::Point: Coordinate2D {
+    // The GraphPath has the functionality we need but in order to use it, we need to convert the path as passed in, which is not particularly efficient
+    let graph_path              = GraphPath::from_merged_paths(path.iter().map(|path| (path, PathDirection::from(path))));
+
+    // Cast a ray towards the point (direction doesn't matter, so we'll cast at a 45-degree angle
+    let ray                     = (*point - P::Point::from_components(&[1.0, 1.0]), *point);
+    let ray_direction           = ray.1 - ray.0;
+    let collisions              = graph_path.ray_collisions(&ray);
+
+    // Total direction is 0 when the ray is outside of the path
+    let mut total_direction     = 0;
+
+    for (collision, curve_t, ray_t, _position) in collisions {
+        // Stop once we reach the target point
+        if ray_t > 1.0 { break; }
+
+        // Get the edge where the collision occured
+        let edge_ref        = match collision {
+            GraphRayCollision::SingleEdge(edge_ref)     => edge_ref,
+            GraphRayCollision::Intersection(edge_ref)   => edge_ref
+        };
+        let edge            = graph_path.get_edge(edge_ref);
+
+        // The direction of the normal and the direction of the path determines the change in the total direction (ie, use a non-zero winding rule)
+        let normal          = edge.normal_at_pos(curve_t);
+        let direction       = ray_direction.dot(&normal).signum() as i32;
+        let path_direction  = edge.label();
+        let direction       = if path_direction == PathDirection::Anticlockwise { -direction } else { direction };
+
+        total_direction     += direction;
+    }
+
+    // Point is inside the path if the ray has crossed an odd number of lines
+    total_direction != 0
+}
