744836b6a9eeca675fcc83ff0f121fc09aedba36
Andrew Hunter <andrew@logicalshift.co.uk>
2021-10-16 16:46:02 +0100

    Fit transformation points into a transformation curve to create a motion effect

--- canvas_animation/src/effects/mod.rs
+++ canvas_animation/src/effects/mod.rs
@@ -1,15 +1,15 @@
 mod repeat;
 mod sequence;
 mod time_curve;
-mod motion_fit;
 mod motion_linear;
 mod effect_region;
 mod frame_by_frame;
+mod transform_fitted;
 
 pub use self::repeat::*;
 pub use self::sequence::*;
 pub use self::time_curve::*;
-pub use self::motion_fit::*;
 pub use self::motion_linear::*;
 pub use self::effect_region::*;
 pub use self::frame_by_frame::*;
+pub use self::transform_fitted::*;
