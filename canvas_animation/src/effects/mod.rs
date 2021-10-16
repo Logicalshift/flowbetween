9d5d2186870e8ffe673202e5ab232330a37ee9f7
Andrew Hunter <andrew@logicalshift.co.uk>
2021-10-16 12:00:32 +0100

    Add a new effect for generating motions fitted to positions

--- canvas_animation/src/effects/mod.rs
+++ canvas_animation/src/effects/mod.rs
@@ -1,6 +1,7 @@
 mod repeat;
 mod sequence;
 mod time_curve;
+mod motion_fit;
 mod motion_linear;
 mod effect_region;
 mod frame_by_frame;
@@ -8,6 +9,7 @@ mod frame_by_frame;
 pub use self::repeat::*;
 pub use self::sequence::*;
 pub use self::time_curve::*;
+pub use self::motion_fit::*;
 pub use self::motion_linear::*;
 pub use self::effect_region::*;
 pub use self::frame_by_frame::*;
