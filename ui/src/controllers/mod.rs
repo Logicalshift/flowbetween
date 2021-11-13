d158e862bba75953fe4c6691675960fdc7f62508
Andrew Hunter <andrew@logicalshift.co.uk>
2021-11-13 19:42:15 +0000

    Rename the stream_controller file

--- ui/src/controllers/mod.rs
+++ ui/src/controllers/mod.rs
@@ -1,15 +1,15 @@
 mod virtual_canvas;
 mod popup_controller;
-mod stream_controller;
 mod controller_event;
 mod controller_action;
 mod controller_resources;
+mod immediate_controller;
 mod createable_controller;
 
 pub use self::virtual_canvas::*;
 pub use self::popup_controller::*;
 pub use self::controller_event::*;
-pub use self::stream_controller::*;
 pub use self::controller_action::*;
 pub use self::controller_resources::*;
+pub use self::immediate_controller::*;
 pub use self::createable_controller::*;
