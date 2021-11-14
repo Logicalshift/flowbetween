89c31a736a20cc7aad2c2d879006a7213b34b500
Andrew Hunter <andrew@logicalshift.co.uk>
2021-11-14 20:46:57 +0000

    Add a test button to the sidebar UI
    (This will check that the click-through works properly)

--- flo/src/sidebar/sidebar_controller.rs
+++ flo/src/sidebar/sidebar_controller.rs
@@ -5,12 +5,43 @@ use flo_binding::*;
 use flo_animation::*;
 
 ///
+/// Creates the user interface for the sidebar
+///
+fn sidebar_ui<Anim: 'static+EditableAnimation>(_model: &FloModel<Anim>) -> BindRef<Control> {
+    use self::Position::*;
+
+    let ui = bind(
+        Control::container()
+            .with(Bounds {
+                x1: Start,
+                y1: Start,
+                x2: End,
+                y2: End
+            })
+            .with(vec![
+                Control::button()
+                    .with("Test")
+                    .with(Bounds {
+                        x1: Start,
+                        y1: Start,
+                        x2: End,
+                        y2: Offset(30.0)
+                    })
+            ])
+        );
+
+    BindRef::from(ui)
+}
+
+///
 /// Creates the sidebar controller
 ///
 pub fn sidebar_controller<Anim: 'static+EditableAnimation>(model: &FloModel<Anim>) -> impl Controller {
-    let model = model.clone();
+    let model       = model.clone();
+    let resources   = ControllerResources::new();
+    let ui          = sidebar_ui(&model);
 
-    ImmediateController::new(ControllerResources::new(), BindRef::from(bind(Control::empty())), 
+    ImmediateController::new(resources, ui, 
         move |_events, _actions, _resources| {
             // Start by taking the model from the main controller
             let model = model.clone();
