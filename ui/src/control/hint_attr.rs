157509beaa89bb8182e35872e653d73ab2846d36
Andrew Hunter <andrew@logicalshift.co.uk>
2021-11-14 20:59:43 +0000

    Add pointer behaviour hints

--- ui/src/control/hint_attr.rs
+++ ui/src/control/hint_attr.rs
@@ -8,11 +8,26 @@ use ::modifier::*;
 ///
 #[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
 pub enum Hint {
-    // Prefer fast drawing over correctness
+    /// Prefer fast drawing over correctness
     FastDrawing,
+
+    /// Set how pointer events are handled by this control
+    PointerBehaviour(PointerBehaviour),
 
     /// Provides a class for this control (modifying its behaviour or appearance)
     Class(String)
+}
+
+///
+/// Possible ways the pointer can be handled by this control
+///
+#[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
+pub enum PointerBehaviour {
+    /// Clicks on this control block clicks to the control underneath (the default behaviour)
+    BlockClicks,
+
+    /// Clicks on this control go through to the control underneath
+    ClickThrough
 }
 
 impl Modifier<Control> for Hint {
