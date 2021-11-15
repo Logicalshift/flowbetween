da050d487cb4c4bc4a4b3d7d66fc930aa3fa50f9
Andrew Hunter <andrew@logicalshift.co.uk>
2021-11-15 08:43:13 +0000

    The sidebar will use subcontrollers, so start with an empty immediate controller and go from there

--- flo/src/sidebar/sidebar_controller.rs
+++ flo/src/sidebar/sidebar_controller.rs
@@ -4,6 +4,10 @@ use flo_ui::*;
 use flo_binding::*;
 use flo_animation::*;
 
+use futures::prelude::*;
+
+use std::sync::*;
+
 ///
 /// Creates the user interface for the sidebar
 ///
@@ -38,17 +42,29 @@ fn sidebar_ui<Anim: 'static+EditableAnimation>(_model: &FloModel<Anim>) -> BindR
 /// Creates the sidebar controller
 ///
 pub fn sidebar_controller<Anim: 'static+EditableAnimation>(model: &FloModel<Anim>) -> impl Controller {
-    let model       = model.clone();
-    let resources   = ControllerResources::new();
+    // TODO: Create the set of subcontrollers
+
+    // Set up the UI
+    let model       = Arc::new(model.clone());
     let ui          = sidebar_ui(&model);
 
-    ImmediateController::new(resources, ui, 
-        move |_events, _actions, _resources| {
+    ImmediateController::empty(move |events, actions, _resources| {
             // Start by taking the model from the main controller
-            let model = model.clone();
+            let model       = model.clone();
+            let ui          = ui.clone();
+            let mut actions = actions;
+            let mut events  = events;
 
             async move {
+                // TODO: Set up the subcontrollers
 
+                // Set up the UI
+                actions.send(ControllerAction::SetUi(ui.clone())).await.ok();
+
+                // Process events
+                while let Some(next_event) = events.next().await {
+
+                }
             }
         })
 }
