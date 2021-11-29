6f6d96d6f78c76f4d8994c261b51e36a3b8edb9e
Andrew Hunter <andrew@logicalshift.co.uk>
2021-11-29 23:26:59 +0000

    Add support for opening and closing sidebar panels by clicking on their titles

--- flo/src/sidebar/sidebar_controller.rs
+++ flo/src/sidebar/sidebar_controller.rs
@@ -26,7 +26,13 @@ enum SidebarAction {
     Unknown,
 
     /// The user has resized the sidebar
-    Resize
+    Resize,
+
+    /// The user has requested a panel to be opened
+    OpenPanel(String),
+
+    /// The user has requested a panel to be closed
+    ClosePanel(String)
 }
 
 ///
@@ -90,13 +96,16 @@ fn sidebar_ui(panels: Vec<&SidebarPanel>, open_panels: BindRef<Vec<String>>, tot
         let mut panel_controls  = vec![];
         let mut last_position   = Start;
         for (panel_id, panel_title, panel_height, panel_active) in panels.iter() {
-            // Fetch the height and weather or not it's active
+            // Fetch the height and weather or not it's open
             let is_active           = panel_active.get();
+            let is_open             = open_panels.contains(panel_id);
 
             // Title control
+            let title_action        = if is_open { format!("ClosePanel {}", panel_id) } else { format!("OpenPanel {}", panel_id) };
             let title_control       = Control::container()
                 .with(Bounds { x1: Start, y1: last_position, x2: End, y2: Offset(TITLE_SIZE as _)} )
                 .with(Appearance::Background(SIDEBAR_TITLE_BACKGROUND))
+                .with((ActionTrigger::Click, title_action))
                 .with(vec![
                     Control::empty().with(Bounds { x1: Start, y1: Start, x2: Offset(28.0), y2: End }),
                     if is_active {
@@ -111,11 +120,12 @@ fn sidebar_ui(panels: Vec<&SidebarPanel>, open_panels: BindRef<Vec<String>>, tot
                 ]);
 
             // Panel content
-            let content_controls    = if open_panels.contains(panel_id) {
+            let content_controls    = if is_open {
                 // Panel is open
                 let panel_height = panel_height.get();
                 vec![
                     Control::container().with(Bounds { x1: Start, y1: After, x2: End, y2: Offset(panel_height as _) })
+                        .with((ActionTrigger::Click, "BackgroundClick"))
                         .with_controller(panel_id)
                 ]
             } else {
@@ -149,6 +159,19 @@ fn sidebar_ui(panels: Vec<&SidebarPanel>, open_panels: BindRef<Vec<String>>, tot
     });
 
     BindRef::from(ui)
+}
+
+///
+/// Parses an OpenPanel or ClosePanel action name (returning SidebarAction::Unknown if the action can't be parsed)
+///
+fn parse_open_close_action(action_name: &str) -> SidebarAction {
+    if action_name.starts_with("OpenPanel ") {
+        SidebarAction::OpenPanel(action_name["OpenPanel ".len()..].to_string())
+    } else if action_name.starts_with("ClosePanel ") {
+        SidebarAction::ClosePanel(action_name["ClosePanel ".len()..].to_string())
+    } else {
+        SidebarAction::Unknown
+    }
 }
 
 ///
@@ -246,13 +269,33 @@ pub fn sidebar_controller<Anim: 'static+EditableAnimation>(model: &FloModel<Anim
 
                     SidebarEvent::Action(action_name, param) => {
                         let action_name: &str   = &action_name;
-                        let action              = SidebarAction::from_str(action_name).unwrap_or(SidebarAction::Unknown);
+                        let action              = SidebarAction::from_str(action_name)
+                            .unwrap_or_else(|_| parse_open_close_action(action_name));
 
                         // Decode the action
                         match (action, param) {
                             (SidebarAction::Resize, ActionParameter::Size(_new_width, new_height)) => {
                                 // The size is used to determine which sidebar items are displayed as 'open'
                                 height.set(new_height as f64);
+                            }
+
+                            (SidebarAction::OpenPanel(panel_id), _) => {
+                                // Add the panel to the start of the open list
+                                let mut open_panels = model.sidebar().open_sidebars.get();
+
+                                open_panels.retain(|existing_id| existing_id != &panel_id);
+                                open_panels.insert(0, panel_id);
+
+                                model.sidebar().open_sidebars.set(open_panels);
+                            }
+
+                            (SidebarAction::ClosePanel(panel_id), _) => {
+                                // Remove the panel from the open list
+                                let mut open_panels = model.sidebar().open_sidebars.get();
+
+                                open_panels.retain(|existing_id| existing_id != &panel_id);
+
+                                model.sidebar().open_sidebars.set(open_panels);
                             }
 
                             _ => { /* Unrecognised action */ }
