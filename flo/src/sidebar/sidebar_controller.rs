45b318c8da92018d7fe577e639eae04063fc5bb7
Andrew Hunter <andrew@logicalshift.co.uk>
2021-11-29 22:15:43 +0000

    Monitor the list of panels and generate events to set the UI or add or remove controllers as needed

--- flo/src/sidebar/sidebar_controller.rs
+++ flo/src/sidebar/sidebar_controller.rs
@@ -1,13 +1,17 @@
 use crate::model::*;
 
 use flo_ui::*;
+use flo_rope::*;
 use flo_binding::*;
 use flo_animation::*;
 
 use futures::prelude::*;
+use futures::stream;
 
+use std::mem;
 use std::sync::*;
 use std::str::{FromStr};
+use std::collections::{HashSet};
 
 #[derive(Clone, PartialEq, Eq, Hash, AsRefStr, Display, EnumString)]
 enum SidebarAction {
@@ -18,7 +22,7 @@ enum SidebarAction {
 }
 
 ///
-/// An event for the sidebar
+/// An event for the sidebar (we convert from controller events so we can mix in events from other sources, such as dealing with panel changes)
 ///
 enum SidebarEvent {
     /// Add a subcontroller with the specified name to the sidebar
@@ -80,6 +84,55 @@ fn sidebar_events<ControllerEvents: Stream<Item=ControllerEvent>>(events: Contro
 }
 
 ///
+/// Generates events for when the list of panels change
+///
+/// These register or deregister controllers and update the UI accordingly
+///
+fn panel_change_events<Anim: 'static+EditableAnimation>(model: &Arc<FloModel<Anim>>) -> impl Stream<Item=SidebarEvent> {
+    let model           = Arc::clone(model);
+
+    // Follow the list of panels defined for the sidebar
+    let panel_changes   = follow(model.sidebar().panels.clone());
+
+    // Keep a list of the panel IDs we know about (these correspond to the new controllers we know about)
+    let known_panels    = Arc::new(Mutex::new(HashSet::<String>::new()));
+
+    // Run the panel changes stream
+    panel_changes
+        .map(move |new_panels| {
+            // Build the list of events for these changes
+            let mut events              = vec![];
+
+            // Work out which controllers were added and which were removed
+            let mut known_panels        = known_panels.lock().unwrap();
+            let mut new_known_panels    = new_panels.read_cells(0..new_panels.len()).map(|panel| panel.identifier().to_string()).collect::<HashSet<_>>();
+
+            let new_panel_ids           = new_known_panels.iter().filter(|id| !known_panels.contains(*id)).cloned().collect::<HashSet<_>>();
+            let deleted_panel_ids       = known_panels.iter().filter(|id| !new_known_panels.contains(*id)).cloned().collect::<Vec<_>>();
+
+            // Send the list of new controllers
+            for panel in new_panels.read_cells(0..new_panels.len()) {
+                if new_panel_ids.contains(panel.identifier()) {
+                    events.push(SidebarEvent::AddController(panel.identifier().to_string(), panel.controller()));
+                }
+            }
+
+            mem::swap(&mut *known_panels, &mut new_known_panels);
+
+            // Update the UI to use the new controllers
+            let new_ui = sidebar_ui(&*model);
+            events.push(SidebarEvent::SetUi(new_ui));
+
+            // Send the list of deleted controllers
+            events.extend(deleted_panel_ids.into_iter().map(|panel_id| SidebarEvent::RemoveController(panel_id)));
+
+            events
+        })
+        .map(|changes| stream::iter(changes))
+        .flatten()
+}
+
+///
 /// Creates the sidebar controller
 ///
 pub fn sidebar_controller<Anim: 'static+EditableAnimation>(model: &FloModel<Anim>) -> impl Controller {
@@ -93,11 +146,14 @@ pub fn sidebar_controller<Anim: 'static+EditableAnimation>(model: &FloModel<Anim
 
     ImmediateController::empty(move |events, actions, _resources| {
         // Start by taking the model from the main controller
-        let model       = model.clone();
-        let height      = height.clone();
+        let model           = model.clone();
+        let height          = height.clone();
 
-        let mut actions = actions;
-        let mut events  = sidebar_events(events);
+        let mut actions     = actions;
+        let events          = sidebar_events(events).boxed();
+        let panel_events    = panel_change_events(&model).boxed();
+
+        let mut events      = stream::select_all(vec![events, panel_events]);
 
         async move {
             // TODO: Set up the subcontrollers
