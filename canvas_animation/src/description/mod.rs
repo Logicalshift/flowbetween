47cca21eb90ae5694fbedd45013708a9376efa7b
Andrew Hunter <andrew@logicalshift.co.uk>
2021-12-30 23:55:41 +0000

    Add a subeffect enum to start to make a way to break down an effect description into a list to display in the UI

--- canvas_animation/src/description/mod.rs
+++ canvas_animation/src/description/mod.rs
@@ -1,6 +1,7 @@
 mod time;
 mod space;
 mod convert;
+mod sub_effect;
 mod base_animation;
 mod effect_description;
 mod region_description;
@@ -8,7 +9,7 @@ mod region_description;
 pub use self::time::*;
 pub use self::space::*;
 pub use self::convert::*;
+pub use self::sub_effect::*;
 pub use self::base_animation::*;
 pub use self::effect_description::*;
 pub use self::region_description::*;
-
---
+++ canvas_animation/src/description/sub_effect.rs
@@ -0,0 +1,10 @@
+///
+/// A sub-effect represents a single part of an effect description
+///
+/// These can be edited individually in the UI. A sub-effect type always corresponds to a
+/// specific type of `EffectDescription`
+///
+pub enum SubEffectType {
+    /// A stop-motion or fitted transformation effect
+    Transform
+}
