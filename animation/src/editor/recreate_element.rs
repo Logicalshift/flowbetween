82400f0b3fc87737836b53442706f4b73131fb5e
Andrew Hunter <andrew@logicalshift.co.uk>
2022-03-15 09:16:51 +0000

    First pass at a routine to pick the creation order for elements
    ... usual problems with groups and order_before...

--- animation/src/editor/recreate_element.rs
+++ animation/src/editor/recreate_element.rs
@@ -3,6 +3,7 @@ use crate::undo::*;
 use crate::traits::*;
 
 use std::iter;
+use std::collections::{HashSet};
 
 impl ReversedEdits {
     ///
@@ -43,6 +44,55 @@ impl ReversedEdits {
         }
 
         reversed
+    }
+
+    ///
+    /// Returns the order that a set of elements should be recreated in to ensure that the resulting elements are put back in the right
+    /// order, with all their attachments intact.
+    ///
+    /// Essentially, the order in which any order before, parent re-ordering and attachment operation will be performed after the
+    /// corresponding element has been created.
+    ///
+    pub fn recreate_order(element_ids: Vec<ElementId>, wrapper_for_element: &impl Fn(ElementId) -> Option<ElementWrapper>) -> Vec<ElementId> {
+        // The elements that are deleted/missing before these elements are re-created
+        let mut waiting_elements    = element_ids.iter().cloned().collect::<HashSet<_>>();
+
+        // Elements waiting to be processed
+        let mut element_ids         = element_ids;
+
+        // The result from the ordering operation
+        let mut ordered_elements    = vec![];
+
+        while let Some(next_element) = element_ids.pop() {
+            // If the element is already in the result, then carry on (eg: we found it via another element)
+            if !waiting_elements.contains(&next_element) { continue; }
+
+            // Fetch the element wrapper
+            let next_wrapper = wrapper_for_element(next_element);
+            let next_wrapper = if let Some(next_wrapper) = next_wrapper { next_wrapper } else { continue; };
+
+            // If there's a dependency in the waiting elements, process that before carrying on
+            if let Some(parent) = next_wrapper.parent {
+                if waiting_elements.contains(&parent) {
+                    element_ids.push(next_element);
+                    element_ids.push(parent);
+                    continue;
+                }
+            }
+
+            if let Some(order_before) = next_wrapper.order_before {
+                if waiting_elements.contains(&order_before) {
+                    element_ids.push(next_element);
+                    element_ids.push(order_before);
+                }
+            }
+
+            // All the dependencies in the list are already deployed, so deploy this element next
+            waiting_elements.remove(&next_element);
+            ordered_elements.push(next_element);
+        }
+
+        ordered_elements
     }
 
     ///
