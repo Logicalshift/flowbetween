39e79e722f06fe6a75465124ac672f4328643c63
Andrew Hunter <andrew@logicalshift.co.uk>
2022-03-18 09:04:28 +0000

    Change Draw::NewPath into Draw::Path(PathOp::NewPath)

--- sqlite_storage/src/round_trip_tests.rs
+++ sqlite_storage/src/round_trip_tests.rs
@@ -1302,12 +1302,12 @@ fn set_and_retrieve_cached_onionskin() {
     let layer           = anim.get_layer_with_id(24).unwrap();
     let cache           = layer.get_canvas_cache_at_time(Duration::from_millis(2000));
 
-    cache.store(CacheType::OnionSkinLayer, Arc::new(vec![Draw::NewPath, Draw::Fill]));
+    cache.store(CacheType::OnionSkinLayer, Arc::new(vec![Draw::Path(PathOp::NewPath), Draw::Fill]));
 
     let cache           = layer.get_canvas_cache_at_time(Duration::from_millis(2000));
     let cached_drawing  = cache.retrieve(CacheType::OnionSkinLayer);
 
-    assert!(cached_drawing == Some(Arc::new(vec![Draw::NewPath, Draw::Fill])));
+    assert!(cached_drawing == Some(Arc::new(vec![Draw::Path(PathOp::NewPath), Draw::Fill])));
 }
 
 #[test]
@@ -1319,7 +1319,7 @@ fn retrieve_or_generate_cached_onionskin() {
     let layer           = anim.get_layer_with_id(24).unwrap();
     let cache           = layer.get_canvas_cache_at_time(Duration::from_millis(2000));
 
-    let cached_drawing  = cache.retrieve_or_generate(CacheType::OnionSkinLayer, Box::new(|| Arc::new(vec![Draw::NewPath, Draw::Fill])));
+    let cached_drawing  = cache.retrieve_or_generate(CacheType::OnionSkinLayer, Box::new(|| Arc::new(vec![Draw::Path(PathOp::NewPath), Draw::Fill])));
 
     // Should initially be a future indicating the cached item will be generated eventually
     assert!(match cached_drawing { CacheProcess::Process(_) => true, _ => false });
@@ -1327,13 +1327,13 @@ fn retrieve_or_generate_cached_onionskin() {
     // ... and eventually evaluate to the drawing we specified in the generate function
     let cached_drawing = executor::block_on(cached_drawing);
 
-    assert!(cached_drawing == Arc::new(vec![Draw::NewPath, Draw::Fill]));
+    assert!(cached_drawing == Arc::new(vec![Draw::Path(PathOp::NewPath), Draw::Fill]));
 
     // Should be able to retrieve instantly next time
-    let cached_drawing  = cache.retrieve_or_generate(CacheType::OnionSkinLayer, Box::new(|| Arc::new(vec![Draw::NewPath, Draw::Fill])));
+    let cached_drawing  = cache.retrieve_or_generate(CacheType::OnionSkinLayer, Box::new(|| Arc::new(vec![Draw::Path(PathOp::NewPath), Draw::Fill])));
 
     assert!(match cached_drawing { CacheProcess::Cached(_) => true, _ => false });
-    assert!(match cached_drawing { CacheProcess::Cached(cached_drawing) => cached_drawing == Arc::new(vec![Draw::NewPath, Draw::Fill]), _ => false });
+    assert!(match cached_drawing { CacheProcess::Cached(cached_drawing) => cached_drawing == Arc::new(vec![Draw::Path(PathOp::NewPath), Draw::Fill]), _ => false });
 }
 
 #[test]
@@ -1345,7 +1345,7 @@ fn invalidate_cached_onionskin() {
     let layer           = anim.get_layer_with_id(24).unwrap();
     let cache           = layer.get_canvas_cache_at_time(Duration::from_millis(2000));
 
-    cache.store(CacheType::OnionSkinLayer, Arc::new(vec![Draw::NewPath, Draw::Fill]));
+    cache.store(CacheType::OnionSkinLayer, Arc::new(vec![Draw::Path(PathOp::NewPath), Draw::Fill]));
     cache.invalidate(CacheType::OnionSkinLayer);
 
     let cache           = layer.get_canvas_cache_at_time(Duration::from_millis(2000));
@@ -1363,7 +1363,7 @@ fn retrieve_cached_onionskin_from_different_time() {
     let layer           = anim.get_layer_with_id(24).unwrap();
     let cache           = layer.get_canvas_cache_at_time(Duration::from_millis(2000));
 
-    cache.store(CacheType::OnionSkinLayer, Arc::new(vec![Draw::NewPath, Draw::Fill]));
+    cache.store(CacheType::OnionSkinLayer, Arc::new(vec![Draw::Path(PathOp::NewPath), Draw::Fill]));
     cache.invalidate(CacheType::OnionSkinLayer);
 
     let cache           = layer.get_canvas_cache_at_time(Duration::from_millis(1500));
