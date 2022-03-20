9c5f0674e8b585e28dc147e9903fe136e886fd21
Andrew Hunter <andrew@logicalshift.co.uk>
2022-03-20 17:46:20 +0000

    Change the way we compare two path components for equality to be more generous with how close they have to be

--- animation/src/traits/path/component.rs
+++ animation/src/traits/path/component.rs
@@ -1,9 +1,11 @@
 use super::point::*;
 
+use flo_curves::*;
+
 ///
 /// Represents an element of a bezier path
 ///
-#[derive(Clone, Copy, PartialEq, Debug)]
+#[derive(Clone, Copy, Debug)]
 pub enum PathComponent {
     /// Move to point
     Move(PathPoint),
@@ -16,6 +18,36 @@ pub enum PathComponent {
 
     /// Close path
     Close
+}
+
+/// 
+/// Returns true if two points are 'close enough' to be considered the same in PartialEq
+///
+fn is_close(a: &PathPoint, b: &PathPoint) -> bool {
+    const MIN_DISTANCE: f64 = 0.02;
+
+    let diff    = *a - *b;
+    let diff_sq = diff.dot(&diff);
+
+    diff_sq < (MIN_DISTANCE * MIN_DISTANCE)
+}
+
+///
+/// Two path components are considered to match if their coordinates are 'close enough' (as when we serialize f64 values we compress them in a way that loses a lot of precision)
+///
+impl PartialEq for PathComponent {
+    fn eq(&self, b: &PathComponent) -> bool {
+        use PathComponent::*;
+
+        match (self, b) {
+            (Move(a), Move(b))                          => is_close(a, b),
+            (Line(a), Line(b))                          => is_close(a, b),
+            (Bezier(a1, a2, a3), Bezier(b1, b2, b3))    => is_close(a1, b1) && is_close(a2, b2) && is_close(a3, b3),
+            (Close, Close)                              => true,
+
+            _ => false,
+        }
+    }
 }
 
 impl PathComponent {
