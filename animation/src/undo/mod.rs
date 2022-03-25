6cb61978b5e87e9e1befc2b23e43223519a8b5d2
Andrew Hunter <andrew@logicalshift.co.uk>
2022-03-25 08:49:48 +0000

    Rename the undo_animation file to undoable_animation to match the struct name

--- animation/src/undo/mod.rs
+++ animation/src/undo/mod.rs
@@ -4,10 +4,10 @@
 
 mod undo_log;
 mod undo_step;
-mod undo_animation;
+mod undoable_animation;
 mod edit_log_reader;
 mod reversed_edits;
 #[cfg(test)] mod tests;
 
-pub use self::undo_animation::*;
+pub use self::undoable_animation::*;
 pub (crate) use self::reversed_edits::*;
