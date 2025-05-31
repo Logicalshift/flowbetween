b82a8d366f8a60b694ef040134cfc80b06d638dc
Andrew Hunter <andrew@logicalshift.co.uk>
2025-05-31 22:44:18 +0100

    Add a way to send DocumentRequests directly to a document via an app document

--- flo2/src/scenery/app/document.rs
+++ flo2/src/scenery/app/document.rs
@@ -1,3 +1,5 @@
+use crate::scenery::document::*;
+
 use flo_scene::*;
 
 use serde::*;
@@ -10,7 +12,10 @@ use std::sync::*;
 ///
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum AppDocumentRequest {
-
+    ///
+    /// Sends a document request to the target scene
+    ///
+    DocumentRequest(DocumentRequest),
 }
 
 impl SceneMessage for AppDocumentRequest {
@@ -24,13 +29,26 @@ pub async fn document(document_scene: Arc<Scene>, input: InputStream<AppDocument
     // Run the scene
     let run_scene = document_scene.run_scene_with_threads(4);
 
+    // Create stream for document requests
+    let mut document_requests = document_scene.send_to_scene(()).unwrap();
+
     // ... and also run a future listening for document requests from the main app
     future::select(
         run_scene.boxed(),
         async move {
             let mut input = input;
             while let Some(request) = input.next().await {
-                let _ = request;
+                match request {
+                    AppDocumentRequest::DocumentRequest(req) => {
+                        document_requests.send(req).await.ok();
+                    }
+                }
             }
         }.boxed()).await;
+
+    // When shutting down the program, also shut down the document if it's still there
+    // TODO: ... and the window, if it's still there
+    if let Ok(mut document_requests) = document_scene.send_to_scene(()) {
+        document_requests.send(DocumentRequest::Close).await.ok();
+    }
 }
