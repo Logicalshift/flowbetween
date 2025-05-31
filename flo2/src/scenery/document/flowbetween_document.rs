17188eb8476788dd223cb53004315f4b8261213b
Andrew Hunter <andrew@logicalshift.co.uk>
2025-05-31 22:09:14 +0100

    Add document requests: drawing and resizing the window

--- flo2/src/scenery/document/flowbetween_document.rs
+++ flo2/src/scenery/document/flowbetween_document.rs
@@ -15,7 +15,14 @@ use std::sync::*;
 ///
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum DocumentRequest {
+    /// Indicates that the pending requests have resolved themselves
+    Idle,
 
+    /// Resizes the document window
+    Resize(usize, usize),
+
+    /// Renders some items to the drawing window
+    Draw(DrawingRequest),
 }
 
 impl SceneMessage for DocumentRequest {
@@ -38,11 +45,88 @@ pub async fn flowbetween_document(_document_scene: Arc<Scene>, input: InputStrea
 
     window_drawing.send(DrawingRequest::Draw(Arc::new(window_setup))).await.ok();
 
+    let mut size = (1000, 1000);
+
+    // The document canvas contains the drawing instructions to regenerate the canvas (except for the 'clear canvas' instruction that begins it)
+    // We re-use this whenever the document is resized
+    let mut document_canvas = Canvas::new();
+    document_canvas.write([Draw::ClearCanvas(Color::Rgba(0.8, 0.8, 0.8, 1.0))].into_iter().collect());
+
+    // Drawing instructions that are waiting for this document scene to become idle
+    let mut pending_drawing = Vec::with_capacity(128);
+
     // TODO: start the other document subprograms
 
-    // Process the events for this document
-    let mut input = input;
-    while let Some(request) = input.next().await {
+    // Process the events for this document (in chunks, so we group together multiple drawing or resize requests if they occur)
+    let mut input = input.ready_chunks(100);
+    while let Some(many_requests) = input.next().await {
+        let mut new_size = size;
 
+        // Process the buffered requests
+        for request in many_requests {
+            match request {
+                DocumentRequest::Draw(DrawingRequest::Draw(drawing)) => {
+                    // Write to the canvas
+                    document_canvas.write(drawing.iter().cloned().collect());
+
+                    // Also write to the pending drawing instructions, ready to pass on to the main window
+                    for draw in drawing.iter() {
+                        match draw {
+                            Draw::ClearCanvas(background) => {
+                                pending_drawing = vec![draw.clone()];
+                            }
+
+                            _ => {
+                                pending_drawing.push(draw.clone());
+                            }
+                        }
+                    }
+                }
+
+                DocumentRequest::Resize(width, height) => {
+                    if new_size != (width, height) {
+                        // Update the size of the canvas
+                        new_size = (width, height);
+
+                        // Reset the pending drawing to the canvas contents
+                        pending_drawing = document_canvas.get_drawing();
+
+                        // Set up the canvas display region after the point that the canvas is cleared
+                        let clear_canvas_idx = pending_drawing.iter().position(|draw| matches!(draw, Draw::ClearCanvas(_)));
+
+                        if let Some(clear_canvas_idx) = clear_canvas_idx {
+                            pending_drawing.splice(clear_canvas_idx+1..clear_canvas_idx+1, [
+                                Draw::CanvasHeight(height as _),
+                                Draw::CenterRegion((0.0, 0.0), (width as _, height as _)),
+                            ]);
+                        }
+                    }
+                }
+
+                DocumentRequest::Idle => {
+                    // Send any pending drawing instructions whenever the scene processing stops
+                    if !pending_drawing.is_empty() {
+                        use std::mem;
+
+                        let mut recent_drawing = Vec::with_capacity(128);
+                        mem::swap(&mut recent_drawing, &mut pending_drawing);
+
+                        window_drawing.send(DrawingRequest::Draw(Arc::new(recent_drawing))).await.ok();
+                    }
+                }
+            }
+        }
+
+        // If the requests included a window size change, flush the pending drawing instructions early
+        if new_size != size {
+            size = new_size;
+
+            use std::mem;
+
+            let mut recent_drawing = Vec::with_capacity(128);
+            mem::swap(&mut recent_drawing, &mut pending_drawing);
+
+            window_drawing.send(DrawingRequest::Draw(Arc::new(recent_drawing))).await.ok();
+        }
     }
 }
