b45930e563f6a5f19327c9f41acd7db6c04e47ea
Andrew Hunter <andrew@logicalshift.co.uk>
2025-06-24 22:48:45 +0100

    Create the physics layer subprogram

--- flo2/src/scenery/ui/physics.rs
+++ flo2/src/scenery/ui/physics.rs
@@ -7,8 +7,12 @@
 //!
 
 use super::physics_tool::*;
+use super::subprograms::*;
 
 use flo_scene::*;
+use flo_draw::*;
+
+use futures::prelude::*;
 
 use ::serde::*;
 use serde::de::{Error as DeError};
@@ -29,6 +33,32 @@ pub enum PhysicsLayer {
 
     /// Adds a floating version of a tool
     Float(PhysicsToolId),
+
+    /// Event to process
+    Event(DrawEvent),
+}
+
+///
+/// Events from a physics layer
+///
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub enum PhysicsEvent {
+    /// Indicates that the specified tool has been selected
+    Select(PhysicsToolId),
+
+    /// Indicatesd that the specified tool has been deselected
+    Deselect(PhysicsToolId),
+}
+
+///
+/// Runs the physics layer subprogram
+///
+pub async fn physics_layer(input: InputStream<PhysicsLayer>, context: SceneContext) {
+    let mut input = input;
+
+    while let Some(request) = input.next().await {
+
+    }
 }
 
 impl Serialize for PhysicsLayer {
@@ -51,4 +81,14 @@ impl<'a> Deserialize<'a> for PhysicsLayer {
 
 impl SceneMessage for PhysicsLayer {
     fn serializable() -> bool { false }
+
+    fn initialise(init_context: &impl SceneInitialisationContext) {
+        init_context.add_subprogram(subprogram_physics_layer(), physics_layer, 20);
+
+        init_context.connect_programs((), subprogram_physics_layer(), StreamId::with_message_type::<PhysicsLayer>()).ok();
+    }
+}
+
+impl SceneMessage for PhysicsEvent {
+
 }
--- flo2/src/scenery/ui/subprograms.rs
+++ flo2/src/scenery/ui/subprograms.rs
@@ -9,3 +9,8 @@ pub fn subprogram_focus() -> SubProgramId { SubProgramId::called("flowbetween::u
 /// ID of the main 'focus' subprogram
 ///
 pub fn subprogram_dialog() -> SubProgramId { SubProgramId::called("flowbetween::ui::dialog") }
+
+///
+/// ID of the physics layer subprogram
+///
+pub fn subprogram_physics_layer() -> SubProgramId { SubProgramId::called("flowbetween::ui::physics_layer") }
