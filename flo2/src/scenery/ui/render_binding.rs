b8050e107cf8d75301a061912230d277cb965fe2
Andrew Hunter <andrew@logicalshift.co.uk>
2025-07-27 16:23:22 +0100

    Use a future, and change how when_changed is used to make the future Send

--- flo2/src/scenery/ui/render_binding.rs
+++ flo2/src/scenery/ui/render_binding.rs
@@ -23,106 +23,106 @@ use std::sync::*;
 /// This subprogram is very useful for drawing dynamically as it removes the need for a program to generate
 /// its own rendering events.
 ///
-pub async fn render_binding_program(input: InputStream<RenderBinding>, context: SceneContext, render_layer: (NamespaceId, LayerId), parent_program: Option<SubProgramId>, rendering: BindRef<Vec<Draw>>) {
-    let mut input = input;
+pub fn render_binding_program(input: InputStream<RenderBinding>, context: SceneContext, render_layer: (NamespaceId, LayerId), parent_program: Option<SubProgramId>, rendering: BindRef<Vec<Draw>>) -> impl Send + Future<Output=()> {
+    async move {
+        let mut input = input;
 
-    // TODO: would be nice to be able to do this with sprites as well as layers (need a custom type for RenderLayer I think)
+        // TODO: would be nice to be able to do this with sprites as well as layers (need a custom type for RenderLayer I think)
 
-    // Need to know the program ID we've been assigned
-    let our_program_id = context.current_program_id().unwrap();
+        // Need to know the program ID we've been assigned
+        let our_program_id = context.current_program_id().unwrap();
 
-    // Connect to the idle request program
-    let mut idle_requests = context.send::<IdleRequest>(()).unwrap();
+        // Connect to the idle request program
+        let mut idle_requests = context.send::<IdleRequest>(()).unwrap();
 
-    // When the canvas is rendered, we send a drawing request
-    let mut draw_requests = context.send::<DrawingRequest>(()).unwrap();
+        // When the canvas is rendered, we send a drawing request
+        let mut draw_requests = context.send::<DrawingRequest>(()).unwrap();
 
-    // We want scene updates so we can stop if the parent program stops
-    if parent_program.is_some() {
-        context.send_message(SceneControl::Subscribe(our_program_id.into())).await.ok();
-    }
+        // We want scene updates so we can stop if the parent program stops
+        if parent_program.is_some() {
+            context.send_message(SceneControl::Subscribe(our_program_id.into())).await.ok();
+        }
 
-    // This releasable is what monitors the render binding
-    let mut render_binding_tracker;
+        // This releasable is what monitors the render binding
+        let mut render_binding_tracker;
 
-    // Render the initial scene, and set up the tracker
-    let (initial_drawing, deps) = BindingContext::bind(|| rendering.get());
+        // Render the initial scene, and set up the tracker
+        render_binding_tracker  = Some(rendering.when_changed(NotifySubprogram::send(RenderBinding::DrawingChanged, &context, our_program_id)));
+        let initial_drawing     = rendering.get();
 
-    render_binding_tracker = Some(deps.when_changed(NotifySubprogram::send(RenderBinding::DrawingChanged, &context, our_program_id)));
+        let mut initial_drawing = initial_drawing;
+        initial_drawing.splice(0..0, vec![
+            Draw::PushState,
+            Draw::Namespace(render_layer.0),
+            Draw::Layer(render_layer.1),
+        ]);
+        initial_drawing.push(Draw::PopState);
 
-    let mut initial_drawing = initial_drawing;
-    initial_drawing.splice(0..0, vec![
-        Draw::PushState,
-        Draw::Namespace(render_layer.0),
-        Draw::Layer(render_layer.1),
-    ]);
-    initial_drawing.push(Draw::PopState);
+        draw_requests.send(DrawingRequest::Draw(Arc::new(initial_drawing))).await.ok();
 
-    draw_requests.send(DrawingRequest::Draw(Arc::new(initial_drawing))).await.ok();
+        // Monitor for events and render the drawing when needed
+        let mut requested_idle = false;
 
-    // Monitor for events and render the drawing when needed
-    let mut requested_idle = false;
+        while let Some(event) = input.next().await {
+            use RenderBinding::*;
 
-    while let Some(event) = input.next().await {
-        use RenderBinding::*;
+            match event {
+                DrawingChanged => {
+                    // Request an idle notification if one isn't already waiting
+                    if !requested_idle {
+                        // Clear the tracker
+                        if let Some(mut render_binding_tracker) = render_binding_tracker.take() {
+                            render_binding_tracker.done();
+                        }
 
-        match event {
-            DrawingChanged => {
-                // Request an idle notification if one isn't already waiting
-                if !requested_idle {
-                    // Clear the tracker
-                    if let Some(mut render_binding_tracker) = render_binding_tracker.take() {
-                        render_binding_tracker.done();
+                        // Request an idle event
+                        idle_requests.send(IdleRequest::WhenIdle(our_program_id)).await.ok();
+                        requested_idle = true;
                     }
-
-                    // Request an idle event
-                    idle_requests.send(IdleRequest::WhenIdle(our_program_id)).await.ok();
-                    requested_idle = true;
                 }
-            }
 
-            Idle => {
-                // The idle event is triggered after the drawing is changed: we need to re-render the drawing
-                requested_idle = false;
+                Idle => {
+                    // The idle event is triggered after the drawing is changed: we need to re-render the drawing
+                    requested_idle = false;
 
-                // Render the drawing, and wait for the rendering to change again
-                let (drawing, deps) = BindingContext::bind(|| rendering.get());
+                    // Render the drawing, and wait for the rendering to change again
+                    render_binding_tracker  = Some(rendering.when_changed(NotifySubprogram::send(RenderBinding::DrawingChanged, &context, our_program_id)));
+                    let drawing             = rendering.get();
 
-                render_binding_tracker = Some(deps.when_changed(NotifySubprogram::send(RenderBinding::DrawingChanged, &context, our_program_id)));
-
-                let mut drawing = drawing;
-                drawing.splice(0..0, vec![
-                    Draw::PushState,
-                    Draw::Namespace(render_layer.0),
-                    Draw::Layer(render_layer.1),
-                ]);
-                drawing.push(Draw::PopState);
-
-                draw_requests.send(DrawingRequest::Draw(Arc::new(drawing))).await.ok();
-            }
-
-            Update(SceneUpdate::Stopped(program_id)) => {
-                if Some(program_id) == parent_program {
-                    // Stop anything from notifying us
-                    if let Some(mut render_binding_tracker) = render_binding_tracker.take() {
-                        render_binding_tracker.done();
-                    }
-
-                    // When the parent program stops, clear the layer that we were rendering
-                    draw_requests.send(DrawingRequest::Draw(Arc::new(vec![
+                    let mut drawing = drawing;
+                    drawing.splice(0..0, vec![
                         Draw::PushState,
                         Draw::Namespace(render_layer.0),
                         Draw::Layer(render_layer.1),
-                        Draw::ClearLayer,
-                        Draw::PopState,
-                    ]))).await.ok();
+                    ]);
+                    drawing.push(Draw::PopState);
 
-                    // Stop running if the parent program has been stopped
-                    break;
+                    draw_requests.send(DrawingRequest::Draw(Arc::new(drawing))).await.ok();
                 }
-            }
 
-            Update(_) => { /* Other updates are ignored */}
+                Update(SceneUpdate::Stopped(program_id)) => {
+                    if Some(program_id) == parent_program {
+                        // Stop anything from notifying us
+                        if let Some(mut render_binding_tracker) = render_binding_tracker.take() {
+                            render_binding_tracker.done();
+                        }
+
+                        // When the parent program stops, clear the layer that we were rendering
+                        draw_requests.send(DrawingRequest::Draw(Arc::new(vec![
+                            Draw::PushState,
+                            Draw::Namespace(render_layer.0),
+                            Draw::Layer(render_layer.1),
+                            Draw::ClearLayer,
+                            Draw::PopState,
+                        ]))).await.ok();
+
+                        // Stop running if the parent program has been stopped
+                        break;
+                    }
+                }
+
+                Update(_) => { /* Other updates are ignored */}
+            }
         }
     }
 }
