83c5a35f666032bf2ebddae196e172df318f304b
Andrew Hunter <andrew@logicalshift.co.uk>
2025-08-08 21:33:31 +0100

    Add some descriptions of a joint to the physics simulation (so we can add joints other the the 'anchor' springs that we have at the moment)

--- flo2/src/scenery/ui/tools/physics_simulation.rs
+++ flo2/src/scenery/ui/tools/physics_simulation.rs
@@ -30,17 +30,32 @@ const TICK_DURATION_S: f64 = 1.0/60.0;
 const MAX_TICKS: usize = 30;
 
 ///
-/// Identifier used to specify a physics tool within the flowbetween app
+/// Identifier of an object in a physics simulation
 ///
 #[derive(Serialize, Deserialize, Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct SimObjectId(Uuid);
 
 impl SimObjectId {
     ///
-    /// Creates a unique new physics tool ID
+    /// Creates a unique new simulation object ID
     ///
     pub fn new() -> Self {
         SimObjectId(Uuid::new_v4())
+    }
+}
+
+///
+/// Indentifier of a joint between objects in a physics simulation
+///
+#[derive(Serialize, Deserialize, Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct SimJointId(Uuid);
+
+impl SimJointId {
+    ///
+    /// Creates a unique new simulation joint ID
+    ///
+    pub fn new() -> Self {
+        SimJointId(Uuid::new_v4())
     }
 }
 
@@ -137,6 +152,45 @@ pub enum PhysicsSimEvent {
 
     /// One or more steps have been simulated
     Step,
+}
+
+///
+/// Basic definition of a joint
+///
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum SimJoint {
+    /// A fixed joint (stops two objects from moving apart)
+    FixedJoint,
+
+    /// A spring joint (pulls an object towards a fixed distance away)
+    SpringJoint { rest_length: f64, stiffness: f64, damping: f64 },
+
+    /// A rope joint (ensures an object can't get more than a certain distance away)
+    RopeJoint { max_dist: f64 },
+}
+
+///
+/// Extra properties that can be applied to a joint
+///
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum SimJointSide {
+    /// The first object that the joint is for
+    First,
+
+    /// The second object that the joint is for
+    Second,
+}
+
+///
+/// Extra properties that can be applied to a joint
+///
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum SimJointProperty {
+    /// Whether or not the objects at both ends of the joint can collide with each other
+    ContactsEnabled(bool),
+
+    /// Offset from the center where the joint connects with the object
+    LocalAnchor(SimJointSide, UiPoint),
 }
 
 ///
