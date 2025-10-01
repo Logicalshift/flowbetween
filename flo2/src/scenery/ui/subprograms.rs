35ebf4fbd16a3c77770755795e745f49332970a5
Andrew Hunter <andrew@logicalshift.co.uk>
2025-10-01 23:03:37 +0100

    Add a subprogram ID for the tool state program

--- flo2/src/scenery/ui/subprograms.rs
+++ flo2/src/scenery/ui/subprograms.rs
@@ -11,6 +11,11 @@ pub fn subprogram_focus() -> SubProgramId { SubProgramId::called("flowbetween::u
 pub fn subprogram_dialog() -> SubProgramId { SubProgramId::called("flowbetween::ui::dialog") }
 
 ///
+/// ID of the tool state subprogram
+///
+pub fn subprogram_tool_state() -> SubProgramId { SubProgramId::called("flowbetween::tool_state") }
+
+///
 /// ID of the physics layer subprogram
 ///
 pub fn subprogram_physics_layer() -> SubProgramId { SubProgramId::called("flowbetween::ui::physics_layer") }
