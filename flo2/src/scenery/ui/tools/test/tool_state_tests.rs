12717ec462bc0d9f8673537278bc824eb661e417
Andrew Hunter <andrew@logicalshift.co.uk>
2025-10-11 22:42:18 +0100

    Add a test that we don't select the same tool twice

--- flo2/src/scenery/ui/tools/test/tool_state_tests.rs
+++ flo2/src/scenery/ui/tools/test/tool_state_tests.rs
@@ -115,6 +115,35 @@ pub fn select_tool_owner() {
 }
 
 #[test]
+pub fn does_not_reselect_same_tool() {
+    let scene = Scene::default();
+
+    let tool_group  = ToolGroupId::new();
+    let tool_type   = ToolTypeId::new();
+    let tool_id_1   = ToolId::new();
+    let tool_id_2   = ToolId::new();
+
+    let test_program_id = SubProgramId::new();
+
+    println!("ToolID1 = {:?}", tool_id_1);
+    println!("ToolID2 = {:?}", tool_id_2);
+
+    TestBuilder::new()
+        .send_message(Tool::SetToolOwner(tool_type, test_program_id.into()))
+        .send_message(Tool::CreateTool(tool_group, tool_type, tool_id_1))
+        .send_message(Tool::CreateTool(tool_group, tool_type, tool_id_2))
+        .expect_message(expect_toolstate(ToolState::AddTool(tool_id_1)))
+        .expect_message(expect_toolstate(ToolState::AddTool(tool_id_2)))
+        .send_message(Tool::Select(tool_id_1))
+        .send_message(Tool::Select(tool_id_1))
+        .expect_message(expect_toolstate(ToolState::Select(tool_id_1)))
+        .send_message(Tool::Select(tool_id_2))
+        .expect_message(expect_toolstate(ToolState::Deselect(tool_id_1)))
+        .expect_message(expect_toolstate(ToolState::Select(tool_id_2)))
+        .run_in_scene(&scene, test_program_id);
+}
+
+#[test]
 pub fn select_tools_in_groups_owner() {
     let scene = Scene::default();
 
