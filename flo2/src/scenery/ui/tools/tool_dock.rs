1d6410d3a7839cde8d3f9e54814afdad70895bb6
Andrew Hunter <andrew@logicalshift.co.uk>
2025-11-24 23:18:34 +0000

    Add a way to send draw events to the dock program

--- flo2/src/scenery/ui/tools/tool_dock.rs
+++ flo2/src/scenery/ui/tools/tool_dock.rs
@@ -5,6 +5,8 @@
 use super::tool_state::*;
 
 use flo_scene::*;
+use flo_draw::*;
+
 use futures::prelude::*;
 use serde::*;
 
@@ -15,11 +17,16 @@ use serde::*;
 pub enum ToolDockMessage {
     /// Updating the tool state for this dock
     ToolState(ToolState),
+
+    /// Drawing event for the window this dock is in
+    DrawEvent(DrawEvent),
 }
 
 impl SceneMessage for ToolDockMessage {
     fn initialise(init_context: &impl SceneInitialisationContext) {
         init_context.connect_programs(StreamSource::Filtered(FilterHandle::for_filter(|tool_state_msgs| tool_state_msgs.map(|msg| ToolDockMessage::ToolState(msg)))), (), StreamId::with_message_type::<ToolState>())
+            .unwrap();
+        init_context.connect_programs(StreamSource::Filtered(FilterHandle::for_filter(|draw_event_msgs| draw_event_msgs.map(|msg| ToolDockMessage::DrawEvent(msg)))), (), StreamId::with_message_type::<DrawEvent>())
             .unwrap();
     }
 }
@@ -34,6 +41,9 @@ pub enum DockPosition {
 
 ///
 /// Runs a tool dock subprogram. This is a location, which can be used with the `Tool::SetToolLocation` message to specify which tools are found in this dock.
+/// 
+/// In order to draw the tool dock at the correct size, this requires `DrawEvent` messages (this does not subscribe to these itself, but
+/// this can be set up by sending `DocumentRequest::SubscribeDrawEvents` for example)
 ///
 pub async fn tool_dock_program(input: InputStream<ToolDockMessage>, context: SceneContext, position: DockPosition) {
     // Run the program
@@ -41,6 +51,7 @@ pub async fn tool_dock_program(input: InputStream<ToolDockMessage>, context: Sce
     while let Some(msg) = input.next().await {
         match msg {
             ToolDockMessage::ToolState(_) => { /* Other toolstate messages are ignored */ }
+            ToolDockMessage::DrawEvent(_) => { /* Other drawing events are ignored */ }
         }
     }
 }
