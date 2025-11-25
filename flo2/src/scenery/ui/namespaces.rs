1464d3821291d61d7e821ce74948d085e4fb097a
Andrew Hunter <andrew@logicalshift.co.uk>
2025-11-25 10:03:36 +0000

    Add a rendering namespace for the tool docks

--- flo2/src/scenery/ui/namespaces.rs
+++ flo2/src/scenery/ui/namespaces.rs
@@ -3,3 +3,4 @@ use once_cell::sync::{Lazy};
 
 pub static DIALOG_LAYER: Lazy<NamespaceId>  = Lazy::new(|| NamespaceId::new());
 pub static PHYSICS_LAYER: Lazy<NamespaceId> = Lazy::new(|| NamespaceId::new());
+pub static DOCK_LAYER: Lazy<NamespaceId> = Lazy::new(|| NamespaceId::new());
