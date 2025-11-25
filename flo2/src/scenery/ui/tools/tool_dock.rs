d851b5ca32424e76835c6c62eeeb18efc6a7f4e9
Andrew Hunter <andrew@logicalshift.co.uk>
2025-11-25 10:03:46 +0000

    Add basic drawing to the tool dock

--- flo2/src/scenery/ui/tools/tool_dock.rs
+++ flo2/src/scenery/ui/tools/tool_dock.rs
@@ -2,13 +2,18 @@
 //! A tool dock contains a fixed set of tools and allows user to select one per tool group.
 //!
 
+use crate::scenery::ui::*;
 use super::tool_state::*;
 
 use flo_scene::*;
 use flo_draw::*;
+use flo_draw::canvas::*;
+use flo_draw::canvas::scenery::*;
 
 use futures::prelude::*;
 use serde::*;
+
+use std::sync::*;
 
 ///
 /// Message sent to a tool dock
@@ -40,18 +45,90 @@ pub enum DockPosition {
 }
 
 ///
+/// Data storage for the tool dock
+///
+struct ToolDock {
+    /// Where in the window to draw the dock
+    position:   DockPosition,
+
+    /// The layer that the dock is drawn on
+    layer:      LayerId,
+
+    /// The namespace that the dock is drawn in
+    namespace:  NamespaceId,
+}
+
+impl ToolDock {
+    ///
+    /// Redraws this tool dock
+    ///
+    pub fn draw(&self, gc: &mut impl GraphicsContext, window_size: (f64, f64)) {
+        // Store the GC state and reset it to the layer this dock is drawn on
+        gc.push_state();
+        gc.namespace(self.namespace);
+        gc.layer(self.layer);
+        gc.clear_layer();
+
+        // Finish up by clearing the state
+        gc.pop_state();
+    }
+}
+
+///
 /// Runs a tool dock subprogram. This is a location, which can be used with the `Tool::SetToolLocation` message to specify which tools are found in this dock.
 /// 
 /// In order to draw the tool dock at the correct size, this requires `DrawEvent` messages (this does not subscribe to these itself, but
 /// this can be set up by sending `DocumentRequest::SubscribeDrawEvents` for example)
 ///
-pub async fn tool_dock_program(input: InputStream<ToolDockMessage>, context: SceneContext, position: DockPosition) {
+pub async fn tool_dock_program(input: InputStream<ToolDockMessage>, context: SceneContext, position: DockPosition, layer: LayerId) {
+    // Tool dock data
+    let mut tool_dock = ToolDock {
+        position:   position,
+        layer:      layer,
+        namespace:  *DOCK_LAYER,
+    };
+
+    // Size of the viewport
+    let mut scale   = 1.0;
+    let mut w       = 0.0;
+    let mut h       = 0.0;
+
     // Run the program
-    let mut input = input;
-    while let Some(msg) = input.next().await {
-        match msg {
-            ToolDockMessage::ToolState(_) => { /* Other toolstate messages are ignored */ }
-            ToolDockMessage::DrawEvent(_) => { /* Other drawing events are ignored */ }
+    let mut input = input.ready_chunks(50);
+    while let Some(msgs) = input.next().await {
+        let mut needs_redraw = false;
+
+        // Process the messages that are waiting
+        for msg in msgs {
+            match msg {
+                ToolDockMessage::DrawEvent(DrawEvent::Resize(new_w, new_h)) => {
+                    // Update the width and height
+                    w = new_w / scale;
+                    h = new_h / scale;
+
+                    needs_redraw = true;
+                }
+
+                ToolDockMessage::DrawEvent(DrawEvent::Scale(new_scale)) => {
+                    // Update the scale, adjust the width and height accordingly
+                    w = (w * scale) / new_scale;
+                    h = (h * scale) / new_scale;
+                    scale = new_scale;
+
+                    needs_redraw = true;
+                }
+
+                ToolDockMessage::ToolState(_) => { /* Other toolstate messages are ignored */ }
+                ToolDockMessage::DrawEvent(_) => { /* Other drawing events are ignored */ }
+            }
+        }
+
+        // Redraw the dock if necessary
+        if needs_redraw {
+            let mut drawing = vec![];
+            tool_dock.draw(&mut drawing, (w, h));
+
+            context.send_message(DrawingRequest::Draw(Arc::new(drawing))).await.ok();
         }
     }
 }
