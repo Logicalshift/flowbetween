930118bcfe99a6ad6273905630d95dcc1457c6d5
Andrew Hunter <andrew@logicalshift.co.uk>
2025-11-29 12:50:07 +0000

    Add subprogram IDs for the two tool docks

--- flo2/src/scenery/document/subprograms.rs
+++ flo2/src/scenery/document/subprograms.rs
@@ -9,3 +9,13 @@ pub fn subprogram_flowbetween_document() -> SubProgramId { SubProgramId::called(
 /// ID of the subprogram where DrawingWindowRequests can be sent to
 ///
 pub fn subprogram_window() -> SubProgramId { SubProgramId::called("flowbetween::document::window") }
+
+///
+/// ID of the left tool dock program
+///
+pub fn subprogram_tool_dock_left() -> SubProgramId { SubProgramId::called("flowbetween::tool_dock::left") }
+
+///
+/// ID of the right tool dock program
+///
+pub fn subprogram_tool_dock_right() -> SubProgramId { SubProgramId::called("flowbetween::tool_dock::right") }
