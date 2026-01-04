72408d8fe9b41ce64f081fb4a55f032f16973e61
Andrew Hunter <andrew@logicalshift.co.uk>
2026-01-04 17:52:13 +0000

    Use a darker colour for the selected tool as this looks better with the icons

--- flo2/src/scenery/ui/colors.rs
+++ flo2/src/scenery/ui/colors.rs
@@ -4,7 +4,7 @@ use flo_draw::canvas::*;
 #[inline] pub fn color_tool_shadow() -> Color                   { Color::Rgba(0.4, 0.4, 0.4, 0.4) }
 #[inline] pub fn color_tool_outline() -> Color                  { Color::Rgba(0.4, 0.7, 1.0, 1.0) }
 #[inline] pub fn color_tool_background() -> Color               { Color::Rgba(0.7, 0.7, 0.7, 0.9) }
-#[inline] pub fn color_tool_background_selected() -> Color      { Color::Rgba(0.8, 0.9, 0.9, 0.9) }
+#[inline] pub fn color_tool_background_selected() -> Color      { Color::Rgba(0.5, 0.6, 0.6, 0.9) }
 #[inline] pub fn color_tool_background_highlighted() -> Color   { Color::Rgba(0.8, 0.85, 0.85, 0.9) }
 #[inline] pub fn color_tool_border_selected() -> Color          { Color::Rgba(0.6, 0.7, 0.8, 1.0) }
 
