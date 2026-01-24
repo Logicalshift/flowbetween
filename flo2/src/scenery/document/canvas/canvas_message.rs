0e8c9f6e13bdaaae1f3f090cde064f53beea9128
Andrew Hunter <andrew@logicalshift.co.uk>
2026-01-24 20:35:09 +0000

    For properties, I think we treat them like a table of values rather than arbitrary structures
    This should let us store and retrieve them fairly efficiently, as well as providing a way to add extra properties to build new features in the future.

--- flo2/src/scenery/document/canvas/canvas_message.rs
+++ flo2/src/scenery/document/canvas/canvas_message.rs
@@ -90,6 +90,33 @@ pub enum CanvasShapeParent {
 }
 
 ///
+/// Identifier for a canvas property
+///
+#[derive(Serialize, Deserialize, Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct CanvasPropertyId(usize);
+
+///
+/// Value of a specific property set on a shape, layer or brush
+///
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, PartialOrd)]
+pub enum CanvasProperty {
+    /// Property with a single float value
+    Float(f64),
+
+    /// Property with a single integer value
+    Int(i64),
+
+    /// Property with a value that's a floating point number
+    FloatList(Vec<f64>),
+
+    /// Property with a value that's a list of integers
+    IntList(Vec<i64>),
+
+    /// Property with a value that's a series of bytes
+    ByteList(Vec<u8>),
+}
+
+///
 /// Basic editing actions for the canvas
 ///
 /// At a basic level, there are a few entities that can exist on a canvas:
