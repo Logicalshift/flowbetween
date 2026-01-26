83f8bb5120eb4c169c180221da11b500df05adc3
Andrew Hunter <andrew@logicalshift.co.uk>
2026-01-26 22:29:41 +0000

    Shape type names will work the same way as property names

--- flo2/src/scenery/document/canvas/shape_type.rs
+++ flo2/src/scenery/document/canvas/shape_type.rs
@@ -1,5 +1,14 @@
 use ::serde::*;
 
+use std::collections::*;
+use std::sync::*;
+
+/// Maps shape type IDs to their names
+static SHAPE_TYPE_NAMES: LazyLock<Mutex<Vec<&'static str>>> = LazyLock::new(|| Mutex::new(vec![]));
+
+/// Hashmap mapping shape type names to IDs
+static SHAPE_TYPE_FOR_NAME: LazyLock<Mutex<HashMap<&'static str, usize>>> = LazyLock::new(|| Mutex::new(HashMap::new()));
+
 ///
 /// Represents the type of a shape
 ///
