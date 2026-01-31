49517eb5acbe7371fa5af7c506eba8bb7f7a8d0a
Andrew Hunter <andrew@logicalshift.co.uk>
2026-01-31 21:52:07 +0000

    Add a way to convert layer IDs to and from strings

--- flo2/src/scenery/document/canvas/layer.rs
+++ flo2/src/scenery/document/canvas/layer.rs
@@ -1,6 +1,8 @@
 use ::serde::*;
 use uuid::*;
 
+use std::str::*;
+
 ///
 /// Identifier used for a layer in the canvas document
 ///
@@ -13,5 +15,20 @@ impl CanvasLayerId {
     ///
     pub fn new() -> Self {
         Self(Uuid::new_v4())
+    }
+
+    ///
+    /// Creates a layer ID from a string
+    ///
+    pub fn from_string(string_guid: &str) -> Self {
+        CanvasLayerId(Uuid::from_str(string_guid).unwrap())
+    }
+
+    ///
+    /// Returns the string representation of this layer ID
+    ///
+    #[inline]
+    pub fn to_string(&self) -> String {
+        self.0.to_string()
     }
 }
