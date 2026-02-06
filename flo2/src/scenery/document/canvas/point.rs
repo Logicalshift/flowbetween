7735db40304f07b3b108bf208d3ac30cffa680ad
Andrew Hunter <andrew@logicalshift.co.uk>
2026-02-06 21:35:31 +0000

    Add an implementation of the Coordinate traits for CanvasPoint

--- flo2/src/scenery/document/canvas/point.rs
+++ flo2/src/scenery/document/canvas/point.rs
@@ -1,10 +1,140 @@
-use ::serde::*;
+use serde::*;
+
+use flo_curves::geo::*;
+
+use std::ops::{Add, DivAssign, Mul, MulAssign, Sub};
 
 ///
 /// Represents a point on the canvas
 ///
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Copy, Clone, Debug, PartialEq)]
 pub struct CanvasPoint {
     pub x: f32,
     pub y: f32,
+}
+
+impl Coordinate2D for CanvasPoint {
+    ///
+    /// X component of this coordinate
+    ///
+    #[inline]
+    fn x(&self) -> f64 {
+        self.x as f64
+    }
+
+    ///
+    /// Y component of this coordinate
+    ///
+    #[inline]
+    fn y(&self) -> f64 {
+        self.y as f64
+    }
+}
+
+impl Add for CanvasPoint {
+    type Output = CanvasPoint;
+
+    #[inline]
+    fn add(self, rhs: Self) -> Self::Output {
+        CanvasPoint {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl Sub for CanvasPoint {
+    type Output = CanvasPoint;
+
+    #[inline]
+    fn sub(self, rhs: Self) -> Self::Output {
+        CanvasPoint {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
+impl Mul<f64> for CanvasPoint {
+    type Output = CanvasPoint;
+
+    #[inline]
+    fn mul(self, rhs: f64) -> Self::Output {
+        CanvasPoint {
+            x: (self.x as f64 * rhs) as f32,
+            y: (self.y as f64 * rhs) as f32,
+        }
+    }
+}
+
+impl MulAssign<f64> for CanvasPoint {
+    #[inline]
+    fn mul_assign(&mut self, rhs: f64) {
+        self.x = (self.x as f64 * rhs) as f32;
+        self.y = (self.y as f64 * rhs) as f32;
+    }
+}
+
+impl DivAssign<f64> for CanvasPoint {
+    #[inline]
+    fn div_assign(&mut self, rhs: f64) {
+        self.x = (self.x as f64 / rhs) as f32;
+        self.y = (self.y as f64 / rhs) as f32;
+    }
+}
+
+impl Coordinate for CanvasPoint {
+    #[inline]
+    fn from_components(components: &[f64]) -> CanvasPoint {
+        CanvasPoint {
+            x: components[0] as f32,
+            y: components[1] as f32,
+        }
+    }
+
+    #[inline]
+    fn origin() -> CanvasPoint {
+        CanvasPoint { x: 0.0, y: 0.0 }
+    }
+
+    #[inline]
+    fn len() -> usize {
+        2
+    }
+
+    #[inline]
+    fn get(&self, index: usize) -> f64 {
+        match index {
+            0 => self.x as f64,
+            1 => self.y as f64,
+            _ => panic!("CanvasPoint only has two components"),
+        }
+    }
+
+    fn from_biggest_components(p1: CanvasPoint, p2: CanvasPoint) -> CanvasPoint {
+        CanvasPoint {
+            x: p1.x.max(p2.x),
+            y: p1.y.max(p2.y),
+        }
+    }
+
+    fn from_smallest_components(p1: CanvasPoint, p2: CanvasPoint) -> CanvasPoint {
+        CanvasPoint {
+            x: p1.x.min(p2.x),
+            y: p1.y.min(p2.y),
+        }
+    }
+
+    #[inline]
+    fn distance_to(&self, target: &CanvasPoint) -> f64 {
+        let dist_x = target.x as f64 - self.x as f64;
+        let dist_y = target.y as f64 - self.y as f64;
+
+        f64::sqrt(dist_x * dist_x + dist_y * dist_y)
+    }
+
+    #[inline]
+    fn dot(&self, target: &Self) -> f64 {
+        (self.x as f64) * (target.x as f64) + (self.y as f64) * (target.y as f64)
+    }
 }
