748782c4852d6bb8b868660f538b5ed8fc74e00e
Andrew Hunter <andrew@logicalshift.co.uk>
2026-02-06 21:50:05 +0000

    Polygon should have a direction so we can rotate it

--- flo2/src/scenery/document/canvas/shape/polygon.rs
+++ flo2/src/scenery/document/canvas/shape/polygon.rs
@@ -7,12 +7,13 @@ use ::serde::*;
 ///
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct CanvasPolygonV1 {
-    pub min:    CanvasPoint,
-    pub max:    CanvasPoint,
-    pub sides:  usize,
+    pub min:        CanvasPoint,
+    pub max:        CanvasPoint,
+    pub direction:  CanvasPoint,
+    pub sides:      usize,
 }
 
 pub type CanvasPolygon = CanvasPolygonV1;
 
 /// Shape type to indicate a shape encoded in V1 canvas polygon format
-pub const CANVAS_POLYGON_V1_TYPE: i64 = 1;
+pub const CANVAS_POLYGON_V1_TYPE: i64 = 3;
