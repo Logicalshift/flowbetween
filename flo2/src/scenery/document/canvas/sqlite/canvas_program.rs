0bf60a80c043025dea7e1ccea48aa0897322995b
Andrew Hunter <andrew@logicalshift.co.uk>
2026-02-07 16:38:38 +0000

    Send update messages when the shapes or layers are changed

--- flo2/src/scenery/document/canvas/sqlite/canvas_program.rs
+++ flo2/src/scenery/document/canvas/sqlite/canvas_program.rs
@@ -1,5 +1,7 @@
 use super::canvas::*;
+use super::super::property::*;
 use super::super::queries::*;
+use super::super::shape::*;
 use super::super::vector_editor::*;
 
 use flo_scene::*;
@@ -7,6 +9,8 @@ use flo_scene::programs::*;
 
 use futures::prelude::*;
 use ::serde::*;
+
+use std::collections::{HashSet};
 
 ///
 /// Messages for the sqlite canvas program
@@ -32,32 +36,62 @@ pub async fn sqlite_canvas_program(input: InputStream<SqliteCanvasRequest>, cont
             Edit(edits) => {
                 use VectorCanvas::*;
 
-                // Series of canvas editing requests
-                let is_changed = edits.iter().any(|edit| !matches!(edit, Subscribe(_)));
+                // Track which layers and shapes are affected by this batch of edits
+                let mut changed_layers = HashSet::new();
+                let mut changed_shapes = HashSet::new();
 
                 for edit in edits {
                     match edit {
-                        AddLayer { new_layer_id, before_layer, }        => { canvas.add_layer(new_layer_id, before_layer).ok(); }
-                        RemoveLayer(layer_id)                           => { canvas.remove_layer(layer_id).ok(); }
-                        ReorderLayer { layer_id, before_layer, }        => { canvas.reorder_layer(layer_id, before_layer).ok(); }
-                        AddShape(shape_id, shape_defn)                  => { canvas.add_shape(shape_id, shape_defn).ok(); }
-                        RemoveShape(shape_id)                           => { canvas.remove_shape(shape_id).ok(); }
-                        SetShapeDefinition(shape_id, shape_defn)        => { canvas.set_shape_definition(shape_id, shape_defn).ok(); }
+                        AddLayer { new_layer_id, before_layer, }        => { changed_layers.insert(new_layer_id); canvas.add_layer(new_layer_id, before_layer).ok(); }
+                        RemoveLayer(layer_id)                           => { changed_layers.insert(layer_id); canvas.remove_layer(layer_id).ok(); }
+                        ReorderLayer { layer_id, before_layer, }        => { changed_layers.insert(layer_id); canvas.reorder_layer(layer_id, before_layer).ok(); }
+                        AddShape(shape_id, shape_defn)                  => { changed_shapes.insert(shape_id); canvas.add_shape(shape_id, shape_defn).ok(); }
+                        RemoveShape(shape_id)                           => { changed_shapes.insert(shape_id); canvas.remove_shape(shape_id).ok(); }
+                        SetShapeDefinition(shape_id, shape_defn)        => { changed_shapes.insert(shape_id); canvas.set_shape_definition(shape_id, shape_defn).ok(); }
                         AddBrush(brush_id)                              => { canvas.add_brush(brush_id).ok(); }
                         RemoveBrush(brush_id)                           => { canvas.remove_brush(brush_id).ok(); }
-                        ReorderShape { shape_id, before_shape, }        => { canvas.reorder_shape(shape_id, before_shape).ok(); }
-                        SetShapeParent(shape_id, parent)                => { canvas.set_shape_parent(shape_id, parent).ok(); }
-                        SetProperty(property_target, properties)        => { canvas.set_properties(property_target, properties).ok(); }
-                        AddShapeBrushes(shape_id, brush_ids)            => { canvas.add_shape_brushes(shape_id, brush_ids).ok(); }
-                        RemoveProperty(property_target, property_list)  => { canvas.delete_properties(property_target, property_list).ok(); }
-                        RemoveShapeBrushes(shape_id, brush_ids)         => { canvas.remove_shape_brushes(shape_id, brush_ids).ok(); }
+                        ReorderShape { shape_id, before_shape, }        => { changed_shapes.insert(shape_id); canvas.reorder_shape(shape_id, before_shape).ok(); }
+
+                        SetShapeParent(shape_id, parent) => {
+                            changed_shapes.insert(shape_id);
+                            match &parent {
+                                CanvasShapeParent::Layer(layer_id)  => { changed_layers.insert(*layer_id); }
+                                CanvasShapeParent::Shape(parent_id) => { changed_shapes.insert(*parent_id); }
+                                CanvasShapeParent::None             => { }
+                            }
+                            canvas.set_shape_parent(shape_id, parent).ok();
+                        }
+
+                        SetProperty(property_target, properties) => {
+                            match &property_target {
+                                CanvasPropertyTarget::Layer(layer_id)    => { changed_layers.insert(*layer_id); }
+                                CanvasPropertyTarget::Shape(shape_id)    => { changed_shapes.insert(*shape_id); }
+                                _                                        => { }
+                            }
+                            canvas.set_properties(property_target, properties).ok();
+                        }
+
+                        AddShapeBrushes(shape_id, brush_ids)            => { changed_shapes.insert(shape_id); canvas.add_shape_brushes(shape_id, brush_ids).ok(); }
+
+                        RemoveProperty(property_target, property_list) => {
+                            match &property_target {
+                                CanvasPropertyTarget::Layer(layer_id)    => { changed_layers.insert(*layer_id); }
+                                CanvasPropertyTarget::Shape(shape_id)    => { changed_shapes.insert(*shape_id); }
+                                _                                        => { }
+                            }
+                            canvas.delete_properties(property_target, property_list).ok();
+                        }
+
+                        RemoveShapeBrushes(shape_id, brush_ids)         => { changed_shapes.insert(shape_id); canvas.remove_shape_brushes(shape_id, brush_ids).ok(); }
                         Subscribe(edit_target)                          => { if let Ok(edit_target) = context.send(edit_target) { subscribers.add_target(edit_target); } }
                     }
                 }
 
-                if is_changed {
-                    // Send a message to indicate what's changed
-                    subscribers.send(()).await;
+                if !changed_layers.is_empty() {
+                    subscribers.send(VectorCanvasUpdate::LayerChanged(changed_layers.into_iter().collect())).await;
+                }
+                if !changed_shapes.is_empty() {
+                    subscribers.send(VectorCanvasUpdate::ShapeChanged(changed_shapes.into_iter().collect())).await;
                 }
             }
 
