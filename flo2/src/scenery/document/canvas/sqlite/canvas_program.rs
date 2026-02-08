07c93895c4cdf25cefb1f18de3695a55004a123c
Andrew Hunter <andrew@logicalshift.co.uk>
2026-02-08 22:01:10 +0000

    Implement the 'brushes' message to query the new brushes

--- flo2/src/scenery/document/canvas/sqlite/canvas_program.rs
+++ flo2/src/scenery/document/canvas/sqlite/canvas_program.rs
@@ -104,7 +104,7 @@ pub async fn sqlite_canvas_program(input: InputStream<SqliteCanvasRequest>, cont
                     DocumentOutline(target)                                      => { canvas.send_vec_query_response(target, &context, |canvas, response| canvas.query_document_outline(response)).await.ok(); },
                     Layers(target, layer_list)                                   => { canvas.send_vec_query_response(target, &context, move |canvas, response| canvas.query_layers_with_shapes(layer_list, response)).await.ok(); },
                     Shapes(target, shape_list)                                   => { canvas.send_vec_query_response(target, &context, move |canvas, response| canvas.query_shapes(shape_list, response)).await.ok(); },
-                    Brushes(target, brush_list)                                  => { todo!() },
+                    Brushes(target, brush_list)                                  => { canvas.send_vec_query_response(target, &context, move |canvas, response| canvas.query_brushes(brush_list, response)).await.ok(); },
                     ShapesInRegion { target, search_layers, region, inclusive, } => { todo!() },
                     ShapesAtPoint { target, search_layers, point, }              => { todo!() },
                 }
