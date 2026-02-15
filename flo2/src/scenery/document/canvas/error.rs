0bdea53a21545e5871e402271b787b451196d961
Andrew Hunter <andrew@logicalshift.co.uk>
2026-02-15 21:00:58 +0000

    Expand the errors to support some more interesting kinds of error

--- flo2/src/scenery/document/canvas/error.rs
+++ flo2/src/scenery/document/canvas/error.rs
@@ -2,6 +2,8 @@ use super::brush::*;
 use super::layer::*;
 use super::shape::*;
 
+use flo_scene::*;
+
 use ::serde::*;
 use std::error::{Error};
 use std::fmt;
@@ -11,6 +13,15 @@ pub enum CanvasError {
     /// Something unexpected went wrong while storing to the canvas
     UnexpectedStorageError(String),
 
+    /// An error occurred while serializing or deserializing a string
+    SerializationError(String),
+
+    /// An error occurred while connecting to something in a scene
+    ConnectionError(ConnectionError),
+
+    /// An error occurred while sending a message in a scene
+    SceneSendError(SceneSendError<()>),
+
     /// Operation failed because a layer does not exist
     NoSuchLayer(CanvasLayerId),
 
@@ -19,17 +30,36 @@ pub enum CanvasError {
 
     /// Operation failed because a brush does not exist
     NoSuchBrush(CanvasBrushId),
+
+    /// A shape does not have a parent
+    ShapeHasNoParent(CanvasShapeId),
 }
 
 impl fmt::Display for CanvasError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             CanvasError::UnexpectedStorageError(msg)    => write!(f, "Unexpected storage error: {}", msg),
+            CanvasError::SerializationError(msg)        => write!(f, "Serialization error: {}", msg),
+            CanvasError::ConnectionError(err)           => write!(f, "Scene connection error: {:?}", err),
+            CanvasError::SceneSendError(err)            => write!(f, "Failed to send message: {:?}", err),
             CanvasError::NoSuchLayer(id)                => write!(f, "No such layer with id: {}", id),
             CanvasError::NoSuchShape(id)                => write!(f, "No such shape with id: {}", id),
             CanvasError::NoSuchBrush(id)                => write!(f, "No such brush with id: {}", id),
+            CanvasError::ShapeHasNoParent(id)           => write!(f, "Shape has no parent: {}", id),
         }
     }
 }
 
-impl Error for CanvasError {}
+impl Error for CanvasError { }
+
+impl From<ConnectionError> for CanvasError {
+    fn from(value: ConnectionError) -> Self {
+        CanvasError::ConnectionError(value)
+    }
+}
+
+impl<TMsg> From<SceneSendError<TMsg>> for CanvasError {
+    fn from(value: SceneSendError<TMsg>) -> Self {
+        CanvasError::SceneSendError(value.map(|_| ()))
+    }
+}
--- flo2/src/scenery/document/canvas/sqlite/error.rs
+++ flo2/src/scenery/document/canvas/sqlite/error.rs
@@ -1,9 +1,16 @@
 use super::super::error::*;
 
 use rusqlite;
+use postcard;
 
 impl From<rusqlite::Error> for CanvasError {
     fn from(value: rusqlite::Error) -> Self {
         CanvasError::UnexpectedStorageError(value.to_string())
+    }
+}
+
+impl From<postcard::Error> for CanvasError {
+    fn from(value: postcard::Error) -> Self {
+        CanvasError::SerializationError(value.to_string())
     }
 }
