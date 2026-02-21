e17d4f64d74f24396ccb5efafd6b37e7ecc57136
Andrew Hunter <andrew@logicalshift.co.uk>
2026-02-21 22:07:06 +0000

    Want to render directly from the vector response so we don't end up cloning shapes if we don't need to

--- flo2/src/scenery/document/canvas/render/layer_renderer.rs
+++ flo2/src/scenery/document/canvas/render/layer_renderer.rs
@@ -6,14 +6,12 @@ use flo_draw::canvas::*;
 ///
 /// Renders the shapes on a layer when described as a set of vector responses
 ///
-pub async fn render_layer(layer: impl Send + IntoIterator<Item=&VectorResponse>, context: &SceneContext) -> Vec<Draw> {
-    // The drawing instructions for this layer
-    let mut drawing = vec![];
-
-    // TODO: need a cache of connections for shapes (probably shared in a type that's passed in)
-
+pub async fn render_layer(layer: impl Send + IntoIterator<Item=VectorResponse>, context: &SceneContext) -> Vec<Draw> {
     // TODO: rendering needs to render as many shapes at once as it can (for perf reasons), but needs multiple passes for groups
     // TODO: so, we find the 'deepest' nodes first, then progresively render the higher nodes when they become available
+
+    // The drawing instructions for this layer
+    let mut drawing = vec![];
 
     drawing
 }
