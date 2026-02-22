7b4ae4b22061cd9d17371563b831dcd39501da82
Andrew Hunter <andrew@logicalshift.co.uk>
2026-02-22 17:50:05 +0000

    Calculate the 'height' of the nodes and use that to decide on the rendering order when writing a layer

--- flo2/src/scenery/document/canvas/render/layer_renderer.rs
+++ flo2/src/scenery/document/canvas/render/layer_renderer.rs
@@ -1,3 +1,4 @@
+use super::shape_renderer::*;
 use super::shape_type_renderer::*;
 use super::super::queries::*;
 
@@ -11,21 +12,46 @@ use std::sync::*;
 ///
 pub async fn render_layer(layer: impl Send + IntoIterator<Item=VectorResponse>, context: &SceneContext) -> Vec<Draw> {
     struct RenderItem {
+        /// Shape to render
         shape:          Arc<ShapeWithProperties>,
+
+        /// Child nodes for the shape if it's a group (must be rendered before this shape can be rendered)
         child_nodes:    Vec<usize>,
+
+        /// Parent node for this shape
         parent_node:    Option<usize>,
-        render:         Option<Arc<Draw>>,
+
+        /// The rendering for this shape, once it's complete
+        render:         Option<Arc<Vec<Draw>>>,
+
+        /// Depth of this shape in the tree (distance from the bottom of the tree)
+        depth:          usize,
+
+        /// Height of this shape in the tree (distance from the top of the tree)
+        height:         usize,
     }
 
-    // Rendering involves multiple passes over the layer
-    let mut render          = vec![];
-    let mut parent_stack    = vec![];
+    // Build up a representation of the items we're going to render (discover the children of group nodes, and the height each node is in the tree)
+    let mut render: Vec<RenderItem> = vec![];
+    let mut parent_stack            = vec![];
+    let mut root_nodes              = vec![];
+    let mut max_height              = 0;
 
     for response in layer {
         match response {
             VectorResponse::StartGroup => {
                 // The following items are parented to this item
                 parent_stack.push(render.len()-1);
+
+                // Update the height of all of the parent nodes
+                for (height, parent_idx) in parent_stack.iter().rev().enumerate() {
+                    if render[*parent_idx].height == height + 1 { break; }
+
+                    render[*parent_idx].height = height + 1;
+                }
+
+                // Update the maximum height of the render stack
+                max_height = parent_stack.len().max(max_height);
             }
 
             VectorResponse::EndGroup => {
@@ -35,21 +61,27 @@ pub async fn render_layer(layer: impl Send + IntoIterator<Item=VectorResponse>, 
 
             VectorResponse::Shape(_shape_id, shape, shape_type, properties) => {
                 // Create a node for this shape
+                let node_idx    = render.len();
                 let parent_idx  = parent_stack.last().copied();
                 let properties  = Arc::new(properties);
                 let group       = vec![];
 
+                // Everything starts out at height 0
                 render.push(RenderItem {
                     shape:          Arc::new(ShapeWithProperties { shape, shape_type, properties, group }),
                     child_nodes:    vec![],
                     parent_node:    parent_idx,
                     render:         None,
+                    depth:          parent_stack.len(),
+                    height:         0,
                 });
 
                 // Add as a child node to the current parent
                 if let Some(parent_idx) = parent_idx {
                     let new_idx = render.len() - 1;
                     render[parent_idx].child_nodes.push(new_idx);
+                } else {
+                    root_nodes.push(node_idx);
                 }
             }
 
@@ -59,22 +91,54 @@ pub async fn render_layer(layer: impl Send + IntoIterator<Item=VectorResponse>, 
         }
     }
 
-    enum RenderOp {
-        /// Render the shape at the specified index, storing in the render queue
-        RenderShape(usize),
-
-        /// Pop a result from the front of the render queue and push to the list of final rendering instructions
-        PushResult,
-
-        /// Pop a result from the front of the render queue and push it to the list of child nodes for the specified item
-        AddChild(usize),
+    // Bin into render passes. By rendering down from the leaves, we ensure that the drawing instructions needed to render each group are available just in time
+    // By processing the whole 'level' in the render tree all in one go, we minimize the number of calls to `render_shapes`
+    let mut render_passes = vec![vec![]; max_height];
+    for (idx, item) in render.iter().enumerate() {
+        render_passes[item.height].push(idx);
     }
 
-    // TODO: rendering needs to render as many shapes at once as it can (for perf reasons), but needs multiple passes for groups
-    // TODO: so, we find the 'deepest' nodes first, then progresively render the higher nodes when they become available
+    // Perform each render pass to build up the drawing instructions for this layer
+    for render_pass_idxs in render_passes.into_iter() {
+        // Fill in the drawing instructions for any child items
+        for render_idx in render_pass_idxs.iter() {
+            let render_idx = *render_idx;
 
-    // The drawing instructions for this layer
+            // Short-circuit if the item isn't a group item
+            if render[render_idx].child_nodes.is_empty() { continue; }
+
+            // Gather the children for this shape
+            let children = render[render_idx].child_nodes.iter()
+                .map(|child_idx| (render[*child_idx].shape.clone(), render[*child_idx].render.clone().unwrap()))
+                .collect::<Vec<_>>();
+
+            // Modify the shape to contain the items in its group
+            // TODO: Rust doesn't want the shape to be invalid even when we're putting it right back, so we end up cloning it, 
+            // would be more efficient to let the field be invalid before overwriting it instead ('safe' if Rust knows that 
+            // when we write it back we're not dropping the original)
+            let item        = &mut render[render_idx];
+            let mut shape   = Arc::unwrap_or_clone(item.shape.clone());
+
+            shape.group = children;
+
+            item.shape  = Arc::new(shape);
+        }
+
+        // Render the shapes in this pass
+        let drawings = render_shapes(render_pass_idxs.iter().map(|idx| Arc::clone(&render[*idx].shape)), context).await;
+
+        // Put the drawings back into the render items
+        for (idx, drawing) in render_pass_idxs.into_iter().zip(drawings) {
+            render[idx].render = Some(drawing);
+        }
+    }
+
+    // The drawing instructions for this layer are now just the root nodes concatenated together
     let mut drawing = vec![];
+
+    for node_idx in root_nodes {
+        drawing.extend(render[node_idx].render.as_ref().unwrap().iter().cloned());
+    }
 
     drawing
 }
