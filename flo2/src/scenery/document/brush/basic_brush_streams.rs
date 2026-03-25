6bbcfb10aa615c4866b2a496a5684afdad480a4b
Andrew Hunter <andrew@logicalshift.co.uk>
2026-03-25 23:01:15 +0000

    Make the pressure, etc non-optional to simplify things a bit when interpolating points

--- flo2/src/scenery/document/brush/basic_brush_streams.rs
+++ flo2/src/scenery/document/brush/basic_brush_streams.rs
@@ -6,8 +6,6 @@ use flo_curves::geo::*;
 use flo_curves::bezier::*;
 use flo_stream::*;
 
-use std::collections::*;
-
 ///
 /// Converts an input stream of brush points to an output stream of brush points with a radius based on the pen pressure
 ///
@@ -15,11 +13,7 @@ pub fn brush_pressure_to_radius_linear(max_radius: f64, input_stream: impl 'stat
     input_stream.map(move |point| {
         let mut point = point;
 
-        if let Some(pressure) = point.pressure {
-            point.daub_radius = Some((pressure.max(0.0).min(1.0)) * max_radius);
-        } else {
-            point.daub_radius = Some(max_radius);
-        }
+        point.daub_radius = Some((point.pressure.max(0.0).min(1.0)) * max_radius);
 
         point
     })
