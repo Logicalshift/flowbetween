32196f666bcf0cbf0ffdfaac415c7268c26b36fa
Andrew Hunter <andrew@logicalshift.co.uk>
2026-03-26 20:50:33 +0000

    First pass at implementing a shape stream

--- flo2/src/scenery/document/brush/mod.rs
+++ flo2/src/scenery/document/brush/mod.rs
@@ -2,10 +2,12 @@ mod basic_brush_streams;
 mod brush_point;
 mod brush_request;
 mod brush_response;
+mod shape_streams;
 mod smoothing_streams;
 
 pub use basic_brush_streams::*;
 pub use brush_point::*;
 pub use brush_request::*;
 pub use brush_response::*;
+pub use shape_streams::*;
 pub use smoothing_streams::*;
