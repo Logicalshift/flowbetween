5c1a25cf07d3caadd1fc8e3fc41576ebcc48ad98
Andrew Hunter <andrew@logicalshift.co.uk>
2026-03-26 09:26:11 +0000

    Put the interpolation routine in a new 'smoothing' file
    (I think once we start adding things like daub brushes we end up with a very large file, so breaking this up early on)

--- flo2/src/scenery/document/brush/mod.rs
+++ flo2/src/scenery/document/brush/mod.rs
@@ -2,8 +2,10 @@ mod basic_brush_streams;
 mod brush_point;
 mod brush_request;
 mod brush_response;
+mod smoothing_streams;
 
 pub use basic_brush_streams::*;
 pub use brush_point::*;
 pub use brush_request::*;
 pub use brush_response::*;
+pub use smoothing_streams::*;
