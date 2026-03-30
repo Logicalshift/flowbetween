6785d73281e830083e718f1c3b8690414d152a58
Andrew Hunter <andrew@logicalshift.co.uk>
2026-03-30 11:05:30 +0100

    Add some bindings for the state and update them as the mouse moves over the canvas

--- flo2/src/scenery/document/tools/brush_tool.rs
+++ flo2/src/scenery/document/tools/brush_tool.rs
@@ -4,20 +4,60 @@ use super::tool::*;
 use crate::scenery::ui::*;
 use crate::scenery::document::subprograms::*;
 
+use flo_binding::*;
+use flo_draw::*;
+use flo_draw::canvas::*;
 use flo_scene::*;
 
 use futures::prelude::*;
+
+use std::sync::*;
 
 ///
 /// Current settings for the brush tool
 ///
 #[derive(Clone)]
 pub struct BrushToolState {
+    /// Position where the tool is hovering
+    hover_pos: Binding<Option<(f64, f64)>>,
+
+    /// Whether or not the tool is selected
+    tool_selected: Binding<bool>,
+
+    /// Whether or not the mouse has entered the tool region
+    mouse_over: Binding<bool>,
+
+    /// Preview for the tool
+    preview: Binding<Arc<Vec<Draw>>>,
 }
 
 impl ToolData for BrushToolState {
     fn initial_position(&self) -> (StreamTarget, (f64, f64)) {
         (subprogram_tool_dock_left().into(), (0.0, 1.0))
+    }
+
+    fn is_duplicate(&mut self, _is_duplicate: bool) { }
+
+    fn selected(&mut self, is_selected: bool) {
+        self.tool_selected.set(is_selected);
+    }
+}
+
+impl Default for BrushToolState {
+    fn default() -> Self {
+        let mut preview = vec![];
+        preview.new_path();
+        preview.circle(0.0, 0.0, 10.0);
+        preview.stroke_color(Color::Rgba(0.6, 0.6, 0.6, 1.0));
+        preview.line_width_pixels(1.0);
+        preview.stroke();
+
+        Self {
+            hover_pos:      bind(None),
+            tool_selected:  bind(false),
+            mouse_over:     bind(false),
+            preview:        bind(Arc::new(preview)),
+        }
     }
 }
 
@@ -26,16 +66,32 @@ impl ToolData for BrushToolState {
 ///
 pub async fn brush_tool_program(input: InputStream<ToolState>, context: SceneContext) {
     // Set up the behaviour
-    let behaviour = ToolBehaviour::new("Brush", || vec![ BrushToolState { } ]);
+    let behaviour = ToolBehaviour::new("Brush", || vec![ BrushToolState::default() ]);
 
     // Ink icon
     let behaviour = behaviour.with_icon_svg(include_bytes!("../../../../../flo/svg/tools/ink.svg"));
 
     // The actual behaviour when focused on the canvas
-    let behaviour = behaviour.with_canvas_program(|input, _context, _data| async move {
+    let behaviour = behaviour.with_canvas_program(|input, _context, data| async move {
         let mut input = input;
         while let Some(msg) = input.next().await {
-            println!("{:?}", msg);
+            match msg {
+                FocusEvent::Pointer(FocusPointerEvent::Pointer(_, PointerAction::Enter, _, state)) => {
+                    data.lock().unwrap().hover_pos.set(state.location_in_canvas);
+                    data.lock().unwrap().mouse_over.set(true);
+                }
+
+                FocusEvent::Pointer(FocusPointerEvent::Pointer(_, PointerAction::Leave, _, _)) => {
+                    data.lock().unwrap().hover_pos.set(None);
+                    data.lock().unwrap().mouse_over.set(false);
+                }
+
+                FocusEvent::Pointer(FocusPointerEvent::Pointer(_, PointerAction::Move, _, state)) => {
+                    data.lock().unwrap().hover_pos.set(state.location_in_canvas);
+                }
+
+                _ => {}
+            }
         }
     });
 
